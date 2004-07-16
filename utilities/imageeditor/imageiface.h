fbe1597ed39bc800c8a9b2b53269e92426f02cca
Renchi Raju <renchi.raju@kdemail.net>
2004-07-16 22:26:42 +0000

    included some utility functions to convert from RGB to HSL and back. Useful for plugins. CCMAIL: digikam-devel@lists.sourceforge.net
    svn path=/trunk/kdeextragear-3/digikam/; revision=330126

--- utilities/imageeditor/imageiface.h
+++ utilities/imageeditor/imageiface.h
@@ -5,7 +5,11 @@
  * Description : 
  * 
  * Copyright 2004 by Renchi Raju
-
+ *
+ * Includes code from gimp version 2.0  
+ * LIBGIMP - The GIMP Library
+ * Copyright (C) 1995-1997 Peter Mattis and Spencer Kimball
+ * 
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published bythe Free Software Foundation;
@@ -23,6 +27,10 @@
 #define IMAGEIFACE_H
 
 #include <qglobal.h>
+
+#define MAX3(a, b, c) (QMAX(QMAX(a,b),b))
+#define MIN3(a, b, c) (QMIN(QMIN(a,b),b))
+#define ROUND(x) ((int) ((x) + 0.5))
 
 class QWidget;
 
@@ -64,6 +72,137 @@ private:
 
     ImageIfacePriv* d;
 };
+
+
+inline static int hsl_value (double n1,
+                      double n2,
+                      double hue)
+{
+    double value;
+
+    if (hue > 255)
+	hue -= 255;
+    else if (hue < 0)
+	hue += 255;
+
+    if (hue < 42.5)
+	value = n1 + (n2 - n1) * (hue / 42.5);
+    else if (hue < 127.5)
+	value = n2;
+    else if (hue < 170)
+	value = n1 + (n2 - n1) * ((170 - hue) / 42.5);
+    else
+	value = n1;
+
+    return ROUND(value * 255.0);
+}
+
+inline static void rgb_to_hsl (int& r, int& g, int& b)
+{
+    double h, s, l;
+    int    min, max;
+    int    delta;
+
+    if (r > g)
+    {
+        max = QMAX (r, b);
+        min = QMIN (g, b);
+    }
+    else
+    {
+        max = QMAX (g, b);
+        min = QMIN (r, b);
+    }
+
+    l = (max + min) / 2.0;
+
+    if (max == min)
+    {
+        s = 0.0;
+        h = 0.0;
+    }
+    else
+    {
+        delta = (max - min);
+
+        if (l < 128)
+            s = 255 * (double) delta / (double) (max + min);
+        else
+            s = 255 * (double) delta / (double) (511 - max - min);
+
+        if (r == max)
+            h = (g - b) / (double) delta;
+        else if (g == max)
+            h = 2 + (b - r) / (double) delta;
+        else
+            h = 4 + (r - g) / (double) delta;
+
+        h = h * 42.5;
+
+        if (h < 0)
+            h += 255;
+        else if (h > 255)
+            h -= 255;
+    }
+
+    r = ROUND (h);
+    g = ROUND (s);
+    b = ROUND (l);
+}
+
+
+inline static void hsl_to_rgb (int& hue, int& saturation, int& lightness)
+{
+    double h, s, l;
+
+    h = hue;
+    s = saturation;
+    l = lightness;
+
+    if (s == 0)
+    {
+        /*  achromatic case  */
+        hue        = (int) l;
+        lightness  = (int) l;
+        saturation = (int) l;
+    }
+    else
+    {
+        double m1, m2;
+
+        if (l < 128)
+            m2 = (l * (255 + s)) / 65025.0;
+        else
+            m2 = (l + s - (l * s) / 255.0) / 255.0;
+
+        m1 = (l / 127.5) - m2;
+
+        /*  chromatic case  */
+        hue        = hsl_value (m1, m2, h + 85);
+        saturation = hsl_value (m1, m2, h);
+        lightness  = hsl_value (m1, m2, h - 85);
+    }
+}
+
+
+inline static int rgb_to_l (int red, int green, int blue)
+{
+    int min, max;
+
+    if (red > green)
+    {
+        max = QMAX (red,   blue);
+        min = QMIN (green, blue);
+    }
+    else
+    {
+        max = QMAX (green, blue);
+        min = QMIN (red,   blue);
+    }
+
+    return ROUND ((max + min) / 2.0);
+}
+
 
 }
 
