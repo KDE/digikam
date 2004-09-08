1b0dd4a71f83920a729eeac541f02031bf5303b4
Gilles Caulier <caulier.gilles@gmail.com>
2004-09-08 16:02:14 +0000

    CVS_SLIENT Fixed i18n
    svn path=/trunk/kdeextragear-3/digikam/; revision=345031

--- imageplugins/histogramviewer.cpp
+++ imageplugins/histogramviewer.cpp
@@ -128,7 +128,7 @@ void HistogramViewer::setupGui(uint *imageData, uint width, uint height)
     m_channelCB->insertItem( i18n("Alpha") );
     m_channelCB->insertItem( i18n("Colors") );
     m_channelCB->setCurrentText( i18n("Luminosity") );
-    QWhatsThis::add( m_channelCB, i18n("<p>Select here the histogram channel to display.<p>"
+    QWhatsThis::add( m_channelCB, i18n("<p>Select here the histogram channel to display:<p>"
                                        "<b>Luminosity</b>: drawing the image luminosity values.<p>"
                                        "<b>Red</b>: drawing the red image channel values.<p>"
                                        "<b>Green</b>: drawing the green image channel values.<p>"
