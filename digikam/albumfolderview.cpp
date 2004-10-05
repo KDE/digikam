8ff8e7c932d3358e4a786107fe73405db6b11975
Jörn Ahrens <joern.ahrens@kdemail.net>
2004-10-05 20:56:47 +0000

    Especially for you, Albert :)
    * Setting album thumbnail through drag and drop
      Setting the dropped image as the album thumbnail. If the ctrl key
      is pressed, when dropping the image, the thumbnail is set without
      a popup menu. Otherwise it is shown a popup menu.
    
    CCMAIL: tomalbers@kde.nl, digikam-devel@lists.sourceforge.net
    
    svn path=/trunk/kdeextragear-3/digikam/; revision=352187

--- digikam/albumfolderview.cpp
+++ digikam/albumfolderview.cpp
@@ -1542,17 +1542,44 @@ void AlbumFolderView::phyAlbumDropEvent(QDropEvent* event, PAlbum *album)
             return;
         }
 
-        if (srcAlbum == destAlbum)
-            return;
-
         char keys_return[32];
         XQueryKeymap(x11Display(), keys_return);
         int id = 0;
-
         int key_1 = XKeysymToKeycode(x11Display(), 0xFFE3);
         int key_2 = XKeysymToKeycode(x11Display(), 0xFFE4);
         int key_3 = XKeysymToKeycode(x11Display(), 0xFFE1);
         int key_4 = XKeysymToKeycode(x11Display(), 0xFFE2);
+
+        if(srcAlbum == destAlbum)
+        {
+            // Setting the dropped image as the album thumbnail
+            // If the ctrl key is pressing when dropping the image the
+            // thumbnail is set without a popup menu
+            if ( ( (keys_return[key_1 / 8]) && (1 << (key_1 % 8)) ) ||
+                 ( (keys_return[key_2 / 8]) && (1 << (key_2 % 8)) ))
+            {
+                id = 12;
+            }
+            else 
+            {
+                QPopupMenu popMenu(this);
+                popMenu.insertItem(i18n("Set as Album Thumbnail"), 12);
+                popMenu.insertSeparator(-1);
+                popMenu.insertItem( SmallIcon("cancel"), i18n("C&ancel") );
+                
+                popMenu.setMouseTracking(true);
+                id = popMenu.exec(QCursor::pos());
+            }
+            
+            if(id == 12)
+            {
+                QString errMsg;
+                AlbumManager::instance()->updatePAlbumIcon(destAlbum, 
+                    urls.first().url(), true, errMsg);
+            }
+            return;
+        }
+
         // If shift key is pressed while dragging, move the drag object without
         // displaying popup menu -> move
         if ( ( (keys_return[key_3 / 8]) && (1 << (key_3 % 8)) ) ||
@@ -1564,7 +1591,7 @@ void AlbumFolderView::phyAlbumDropEvent(QDropEvent* event, PAlbum *album)
         // displaying popup menu -> copy
         else if ( ( (keys_return[key_1 / 8]) && (1 << (key_1 % 8)) ) ||
                   ( (keys_return[key_2 / 8]) && (1 << (key_2 % 8)) ))
-        {        
+        {   
             id = 11;
         }
         else
