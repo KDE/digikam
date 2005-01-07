e5e943ccba9f30f602ba43efb2096a9f900f460f
Renchi Raju <renchi.raju@kdemail.net>
2005-01-07 02:54:49 +0000

    just for you, Tom. :)
    svn path=/trunk/kdeextragear-3/digikam/; revision=376010

--- version.h
+++ version.h
@@ -22,7 +22,7 @@
 #ifndef DIGIKAM_VERSION_H
 #define DIGIKAM_VERSION_H
 
-static const char digikam_version[] = "0.7.1";
+static const char digikam_version[] = "0.7.2-cvs";
 
 #endif // DIGIKAM_VERSION_H
 
