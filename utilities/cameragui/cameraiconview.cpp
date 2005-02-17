43e5ce0f962be021c534237271447d28f77c42de
Renchi Raju <renchi.raju@kdemail.net>
2005-02-17 15:15:36 +0000

    use localtime and not utc to generate the exif based filename BUGS: 99544
    svn path=/trunk/kdeextragear-3/digikam/; revision=390273

--- utilities/cameragui/cameraiconview.cpp
+++ utilities/cameragui/cameraiconview.cpp
@@ -153,11 +153,10 @@ QString CameraIconView::getTemplatedName(const QString& templ,
     else
         ext = ext.right( ext.length() - pos - 1);
 
-    struct tm* time_tm = gmtime(&itemInfo->mtime);
-    char* s = new char[100];
+    struct tm* time_tm = ::localtime(&itemInfo->mtime);
+    char s[100];
     strftime(s, 100, QFile::encodeName(dname), time_tm);
     dname  = s;
-    delete [] s;
 
     dname.sprintf(QFile::encodeName(dname), position+1);
     dname.replace("/","");
