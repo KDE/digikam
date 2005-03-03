0aa290dfb5f365ca4732e95f1f7707ac4703054f
Gilles Caulier <caulier.gilles@gmail.com>
2005-03-03 06:51:22 +0000

    Fix release number to 0.7.3-beta1
    svn path=/trunk/kdeextragear-3/digikam/; revision=394529

--- version.h
+++ version.h
@@ -22,7 +22,7 @@
 #ifndef DIGIKAM_VERSION_H
 #define DIGIKAM_VERSION_H
 
-static const char digikam_version[] = "0.7.2";
+static const char digikam_version[] = "0.7.3-beta1";
 
 #endif // DIGIKAM_VERSION_H
 
