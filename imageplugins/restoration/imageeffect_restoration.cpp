fe42d7d2bcba7dea701b79bc67839a96199b7c93
Gilles Caulier <caulier.gilles@gmail.com>
2005-03-30 11:50:04 +0000

    Fix (c)
    svn path=/trunk/kdeextragear-3/digikamimageplugins/; revision=401885

--- imageplugins/restoration/imageeffect_restoration.cpp
+++ imageplugins/restoration/imageeffect_restoration.cpp
@@ -7,9 +7,6 @@
  * 
  * Copyright 2005 by Gilles Caulier
  *
- * Some code come from the CImg Gimp plugin by Victor Stinner.
- * See: http://www.girouette-stinner.com/castor/gimp.html
- *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
