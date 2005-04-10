2ffce7978317784e222c57ad8935f674e6fd438b
Danny Allen <dannya@kde.org>
2005-04-10 12:22:03 +0000

    Make plugin use existing "shear" action icon where possible...
    svn path=/trunk/kdeextragear-3/digikamimageplugins/; revision=404554

--- imageplugins/sheartool/imageplugin_sheartool.cpp
+++ imageplugins/sheartool/imageplugin_sheartool.cpp
@@ -39,7 +39,7 @@ K_EXPORT_COMPONENT_FACTORY( digikamimageplugin_sheartool,
 ImagePlugin_ShearTool::ImagePlugin_ShearTool(QObject *parent, const char*, const QStringList &)
                      : Digikam::ImagePlugin(parent, "ImagePlugin_ShearTool")
 {
-    m_sheartoolAction = new KAction(i18n("Shear..."), "sheartool", 0, 
+    m_sheartoolAction = new KAction(i18n("Shear..."), "shear", 0, 
                             this, SLOT(slotShearTool()),
                             actionCollection(), "imageplugin_sheartool");
     
