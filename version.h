984c417b0fc06a69f4fa4fd34a9cfb1740eadb1a
Gilles Caulier <caulier.gilles@gmail.com>
2005-04-25 07:17:39 +0000

    0.7.3-beta2 tag
    svn path=/trunk/kdeextragear-3/digikam/; revision=407699

--- version.h
+++ version.h
@@ -22,7 +22,7 @@
 #ifndef DIGIKAM_VERSION_H
 #define DIGIKAM_VERSION_H
 
-static const char digikam_version[] = "0.7.3-beta1";
+static const char digikam_version[] = "0.7.3-beta2";
 
 #endif // DIGIKAM_VERSION_H
 
