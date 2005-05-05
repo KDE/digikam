e68771c2a591823afa3807b6ba17e96d73f77767
Jörn Ahrens <joern.ahrens@kdemail.net>
2005-05-05 16:22:09 +0000

    typo
    svn path=/branches/digikam/0.8-experimental/; revision=409774

--- digikam/digikam/sidebar.cpp
+++ digikam/digikam/sidebar.cpp
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  * ============================================================ */
 
-/** @file sidebar.h */
+/** @file sidebar.cpp */
 
 #include "sidebar.h"
 
