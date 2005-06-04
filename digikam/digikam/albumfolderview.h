c1503c097c6541f9786f94411b7cdeedec448191
Jörn Ahrens <joern.ahrens@kdemail.net>
2005-06-04 10:43:57 +0000

    Started rewrite of AlbumFolderView
    svn path=/branches/digikam/0.8-experimental/; revision=422030

--- digikam/digikam/albumfolderview.h
+++ digikam/digikam/albumfolderview.h
@@ -15,7 +15,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * ============================================================ */
-
+ 
 /** @file albumfoldeview.h */
 
 #ifndef _ALBUMFOLDERVIEW_H_
