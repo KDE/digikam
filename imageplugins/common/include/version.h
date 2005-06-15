b8d78d9b2add092190fa5b96d48d16f0c21adce2
Gilles Caulier <caulier.gilles@gmail.com>
2005-06-15 06:31:20 +0000

    0.7.3
    svn path=/trunk/extragear/graphics/digikamimageplugins/; revision=425612

--- imageplugins/common/include/version.h
+++ imageplugins/common/include/version.h
@@ -23,7 +23,7 @@
 #ifndef DIGIKAMIMAGEPLUGINS_VERSION_H
 #define DIGIKAMIMAGEPLUGINS_VERSION_H
 
-static const char digikamimageplugins_version[] = "0.7.3-beta2";
+static const char digikamimageplugins_version[] = "0.7.3";
 
 #endif // DIGIKAMIMAGEPLUGINS_VERSION_H
 
