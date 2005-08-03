a497c5d2f9c4af2b0f5aad175d56466b15058dd2
Tom Albers <toma@kde.org>
2005-08-03 20:07:00 +0000

    This is how it should work
    svn path=/trunk/extragear/graphics/digikam/; revision=442753

--- digikam/squeezedcombobox.h
+++ digikam/squeezedcombobox.h
@@ -35,7 +35,7 @@ class SqueezedComboBox;
  * the user find the correct entry. It is automatically
  * activated when starting a SqueezedComboBox. This is
  * inherited from QToolTip
- * 
+ * @image html squeezedcombobox.png "This is how it looks"
  * @author Tom Albers
  */
 class SqueezedComboBoxTip : public QToolTip
