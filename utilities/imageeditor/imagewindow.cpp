5e64109e3a4239f3231f92f26f6c7c2f1399578e
Frank Osterfeld <frank.osterfeld@kdemail.net>
2005-09-05 14:21:12 +0000

    "Overwrite?" dialog: replace yes/no by overwrite/cancel, use message text from kwrite
    Forgot that yesterday somehow...
    CCMAIL: caulier.gilles@free.fr
    
    svn path=/trunk/extragear/graphics/digikam/; revision=457307

--- utilities/imageeditor/imagewindow.cpp
+++ utilities/imageeditor/imagewindow.cpp
@@ -1073,9 +1073,15 @@ void ImageWindow::slotSaveAs()
     if ( fi.exists() )
     {
         int result =
-            KMessageBox::warningYesNo( this, i18n("About to overwrite file %1. "
-                                                  "Are you sure you want to continue?")
-                                       .arg(newURL.filename()) );
+
+            KMessageBox::warningYesNo( this, 
+                                       i18n("A file named \"%1\" already "
+                                            "exists. Are you sure you want "
+                                            "to overwrite it?")
+                                       .arg(newURL.filename()),
+                                       i18n("Overwrite File?"),
+                                       i18n("Overwrite"),
+                                       i18n("Cancel") );
 
         if (result != KMessageBox::Yes)
             return;
