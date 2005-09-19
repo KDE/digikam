53773e72d5ce857a3d5bd11fc81d681d6ae77aab
Gilles Caulier <caulier.gilles@gmail.com>
2005-09-19 19:41:40 +0000

    add *.cr2 canon RAW file format
    svn path=/trunk/extragear/graphics/digikam/; revision=462042

--- digikam/albumsettings.cpp
+++ digikam/albumsettings.cpp
@@ -116,7 +116,7 @@ void AlbumSettings::init()
     d->imageFilefilter = "*.png *.jpg *.jpeg *.tif *.tiff *.gif *.bmp *.xpm *.ppm *.pnm *.xcf *.pcx";
     d->movieFilefilter = "*.mpeg *.mpg *.avi *.mov *.wmf *.asf";
     d->audioFilefilter = "*.ogg *.mp3 *.wma *.wav";
-    d->rawFilefilter   = "*.crw *.nef *.raf *.mrw *.orf *.pef *.x3f";
+    d->rawFilefilter   = "*.crw *.cr2 *.nef *.raf *.mrw *.orf *.pef *.x3f";
     d->thumbnailSize   = ThumbnailSize::Medium;
 
     d->showToolTips = true;
