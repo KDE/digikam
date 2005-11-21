138c7e303d35b0f6e26d23d1c5fbabf818245287
Tom Albers <toma@kde.org>
2005-11-21 19:04:10 +0000

    Prepping final, added me as author in ~lsm file, removed wrong comment of joern.
    svn path=/trunk/extragear/graphics/digikam/; revision=482105

--- version.h
+++ version.h
@@ -22,7 +22,7 @@
 #ifndef DIGIKAM_VERSION_H
 #define DIGIKAM_VERSION_H
 
-static const char digikam_version[] = "0.8.0-rc";
+static const char digikam_version[] = "0.8.0";
 
 #endif // DIGIKAM_VERSION_H
 
