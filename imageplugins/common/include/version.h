8c2e98489fc9c4636cad10b1f2f559e1ca96ce8a
Gilles Caulier <caulier.gilles@gmail.com>
2005-11-24 14:46:50 +0000

    fix release ID
    svn path=/trunk/extragear/graphics/digikamimageplugins/; revision=482912

--- imageplugins/common/include/version.h
+++ imageplugins/common/include/version.h
@@ -23,7 +23,7 @@
 #ifndef DIGIKAMIMAGEPLUGINS_VERSION_H
 #define DIGIKAMIMAGEPLUGINS_VERSION_H
 
-static const char digikamimageplugins_version[] = "0.8.0";
+static const char digikamimageplugins_version[] = "0.9.0-svn";
 
 #endif // DIGIKAMIMAGEPLUGINS_VERSION_H
 
