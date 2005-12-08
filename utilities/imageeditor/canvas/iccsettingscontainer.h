1a9d660d840ec93ef11755c978318c18e8b34ebe
Francisco J. Cruz <fj.cruz@supercable.es>
2005-12-08 15:32:55 +0000

    New class added to storage color management settings in only one common structure.
    CCMAIL:digikam-devel@kde.org
    
    svn path=/trunk/extragear/graphics/digikam/; revision=486728

---
+++ utilities/imageeditor/canvas/iccsettingscontainer.h
@@ -0,0 +1,50 @@
+/* ============================================================
+ * Author: F.J. Cruz <fj.cruz@supercable.es>
+ * Date  : 2005-12-08
+ * Description : ICC Settings Container..
+ * 
+ * Copyright 2005 by  F.J. Cruz
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+#ifndef ICCSETTINGSCONTAINER_H
+#define ICCSETTINGSCONTAINER_H
+
+#include <qstring.h>
+
+/**
+	@author digiKam team <digikam-devel@kde.org>
+*/
+class ICCSettingsContainer{
+public:
+    ICCSettingsContainer();
+
+    ~ICCSettingsContainer();
+
+    bool    enableCMSetting;
+    // FALSE -> apply
+    // TRUE  -> ask
+    bool    askOrApplySetting;
+    bool    BPCSetting;
+
+    int     renderingSetting;
+
+    QString workspaceSetting;
+    QString monitorSetting;
+    QString inputSetting;
+    QString proofSetting;
+    
+
+};
+
+#endif
