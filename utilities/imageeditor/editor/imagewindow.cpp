4ca1f2d81ffa39008e65136a6178b0e5d257027f
Gilles Caulier <caulier.gilles@gmail.com>
2006-01-09 12:31:20 +0000

    digikam from trunk : IE/showfoto bug fix about image file format pattern to use in saveAs method
    CCMAIL : digikam-devel@kde.org
    
    svn path=/trunk/extragear/graphics/digikam/; revision=495912

--- utilities/imageeditor/editor/imagewindow.cpp
+++ utilities/imageeditor/editor/imagewindow.cpp
@@ -1105,11 +1105,11 @@ bool ImageWindow::saveAs()
             QStringList imgExtList = QStringList::split(" ", mimetypes);
             for (QStringList::ConstIterator it = imgExtList.begin() ; it != imgExtList.end() ; it++)
             {    
-                imgExtPattern.append (KImageIO::typeForMime(*it));
+                imgExtPattern.append (KImageIO::typeForMime(*it).upper());
                 imgExtPattern.append (" ");
             }    
-            if ( imgExtPattern.contains("*.TIFF") ) imgExtPattern.append (" *.TIF");
-            if ( imgExtPattern.contains("*.JPEG") ) imgExtPattern.append (" *.JPG");
+            if ( imgExtPattern.contains("TIFF") ) imgExtPattern.append (" TIF");
+            if ( imgExtPattern.contains("JPEG") ) imgExtPattern.append (" JPG");
     
             if ( !imgExtPattern.contains( format.upper() ) )
             {
