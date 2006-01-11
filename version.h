77adf855c8342307f8e7f50301bb66e679266ae0
Gilles Caulier <caulier.gilles@gmail.com>
2006-01-11 10:46:46 +0000

    fix release number
    svn path=/branches/stable/extragear/graphics/digikam/; revision=496796

--- version.h
+++ version.h
@@ -22,7 +22,7 @@
 #ifndef DIGIKAM_VERSION_H
 #define DIGIKAM_VERSION_H
 
-static const char digikam_version[] = "0.8.1-svn";
+static const char digikam_version[] = "0.8.1";
 
 #endif // DIGIKAM_VERSION_H
 
