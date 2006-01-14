735ca6a40f2e07136f830bc88b9325f7d2e9a0a8
Tom Albers <toma@kde.org>
2006-01-14 22:38:17 +0000

    Achim always makes digiKam crash...
    svn path=/branches/stable/extragear/graphics/digikam/; revision=498201

--- digikam/digikamapp.cpp
+++ digikam/digikamapp.cpp
@@ -928,6 +928,8 @@ void DigikamApp::slotDownloadImages()
     QString cameraGuiPath = convertToLocalUrl(mCameraGuiPath);
     kdDebug() << "IN: " << mCameraGuiPath << " OUT: " << cameraGuiPath << endl;
     
+    if (cameraGuiPath.isNull())
+        return;
     
     bool alreadyThere = false;
     for (uint i = 0 ; i != actionCollection()->count() ; i++)
