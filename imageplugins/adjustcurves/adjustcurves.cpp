ad35525a5ba5888141ff8e83d10b29c026e53efd
Gilles Caulier <caulier.gilles@gmail.com>
2006-01-16 11:41:23 +0000

    digikam from trunk : fix broken dialog layout if button i18n is large
    svn path=/trunk/extragear/graphics/digikamimageplugins/; revision=498819

--- imageplugins/adjustcurves/adjustcurves.cpp
+++ imageplugins/adjustcurves/adjustcurves.cpp
@@ -4,7 +4,7 @@
  * Date  : 2004-12-01
  * Description : image histogram adjust curves.
  *
- * Copyright 2004-2005 by Gilles Caulier
+ * Copyright 2004-2006 by Gilles Caulier
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -95,7 +95,7 @@ AdjustCurveDialog::AdjustCurveDialog(QWidget* parent, QString title, QFrame* ban
                                        digikamimageplugins_version,
                                        I18N_NOOP("An image-histogram-curves adjustment plugin for digiKam."),
                                        KAboutData::License_GPL,
-                                       "(c) 2004-2005, Gilles Caulier",
+                                       "(c) 2004-2006, Gilles Caulier",
                                        0,
                                        "http://extragear.kde.org/apps/digikamimageplugins");
 
@@ -250,8 +250,12 @@ AdjustCurveDialog::AdjustCurveDialog(QWidget* parent, QString title, QFrame* ban
     
     // -------------------------------------------------------------
 
+    KIconLoader icon;
     m_resetButton = new QPushButton(i18n("&Reset"), gboxSettings);
-    QWhatsThis::add( m_resetButton, i18n("<p>Reset curves' values from the current selected channel."));
+    m_resetButton->setPixmap( icon.loadIcon( "locationbar_erase", (KIcon::Group)KIcon::Toolbar ) );
+    QToolTip::add( m_resetButton, i18n( "Reset current channel curves' values." ) );
+    QWhatsThis::add( m_resetButton, i18n("<p>If you press this button, all curves' values from the current selected channel "
+                                         "will be reset to the default values."));
 
     QHBoxLayout* l3 = new QHBoxLayout();
     l3->addWidget(m_curveType);
