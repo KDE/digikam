d9c046ab3af59c018bbaa1ec2bcf3efa89b1331a
Gilles Caulier <caulier.gilles@gmail.com>
2006-01-20 14:32:10 +0000

    digikam from trunk : to prevent any dupplicate code in showfoto and IE, sving context class must be declared outside. Need to use it in gui implementation
    CCMAIL: marcel.wiesweg@gmx.de
    
    
    svn path=/trunk/extragear/graphics/digikam/; revision=500540

---
+++ utilities/imageeditor/editor/savingcontextcontainer.h
@@ -0,0 +1,67 @@
+/* ============================================================
+ * Authors: Gilles Caulier <caulier dot gilles at free.fr>
+ *          Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Date   : 2006-01-20
+ * Description : image editor GUI saving context container
+ *
+ * Copyright 2006 by Gilles Caulier, Marcel Wiesweg
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef SAVINGCONTEXTCONTAINER_H
+#define SAVINGCONTEXTCONTAINER_H
+
+// QT includes.
+
+#include <qstring.h>
+
+// KDE includes
+
+#include <kurl.h>
+#include <kprogress.h>
+#include <ktempfile.h>
+
+namespace Digikam
+{
+
+class SavingContextContainer
+{
+
+public:
+
+    SavingContextContainer()
+    {
+        fromSave                = false;
+        saveTempFile            = 0;
+        fileExists              = false;
+        progressDialog          = 0;
+        synchronousSavingResult = false;
+    }
+
+    bool                     fromSave;
+    bool                     synchronousSavingResult;
+    bool                     fileExists;
+    
+    QString                  format;
+
+    KProgressDialog         *progressDialog;
+
+    KURL                     saveURL;
+
+    KTempFile               *saveTempFile;
+};
+
+} // namespace Digikam
+
+#endif /* SAVINGCONTEXTCONTAINER_H */
