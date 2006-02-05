07758e1480fd64cf96d3884afe9d9d7057cfe95f
Gilles Caulier <caulier.gilles@gmail.com>
2006-02-05 20:45:17 +0000

    why 'jpg' and 'tif' mime types are includes in RAW file mime types ?
    svn path=/branches/stable/extragear/graphics/digikam/; revision=506139

--- libs/dcraw/rawfiles.h
+++ libs/dcraw/rawfiles.h
@@ -22,6 +22,6 @@
 #ifndef RAW_FILES_H
 #define RAW_FILES_H
 
-static const char raw_file_extentions[] = "*.bay *.bmq *.cr2 *.crw *.cs1 *.dc2 *.dcr *.dng *.erf *.fff *.hdr *.jpg *.k25 *.kdc *.mdc *.mos *.mrw *.nef *.orf *.pef *.pxn *.raf *.raw *.rdc *.sr2 *.srf *.tif *.x3f";
+static const char raw_file_extentions[] = "*.bay *.bmq *.cr2 *.crw *.cs1 *.dc2 *.dcr *.dng *.erf *.fff *.hdr *.k25 *.kdc *.mdc *.mos *.mrw *.nef *.orf *.pef *.pxn *.raf *.raw *.rdc *.sr2 *.srf *.x3f";
 
 #endif // RAW_FILES_H
