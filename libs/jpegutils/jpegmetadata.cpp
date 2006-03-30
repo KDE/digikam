a247890c18d5e1406bb55813cae71be945676bf7
Gilles Caulier <caulier.gilles@gmail.com>
2006-03-30 10:02:34 +0000

    digikam from trunk : Removing libkexif depency from digiKam core.
    - jpegmetadata us now dmetadata to get comments and datetime.
    - All metadata extractions are now drived by DMetadata class using Exiv2 library.
    
    To packagers : please update your packages depencies.
    
    CCMAIL: digikam-devel@kde.org
    
    svn path=/trunk/extragear/graphics/digikam/; revision=524324

--- libs/jpegutils/jpegmetadata.cpp
+++ libs/jpegutils/jpegmetadata.cpp
@@ -18,57 +18,14 @@
  * 
  * ============================================================ */
 
-#define M_COM  0xFE
-#define M_EXIF 0xE1
-
-#define XMD_H 1
-
-// C+ includes.
-
-#include <cstdio>
-
-// C ansi includes.
-
-extern "C" 
-{
-#include <setjmp.h>
-#include <jpeglib.h>
-}
-
-// Qt includes.
-
-#include <qfile.h>
-
 // KDE includes.
 
 #include <kdebug.h>
 
-// LibKExif includes.
-
-#include <libkexif/kexifdata.h>
-
 // Local includes.
 
+#include "dmetadata.h"
 #include "jpegmetadata.h"
-
-struct readJPEGMetaData_error_mgr : public jpeg_error_mgr
-{
-    jmp_buf setjmp_buffer;
-};
-
-extern "C"
-{
-    static void readJPEGMetaData_error_exit(j_common_ptr cinfo)
-    {
-        readJPEGMetaData_error_mgr* myerr =
-            (readJPEGMetaData_error_mgr*) cinfo->err;
-
-        char buffer[JMSG_LENGTH_MAX];
-        (*cinfo->err->format_message)(cinfo, buffer);
-        kdWarning() << buffer << endl;
-        longjmp(myerr->setjmp_buffer, 1);
-    }
-}
 
 namespace Digikam
 {
@@ -79,84 +36,21 @@ void readJPEGMetaData(const QString& filePath,
 {
     comments = QString();
     datetime = QDateTime();
-
-    FILE *input_file = fopen(QFile::encodeName(filePath), "rb");
-    if (!input_file)
-        return;
     
-    struct jpeg_decompress_struct     srcinfo;
-    struct readJPEGMetaData_error_mgr jerr;
+    DMetadata metadata(filePath);
 
-    srcinfo.err             = jpeg_std_error(&jerr);
-    srcinfo.err->error_exit = readJPEGMetaData_error_exit;
-
-    if (setjmp(jerr.setjmp_buffer))
-    {
-        jpeg_destroy_decompress(&srcinfo);
-        fclose(input_file);
-        return;
-    }
+    // Trying to get comments from image :
+    // In first, from standard JPEG comments, or
+    // In second, from Exif comments tag, or
+    // In third, from IPTC comments tag.
+        
+    comments = metadata.getImageComment();
     
-    jpeg_create_decompress(&srcinfo);
-
-
-    unsigned short header;
+    // Trying to get date and time from image :
+    // In first, from Exif date & time tags, or
+    // In second, from IPTC date & time tags.
     
-    if (fread(&header, 2, 1, input_file) != 1)
-    {
-        fclose(input_file);
-        return;
-    }
-
-    if (header != 0xd8ff)
-    {
-        // not a jpeg file
-        fclose(input_file);
-        return;
-    }
-    
-    fseek(input_file, 0L, SEEK_SET);
-
-    jpeg_stdio_src(&srcinfo, input_file);
-
-    jpeg_save_markers(&srcinfo, M_COM, 0xFFFF);
-    jpeg_save_markers(&srcinfo, M_EXIF, 0xFFFF);
-
-    (void) jpeg_read_header(&srcinfo, true);
-
-    jpeg_saved_marker_ptr marker = srcinfo.marker_list;
-    while (marker)
-    {
-        if (marker->marker == M_COM)
-        {
-            if (!marker->data || !marker->data_length)
-            {
-                marker=marker->next;
-                continue;
-            }
-
-            comments = QString::fromAscii((const char*)marker->data,
-                                          marker->data_length);
-        }
-        else if (marker->marker == M_EXIF)
-        {
-            KExifData exifData;
-            if (!exifData.readFromData((char*)marker->data,
-                                       marker->data_length))
-            {
-                marker=marker->next;
-                continue;
-            }
-
-            datetime = exifData.getExifDateTime();
-        }
-
-        marker = marker->next;
-    }
-
-    jpeg_destroy_decompress(&srcinfo);
-
-    fclose(input_file);
+    datetime = metadata.getDateTime();
 }
 
 } // namespace Digikam
