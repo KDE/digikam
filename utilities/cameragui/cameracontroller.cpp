6f3287ddea19996ea065178274cd4744394f5cd4
Gilles Caulier <caulier.gilles@gmail.com>
2006-04-11 08:37:40 +0000

    digikam from trunk : camera controller bug fix : set properlly IPTC tags image Credits if only this option is selected from camera interface.
    svn path=/trunk/extragear/graphics/digikam/; revision=528496

--- utilities/cameragui/cameracontroller.cpp
+++ utilities/cameragui/cameracontroller.cpp
@@ -272,7 +272,7 @@ void CameraThread::run()
 
             if (!thumbnail.isNull())
             {
-                thumbnail = thumbnail.smoothScale(128,128,QImage::ScaleMin);
+                thumbnail = thumbnail.smoothScale(128, 128, QImage::ScaleMin);
         
                 CameraEvent* event = new CameraEvent(CameraEvent::gp_thumbnailed);
                 event->map.insert("folder", QVariant(folder));
@@ -344,7 +344,7 @@ void CameraThread::run()
                     exifRotate(tempURL.path());
                 }
 
-                if (fixDateTime || setPhotographerId)
+                if (fixDateTime || setPhotographerId || setCredits)
                 {
                     sendInfo(i18n("Set Metadata tags to file %1...").arg(file));
                     DMetadata metadata(tempURL.path());
