2a38e28d30987ce84f57adacc35c21624c0397a8
Gilles Caulier <caulier.gilles@gmail.com>
2006-06-09 13:23:47 +0000

    digikam from trunk: fix gcc 4.1 warning
    svn path=/trunk/extragear/graphics/digikam/; revision=549661

--- libs/dimg/dimg.cpp
+++ libs/dimg/dimg.cpp
@@ -454,22 +454,22 @@ DImg::FORMAT DImg::fileFormat(const QString& filePath)
     
     const int headerLen = 8;
     unsigned char header[headerLen];
-    
+
     if (fread(&header, 8, 1, f) != 1)
     {
         kdDebug() << k_funcinfo << "Failed to read header" << endl;
         fclose(f);
         return NONE;
     }
-    
+
     fclose(f);
-    
+
     DcrawParse rawFileParser;
-    uchar jpegID[2]    = { 0xFF, 0xD8 };   
+    uchar jpegID[2]    = { 0xFF, 0xD8 };
     uchar tiffBigID[2] = { 0x4D, 0x4D };
     uchar tiffLilID[2] = { 0x49, 0x49 };
-    uchar pngID[8]     = {'\211', 'P', 'N', 'G', '\r', '\n', '\032', '\n'};
-    
+    uchar pngID[8]     = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
+
     if (memcmp(&header, &jpegID, 2) == 0)            // JPEG file ?
     {
         return JPEG;
