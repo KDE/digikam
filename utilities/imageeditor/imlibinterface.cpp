1a054f30d955ce3ad554598131c4404d652530d8
Gilles Caulier <caulier.gilles@gmail.com>
2006-06-09 14:22:09 +0000

    digikam from trunk: fix gcc 4.1 warning
    svn path=/branches/stable/extragear/graphics/digikam/; revision=549690

--- utilities/imageeditor/imlibinterface.cpp
+++ utilities/imageeditor/imlibinterface.cpp
@@ -221,7 +221,7 @@ int ImlibInterface::fileFormat(const QString& filePath)
     uchar jpegID[2]    = { 0xFF, 0xD8 };   
     uchar tiffBigID[2] = { 0x4D, 0x4D };
     uchar tiffLilID[2] = { 0x49, 0x49 };
-    uchar pngID[8]     = {'\211', 'P', 'N', 'G', '\r', '\n', '\032', '\n'};
+    uchar pngID[8]     = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
 
     if (memcmp(&header, &jpegID, 2) == 0)            // JPEG file ?
     {
