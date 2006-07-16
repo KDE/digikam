ef31f2fdf289f2bf0bbd5dc91b392827f13266cb
Gilles Caulier <caulier.gilles@gmail.com>
2006-07-16 09:14:32 +0000

    compile
    svn path=/trunk/extragear/graphics/digikam/; revision=562918

--- utilities/imageeditor/canvas/dimginterface.cpp
+++ utilities/imageeditor/canvas/dimginterface.cpp
@@ -515,14 +515,14 @@ void DImgInterface::saveAs(const QString& fileName, IOFileSettingsContainer *iof
 
     // Update Iptc preview.
 
-    // TODO: see B.K.O #130525. The a JPEG segment is limited to 64K. If IPTC byte array 
+    // TODO: see B.K.O #130525. a JPEG segment is limited to 64K. If the IPTC byte array is
     // bigger than 64K duing of image preview tag size, the target JPEG image will be
     // broken. Note that IPTC image preview tag is limited to 256K!!!
     // Temp. solution to disable IPTC preview record in JPEG file until a right solution 
     // will be found into Exiv2.
     // Note : There is no limitation with TIFF and PNG about IPTC byte array size.
 
-    if ( !mimeType.upper() == QString("JPG") || !mimeType.upper() == QString("JPEG") ) 
+    if ( mimeType.upper() != QString("JPG") && mimeType.upper() != QString("JPEG") ) 
     {
         QImage preview = d->image.smoothScale(800, 600, QSize::ScaleMin).copyQImage();
         meta.setImagePreview(preview);
