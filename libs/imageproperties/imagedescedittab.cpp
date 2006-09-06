a6ea7956d36161e128076479726e4a92dda98293
Gilles Caulier <caulier.gilles@gmail.com>
2006-09-06 13:09:45 +0000

    digikam from trunk :using KPopupMenu instead QPopupMenu
    svn path=/trunk/extragear/graphics/digikam/; revision=581472

--- libs/imageproperties/imagedescedittab.cpp
+++ libs/imageproperties/imagedescedittab.cpp
@@ -573,10 +573,11 @@ void ImageDescEditTab::slotRightButtonClicked(QListViewItem *item, const QPoint 
     {
         popmenu.insertItem(SmallIcon("pencil"),      i18n("Edit Tag Properties..."), 11);
         popmenu.insertItem(SmallIcon("reload_page"), i18n("Reset Tag Icon"),         13);        
+        popmenu.insertSeparator(-1);
         popmenu.insertItem(SmallIcon("edittrash"),   i18n("Delete Tag"),             12);
     }
 
-    popmenu.insertSeparator();
+    popmenu.insertSeparator(-1);
     popmenu.insertItem(i18n("Select All"),       14);
     popmenu.insertItem(i18n("Deselect"),         15);
     popmenu.insertItem(i18n("Invert Selection"), 16);
