3a9bf1c7e72a90243ae66e38b228a14db3002fe9
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2007-01-09 21:41:08 +0000

    Introducing the class MetadataHub. It is supposed to be the backend for - Support for multiple selection - unify code to write metadata
    The class can be used to
    - read from ImageInfo objects
    - read from DMetadata objects
    - merge the information
    - change the contained data fields (date, comment, rating, tags)
    - write to Metadata
    - write to ImageInfo
    - retrieve the value of the data fields
    
    Many datasets can be merged. If the sets contain different data,
    the status of the field (date, comment, rating, single tags) will be "disjoint",
    the status will be "available" if all contained the same data.
    
    Note that the code is not yet in use or tested.
    
    
    svn path=/trunk/extragear/graphics/digikam/; revision=621841

---
+++ digikam/metadatahub.cpp
@@ -0,0 +1,632 @@
+/* ============================================================
+ * Authors: Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Date   : 2007-01-05
+ * Description : Metadata handling
+ * 
+ * Copyright      2007 by Marcel Wiesweg
+ *
+ * This program is free software you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// Qt includes
+
+#include <qfileinfo.h>
+
+// KDE includes
+
+// Local includes
+
+#include "ddebug.h"
+#include "imageinfo.h"
+#include "album.h"
+#include "albummanager.h"
+#include "albumsettings.h"
+#include "imageattributeswatch.h"
+#include "metadatahub.h"
+
+namespace Digikam
+{
+
+class MetadataHubPriv
+{
+public:
+
+    MetadataHubPriv()
+    {
+        dateTimeStatus = MetadataHub::MetadataInvalid;
+        ratingStatus   = MetadataHub::MetadataInvalid;
+        commentStatus  = MetadataHub::MetadataInvalid;
+
+        rating         = -1;
+        highestRating  = -1;
+
+        count          = 0;
+
+        dbmode         = MetadataHub::WithDatabase;
+    }
+
+    MetadataHub::Status dateTimeStatus;
+    MetadataHub::Status commentStatus;
+    MetadataHub::Status ratingStatus;
+
+    QDateTime           dateTime;
+    QDateTime           lastDateTime;
+    QString             comment;
+    int                 rating;
+    int                 highestRating;
+
+    int                 count;
+
+    QMap<TAlbum *, MetadataHub::TagStatus> tags;
+    QStringList            tagList;
+    MetadataHub::DatabaseMode dbmode;
+
+    template <class T> void loadWithInterval(const T &data, T &storage, T &highestStorage, MetadataHub::Status &status);
+    template <class T> void loadSingleValue(const T &data, T &storage, MetadataHub::Status &status);
+
+};
+
+MetadataWriteSettings::MetadataWriteSettings()
+{
+    saveComments           = false;
+    saveDateTime           = false;
+    saveIptcRating         = false;
+    saveIptcTags           = false;
+    saveIptcPhotographerId = false;
+    saveIptcCredits        = false;
+}
+
+MetadataWriteSettings::MetadataWriteSettings(AlbumSettings *albumSettings)
+{
+    saveComments           = albumSettings->getSaveComments();
+    saveDateTime           = albumSettings->getSaveDateTime();
+    saveIptcRating         = albumSettings->getSaveIptcRating();
+    saveIptcTags           = albumSettings->getSaveIptcTags();
+    saveIptcPhotographerId = albumSettings->getSaveIptcPhotographerId();
+    saveIptcCredits        = albumSettings->getSaveIptcCredits();
+
+    iptcAuthor             = albumSettings->getIptcAuthor();
+    iptcAuthorTitle        = albumSettings->getIptcAuthorTitle();
+    iptcCredit             = albumSettings->getIptcCredit();
+    iptcSource             = albumSettings->getIptcSource();
+    iptcCopyright          = albumSettings->getIptcCopyright();
+}
+
+MetadataHub::MetadataHub(DatabaseMode dbmode)
+{
+    d = new MetadataHubPriv;
+    d->dbmode = dbmode;
+}
+
+MetadataHub::~MetadataHub()
+{
+    delete d;
+}
+
+MetadataHub::MetadataHub(const MetadataHub &other)
+{
+    d = new MetadataHubPriv(*other.d);
+}
+
+MetadataHub &MetadataHub::operator=(const MetadataHub &other)
+{
+    (*d) = (*other.d);
+    return *this;
+}
+
+void MetadataHub::reset()
+{
+    (*d) = MetadataHubPriv();
+}
+
+// --------------------------------------------------
+
+void MetadataHub::load(ImageInfo *info)
+{
+    // DatabaseMode == WithDatabase is assumed
+    d->count++;
+
+    load(info->dateTime(), info->caption(), info->rating());
+
+    AlbumManager *man = AlbumManager::instance();
+    QValueList<int> tagIDs = info->tagIDs();
+    QValueList<TAlbum *> loadedTags;
+
+    for (QValueList<int>::iterator it = tagIDs.begin(); it != tagIDs.end(); ++it)
+    {
+        TAlbum *album = man->findTAlbum(*it);
+        if (!album)
+        {
+            DWarning() << k_funcinfo << "Tag id " << *it << " not found in database." << endl;
+            continue;
+        }
+        loadedTags.append(album);
+    }
+
+    loadTags(loadedTags);
+}
+
+void MetadataHub::load(const DMetadata &metadata)
+{
+    d->count++;
+
+    QString     comment;
+    QStringList keywords;
+    QDateTime   datetime;
+    int         rating;
+
+    // Try to get comments from image :
+    // In first, from standard JPEG comments, or
+    // In second, from EXIF comments tag, or
+    // In third, from IPTC comments tag.
+
+    comment = metadata.getImageComment();
+
+    // Try to get date and time from image :
+    // In first, from EXIF date & time tags, or
+    // In second, from IPTC date & time tags.
+
+    datetime = metadata.getImageDateTime();
+
+    // Try to get image rating from IPTC Urgency tag 
+    // else use file system time stamp.
+    rating = metadata.getImageRating();
+
+    if ( !datetime.isValid() )
+    {
+        QFileInfo info( metadata.getFilePath() );
+        datetime = info.lastModified();
+    }
+
+    load(datetime, comment, rating);
+
+    // Try to get image tags from IPTC keywords tags.
+
+    if (d->dbmode == WithDatabase)
+    {
+        AlbumManager *man = AlbumManager::instance();
+        QStringList tagPaths = metadata.getImageKeywords();
+        QValueList<TAlbum *> loadedTags;
+
+        for (QStringList::iterator it = tagPaths.begin(); it != tagPaths.end(); ++it)
+        {
+            TAlbum *album = man->findTAlbum(*it);
+            if (!album)
+            {
+                DWarning() << k_funcinfo << "Tag id " << *it << " not found in database." << endl;
+                continue;
+            }
+            loadedTags.append(album);
+        }
+
+        loadTags(loadedTags);
+    }
+    else
+    {
+        QStringList newTagPaths = metadata.getImageKeywords();
+        if (d->count == 1)
+        {
+            d->tagList = newTagPaths;
+        }
+        else
+        {
+            // a simple intersection
+            for (QStringList::iterator it = d->tagList.begin(); it != d->tagList.end(); ++it)
+            {
+                QStringList::iterator newTagListIt = newTagPaths.find(*it);
+                if (it == newTagPaths.end())
+                {
+                    // it's not in the newTagPaths list. Remove it from intersection list.
+                    it = d->tagList.remove(it);
+                }
+            }
+        }
+    }
+}
+
+void MetadataHub::load(const QString &filePath)
+{
+    DMetadata metadata(filePath);
+    load(metadata); // increments count
+}
+
+// private common code to merge tags
+void MetadataHub::loadTags(const QValueList<TAlbum *> &loadedTags)
+{
+    // get copy of tags
+    QValueList<TAlbum *> previousTags = d->tags.keys();
+
+    // first go through all tags contained in this set
+    for (QValueList<TAlbum *>::const_iterator it = loadedTags.begin(); it != loadedTags.end(); ++it)
+    {
+        QMap<TAlbum *, TagStatus>::iterator mapIt = d->tags.find(*it);
+        if (mapIt == d->tags.end() || mapIt.data() == MetadataInvalid)
+        {
+            if (d->count == 1)
+                // there were no previous sets that could have contained the set
+                mapIt.data() = TagStatus(MetadataAvailable, true);
+            else
+                // previous sets did not contain the tag, we do => disjoint
+                mapIt.data() = TagStatus(MetadataDisjoint, true);
+        }
+        else if (mapIt.data() == TagStatus(MetadataAvailable, false))
+        {
+            // set to explicitly not contained, but we contain it => disjoint
+            mapIt.data() = TagStatus(MetadataDisjoint, true);
+        }
+        // else if mapIt.data() ==  MetadataAvailable, true: all right, we contain it too
+        // else if mapIt.data() ==  MetadataDisjoint: it's already disjoint
+
+        // remove from the list to signal that this tag has been handled
+        previousTags.remove(*it);
+    }
+
+    // Those tags which had been set as MetadataAvailable before,
+    // but are not contained in this set, have to be set to MetadataDisjoint
+    for (QValueList<TAlbum *>::iterator it = previousTags.begin(); it != previousTags.end(); ++it)
+    {
+        QMap<TAlbum *, TagStatus>::iterator mapIt = d->tags.find(*it);
+        if (mapIt != d->tags.end() && mapIt.data() == TagStatus(MetadataAvailable, true))
+            mapIt.data() = TagStatus(MetadataDisjoint, true);
+    }
+}
+
+// private common code to load dateTime, comment, rating
+void MetadataHub::load(const QDateTime &dateTime, const QString &comment, int rating)
+{
+    if (dateTime.isValid())
+    {
+        d->loadWithInterval<QDateTime>(dateTime, d->dateTime, d->lastDateTime, d->dateTimeStatus);
+    }
+
+    d->loadWithInterval<int>(rating, d->rating, d->highestRating, d->ratingStatus);
+
+    d->loadSingleValue<QString>(comment, d->comment, d->commentStatus);
+}
+
+// template method to share code for dateTime and rating
+template <class T> void MetadataHubPriv::loadWithInterval(const T &data, T &storage, T &highestStorage, MetadataHub::Status &status)
+{
+    switch (status)
+    {
+        case MetadataHub::MetadataInvalid:
+            storage = data;
+            status = MetadataHub::MetadataAvailable;
+            break;
+        case MetadataHub::MetadataAvailable:
+            // we have two values. If they are equal, status is unchanged
+            if (data == storage)
+                break;
+            // they are not equal. We need to enter the disjoint state.
+            status = MetadataHub::MetadataDisjoint;
+            if (data > storage)
+            {
+                highestStorage = data;
+            }
+            else
+            {
+                highestStorage = storage;
+                storage = data;
+            }
+            break;
+        case MetadataHub::MetadataDisjoint:
+            // smaller value is stored in storage
+            if (data < storage)
+                storage = data;
+            else if (highestStorage < data)
+                highestStorage = data;
+            break;
+    }
+}
+
+// template method used by comment
+template <class T> void MetadataHubPriv::loadSingleValue(const T &data, T &storage, MetadataHub::Status &status)
+{
+    switch (status)
+    {
+        case MetadataHub::MetadataInvalid:
+            storage = data;
+            status = MetadataHub::MetadataAvailable;
+            break;
+        case MetadataHub::MetadataAvailable:
+            // we have two values. If they are equal, status is unchanged
+            if (data == storage)
+                break;
+            // they are not equal. We need to enter the disjoint state.
+            status = MetadataHub::MetadataDisjoint;
+            break;
+        case MetadataHub::MetadataDisjoint:
+            break;
+    }
+}
+
+// --------------------------------------------------
+
+void MetadataHub::write(ImageInfo *info)
+{
+    if (d->commentStatus == MetadataAvailable)
+        info->setCaption(d->comment);
+    if (d->dateTimeStatus == MetadataAvailable)
+        info->setDateTime(d->dateTime);
+    if (d->ratingStatus == MetadataAvailable)
+        info->setRating(d->rating);
+
+    for (QMap<TAlbum *, TagStatus>::iterator it = d->tags.begin(); it != d->tags.end(); ++it)
+    {
+        if (it.data() == MetadataAvailable)
+        {
+            if (it.data().hasTag)
+                info->setTag(it.key()->id());
+            else
+                info->removeTag(it.key()->id());
+        }
+    }
+}
+
+bool MetadataHub::write(DMetadata &metadata, const MetadataWriteSettings &settings)
+{
+    bool dirty = false;
+
+    if (settings.saveComments && d->commentStatus == MetadataAvailable)
+    {
+        // Store comments in image as JFIF comments, Exif comments, and Iptc Comments.
+        metadata.setImageComment(d->comment);
+        dirty = true;
+    }
+
+    if (settings.saveDateTime && d->dateTimeStatus == MetadataAvailable)
+    {
+        // Store Image Date & Time as Exif and Iptc tags.
+        metadata.setImageDateTime(d->dateTime, false);
+        dirty = true;
+    }
+
+    if (settings.saveIptcRating && d->ratingStatus == MetadataAvailable)
+    {
+        // Store Image rating as Iptc tag.
+        metadata.setImageRating(d->rating);
+        dirty = true;
+    }
+
+    if (settings.saveIptcTags)
+    {
+        // Store tag paths as Iptc keywords tags.
+
+        // DatabaseMode == WithDatabase is assumed.
+        // To fix this constraint (not needed currently), an oldKeywords parameter is needed
+
+        // create list of keywords to be added and to be removed
+        QStringList newKeywords, oldKeywords;
+        for (QMap<TAlbum *, TagStatus>::iterator it = d->tags.begin(); it != d->tags.end(); ++it)
+        {
+            // it is important that MetadataDisjoint keywords are not touched
+            if (it.data() == MetadataAvailable)
+            {
+                // This works for single and multiple selection.
+                // In both situations, tags which had originally been loaded
+                // have explicitly been removed with setTag.
+                if (it.data().hasTag)
+                    newKeywords.append(it.key()->tagPath(false));
+                else
+                    oldKeywords.append(it.key()->tagPath(false));
+            }
+        }
+        metadata.setImageKeywords(oldKeywords, newKeywords);
+        dirty = true;
+    }
+
+    if (settings.saveIptcPhotographerId)
+    {
+        // Store Photograph identity into the Iptc tags.
+        metadata.setImagePhotographerId(settings.iptcAuthor,
+                                        settings.iptcAuthorTitle);
+        dirty = true;
+    }
+
+    if (settings.saveIptcCredits)
+    {
+        // Store Photograph identity into the Iptc tags.
+        metadata.setImageCredits(settings.iptcCredit,
+                                 settings.iptcSource,
+                                 settings.iptcCopyright);
+        dirty = true;
+    }
+
+    return dirty;
+}
+
+void MetadataHub::write(const QString &filePath, const MetadataWriteSettings &settings)
+{
+    DMetadata metadata(filePath);
+    if (write(metadata, settings))
+    {
+        metadata.applyChanges();
+        ImageAttributesWatch::instance()->fileMetadataChanged(filePath);
+    }
+}
+
+MetadataWriteSettings MetadataHub::defaultWriteSettings()
+{
+    if (AlbumSettings::instance())
+        return MetadataWriteSettings(AlbumSettings::instance());
+    else
+        // is this check necessary?
+        return MetadataWriteSettings();
+}
+
+// --------------------------------------------------
+
+MetadataHub::Status MetadataHub::dateTimeStatus() const
+{
+    return d->dateTimeStatus;
+}
+
+MetadataHub::Status MetadataHub::commentStatus() const
+{
+    return d->commentStatus;
+}
+
+MetadataHub::Status MetadataHub::ratingStatus() const
+{
+    return d->ratingStatus;
+}
+
+MetadataHub::TagStatus MetadataHub::tagStatus(int albumId) const
+{
+    if (d->dbmode == NoDatabase)
+        return TagStatus(MetadataInvalid);
+    return tagStatus(AlbumManager::instance()->findTAlbum(albumId));
+}
+
+MetadataHub::TagStatus MetadataHub::tagStatus(const QString &tagPath) const
+{
+    if (d->dbmode == NoDatabase)
+        return TagStatus(MetadataInvalid);
+    return tagStatus(AlbumManager::instance()->findTAlbum(tagPath));
+}
+
+MetadataHub::TagStatus MetadataHub::tagStatus(TAlbum *album) const
+{
+    if (!album)
+        return TagStatus(MetadataInvalid);
+    QMap<TAlbum *, TagStatus>::iterator mapIt = d->tags.find(album);
+    if (mapIt == d->tags.end())
+        return TagStatus(MetadataInvalid);
+    return mapIt.data();
+}
+
+
+QDateTime MetadataHub::dateTime() const
+{
+    return d->dateTime;
+}
+
+QString MetadataHub::comment() const
+{
+    return d->comment;
+}
+
+int MetadataHub::rating() const
+{
+    return d->rating;
+}
+
+void MetadataHub::dateTimeInterval(QDateTime &lowest, QDateTime &highest) const
+{
+    switch (d->dateTimeStatus)
+    {
+        case MetadataInvalid:
+            lowest = highest = QDateTime();
+            break;
+        case MetadataAvailable:
+            lowest = highest = d->dateTime;
+            break;
+        case MetadataDisjoint:
+            lowest = d->dateTime;
+            highest = d->lastDateTime;
+            break;
+    }
+}
+
+void MetadataHub::ratingInterval(int &lowest, int &highest) const
+{
+    switch (d->ratingStatus)
+    {
+        case MetadataInvalid:
+            lowest = highest = -1;
+            break;
+        case MetadataAvailable:
+            lowest = highest = d->rating;
+            break;
+        case MetadataDisjoint:
+            lowest = d->rating;
+            highest = d->highestRating;
+            break;
+    }
+}
+
+QStringList MetadataHub::keywords() const
+{
+    if (d->dbmode == NoDatabase)
+        return d->tagList;
+    else
+    {
+        QStringList tagList;
+        for (QMap<TAlbum *, TagStatus>::iterator it = d->tags.begin(); it != d->tags.end(); ++it)
+        {
+            if (it.data() == TagStatus(MetadataAvailable, true))
+                tagList.append(it.key()->tagPath(false));
+        }
+        return tagList;
+    }
+}
+
+QMap<TAlbum *, MetadataHub::TagStatus> MetadataHub::tags() const
+{
+    // DatabaseMode == WithDatabase is assumed
+    return d->tags;
+}
+
+QMap<int, MetadataHub::TagStatus> MetadataHub::tagIDs() const
+{
+    // DatabaseMode == WithDatabase is assumed
+    QMap<int, TagStatus> intmap;
+    for (QMap<TAlbum *, TagStatus>::iterator it = d->tags.begin(); it != d->tags.end(); ++it)
+    {
+        intmap.insert(it.key()->id(), it.data());
+    }
+    return intmap;
+}
+
+
+// --------------------------------------------------
+
+void MetadataHub::setDateTime(const QDateTime &dateTime, Status status)
+{
+    d->dateTimeStatus = status;
+    d->dateTime       = dateTime;
+}
+
+void MetadataHub::setComment(const QString &comment, Status status)
+{
+    d->commentStatus  = status;
+    d->comment        = comment;
+}
+
+void MetadataHub::setRating(int rating, Status status)
+{
+    d->ratingStatus   = status;
+    d->rating         = rating;
+}
+
+void MetadataHub::setTag(TAlbum *tag, bool hasTag, Status status)
+{
+    // DatabaseMode == WithDatabase is assumed
+    d->tags[tag] = TagStatus(status, hasTag);
+}
+
+void MetadataHub::setTag(int albumID, bool hasTag, Status status)
+{
+    // DatabaseMode == WithDatabase is assumed
+    TAlbum *album = AlbumManager::instance()->findTAlbum(albumID);
+    if (!album)
+    {
+        DWarning() << k_funcinfo << "Tag ID " << albumID << " not found in database." << endl;
+        return;
+    }
+    setTag(album, hasTag, status);
+}
+
+
+}
+
