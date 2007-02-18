322f4341eaead2826e1f3b90e51cbdb194ea9cd0
Gerhard Kulzer <gerhard@kulzer.net>
2007-02-18 10:24:47 +0000

    correct email and spelling errors found in http://www.englishbreakfastnetwork.org/krazy/reports/extragear/graphics/digikam/index.html
    svn path=/trunk/extragear/graphics/digikam/; revision=634762

--- digikam/icongroupitem.cpp
+++ digikam/icongroupitem.cpp
@@ -3,7 +3,7 @@
  * Date  : 2005-04-24
  * Description : 
  * 
- * Copyright 2005 by Renchi Raju
+ * Copyright 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
