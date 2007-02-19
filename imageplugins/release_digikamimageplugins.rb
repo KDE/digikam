91765033c540edcd1bc8873c3ea049a5783d9512
Gerhard Kulzer <gerhard@kulzer.net>
2007-02-19 11:09:22 +0000

    0.9.1-rc1
    svn path=/trunk/extragear/graphics/digikamimageplugins/; revision=635187

--- imageplugins/release_digikamimageplugins.rb
+++ imageplugins/release_digikamimageplugins.rb
@@ -9,7 +9,7 @@
 
 name       = "digikamimageplugins"
 egmodule   = "graphics"
-version    = "0.9.1-beta1"
+version    = "0.9.1-rc1"
 docs       = "no"
 
 svnbase	   = "svn+ssh://gkulzer@svn.kde.org/home/kde"
