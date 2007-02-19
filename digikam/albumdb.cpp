6f5dc1a87e5f940b2ef0047254c6ec8d70e71a4a
Gerhard Kulzer <gerhard@kulzer.net>
2007-02-19 11:04:30 +0000

    move email at Author to Copyright line http://www.englishbreakfastnetwork.org/krazy/reports/extragear/graphics/digikam/index.html
    svn path=/trunk/extragear/graphics/digikam/; revision=635182

--- digikam/albumdb.cpp
+++ digikam/albumdb.cpp
@@ -1,13 +1,13 @@
 /* ============================================================
- * Authors: Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Authors: Renchi Raju
  *          Gilles Caulier 
- *          Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *          Marcel Wiesweg
  * Date   : 2004-06-18
  * Description : database album interface.
  *
- * Copyright 2004-2005 by Renchi Raju
+ * Copyright 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
  * Copyright 2006 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright 2006 by Marcel Wiesweg
+ * Copyright 2006 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  * 
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
