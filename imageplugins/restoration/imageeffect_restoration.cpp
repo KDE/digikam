f68f58e47899365a0a8a2847968b9f3154ada540
Gerhard Kulzer <gerhard@kulzer.net>
2007-02-19 11:01:09 +0000

    rpl  'QString::null' 'QString()'
    svn path=/trunk/extragear/graphics/digikamimageplugins/; revision=635177

--- imageplugins/restoration/imageeffect_restoration.cpp
+++ imageplugins/restoration/imageeffect_restoration.cpp
@@ -5,7 +5,7 @@
  * Description : a digiKam image editor plugin to restore 
  *               a photograph
  * 
- * Copyright 2005 by Gilles Caulier
+ * Copyright 2005 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -92,7 +92,7 @@ ImageEffect_Restoration::ImageEffect_Restoration(QWidget* parent, QString title,
     m_mainTab->addTab( firstPage, i18n("Preset") );
 
     KURLLabel *cimgLogoLabel = new KURLLabel(firstPage);
-    cimgLogoLabel->setText(QString::null);
+    cimgLogoLabel->setText(QString());
     cimgLogoLabel->setURL("http://cimg.sourceforge.net");
     KGlobal::dirs()->addResourceType("cimg-logo", KGlobal::dirs()->kde_default("data") + "digikamimageplugins/data");
     QString directory = KGlobal::dirs()->findResourceDir("cimg-logo", "cimg-logo.png");
