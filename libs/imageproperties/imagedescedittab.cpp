f19299253f9957efe1571d27115f7c14cfba27bb
Gerhard Kulzer <gerhard@kulzer.net>
2007-02-20 19:36:21 +0000

    "(" -> '('
    svn path=/trunk/extragear/graphics/digikam/; revision=635740

--- libs/imageproperties/imagedescedittab.cpp
+++ libs/imageproperties/imagedescedittab.cpp
@@ -1419,7 +1419,7 @@ void ImageDescEditTab::updateRecentTags()
                         icon = loader->getStandardTagIcon(album, 20);
                     }
                 }
-                QString text = album->title() + " (" + ((TAlbum*)album->parent())->prettyURL() + ")";
+                QString text = album->title() + ' (' + ((TAlbum*)album->parent())->prettyURL() + ')';
                 menu->insertItem(icon, text, album->id());
             }
         }
