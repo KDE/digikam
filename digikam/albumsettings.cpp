14731e7562316f8647828df539729c4a6b25183a
Gilles Caulier <caulier.gilles@gmail.com>
2007-03-14 15:08:11 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=642497

--- digikam/albumsettings.cpp
+++ digikam/albumsettings.cpp
@@ -1,11 +1,11 @@
 /* ============================================================
  * Authors: Renchi Raju <renchi@pooh.tam.uiuc.edu>
- *          Caulier Gilles 
+ *          Caulier Gilles <caulier dot gilles at gmail dot com>
  * Date   : 2003-16-10
  * Description : albums settings interface
  *
- * Copyright 2003-2004 by Renchi Raju by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright 2003-2004 by Renchi Raju by Gilles Caulier 
+ * Copyright 2005-2007 by Gilles Caulier 
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
