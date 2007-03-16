a70bfc0311181b96a35dc02c0bc6b10c7836b142
Gilles Caulier <caulier.gilles@gmail.com>
2007-03-16 07:33:09 +0000

    polish
    svn path=/trunk/extragear/graphics/digikamimageplugins/; revision=643040

--- imageplugins/blowup/imageplugin_blowup.cpp
+++ imageplugins/blowup/imageplugin_blowup.cpp
@@ -1,10 +1,9 @@
 /* ============================================================
- * File  : imageplugin_blowup.cpp
- * Author: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date  : 2005-04-07
+ * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Date   : 2005-04-07
  * Description : 
  * 
- * Copyright 2005 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright 2005-2007 by Gilles Caulier 
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -31,6 +30,7 @@
 
 #include "imageeffect_blowup.h"
 #include "imageplugin_blowup.h"
+#include "imageplugin_blowup.moc"
 
 K_EXPORT_COMPONENT_FACTORY( digikamimageplugin_blowup,
                             KGenericFactory<ImagePlugin_BlowUp>("digikamimageplugin_blowup"));
@@ -61,5 +61,3 @@ void ImagePlugin_BlowUp::slotBlowUp()
     DigikamBlowUpImagesPlugin::ImageEffect_BlowUp dlg(parentWidget());
     dlg.exec();
 }
-
-#include "imageplugin_blowup.moc"
--- imageplugins/blowup/imageplugin_blowup.h
+++ imageplugins/blowup/imageplugin_blowup.h
@@ -1,10 +1,9 @@
 /* ============================================================
- * File  : imageplugin_blowup.h
- * Author: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date  : 2005-04-07
+ * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Date   : 2005-04-07
  * Description : 
  * 
- * Copyright 2005 by Gilles Caulier
+ * Copyright 2005-2007 by Gilles Caulier
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -18,7 +17,6 @@
  * GNU General Public License for more details.
  * 
  * ============================================================ */
-
 
 #ifndef IMAGEPLUGIN_BLOWUP_H
 #define IMAGEPLUGIN_BLOWUP_H
@@ -48,7 +46,6 @@ private:
 private slots:
 
     void slotBlowUp();
-
 };
     
 #endif /* IMAGEPLUGIN_BLOWUP_H */
