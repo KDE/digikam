7b369d07afaebcf2923ecb7a2b7ac4812af38e04
Gilles Caulier <caulier.gilles@gmail.com>
2007-03-21 10:25:35 +0000

    digikam from trunk : adjust levels plugin is now ported to digiKam core
    svn path=/trunk/extragear/graphics/digikam/; revision=644938

---
+++ imageplugins/adjustlevels/adjustlevels.h
@@ -0,0 +1,147 @@
+/* ============================================================
+ * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Date   : 2004-07-20
+ * Description : image histogram adjust levels. 
+ * 
+ * Copyright 2004-2007 by Gilles Caulier
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef ADJUSTLEVELS_H
+#define ADJUSTLEVELS_H
+
+// Digikam includes.
+
+#include "imagedlgbase.h"
+#include "dimg.h"
+
+class QComboBox;
+class QSpinBox;
+class QPushButton;
+class QHButtonGroup;
+
+class KDoubleSpinBox;
+class KGradientSelector;
+class KDoubleNumInput;
+
+namespace Digikam
+{   
+class HistogramWidget;
+class ImageWidget;
+class ImageLevels;
+}
+
+namespace DigikamAdjustLevelsImagesPlugin
+{
+
+class AdjustLevelDialog : public Digikam::ImageDlgBase
+{
+    Q_OBJECT
+
+public:
+
+    AdjustLevelDialog(QWidget *parent);
+    ~AdjustLevelDialog();
+
+private:
+
+    void readUserSettings();
+    void writeUserSettings();
+    void resetValues();
+    void finalRendering(); 
+    void adjustSliders(int minIn, double gamIn, int maxIn, int minOut, int maxOut);
+    
+private slots:
+
+    void slotUser2();
+    void slotUser3();
+    void slotEffect();
+    void slotResetCurrentChannel();
+    void slotAutoLevels();
+    void slotChannelChanged(int channel);
+    void slotScaleChanged(int scale);
+    void slotAdjustSliders();
+    void slotGammaInputchanged(double val);
+    void slotAdjustMinInputSpinBox(int val);
+    void slotAdjustMaxInputSpinBox(int val);
+    void slotAdjustMinOutputSpinBox(int val);
+    void slotAdjustMaxOutputSpinBox(int val);
+    void slotSpotColorChanged(const Digikam::DColor &color);
+    void slotColorSelectedFromTarget(const Digikam::DColor &color);
+    void slotPickerColorButtonActived();    
+
+private:
+    
+    enum HistogramScale
+    {
+        Linear=0,
+        Logarithmic
+    };
+    
+    enum ColorChannel
+    {
+        LuminosityChannel=0,
+        RedChannel,
+        GreenChannel,
+        BlueChannel,
+        AlphaChannel
+    };
+    
+    enum ColorPicker
+    {
+        BlackTonal=0,
+        GrayTonal,
+        WhiteTonal
+    };
+
+    uchar                     *m_destinationPreviewData;
+    
+    int                        m_histoSegments;
+    int                        m_currentPreviewMode;
+    
+    QComboBox                 *m_channelCB;    
+    
+    QSpinBox                  *m_minInput;
+    QSpinBox                  *m_maxInput;
+    QSpinBox                  *m_minOutput;
+    QSpinBox                  *m_maxOutput;
+    
+    QPushButton               *m_autoButton;
+    QPushButton               *m_resetButton;
+    QPushButton               *m_pickBlack;
+    QPushButton               *m_pickGray;
+    QPushButton               *m_pickWhite;
+    
+    QHButtonGroup             *m_pickerColorButtonGroup;
+    QHButtonGroup             *m_scaleBG;
+
+    KDoubleNumInput           *m_gammaInput;
+    
+    KGradientSelector         *m_hGradientMinInput;
+    KGradientSelector         *m_hGradientMaxInput;
+    KGradientSelector         *m_hGradientMinOutput;
+    KGradientSelector         *m_hGradientMaxOutput;
+    
+    Digikam::HistogramWidget  *m_levelsHistogramWidget;
+    Digikam::HistogramWidget  *m_histogramWidget;
+    
+    Digikam::ImageWidget      *m_previewWidget;
+    
+    Digikam::ImageLevels      *m_levels;
+    Digikam::DImg              m_originalImage;
+};
+
+}  // NameSpace DigikamAdjustLevelsImagesPlugin
+
+#endif /* ADJUSTLEVELS_H */
---
+++ imageplugins/adjustlevels/imageplugin_adjustlevels.cpp
@@ -0,0 +1,67 @@
+/* ============================================================
+ * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Date   : 2004-06-04
+ * Description : 
+ * 
+ * Copyright 2004-2007 by Gilles Caulier 
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// KDE includes.
+  
+#include <klocale.h>
+#include <kgenericfactory.h>
+#include <klibloader.h>
+#include <kaction.h>
+#include <kcursor.h>
+
+// Local includes.
+
+#include "ddebug.h"
+#include "adjustlevels.h"
+#include "imageplugin_adjustlevels.h"
+#include "imageplugin_adjustlevels.moc"
+
+K_EXPORT_COMPONENT_FACTORY( digikamimageplugin_adjustlevels,
+                            KGenericFactory<ImagePlugin_AdjustLevels>("digikamimageplugin_adjustlevels"))
+
+ImagePlugin_AdjustLevels::ImagePlugin_AdjustLevels(QObject *parent, const char*,
+                                                   const QStringList &)
+                        : Digikam::ImagePlugin(parent, "ImagePlugin_AdjustLevels")
+{
+    m_levelsAction = new KAction(i18n("Levels Adjust..."), "adjustlevels", 
+                                 CTRL+Key_L,         // NOTE: Photoshop 7 use CTRL+L. 
+                                 this, SLOT(slotLevelsAdjust()),
+                                 actionCollection(), "imageplugin_adjustlevels");
+
+    setXMLFile("digikamimageplugin_adjustlevels_ui.rc");
+    
+    DDebug() << "ImagePlugin_AdjustLevels plugin loaded" << endl;
+}
+
+ImagePlugin_AdjustLevels::~ImagePlugin_AdjustLevels()
+{
+}
+
+void ImagePlugin_AdjustLevels::setEnabledActions(bool enable)
+{
+    m_levelsAction->setEnabled(enable);
+}
+
+void ImagePlugin_AdjustLevels::slotLevelsAdjust()
+{
+    DigikamAdjustLevelsImagesPlugin::AdjustLevelDialog dlg(parentWidget());
+    dlg.exec();
+}
+
