211a5d29b10b53caf20b88da96495759c87e7e6d
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2007-04-01 14:25:59 +0000

    This class shall ease and unify the usage of digikamalbums:/, digikamtags:/, digikamdates:/ and digikamsearch: URLs.
    svn path=/branches/work/digikam-dbconsolidation/; revision=648846

---
+++ libs/database/databaseurl.cpp
@@ -0,0 +1,251 @@
+/* ============================================================
+ * Authors: Marcel Wiesweg
+ * Date   : 2007-03-19
+ * Description : database interface.
+ * 
+ * Copyright 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// Qt includes
+
+#include <qstringlist.h>
+
+// Local includes
+
+#include "databaseurl.h"
+
+namespace Digikam
+{
+
+DatabaseUrl DatabaseUrl::fromFileUrl(const KURL &fileUrl,
+                                     const KURL &albumRoot,
+                                     const DatabaseParameters &parameters)
+{
+    DatabaseUrl url;
+    url.setProtocol("digikamalbums");
+    // get album root path without trailing slash
+    QString albumRootPath = albumRoot.path(-1);
+    // get the hierarchy below the album root
+    QString pathUnderRoot = fileUrl.path().remove(albumRootPath);
+    url.setPath(pathUnderRoot);
+    url.addQueryItem("albumRoot", albumRootPath);
+    url.setParameters(parameters);
+    return url;
+}
+
+DatabaseUrl DatabaseUrl::fromAlbumAndName(const QString &name,
+                                          const QString &album,
+                                          const KURL &albumRoot,
+                                          const DatabaseParameters &parameters)
+{
+    DatabaseUrl url;
+    url.setProtocol("digikamalbums");
+
+    url.setPath("/");
+    url.addPath(album);
+    url.addPath(name);
+
+    url.addQueryItem("albumRoot", albumRoot.path(-1));
+    url.setParameters(parameters);
+    return url;
+}
+
+DatabaseUrl DatabaseUrl::fromTagIds(const QValueList<int> tagIds,
+                                   const DatabaseParameters &parameters)
+{
+    DatabaseUrl url;
+    url.setProtocol("digikamtags");
+
+    for (QValueList<int>::const_iterator it = tagIds.begin(); it != tagIds.end(); ++it)
+    {
+        url.addPath('/' + QString::number(*it));
+    }
+
+    url.setParameters(parameters);
+    return url;
+}
+
+DatabaseUrl DatabaseUrl::fromDate(const QDate &date,
+                                  const DatabaseParameters &parameters)
+{
+    DatabaseUrl url;
+    url.setProtocol("digikamdates");
+
+    url.setPath(date.toString(Qt::ISODate));
+
+    url.setParameters(parameters);
+    return url;
+}
+
+DatabaseUrl DatabaseUrl::fromSearchUrl(const KURL &searchURL,
+                          const DatabaseParameters &parameters)
+{
+    DatabaseUrl url(searchURL);
+    url.setParameters(parameters);
+    return url;
+}
+
+
+
+DatabaseUrl::DatabaseUrl(const KURL &digikamalbumsUrl)
+    : KURL(digikamalbumsUrl)
+{
+}
+
+DatabaseUrl::DatabaseUrl(const DatabaseUrl &url)
+    : KURL(url)
+{
+}
+
+DatabaseUrl::DatabaseUrl()
+{
+}
+
+DatabaseUrl &DatabaseUrl::operator=(const KURL &digikamalbumsUrl)
+{
+    KURL::operator=(digikamalbumsUrl);
+    return *this;
+}
+
+DatabaseUrl &DatabaseUrl::operator=(const DatabaseUrl &url)
+{
+    KURL::operator=(url);
+    return *this;
+}
+
+bool DatabaseUrl::operator==(const KURL &digikamalbumsUrl)
+{
+    return KURL::operator==(digikamalbumsUrl);
+}
+
+/*
+DatabaseUrl::operator DatabaseParameters() const
+{
+    return parameters();
+}*/
+
+
+
+// --- Database parameters ---
+
+DatabaseParameters DatabaseUrl::parameters() const
+{
+    return DatabaseParameters(*this);
+}
+
+void DatabaseUrl::setParameters(const DatabaseParameters &parameters)
+{
+    parameters.insertInUrl(*this);
+}
+
+
+// --- Protocol ---
+
+bool DatabaseUrl::isAlbumUrl() const
+{
+    return protocol() == QString("digikamalbums");
+}
+
+bool DatabaseUrl::isTagUrl() const
+{
+    return protocol() == QString("digikamtags");
+}
+
+bool DatabaseUrl::isDateUrl() const
+{
+    return protocol() == QString("digikamdates");
+}
+
+bool DatabaseUrl::isSearchUrl() const
+{
+    return protocol() == QString("digikamsearch");
+}
+
+
+// --- Album URL ---
+
+KURL DatabaseUrl::albumRoot() const
+{
+    QString albumRoot = queryItem("albumRoot");
+    if (!albumRoot.isNull())
+    {
+        KURL albumRootUrl;
+        albumRootUrl.setPath(albumRoot);
+        return albumRootUrl;
+    }
+    return KURL();
+}
+
+QString DatabaseUrl::album() const
+{
+    // strip the trailing slash from result
+    // do not ignore trailing slash in the path - albums have a trailing slash
+    return directory(true, false);
+}
+
+QString DatabaseUrl::name() const
+{
+    // do not ignore trailing slash in the path - albums have a trailing slash
+    return fileName(false);
+}
+
+KURL DatabaseUrl::fileUrl() const
+{
+    KURL fileUrl(albumRoot());
+    fileUrl.addPath(path());
+    return fileUrl;
+}
+
+
+// --- Tag URL ---
+
+int DatabaseUrl::tagId() const
+{
+    if (path() == "/")
+        return -1;
+    return fileName().toInt();
+}
+
+QValueList<int> DatabaseUrl::tagIds() const
+{
+    QValueList<int> ids;
+    QStringList stringIds = QStringList::split('/', path());
+    for (QStringList::iterator it = stringIds.begin(); it != stringIds.end(); ++it)
+    {
+        ids << (*it).toInt();
+    }
+    return ids;
+}
+
+// --- Date URL ---
+
+QDate DatabaseUrl::date() const
+{
+    return QDate::fromString(path(), Qt::ISODate);
+}
+
+// --- Search URL ---
+
+KURL DatabaseUrl::searchUrl() const
+{
+    KURL url(*this);
+    DatabaseParameters::removeFromUrl(url);
+    return url;
+}
+
+
+
+}
+
---
+++ libs/database/databaseurl.h
@@ -0,0 +1,193 @@
+/* ============================================================
+ * Authors: Marcel Wiesweg
+ * Date   : 2007-03-19
+ * Description : database interface.
+ * 
+ * Copyright 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef DATABASEURL_H
+#define DATABASEURL_H
+
+#include <qdatetime.h>
+#include <qvaluelist.h>
+
+#include <kurl.h>
+
+#include "digikam_export.h"
+#include "databaseparameters.h"
+#include "databaseaccess.h"
+
+namespace Digikam
+{
+
+class DIGIKAM_EXPORT DatabaseUrl : public KURL
+{
+public:
+
+    /**
+      * This class shall facilitate the usage of
+      * digikamalbums:/, digikamtags:/, digikamdates:/ and digikamsearch: URLs.
+      * It provides functions to set and get the parameters stored in such a URL.
+      * (with the exception of the search parameters in a search URL, which
+      *  are out of the scope of this class.)
+      */
+
+    /**
+      * Create a digikamalbums:/ URL from a file:// URL.
+      * The file URL can point to a file or a directory (an album in this case).
+      * The additional information stored in the URL need to be supplied as well:
+      * - The album root in which the entity pointed to is stored.
+      *   This is the left part of the file URL.
+      *   (if the file is "/media/fotos/Summer 2007/001.jpg", the album root may be "/media/fotos")
+      * - The parameters of the database that is referenced
+      */
+    static DatabaseUrl fromFileUrl(const KURL &fileUrl,
+                                   const KURL &albumRoot,
+                                   const DatabaseParameters &parameters = DatabaseAccess::parameters());
+
+    /**
+     * Create a digikamalbums:/ url from an album name and an image in this album.
+     * If name is empty, the album is referenced.
+     * Other parameters as above.
+     */
+    static DatabaseUrl fromAlbumAndName(const QString &name,
+                                        const QString &album,
+                                        const KURL &albumRoot,
+                                        const DatabaseParameters &parameters = DatabaseAccess::parameters());
+
+    /**
+     * Create a digikamtags:/ url from a list of tag IDs, where this list is the tag hierarchy
+     * of the referenced tag, with the topmost parent first, and the tag last in the list.
+     * An empty list references the root tag.
+     */
+    static DatabaseUrl fromTagIds(const QValueList<int> tagIds,
+                                  const DatabaseParameters &parameters = DatabaseAccess::parameters());
+
+    /**
+     * Create a digikamdates:/ url from the given date.
+     */
+    static DatabaseUrl fromDate(const QDate &date,
+                                const DatabaseParameters &parameters = DatabaseAccess::parameters());
+
+    /**
+     * Create a digikamsearch: URL.
+     * It is assumed that the URL provided is a digikamsearch: URL
+     * The database parameters will be added to the provided URL.
+     * Populating the query items is out of the scope of this class.
+     */
+    static DatabaseUrl fromSearchUrl(const KURL &searchURL,
+                                     const DatabaseParameters &parameters = DatabaseAccess::parameters());
+    /**
+      * Create a DatabaseUrl object from a KURL, to retrieve the information stored
+      */
+    DatabaseUrl(const KURL &digikamUrl);
+
+    /**
+     * Create an invalid database URL
+     */
+    DatabaseUrl();
+
+    /**
+     * These test for the protocol of this URL.
+     * The protocol string is of course available via protocol().
+     */
+    bool isAlbumUrl() const;
+    bool isTagUrl() const;
+    bool isDateUrl() const;
+    bool isSearchUrl() const;
+
+    /**
+     * Returns the DatabaseParameters stored in this URL.
+     * Applicable to all protocols.
+     */
+    DatabaseParameters parameters() const;
+    /**
+     * Change the database parameters stored in this URL
+     * Applicable to all protocols.
+     */
+    void setParameters(const DatabaseParameters &parameters);
+
+
+    /**
+     * The following methods are only applicable for a certain protocol each.
+     * If the URL has another protocol, the return value of these methods is undefined.
+     */
+
+
+    /// Album URL
+
+    /** Returns the album root of the file or album referenced by this URL
+     *  In the example above, this is "/media/fotos"
+     */
+    KURL albumRoot() const;
+    /** Returns the album: This is the directory hierarchy below the album root.
+     *  In the example above, the album is "/Summer 2007"
+     */
+    QString album() const;
+    /**
+     * Returns the file name. In the example above, this is "001.jpg"
+     */
+    QString name() const;
+    /**
+     * Converts this digikamalbums:// URL to a file:// URL
+     */
+    KURL fileUrl() const;
+
+
+    /// Tag URL
+
+    /**
+     * Returns the tag ID, or -1 if the root tag is referenced
+     */
+    int tagId() const;
+
+    /**
+     * Returns the tag ids of all tags in the tag path of this tag,
+     * the topmost tag in the hierarchy first.
+     */
+    QValueList<int> tagIds() const;
+
+
+    /// Date URL
+
+    /**
+     * Return the referenced date
+     */
+    QDate date() const;
+
+
+    /// Search URL
+
+    /**
+     * Return the search URL as provided to fromSearchUrl().
+     * The returned URL has database information removed.
+     */
+    KURL searchUrl() const;
+
+
+    DatabaseUrl(const DatabaseUrl &url);
+
+    DatabaseUrl &operator=(const KURL &digikamalbumsUrl);
+    DatabaseUrl &operator=(const DatabaseUrl &url);
+
+    bool operator==(const KURL &digikamalbumsUrl);
+
+};
+
+}
+
+#endif
+
