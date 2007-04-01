7a77c0249954104734108b47a97ea1fcae1be350
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2007-04-01 14:31:29 +0000

    All code from the four db ioslaves listing files from the db (except for search query building) will now be concentrated in this file.
    svn path=/branches/work/digikam-dbconsolidation/; revision=648850

---
+++ libs/database/imagelister.cpp
@@ -0,0 +1,464 @@
+/* ============================================================
+ * Authors: Marcel Wiesweg
+ *          Renchi Raju
+ * Date   : 2007-03-20
+ * Description : database interface.
+ * 
+ * Copyright 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// C++ includes.
+
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <cerrno>
+
+// Qt includes.
+
+#include <qfile.h>
+#include <qfileinfo.h>
+#include <qdatastream.h>
+#include <qregexp.h>
+#include <qdir.h>
+
+// KDE includes.
+
+#include <kglobal.h>
+#include <klocale.h>
+#include <kinstance.h>
+#include <kfilemetainfo.h>
+#include <kmimetype.h>
+#include <kdebug.h>
+#include <klargefile.h>
+
+// LibKDcraw includes.
+
+#include <libkdcraw/rawfiles.h>
+
+// Local includes
+
+#include "albumdb.h"
+#include "databaseaccess.h"
+#include "dmetadata.h"
+#include "imagelister.h"
+
+namespace Digikam
+{
+
+
+QDataStream &operator<<(QDataStream &os, const ImageListerRecord &record)
+{
+    os << record.imageID;
+    os << record.albumID;
+    os << record.name;
+    os << record.albumName;
+    os << record.albumRoot;
+    os << record.dateTime;
+    os << record.size;
+    os << record.dims;
+    return os;
+}
+
+QDataStream &operator>>(QDataStream &ds, ImageListerRecord &record)
+{
+    ds >> record.imageID;
+    ds >> record.albumID;
+    ds >> record.name;
+    ds >> record.albumName;
+    ds >> record.albumRoot;
+    ds >> record.dateTime;
+    ds >> record.size;
+    ds >> record.dims;
+    return ds;
+}
+
+KIO::TransferJob *ImageLister::startListJob(const DatabaseUrl &url, const QString &filter,
+                                            int getDimension, int extraValue)
+{
+    QByteArray ba;
+    QDataStream ds(ba, IO_WriteOnly);
+    ds << url;
+    ds << filter;
+    ds << getDimension;
+    if (extraValue != -1)
+        ds << extraValue;
+
+    return new KIO::TransferJob(url, KIO::CMD_SPECIAL,
+                                ba, QByteArray(), false);
+}
+
+QValueList<QRegExp> ImageLister::makeFilterList( const QString &filter )
+{
+    QValueList<QRegExp> regExps;
+    if ( filter.isEmpty() )
+        return regExps;
+
+    QChar sep( ';' );
+    int i = filter.find( sep, 0 );
+    if ( i == -1 && filter.find( ' ', 0 ) != -1 )
+        sep = QChar( ' ' );
+
+    QStringList list = QStringList::split( sep, filter );
+    QStringList::Iterator it = list.begin();
+    while ( it != list.end() ) {
+        regExps << QRegExp( (*it).stripWhiteSpace(), false, true );
+        ++it;
+    }
+    return regExps;
+}
+
+bool ImageLister::matchFilterList(const QValueList<QRegExp>& filters,
+                                  const QString &fileName )
+{
+    QValueList<QRegExp>::ConstIterator rit = filters.begin();
+    while ( rit != filters.end() ) {
+        if ( (*rit).exactMatch(fileName) )
+            return true;
+        ++rit;
+    }
+    return false;
+}
+
+QSize ImageLister::retrieveDimension(const QString &filePath)
+{
+    QFileInfo fileInfo(filePath);
+    QString rawFilesExt(raw_file_extentions);
+    QString ext = fileInfo.extension(false).upper();
+
+    if (!ext.isEmpty() && rawFilesExt.upper().contains(ext))
+    {
+        Digikam::DMetadata metaData(filePath);
+        return metaData.getImageDimensions();
+    }
+    else
+    {
+        KFileMetaInfo metaInfo(filePath);
+        if (metaInfo.isValid())
+        {
+            if (metaInfo.containsGroup("Jpeg EXIF Data"))
+            {
+                return metaInfo.group("Jpeg EXIF Data").
+                         item("Dimensions").value().toSize();
+            }
+            else if (metaInfo.containsGroup("General"))
+            {
+                return metaInfo.group("General").
+                         item("Dimensions").value().toSize();
+            }
+            else if (metaInfo.containsGroup("Technical"))
+            {
+                return metaInfo.group("Technical").
+                         item("Dimensions").value().toSize();
+            }
+        }
+    }
+    return QSize();
+}
+
+
+void ImageLister::list(ImageListerReceiver *receiver, const DatabaseUrl &url,
+                       const QString &filter, bool getDimension)
+{
+    if (url.isAlbumUrl())
+    {
+        QString albumRoot = url.albumRoot().path();
+        QString album     = url.album();
+        listAlbum(receiver, albumRoot, album, filter, getDimension);
+    }
+    else if (url.isTagUrl())
+    {
+        listTag(receiver, url.tagId(), filter, getDimension);
+    }
+    else if (url.isDateUrl())
+    {
+        listMonth(receiver, url.date(), filter, getDimension);
+    }
+}
+
+void ImageLister::listAlbum(ImageListerReceiver *receiver,
+                            const QString &albumRoot, const QString &album,
+                            const QString &filter, bool getDimension)
+{
+    int albumid;
+
+    {
+        DatabaseAccess access;
+        albumid = access.db()->getAlbumForPath(album, false);
+
+        if (albumid == -1)
+            return;
+    }
+
+    listAlbum(receiver, albumRoot, album, albumid, filter, getDimension);
+}
+
+void ImageLister::listAlbum(ImageListerReceiver *receiver,
+                            const QString &albumRoot, const QString &album, int albumid,
+                            const QString &filter, bool getDimensions)
+{
+    QString base      = albumRoot + album;
+
+    QStringList values;
+
+    {
+        DatabaseAccess access;
+        access.db()->execSql(QString("SELECT id, name, datetime FROM Images "
+                                     "WHERE dirid = %1;")
+                                    .arg(albumid),
+                             &values);
+        /*
+        With rating:
+        SELECT Images.id, Images.name, Images.datetime, Albums.url, ImageProperties.value
+        FROM Images
+        LEFT OUTER JOIN Albums ON Images.dirid = Albums.id
+        LEFT OUTER JOIN ImageProperties ON (ImageProperties.imageid = Images.id AND ImageProperties.property="rating")
+        WHERE Images.dirid=1;
+        */
+    }
+
+    QValueList<QRegExp> regex = makeFilterList(filter);
+    struct stat stbuf;
+    for (QStringList::iterator it = values.begin(); it != values.end();)
+    {
+        ImageListerRecord record;
+        record.imageID   = (*it).toLongLong();
+        ++it;
+        record.name      = *it;
+        ++it;
+        record.dateTime  = QDateTime::fromString(*it, Qt::ISODate);
+        ++it;
+
+        record.albumID   = albumid;
+        record.albumName = album;
+        record.albumRoot = albumRoot;
+
+        if (!matchFilterList(regex, record.name))
+            continue;
+
+        QString filePath = base + '/' + record.name;
+
+        if (::stat(QFile::encodeName(filePath), &stbuf) != 0)
+            continue;
+        record.size = static_cast<size_t>(stbuf.st_size);
+
+        if (getDimensions)
+            record.dims = retrieveDimension(filePath);
+
+        receiver->receive(record);
+    }
+}
+
+void ImageLister::listTag(ImageListerReceiver *receiver,
+                          int tagId, const QString &filter, bool getDimensions)
+{
+    QString albumRoot = DatabaseAccess::albumRoot();
+
+    QStringList values;
+
+    {
+        DatabaseAccess access;
+        access.db()->execSql( QString( "SELECT DISTINCT Images.id, Images.name, Images.dirid, \n "
+                                       "       Images.datetime, Albums.url \n "
+                                       " FROM Images, Albums \n "
+                                       " WHERE Images.id IN \n "
+                                       "       (SELECT imageid FROM ImageTags \n "
+                                       "        WHERE tagid=%1 \n "
+                                       "           OR tagid IN (SELECT id FROM TagsTree WHERE pid=%2)) \n "
+                                       "   AND Albums.id=Images.dirid \n " )
+                               .arg(tagId)
+                               .arg(tagId), &values );
+    }
+
+    QValueList<QRegExp> regex = makeFilterList(filter);
+    struct stat stbuf;
+    for (QStringList::iterator it = values.begin(); it != values.end();)
+    {
+        ImageListerRecord record;
+        record.imageID   = (*it).toLongLong();
+        ++it;
+        record.name      = *it;
+        ++it;
+        record.albumID   = (*it).toInt();
+        ++it;
+        record.dateTime  = QDateTime::fromString(*it, Qt::ISODate);
+        ++it;
+        record.albumName = *it;
+        ++it;
+
+        record.albumRoot = albumRoot;
+
+        if (!matchFilterList(regex, record.name))
+            continue;
+
+        QString path = albumRoot + record.albumName + '/' + record.name;
+
+        if (::stat(QFile::encodeName(path), &stbuf) != 0)
+            continue;
+        record.size = static_cast<size_t>(stbuf.st_size);
+
+        if (getDimensions)
+            record.dims = retrieveDimension(path);
+
+        receiver->receive(record);
+    }
+}
+
+void ImageLister::listMonth(ImageListerReceiver *receiver,
+                            const QDate &date, const QString &filter, bool getDimensions)
+{
+    QString albumRoot = DatabaseAccess::albumRoot();
+
+    QString moStr1, moStr2;
+    moStr1.sprintf("%.2d", date.month());
+    moStr2.sprintf("%.2d", date.month()+1);
+
+    QStringList values;
+
+    {
+        DatabaseAccess access;
+        access.db()->execSql(QString("SELECT Images.id, Images.name, Images.dirid, \n "
+                                     "  Images.datetime, Albums.url \n "
+                                     "FROM Images, Albums \n "
+                                     "WHERE Images.datetime < '%1-%2-01' \n "
+                                     "AND Images.datetime >= '%3-%4-01' \n "
+                                     "AND Albums.id=Images.dirid \n "
+                                     "ORDER BY Albums.id;")
+                             .arg(date.year(),4)
+                             .arg(moStr2)
+                             .arg(date.year(),4)
+                             .arg(moStr1,2),
+                             &values);
+    }
+
+    QValueList<QRegExp> regex = makeFilterList(filter);
+    struct stat stbuf;
+    for (QStringList::iterator it = values.begin(); it != values.end();)
+    {
+        ImageListerRecord record;
+        record.imageID   = (*it).toLongLong();
+        ++it;
+        record.name      = *it;
+        ++it;
+        record.albumID   = (*it).toInt();
+        ++it;
+        record.dateTime  = QDateTime::fromString(*it, Qt::ISODate);
+        ++it;
+        record.albumName = *it;
+        ++it;
+
+        record.albumRoot = albumRoot;
+
+        if (!matchFilterList(regex, record.name))
+            continue;
+
+        QString path = albumRoot + record.albumName + '/' + record.name;
+
+        if (::stat(QFile::encodeName(path), &stbuf) != 0)
+            continue;
+        record.size = static_cast<size_t>(stbuf.st_size);
+
+        if (getDimensions)
+            record.dims = retrieveDimension(path);
+
+        receiver->receive(record);
+    }
+}
+
+void ImageLister::listSearch(ImageListerReceiver *receiver,
+                             const QString &sqlConditionalExpression, const QString &filter, bool getDimensions,
+                             bool getSize, int limit)
+{
+    QString albumRoot = DatabaseAccess::albumRoot();
+
+    QStringList values;
+    QString sqlQuery;
+    QString errMsg;
+
+    // query head
+    sqlQuery = "SELECT Images.id, Images.name, Images.dirid, Images.datetime, Albums.url "
+                "FROM Images, Albums LEFT JOIN ImageProperties ON Images.id = Imageproperties.imageid "
+                "WHERE ( ";
+
+    // query body
+    sqlQuery += sqlConditionalExpression;
+
+    // query tail
+    sqlQuery += " ) ";
+    sqlQuery += " AND (Albums.id=Images.dirid) ";
+
+    if (limit > 0)
+        sqlQuery += QString(" LIMIT %1; ").arg(limit);
+    else
+        sqlQuery += ";";
+
+    bool executionSuccess;
+    {
+        DatabaseAccess access;
+        executionSuccess = access.db()->execSql(sqlQuery, &values, &errMsg);
+    }
+
+    if (!executionSuccess)
+    {
+        receiver->error(errMsg);
+        return;
+    }
+
+    QValueList<QRegExp> regex = makeFilterList(filter);
+    struct stat stbuf;
+    for (QStringList::iterator it = values.begin(); it != values.end();)
+    {
+        ImageListerRecord record;
+        record.imageID   = (*it).toLongLong();
+        ++it;
+        record.name      = *it;
+        ++it;
+        record.albumID   = (*it).toInt();
+        ++it;
+        record.dateTime  = QDateTime::fromString(*it, Qt::ISODate);
+        ++it;
+        record.albumName = *it;
+        ++it;
+
+        record.albumRoot = albumRoot;
+
+        if (!matchFilterList(regex, record.name))
+            continue;
+
+        QString path = albumRoot + record.albumName + '/' + record.name;
+
+        if (getSize)
+        {
+            if (::stat(QFile::encodeName(path), &stbuf) != 0)
+                continue;
+            record.size = static_cast<size_t>(stbuf.st_size);
+        }
+        else
+        {
+            record.size = 0;
+        }
+
+        if (getDimensions)
+            record.dims = retrieveDimension(path);
+
+        receiver->receive(record);
+    }
+}
+
+}
+
+
+
---
+++ libs/database/imagelister.h
@@ -0,0 +1,122 @@
+/* ============================================================
+ * Authors: Marcel Wiesweg
+ *          Renchi Raju
+ * Date   : 2007-03-20
+ * Description : database interface.
+ *
+ * Copyright 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef IMAGELISTER_H
+#define IMAGELISTER_H
+
+#include <stddef.h>
+
+#include <qstring.h>
+#include <qdatastream.h>
+#include <qvaluelist.h>
+
+#include <kio/job.h>
+
+#include "digikam_export.h"
+#include "imageinfo.h"
+#include "imageinfolist.h"
+#include "databaseaccess.h"
+#include "databaseurl.h"
+#include "imagelisterrecord.h"
+#include "imagelisterreceiver.h"
+
+namespace Digikam
+{
+
+class DIGIKAM_EXPORT ImageLister
+{
+public:
+
+    /**
+     * Create a TransferJob for the "special" method of one of the database ioslaves,
+     * referenced by the URL.
+     * Three or four parameters will be sent to the "special" method:
+     * KURL, QString, int (, int).
+     * @param extraValue If -1, nothing is sent. If it takes another value,
+     *                   this value will be sent as a fourth parameter.
+     */
+    static KIO::TransferJob *startListJob(const DatabaseUrl &url, const QString &filter,
+                                          int getDimension, int extraValue = -1);
+
+
+    /**
+     * Convenience method for Album, Tag and Date URLs, _not_ for Search URLs.
+     */
+    void list(ImageListerReceiver *receiver,
+              const DatabaseUrl &url,
+              const QString &filter, bool getDimension);
+
+    /**
+      * List images in the Album (physical album) specified by albumRoot, album (and albumid).
+      * The results will be fed to the specified receiver.
+      * @param filter The file format filter to use
+      * @param getDimension retrieve dimension - slow!. If false, dimension will be QSize()
+      */
+    void listAlbum(ImageListerReceiver *receiver,
+                   const QString &albumRoot, const QString &album,
+                   const QString &filter, bool getDimensions);
+    void listAlbum(ImageListerReceiver *receiver,
+                   const QString &albumRoot, const QString &album, int albumid,
+                   const QString &filter, bool getDimensions);
+    /**
+     * List the images which have assigned the tag specified by tagId
+     */
+    void listTag(ImageListerReceiver *receiver,
+                 int tagId, const QString &filter, bool getDimensions);
+    /**
+      * List those images whose date lies in the month specified by the date
+      */
+    void listMonth(ImageListerReceiver *receiver,
+                   const QDate &date, const QString &filter, bool getDimensions);
+
+    /**
+     * Execute the search specified by a SQL expression
+     * @param getSize stat the file size. If false, size will be 0
+     * @param limit limit the count of the result set. If limit = 0, then no limit is set.
+     */
+    void listSearch(ImageListerReceiver *receiver,
+                    const QString &sqlConditionalExpression,
+                    const QString &filter, bool getDimensions,
+                    bool getSize = true, int limit = 0);
+
+    /**
+     * Tool method to retrieve the dimension from a file. Not a database query, slow!
+     */
+    static QSize retrieveDimension(const QString &filePath);
+    /**
+     * Tool method:
+     * Create a filter list from a list of file suffixes
+     */
+    static QValueList<QRegExp> makeFilterList(const QString &filter);
+    /**
+     * Tool method:
+     * Tests whether a filename matches a filter list created with makeFilterList
+     */
+    static bool matchFilterList(const QValueList<QRegExp>& filters,
+                                const QString &fileName );
+};
+
+}
+
+
+#endif
+
