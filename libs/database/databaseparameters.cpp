11d7cb334fc1c0ec597ac57468ac7e63e98f96f2
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2007-04-01 14:28:45 +0000

    This is a simple parameters wrapper to pass all parameters necessary to specify a database connection. Not specific to SQLite, rather inspired by the Qt 4 SQL API and thus as generaly as it gets.
    svn path=/branches/work/digikam-dbconsolidation/; revision=648848

---
+++ libs/database/databaseparameters.cpp
@@ -0,0 +1,119 @@
+/* ============================================================
+ * Authors: Marcel Wiesweg
+ * Date   : 2007-03-18
+ * Description : database interface.
+ * 
+ * Copyright 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#include <qdir.h>
+
+#include "databaseparameters.h"
+
+namespace Digikam
+{
+
+DatabaseParameters::DatabaseParameters()
+    : port(-1)
+{
+}
+
+DatabaseParameters::DatabaseParameters(const QString &type,
+                                       const QString &databaseName,
+                                       const QString &connectOptions,
+                                       const QString &hostName,
+                                       int port,
+                                       const QString &userName,
+                                       const QString &password)
+    : databaseType(type), databaseName(databaseName),
+      connectOptions(connectOptions), hostName(hostName),
+      port(port), userName(userName),
+      password(password)
+{
+}
+
+DatabaseParameters::DatabaseParameters(const KURL &url)
+    : port(-1)
+{
+    databaseType   = url.queryItem("databaseType");
+    databaseName   = url.queryItem("databaseName");
+    connectOptions = url.queryItem("connectOptions");
+    hostName       = url.queryItem("hostName");
+    QString queryPort = url.queryItem("port");
+    if (!queryPort.isNull())
+        port = queryPort.toInt();
+    userName       = url.queryItem("userName");
+    password       = url.queryItem("password");
+}
+
+bool DatabaseParameters::operator==(const DatabaseParameters &other)
+{
+    return databaseType   == other.databaseType &&
+           databaseName   == other.databaseName &&
+           connectOptions == other.connectOptions &&
+           hostName       == other.hostName &&
+           port           == other.port &&
+           userName       == other.userName &&
+           password       == other.password;
+}
+
+bool DatabaseParameters::operator!=(const DatabaseParameters &other)
+{
+    return !operator==(other);
+}
+
+DatabaseParameters DatabaseParameters::parametersForSQLite(const QString &databaseFile)
+{
+    // only the database name is needed
+    return DatabaseParameters("QSQLITE", databaseFile);
+}
+
+DatabaseParameters DatabaseParameters::parametersForSQLiteDefaultFile(const QString &directory)
+{
+    QString filePath = directory + '/' + "digikam3.db";
+    filePath = QDir::cleanDirPath(filePath);
+    return parametersForSQLite(filePath);
+}
+
+void DatabaseParameters::insertInUrl(KURL &url) const
+{
+    removeFromUrl(url);
+
+    url.addQueryItem("databaseType", databaseType);
+    url.addQueryItem("databaseName", databaseName);
+    if (!connectOptions.isNull())
+        url.addQueryItem("connectOptions", connectOptions);
+    if (!hostName.isNull())
+        url.addQueryItem("hostName", hostName);
+    if (port != -1)
+        url.addQueryItem("port", QString::number(port));
+    if (!userName.isNull())
+        url.addQueryItem("userName", userName);
+    if (!password.isNull())
+        url.addQueryItem("password", password);
+}
+
+void DatabaseParameters::removeFromUrl(KURL &url)
+{
+    url.removeQueryItem("databaseType");
+    url.removeQueryItem("databaseName");
+    url.removeQueryItem("connectOptions");
+    url.removeQueryItem("hostName");
+    url.removeQueryItem("port");
+    url.removeQueryItem("userName");
+    url.removeQueryItem("password");
+}
+
+}
---
+++ libs/database/databaseparameters.h
@@ -0,0 +1,85 @@
+/* ============================================================
+ * Authors: Marcel Wiesweg
+ * Date   : 2007-03-18
+ * Description : database interface.
+ * 
+ * Copyright 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef DATABASEPARAMETERS_H
+#define DATABASEPARAMETERS_H
+
+// Qt includes
+
+#include <qstring.h>
+
+// KDE includes
+
+#include <kurl.h>
+
+// Local includes
+
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class DIGIKAM_EXPORT DatabaseParameters
+{
+public:
+
+    /**
+      * This class encapsulates all parameters needed to establish
+      * a connection to a database (inspired by the API of QT SQL of Qt4).
+      * The values can be read from and written to a KURL.
+      */
+
+    DatabaseParameters(const QString &type,
+                       const QString &databaseName,
+                       const QString &connectOptions = QString(),
+                       const QString &hostName = QString(),
+                       int port = -1,
+                       const QString &userName = QString(),
+                       const QString &password = QString());
+
+    DatabaseParameters(const KURL &url);
+    DatabaseParameters();
+
+    QString databaseType;
+    QString databaseName;
+    QString connectOptions;
+    QString hostName;
+    int port;
+    QString userName;
+    QString password;
+
+    void insertInUrl(KURL &url) const;
+    bool operator==(const DatabaseParameters &other);
+    bool operator!=(const DatabaseParameters &other);
+
+    /**
+     * Convenience method to create a DatabaseParameters object for an
+     * SQLITE 3 database specified by the local file path.
+     */
+    static DatabaseParameters parametersForSQLite(const QString &databaseFile);
+    static DatabaseParameters parametersForSQLiteDefaultFile(const QString &directory);
+
+    static void removeFromUrl(KURL &url);
+};
+
+}
+
+#endif
+
