186a69671b8429f3afb1ba31f95771c6cf14d35a
Gerhard Kulzer <gerhard@kulzer.net>
2007-04-14 16:47:32 +0000

    typo Childs -> Children BUG:144214
    svn path=/trunk/extragear/graphics/digikam/; revision=653934

--- digikam/tagfilterview.cpp
+++ digikam/tagfilterview.cpp
@@ -782,7 +782,7 @@ void TagFilterView::slotContextMenu(QListViewItem* it, const QPoint&, int)
     if (item)
     {
         selectTagsMenu.insertSeparator(-1);
-        selectTagsMenu.insertItem(i18n("Childs"),     17);
+        selectTagsMenu.insertItem(i18n("Children"),     17);
         selectTagsMenu.insertItem(i18n("Parents"),    19);
     }
     popmenu.insertItem(i18n("Select"), &selectTagsMenu);
@@ -792,7 +792,7 @@ void TagFilterView::slotContextMenu(QListViewItem* it, const QPoint&, int)
     if (item)
     {
         deselectTagsMenu.insertSeparator(-1);
-        deselectTagsMenu.insertItem(i18n("Childs"),   18);
+        deselectTagsMenu.insertItem(i18n("Children"),   18);
         deselectTagsMenu.insertItem(i18n("Parents"),  20);
     }
     popmenu.insertItem(i18n("Deselect"), &deselectTagsMenu);
@@ -804,7 +804,7 @@ void TagFilterView::slotContextMenu(QListViewItem* it, const QPoint&, int)
     toggleAutoMenu.setCheckable(true);
     toggleAutoMenu.insertItem(i18n("None"),    21);
     toggleAutoMenu.insertSeparator(-1);
-    toggleAutoMenu.insertItem(i18n("Childs"),  22);
+    toggleAutoMenu.insertItem(i18n("Children"),  22);
     toggleAutoMenu.insertItem(i18n("Parents"), 23);
     toggleAutoMenu.insertItem(i18n("Both"),    24);
     toggleAutoMenu.setItemChecked(21 + d->toggleAutoTags, true);
@@ -938,7 +938,7 @@ void TagFilterView::slotContextMenu(QListViewItem* it, const QPoint&, int)
             d->toggleAutoTags = NoToggleAuto;
             break;
         }
-        case 22:   // Toggle auto Childs tags.
+        case 22:   // Toggle auto Children tags.
         {
             d->toggleAutoTags = Children;
             break;
@@ -948,7 +948,7 @@ void TagFilterView::slotContextMenu(QListViewItem* it, const QPoint&, int)
             d->toggleAutoTags = Parents;
             break;
         }
-        case 24:   // Toggle auto Childs and Parents tags.
+        case 24:   // Toggle auto Children and Parents tags.
         {
             d->toggleAutoTags = ChildrenAndParents;
             break;
