e069ec8daae2427a2769a84ff703f5c1ca909b48
Gilles Caulier <caulier.gilles@gmail.com>
2007-04-15 07:59:05 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=654127

--- libs/widgets/common/statusprogressbar.h
+++ libs/widgets/common/statusprogressbar.h
@@ -1,6 +1,6 @@
 /* ============================================================
  * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date   : 2006-01-24
+ * Date   : 2007-01-24
  * Description : a progress bar used to display file access
  *               progress or a text in status bar.
  *
@@ -72,4 +72,4 @@ private:
 
 }  // namespace Digikam
 
-#endif /* IOFILEPROGRESSBAR_H */
+#endif /* STATUSPROGRESSBAR_H */
