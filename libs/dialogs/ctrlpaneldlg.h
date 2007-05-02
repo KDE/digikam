97b28748031630afecc1310e239ba31a4bc41596
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-02 18:48:45 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=660445

--- libs/dialogs/ctrlpaneldlg.h
+++ libs/dialogs/ctrlpaneldlg.h
@@ -1,10 +1,10 @@
 /* ============================================================
- * Author: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date  : 2005-05-07
+ * Authors     : Gilles Caulier 
+ * Date        : 2005-05-07
  * Description : A threaded filter control panel dialog for 
  *               image editor plugins using DImg
  * 
- * Copyright 2005-2007 by Gilles Caulier
+ * Copyright 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
--- libs/dialogs/imagedlgbase.h
+++ libs/dialogs/imagedlgbase.h
@@ -1,12 +1,12 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date   : 2005-07-23
+ * Authors     : Gilles Caulier
+ * Date        : 2005-07-23
  * Description : simple plugins dialog without threadable 
  *               filter interface. The dialog layout is 
  *               designed to accept custom widgets in 
  *               preview and settings area.
  * 
- * Copyright 2005-2007 by Gilles Caulier
+ * Copyright 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
