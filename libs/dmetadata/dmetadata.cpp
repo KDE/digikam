c62fee80be5320ae7698efdfb5f875ec43e0c442
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-02 19:29:07 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=660455

--- libs/dmetadata/dmetadata.cpp
+++ libs/dmetadata/dmetadata.cpp
@@ -1,10 +1,11 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- *          Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
- * Date  : 2006-02-23
+ * Authors     : Gilles Caulier 
+ *               Marcel Wiesweg 
+ * Date        : 2006-02-23
  * Description : image metadata interface
  *
- * Copyright 2006-2007 by Gilles Caulier and Marcel Wiesweg
+ * Copyright 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright 2006-2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
