f1a2513d567e99d4824fa070242c687a8664aaa0
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-03 07:22:43 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=660608

--- showfoto/setup/setup.h
+++ showfoto/setup/setup.h
@@ -1,9 +1,9 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date   : 2005-04-02
+ * Authors     : Gilles Caulier
+ * Date        : 2005-04-02
  * Description : showfoto setup dialog.
  * 
- * Copyright 2005-2007 by Gilles Caulier
+ * Copyright 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
