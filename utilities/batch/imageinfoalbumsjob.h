917a38ba0222c69dd5db6aa17e7c4871cbd59f51
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-03 10:19:52 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=660649

--- utilities/batch/imageinfoalbumsjob.h
+++ utilities/batch/imageinfoalbumsjob.h
@@ -1,9 +1,9 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date   : 2007-14-02
+ * Authors     : Gilles Caulier
+ * Date        : 2007-14-02
  * Description : interface to get image info from an albums list.
  *
- * Copyright 2007 by Gilles Caulier
+ * Copyright 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
