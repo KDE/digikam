bc7749cbe286d8613257434bde3e6af7f727886e
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-04 17:51:35 +0000

    add link to freedesktop.org
    svn path=/trunk/extragear/graphics/digikam/; revision=661151

--- kioslave/digikamthumbnail.cpp
+++ kioslave/digikamthumbnail.cpp
@@ -4,11 +4,14 @@
  * http://www.digikam.org
  * 
  * Date        : 2003-01-15
- * Description : digiKam KIO slave to get image thumbnails.
+ * Description : digiKam KIO slave to get image thumbnails. 
+ *               This kio-slave support this freedesktop 
+ *               specification about thumbnails mamagement:
+ *               http://jens.triq.net/thumbnail-spec
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
  * Copyright (C) 2003-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
- *
+ * 
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
