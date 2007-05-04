cf09dff8823e663aa71569c1d8519bd94e627ac1
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-04 18:19:47 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=661158

--- imageplugins/adjustcurves/imageplugin_adjustcurves.h
+++ imageplugins/adjustcurves/imageplugin_adjustcurves.h
@@ -3,10 +3,10 @@
  * This file is a part of digiKam project
  * http://www.digikam.org
  *
- * Date   : 2004-12-01
+ * Date        : 2004-12-01
  * Description : image histogram adjust curves. 
  * 
- * Copyright 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
