c3a9fa8b10e080b2d0a7b258b741c9b32ec6148c
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-04 13:24:27 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=661050

--- utilities/cameragui/downloadsettingscontainer.h
+++ utilities/cameragui/downloadsettingscontainer.h
@@ -1,9 +1,12 @@
 /* ============================================================
- * Authors     : Gilles Caulier 
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
  * Date        : 2006-21-07
  * Description : Camera item download settings container.
  * 
- * Copyright 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com> 
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com> 
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
--- utilities/cameragui/renamecustomizer.cpp
+++ utilities/cameragui/renamecustomizer.cpp
@@ -1,12 +1,14 @@
 /* ============================================================
- * Authors     : Renchi Raju 
- *               Caulier Gilles 
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
  * Date        : 2004-09-19
  * Description : a options group to set renaming files
  *               operations during camera downloading
  *
- * Copyright 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
