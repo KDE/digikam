70a4d28cbf225aa61905b2014f02bdec7e81d9f0
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-04 13:02:27 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=661039

--- showfoto/main.cpp
+++ showfoto/main.cpp
@@ -1,12 +1,14 @@
 /* ============================================================
- * Authors     : Renchi Raju 
- *               Gilles Caulier 
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
  * Date        : 2004-11-22
  * Description : showfoto is a stand alone version of image 
  *               editor with no support of digiKam database.
  *
- * Copyright 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
--- showfoto/setup/setup.cpp
+++ showfoto/setup/setup.cpp
@@ -1,9 +1,12 @@
 /* ============================================================
- * Authors     : Gilles Caulier
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
  * Date        : 2005-04-02
  * Description : showfoto setup dialog.
  *
- * Copyright 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
