964762b9ca9fd73275a979568e23fae02076ee51
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-09 13:31:22 +0000

    Use Digikam namespace. Fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=662882

--- digikam/constants.h
+++ digikam/constants.h
@@ -1,9 +1,12 @@
 /* ============================================================
- * Authors     : Brian Remedios
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
  * Date        : 2007-04-03
  * Description : Constants for Digikam-specific fields
- *
- * Copyright 2007 by Brian Remedios
+ * 
+ * Copyright (C) 2007 by Brian Remedios <brianremedios at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -15,9 +18,15 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
  * ============================================================ */
+
+namespace Digikam
+{
 
 // Field value limits for all Digikam-specific fields (not EXIF/IPTC fields)
 
 static const int RatingMin = 0;
 static const int RatingMax = 5;
+
+}  // namespace Digikam
