dcaff8a96f050185f788e28831f0292db22eebaa
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-18 08:48:25 +0000

    digikam from trunk : Light Table view implementation.
    - Regorganize signal slots connection to be more readable.
    - When we synchronize Left and Right panels, only disconect the signals/slots between preview widgets, not all using blocksignals, else all internals panel signals will not be dispatched.
    
    svn path=/trunk/extragear/graphics/digikam/; revision=665857

--- utilities/lighttable/lighttableview.cpp
+++ utilities/lighttable/lighttableview.cpp
@@ -84,52 +84,54 @@ LightTableView::LightTableView(QWidget *parent)
     d->grid->setColStretch(1, 10),
     d->grid->setRowStretch(0, 10),
 
-    // -------------------------------------------------------------------
+    // Left panel connections ------------------------------------------------
 
     connect(d->leftPreview, SIGNAL(signalZoomFactorChanged(double)),
             this, SIGNAL(signalLeftZoomFactorChanged(double)));
+
+    connect(d->leftPreview, SIGNAL(contentsMoving(int, int)),
+            this, SLOT(slotLeftContentsMoved(int, int)));
+
+    connect(d->leftPreview, SIGNAL(signalSlideShow()),
+            this, SIGNAL(signalSlideShow()));
+
+    connect(d->leftPreview, SIGNAL(signalDeleteItem(ImageInfo*)),
+            this, SIGNAL(signalDeleteItem(ImageInfo*)));
+
+    connect(d->leftPreview, SIGNAL(signalEditItem(ImageInfo*)),
+            this, SIGNAL(signalEditItem(ImageInfo*)));
+
+    connect(d->leftPreview, SIGNAL(signalDroppedItems(const ImageInfoList&)),
+            this, SIGNAL(signalLeftDroppedItems(const ImageInfoList&)));
+
+    connect(d->leftPreview, SIGNAL(signalPreviewLoaded(bool)),
+            this, SLOT(slotLeftPreviewLoaded(bool)));
+
+    connect(d->leftPreview, SIGNAL(signalLeftButtonClicked()),
+            this, SIGNAL(signalLeftPanelLeftButtonClicked()));
+
+    // Right panel connections ------------------------------------------------
     
     connect(d->rightPreview, SIGNAL(signalZoomFactorChanged(double)),
             this, SIGNAL(signalRightZoomFactorChanged(double)));
 
-    connect(d->leftPreview, SIGNAL(signalSlideShow()),
-            this, SIGNAL(signalSlideShow()));
+    connect(d->rightPreview, SIGNAL(contentsMoving(int, int)),
+            this, SLOT(slotRightContentsMoved(int, int)));
     
     connect(d->rightPreview, SIGNAL(signalDeleteItem(ImageInfo*)),
-            this, SIGNAL(signalDeleteItem(ImageInfo*)));
-
-    connect(d->leftPreview, SIGNAL(signalDeleteItem(ImageInfo*)),
             this, SIGNAL(signalDeleteItem(ImageInfo*)));
     
     connect(d->rightPreview, SIGNAL(signalEditItem(ImageInfo*)),
             this, SIGNAL(signalEditItem(ImageInfo*)));
 
-    connect(d->leftPreview, SIGNAL(signalEditItem(ImageInfo*)),
-            this, SIGNAL(signalEditItem(ImageInfo*)));
-
     connect(d->rightPreview, SIGNAL(signalDroppedItems(const ImageInfoList&)),
             this, SIGNAL(signalRightDroppedItems(const ImageInfoList&)));
 
-    connect(d->leftPreview, SIGNAL(signalDroppedItems(const ImageInfoList&)),
-            this, SIGNAL(signalLeftDroppedItems(const ImageInfoList&)));
-    
     connect(d->rightPreview, SIGNAL(signalSlideShow()),
             this, SIGNAL(signalSlideShow()));
 
-    connect(d->leftPreview, SIGNAL(contentsMoving(int, int)),
-            this, SLOT(slotLeftContentsMoved(int, int)));
-
-    connect(d->rightPreview, SIGNAL(contentsMoving(int, int)),
-            this, SLOT(slotRightContentsMoved(int, int)));
-
-    connect(d->leftPreview, SIGNAL(signalPreviewLoaded(bool)),
-            this, SLOT(slotLeftPreviewLoaded(bool)));
-
     connect(d->rightPreview, SIGNAL(signalPreviewLoaded(bool)),
             this, SLOT(slotRightPreviewLoaded(bool)));
-
-    connect(d->leftPreview, SIGNAL(signalLeftButtonClicked()),
-            this, SIGNAL(signalLeftPanelLeftButtonClicked()));
 
     connect(d->rightPreview, SIGNAL(signalLeftButtonClicked()),
             this, SIGNAL(signalRightPanelLeftButtonClicked()));
@@ -285,11 +287,21 @@ void LightTableView::slotLeftContentsMoved(int x, int y)
 {
     if (d->syncPreview && !d->leftLoading)
     {
-        d->rightPreview->blockSignals(true);
+        disconnect(d->rightPreview, SIGNAL(signalZoomFactorChanged(double)),
+                   this, SIGNAL(signalRightZoomFactorChanged(double)));
+    
+        disconnect(d->rightPreview, SIGNAL(contentsMoving(int, int)),
+                   this, SLOT(slotRightContentsMoved(int, int)));
+
         setRightZoomFactor(d->leftPreview->zoomFactor());
         emit signalRightZoomFactorChanged(d->leftPreview->zoomFactor());
         d->rightPreview->setContentsPos(x, y);
-        d->rightPreview->blockSignals(false);
+
+        connect(d->rightPreview, SIGNAL(signalZoomFactorChanged(double)),
+                this, SIGNAL(signalRightZoomFactorChanged(double)));
+    
+        connect(d->rightPreview, SIGNAL(contentsMoving(int, int)),
+                this, SLOT(slotRightContentsMoved(int, int)));
     }
 }
 
@@ -297,11 +309,22 @@ void LightTableView::slotRightContentsMoved(int x, int y)
 {
     if (d->syncPreview && !d->rightLoading)
     {
-        d->leftPreview->blockSignals(true);
+        disconnect(d->leftPreview, SIGNAL(signalZoomFactorChanged(double)),
+                   this, SIGNAL(signalLeftZoomFactorChanged(double)));
+    
+        disconnect(d->leftPreview, SIGNAL(contentsMoving(int, int)),
+                   this, SLOT(slotLeftContentsMoved(int, int)));
+
+
         setLeftZoomFactor(d->rightPreview->zoomFactor());
         emit signalLeftZoomFactorChanged(d->rightPreview->zoomFactor());
         d->leftPreview->setContentsPos(x, y);
-        d->leftPreview->blockSignals(false);
+
+        connect(d->leftPreview, SIGNAL(signalZoomFactorChanged(double)),
+                this, SIGNAL(signalLeftZoomFactorChanged(double)));
+    
+        connect(d->leftPreview, SIGNAL(contentsMoving(int, int)),
+                this, SLOT(slotLeftContentsMoved(int, int)));
     }
 }
 
