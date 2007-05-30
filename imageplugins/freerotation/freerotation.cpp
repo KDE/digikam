e78db65aa64ccab072b41b4ef5bd55eec3452d71
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-30 12:19:10 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=669785

--- imageplugins/freerotation/freerotation.cpp
+++ imageplugins/freerotation/freerotation.cpp
@@ -1,13 +1,13 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date   : 2005-07-18
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2005-07-18
  * Description : Free rotation threaded image filter.
  * 
- * Copyright 2005-2007 by Gilles Caulier
- *
- * Original FreeRotation algorithms copyrighted 2004 by 
- * Pieter Z. Voloshyn <pieter dot voloshyn at gmail dot com>.
- *
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * 
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
@@ -20,7 +20,7 @@
  * GNU General Public License for more details.
  * 
  * ============================================================ */
- 
+
 // Degrees to radian convertion coeff (PI/180). To optimize computation.
 #define DEG2RAD 0.017453292519943
 
--- imageplugins/freerotation/imageeffect_freerotation.cpp
+++ imageplugins/freerotation/imageeffect_freerotation.cpp
@@ -1,22 +1,25 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date   : 2004-11-28
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2004-11-28
  * Description : a digiKam image editor plugin to process image
  *               free rotation.
- *
- * Copyright 2004-2007 by Gilles Caulier 
- *
+ * 
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * 
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * ============================================================ */
 
 // Qt includes.
