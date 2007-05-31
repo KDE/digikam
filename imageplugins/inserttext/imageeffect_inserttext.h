39d9d2e72e7fd1b7e1c3e36062d6206227b9d81f
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-31 08:41:11 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=670075

--- imageplugins/inserttext/imageeffect_inserttext.h
+++ imageplugins/inserttext/imageeffect_inserttext.h
@@ -1,11 +1,13 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- * Date   : 2005-02-14
- * Description : a digiKam image plugin for insert text  
- *               to an image.
- * 
- * Copyright 2005-2007 by Gilles Caulier
  *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2005-02-14
+ * Description : a plugin to insert a text over an image.
+ * 
+ * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * 
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
