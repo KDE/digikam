528652cc04b4824f867803b6119332d71692b18a
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-31 08:55:33 +0000

    missing (c)
    svn path=/trunk/extragear/graphics/digikam/; revision=670081

--- imageplugins/oilpaint/oilpaint.cpp
+++ imageplugins/oilpaint/oilpaint.cpp
@@ -9,6 +9,9 @@
  * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2006-2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  * 
+ * Original OilPaint algorithm copyrighted 2004 by 
+ * Pieter Z. Voloshyn <pieter dot voloshyn at gmail dot com>.
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
