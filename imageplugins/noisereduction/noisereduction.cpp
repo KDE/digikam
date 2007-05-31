a81d9afeba65de347bf2b1873c7a960eb9df78f2
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-31 08:49:28 +0000

    fix header
    svn path=/trunk/extragear/graphics/digikam/; revision=670077

--- imageplugins/noisereduction/noisereduction.cpp
+++ imageplugins/noisereduction/noisereduction.cpp
@@ -1,10 +1,12 @@
 /* ============================================================
- * Authors: Gilles Caulier <caulier dot gilles at gmail dot com>
- *          Peter Heckert <peter dot heckert at arcor dot de>
- * Date   : 2005-05-25
- * Description : Noise reduction threaded image filter.
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2005-05-25
+ * Description : Noise Reduction threaded image filter.
  * 
- * Copyright 2005-2007 by Gilles Caulier 
+ * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * 
  * Original Noise Filter algorithm copyright (C) 2005 
  * Peter Heckert <peter dot heckert at arcor dot de>
