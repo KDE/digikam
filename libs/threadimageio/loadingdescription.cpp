afbec27be17f229b4a28022aba4b70b772338b53
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2007-06-10 21:01:37 +0000

    Merge to 673684
    svn path=/branches/work/digikam-dbconsolidation/; revision=673689

--- libs/threadimageio/loadingdescription.cpp
+++ libs/threadimageio/loadingdescription.cpp
@@ -28,31 +28,70 @@
 namespace Digikam
 {
 
+bool LoadingDescription::PreviewParameters::operator==(const PreviewParameters &other) const
+{
+    return isPreview  == other.isPreview
+            && size       == other.size
+            && exifRotate == other.exifRotate;
+}
+
+LoadingDescription::LoadingDescription(const QString &filePath)
+    : filePath(filePath)
+{
+    rawDecodingSettings = KDcrawIface::RawDecodingSettings();
+}
+
+LoadingDescription::LoadingDescription(const QString &filePath, KDcrawIface::RawDecodingSettings settings)
+    : filePath(filePath), rawDecodingSettings(settings)
+{
+}
+
+LoadingDescription::LoadingDescription(const QString &filePath, int size, bool exifRotate)
+    : filePath(filePath)
+{
+    rawDecodingSettings = KDcrawIface::RawDecodingSettings();
+    previewParameters.isPreview  = false;
+    previewParameters.size       = size;
+    previewParameters.exifRotate = exifRotate;
+}
+
 QString LoadingDescription::cacheKey() const
 {
     // Here we have the knowledge which LoadingDescriptions / RawFileDecodingSettings
     // must be cached separately.
-    // When the Raw loading settings are changed in setup, the cache is cleaned,
-    // so we do not need to store check for every option here.
+    // Current assumption:
+    // Eight-bit images are needed for LightTable, and if 16-bit is enabled,
+    // 16-bit half size images for the histogram sidebar,
+    // and 16-bit full size images for the image editor.
+    // Use previewParameters.size, not isPreview - if it is 0, full loading is used.
+
+    QString suffix = rawDecodingSettings.sixteenBitsImage ? "-16" : "-8";
+
     if (rawDecodingSettings.halfSizeColorImage)
-        return filePath + "-halfSizeColorImage";
+        return filePath + suffix + "-halfSizeColorImage";
     else if (previewParameters.size)
-        return filePath + "-previewImage";
+        return filePath + suffix + "-previewImage";
     else
-        return filePath;
+        return filePath + suffix;
 }
 
 QStringList LoadingDescription::lookupCacheKeys() const
 {
     // Build a hierarchy which cache entries may be used for this LoadingDescription.
-    // Typically, the first is the best, but an actual loading operation will use a faster
-    // way and will effectively add the last entry of the list to the cache
+    // Typically, the first is the best, but an actual loading operation may use a
+    // lower-quality loading and will effectively only add the last entry of the
+    // list to the cache, although it can accept the first if already available.
+    // Sixteen-bit images cannot be used used instead of eight-bit ones because
+    // color management is needed to display them.
+
+    QString suffix = rawDecodingSettings.sixteenBitsImage ? "-16" : "-8";
+
     QStringList keys;
-    keys.append(filePath);
+    keys.append(filePath + suffix);
     if (rawDecodingSettings.halfSizeColorImage)
-        keys.append(filePath + "-halfSizeColorImage");
+        keys.append(filePath + suffix + "-halfSizeColorImage");
     if (previewParameters.size)
-        keys.append(filePath + "-previewImage");
+        keys.append(filePath + suffix + "-previewImage");
     return keys;
 }
 
@@ -65,11 +104,9 @@ bool LoadingDescription::isReducedVersion() const
 
 bool LoadingDescription::operator==(const LoadingDescription &other) const
 {
-    // NOTE: If we start loading RAW files with different loading settings in parallel,
-    //       this and the next methods must be better implemented!
     return filePath == other.filePath &&
-            rawDecodingSettings.halfSizeColorImage == other.rawDecodingSettings.halfSizeColorImage &&
-            previewParameters.size == other.previewParameters.size;
+            rawDecodingSettings == other.rawDecodingSettings &&
+            previewParameters == other.previewParameters;
 }
 
 bool LoadingDescription::equalsIgnoreReducedVersion(const LoadingDescription &other) const
@@ -79,10 +116,18 @@ bool LoadingDescription::equalsIgnoreReducedVersion(const LoadingDescription &ot
 
 bool LoadingDescription::equalsOrBetterThan(const LoadingDescription &other) const
 {
+    // This method is similar to operator==. But it returns true as well if other
+    // Loads a "better" version than this.
+    // Preview parameters must have the same size, or other has no size restriction.
+    // All raw decoding settings must be equal, only the half size parameter is allowed to vary.
+
+    KDcrawIface::RawDecodingSettings fullSize = other.rawDecodingSettings;
+    fullSize.halfSizeColorImage = false;
+
     return filePath == other.filePath &&
             (
-             (rawDecodingSettings.halfSizeColorImage == other.rawDecodingSettings.halfSizeColorImage) ||
-             other.rawDecodingSettings.halfSizeColorImage
+             rawDecodingSettings == other.rawDecodingSettings ||
+             rawDecodingSettings == fullSize
             ) &&
             (
              (previewParameters.size == other.previewParameters.size) ||
@@ -93,9 +138,12 @@ bool LoadingDescription::equalsOrBetterThan(const LoadingDescription &other) con
 QStringList LoadingDescription::possibleCacheKeys(const QString &filePath)
 {
     QStringList keys;
-    keys.append(filePath);
-    keys.append(filePath + "-halfSizeColorImage");
-    keys.append(filePath + "-previewImage");
+    keys.append(filePath + "-16");
+    keys.append(filePath + "-16-halfSizeColorImage");
+    keys.append(filePath + "-16-previewImage");
+    keys.append(filePath + "-8");
+    keys.append(filePath + "-8-halfSizeColorImage");
+    keys.append(filePath + "-8-previewImage");
     return keys;
 }
 
