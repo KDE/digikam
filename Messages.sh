b97d1dc226cb7a387d7709885494a48033d3b910
Laurent Montel <montel@kde.org>
2007-06-16 10:04:15 +0000

    Port++ Clean up
    svn path=/trunk/extragear/graphics/digikam/; revision=676213

---
+++ Messages.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+$PREPARETIPS > tips.cpp
+$EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
+$XGETTEXT `find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C` -o $podir/digikam.pot
+rm -f tips.cpp
