36b9469b004540e3e17da80dc0e814739528460e
Gilles Caulier <caulier.gilles@gmail.com>
2007-07-03 14:41:07 +0000

    restore colorgradientwidget as QWidget.
    svn path=/trunk/extragear/graphics/digikam/; revision=682844

--- libs/widgets/common/colorgradientwidget.cpp
+++ libs/widgets/common/colorgradientwidget.cpp
@@ -22,11 +22,10 @@
  * ============================================================ */
 
 // Qt includes. 
-
+ 
 #include <QImage>
 #include <QPainter>
-#include <QPixmap>
-
+ 
 // KDE includes.
 
 #include <kimageeffect.h>
@@ -41,41 +40,40 @@ namespace Digikam
 
 class ColorGradientWidgetPriv
 {
-
+    
 public:
 
     ColorGradientWidgetPriv(){}
 
     int    orientation;
-
+    
     QColor color1;
     QColor color2;
 };
 
 ColorGradientWidget::ColorGradientWidget(int o, int size, QWidget *parent)
-                   : Q3Frame(parent)
+                   : QWidget(parent)
 {
     d = new ColorGradientWidgetPriv;
     d->orientation = o;
-
-    setFrameStyle(Q3Frame::Box|Q3Frame::Plain);
-    setLineWidth(1);
-    setAttribute(Qt::WA_DeleteOnClose);
-
-    if ( d->orientation == Qt::Horizontal )
-        setFixedHeight( size );
-    else
-        setFixedWidth( size );
-
     d->color1.setRgb( 0, 0, 0 );
     d->color2.setRgb( 255, 255, 255 );
-}
+
+    setAttribute(Qt::WA_DeleteOnClose);
+    
+    if ( d->orientation == Horizontal )
+        setFixedHeight( size );
+    else
+        setFixedWidth( size );    
+
+    setContentsMargins(1, 1, 1, 1);
+}      
 
 ColorGradientWidget::~ColorGradientWidget()
 {
     delete d;
 }
-
+    
 void ColorGradientWidget::setColors( const QColor &col1, const QColor &col2 )
 {
     d->color1 = col1;
@@ -83,30 +81,32 @@ void ColorGradientWidget::setColors( const QColor &col1, const QColor &col2 )
     update();
 }
 
-void ColorGradientWidget::drawContents(QPainter *p)
+void ColorGradientWidget::paintEvent( QPaintEvent * )
 {
     QImage image(contentsRect().width(), contentsRect().height(), QImage::Format_ARGB32);
 
-    QColor col, color1, color2;
+    QColor col, color1, color2, colorf;
     float scale;
-
+    
     // Widget is disable : drawing grayed frame.
     if ( !isEnabled() )
     {
-       color1 = palette().color(QPalette::Disabled, QPalette::Foreground);
-       color2 = palette().color(QPalette::Disabled, QPalette::Background);
+        color1 = palette().color(QPalette::Disabled, QPalette::Foreground);
+        color2 = palette().color(QPalette::Disabled, QPalette::Background);
+        colorf = palette().color(QPalette::Disabled, QPalette::Foreground);
     }
     else 
     {
-       color1 = d->color1;
-       color2 = d->color2;
+        color1 = d->color1;
+        color2 = d->color2;
+        colorf = palette().color(QPalette::Active, QPalette::Foreground);
     }
 
     int redDiff   = color2.red()   - color1.red();
     int greenDiff = color2.green() - color1.green();
     int blueDiff  = color2.blue()  - color1.blue();
 
-    if ( d->orientation == Qt::Vertical )
+    if ( d->orientation == Vertical )
     {
         for ( int y = 0; y < image.height(); y++ )
         {
@@ -116,7 +116,7 @@ void ColorGradientWidget::drawContents(QPainter *p)
                         color1.blue()  + int(blueDiff  * scale) );
 
             unsigned int *p = (uint *) image.scanLine( y );
-
+            
             for ( int x = 0; x < image.width(); x++ )
                 *p++ = col.rgb();
         }
@@ -151,10 +151,14 @@ void ColorGradientWidget::drawContents(QPainter *p)
                                  color1.blue()  + blueDiff  * s / psize );
     }
 
-    KImageEffect::dither(image, ditherPalette, psize);
+    KImageEffect::dither( image, ditherPalette, psize );
 
     QPixmap pm = QPixmap::fromImage(image);
-    p->drawPixmap(contentsRect(), pm);
+    QPainter p(this);
+    p.drawPixmap(contentsRect(), pm);
+    p.setPen(colorf);
+    p.drawRect(rect());
+    p.end();
 }
 
 }  // namespace Digikam
--- libs/widgets/common/colorgradientwidget.h
+++ libs/widgets/common/colorgradientwidget.h
@@ -2,11 +2,11 @@
  *
  * This file is a part of digiKam project
  * http://www.digikam.org
- *
+ * 
  * Date        : 2004-07-28
  * Description : a color gradient widget
  * 
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2007 by Gilles Caulier<caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -21,12 +21,13 @@
  * 
  * ============================================================ */
 
+
 #ifndef COLORGRADIENTWIDGET_H
 #define COLORGRADIENTWIDGET_H
 
 // KDE includes.
 
-#include <Q3Frame>
+#include <QWidget>
 #include <QColor>
 
 // Local includes
@@ -38,34 +39,34 @@ namespace Digikam
 
 class ColorGradientWidgetPriv;
 
-class DIGIKAM_EXPORT ColorGradientWidget : public Q3Frame
+class DIGIKAM_EXPORT ColorGradientWidget : public QWidget
 {
 Q_OBJECT
 
 public:
-
+    
     enum Orientation
     {
-        Horizontal=0,
-        Vertical
+        Horizontal=0,      
+        Vertical         
     };
-
+      
 public:
-
+    
     ColorGradientWidget( int o, int size, QWidget *parent=0 );
     ~ColorGradientWidget();
-
+  
     void setColors( const QColor &col1, const QColor &col2 );
 
 protected:
 
-    void drawContents(QPainter *);
-
+    void paintEvent( QPaintEvent * );
+    
 private:
 
     ColorGradientWidgetPriv* d;
 };
 
-}   // namespace Digikam
+} // namespace Digikam
 
 #endif /* COLORGRADIENTWIDGET_H */
