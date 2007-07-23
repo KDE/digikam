c0ec7745c47a88781e8ed596f7b9ce890d0af54b
Gilles Caulier <caulier.gilles@gmail.com>
2007-07-23 11:40:03 +0000

    digiKam from trunk (KDE4) : rating widget use now a pre-computed star based on polygon drawed by QPainter
    svn path=/trunk/extragear/graphics/digikam/; revision=691298

--- digikam/ratingwidget.cpp
+++ digikam/ratingwidget.cpp
@@ -22,15 +22,16 @@
  *
  * ============================================================ */
 
+// C++ includes.
+
+#include <cmath>
+
 // Qt includes.
 
 #include <QPainter>
 #include <QPalette>
 #include <QPixmap>
-
-// KDE includes.
-
-#include <kstandarddirs.h>
+#include <QPolygon>
 
 // Local includes.
 
@@ -48,22 +49,34 @@ public:
 
     RatingWidgetPriv()
     {
-        rating    = 0;
+        rating = 0;
+
+        // Pre-computed star polygon for a 15x15 pixmap.
+        starPolygon << QPoint(0,  6);
+        starPolygon << QPoint(5,  5);
+        starPolygon << QPoint(7,  0);
+        starPolygon << QPoint(9,  5);
+        starPolygon << QPoint(14, 6);
+        starPolygon << QPoint(10, 9);
+        starPolygon << QPoint(11, 14);
+        starPolygon << QPoint(7,  11);
+        starPolygon << QPoint(3,  14);
+        starPolygon << QPoint(4,  9);
     }
 
-    int     rating;
+    int      rating;
     
-    QString ratingPixPath;
+    QPolygon starPolygon;
 
-    QPixmap selPixmap;
-    QPixmap regPixmap;
+    QPixmap  selPixmap;      // Selected star.
+    QPixmap  regPixmap;      // Regular star.
+    QPixmap  disPixmap;      // Disable star.
 };
 
 RatingWidget::RatingWidget(QWidget* parent)
             : QWidget(parent)
 {
     d = new RatingWidgetPriv;
-    d->ratingPixPath = KStandardDirs::locate("data", "digikam/data/rating.png");
 
     slotThemeChanged();
 
@@ -122,17 +135,30 @@ void RatingWidget::paintEvent(QPaintEvent*)
 {
     QPainter p(this);
 
-    int x = 0;
-    for (int i=0; i<d->rating; i++)
+    // Widget is disable : drawing grayed frame.
+    if (!isEnabled())
     {
-        p.drawPixmap(x, 0, d->selPixmap);
-        x += d->selPixmap.width();
+        int x = 0;
+        for (int i = 0; i < RatingMax; i++)
+        {
+            p.drawPixmap(x, 0, d->disPixmap);
+            x += d->disPixmap.width();
+        }
     }
-
-    for (int i=d->rating; i<RatingMax; i++)
+    else
     {
-        p.drawPixmap(x, 0, d->regPixmap);
-        x += d->regPixmap.width();
+        int x = 0;
+        for (int i = 0; i < d->rating; i++)
+        {
+            p.drawPixmap(x, 0, d->selPixmap);
+            x += d->selPixmap.width();
+        }
+    
+        for (int i = d->rating; i < RatingMax; i++)
+        {
+            p.drawPixmap(x, 0, d->regPixmap);
+            x += d->regPixmap.width();
+        }
     }
 
     p.end();
@@ -140,20 +166,44 @@ void RatingWidget::paintEvent(QPaintEvent*)
 
 void RatingWidget::slotThemeChanged()
 {
-    d->regPixmap = QPixmap(d->ratingPixPath);
-    d->selPixmap = d->regPixmap;
+    d->regPixmap = QPixmap(15, 15);
+    d->regPixmap.fill(Qt::transparent); 
+    d->selPixmap = QPixmap(15, 15);
+    d->selPixmap.fill(Qt::transparent); 
+    d->disPixmap = QPixmap(15, 15);
+    d->disPixmap.fill(Qt::transparent); 
 
-    QPainter painter(&d->regPixmap);
-    painter.fillRect(0, 0, d->regPixmap.width(), d->regPixmap.height(),
-                     palette().color(QPalette::Dark));
-    painter.end();
+    QPainter p1(&d->regPixmap);
+    p1.setRenderHint(QPainter::Antialiasing, true);
+    p1.setBrush(palette().color(QPalette::Dark));
+    p1.setPen(Qt::black);
+    for (int i = 0; i < RatingMax; ++i) 
+    {
+        p1.drawPolygon(d->starPolygon, Qt::WindingFill);
+    }
+    p1.end();
 
-    QPainter painter2(&d->selPixmap);
-    painter2.fillRect(0, 0, d->selPixmap.width(), d->selPixmap.height(),
-                      ThemeEngine::componentData()->textSpecialRegColor());
-    painter2.end();
+    QPainter p2(&d->selPixmap);
+    p2.setRenderHint(QPainter::Antialiasing, true);
+    p2.setBrush(ThemeEngine::componentData()->textSpecialRegColor());
+    p2.setPen(Qt::black);
+    for (int i = 0; i < RatingMax; ++i) 
+    {
+        p2.drawPolygon(d->starPolygon, Qt::WindingFill);
+    }
+    p2.end();
+
+    QPainter p3(&d->disPixmap);
+    p3.setRenderHint(QPainter::Antialiasing, true);
+    p3.setBrush(palette().color(QPalette::Disabled, QPalette::Background));
+    p3.setPen(palette().color(QPalette::Disabled, QPalette::Foreground));
+    for (int i = 0; i < RatingMax; ++i) 
+    {
+        p3.drawPolygon(d->starPolygon, Qt::WindingFill);
+    }
+    p3.end();
     
-    setFixedSize(QSize(d->regPixmap.width()*5, d->regPixmap.height()));
+    setFixedSize(QSize(d->regPixmap.width()*RatingMax, d->regPixmap.height()));
     update();
 }
 
