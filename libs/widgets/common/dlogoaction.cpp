b898126d19b91ce5534d897e6df063d163430e1c
Gilles Caulier <caulier.gilles@gmail.com>
2007-08-28 07:39:47 +0000

    missing files
    svn path=/trunk/extragear/graphics/digikam/; revision=705624

---
+++ libs/widgets/common/dlogoaction.cpp
@@ -0,0 +1,94 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2007-27-08
+ * Description : an tool bar action object to display logo
+ * 
+ * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// Qt includes.
+
+#include <QPixmap>
+
+// KDE includes.
+
+#include <kurllabel.h>
+#include <ktoolbar.h>
+#include <kiconloader.h>
+#include <kpopupmenu.h>
+#include <ktoolinvocation.h>
+#include <kstandarddirs.h>
+#include <klocale.h>
+
+// Local includes.
+
+#include "dlogoaction.h"
+
+namespace Digikam 
+{
+
+DLogoAction::DLogoAction(QObject* parent, const char* name)
+           : KAction(parent, name)
+{
+}
+
+int DLogoAction::plug(QWidget *widget, int index)
+{
+    if (kapp && !kapp->authorizeKAction(name()))
+        return -1;
+
+    if ( widget->inherits( "KToolBar" ) )
+    {
+        KToolBar *bar = (KToolBar *)widget;
+
+        int id = getToolButtonID();
+
+        KURLLabel *pixmapLogo = new KURLLabel("http://www.digikam.org", QString(), bar);
+        pixmapLogo->setMargin(0);
+        pixmapLogo->setScaledContents(false);
+        pixmapLogo->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+        pixmapLogo->setTooltips(pixmapLogo, i18n("Visit digiKam project website"));
+        KGlobal::dirs()->addResourceType("logo-digikam", KGlobal::dirs()->kde_default("data") + "digikam/data");
+        QString directory = KGlobal::dirs()->findResourceDir("logo-digikam", "logo-digikam.png");
+        pixmapLogo->setPixmap(QPixmap( directory + "logo-digikam.png" ));
+
+        bar->insertWidget(id, pixmapLogo->width(), pixmapLogo);
+        bar->alignItemRight(id);
+
+        addContainer(bar, id);
+
+        connect(bar, SIGNAL(destroyed()), 
+                this, SLOT(slotDestroyed()));
+
+        connect(pixmapLogo, SIGNAL(leftClickedURL(const QString&)),
+                this, SLOT(slotProcessURL(const QString&)));    
+
+        return containerCount() - 1;
+    }
+
+    int containerId = KAction::plug( widget, index );
+
+    return containerId;
+}
+
+void DLogoAction::slotProcessURL(const QString& url)
+{
+    KToolInvocation::invokeBrowser(url);
+}
+
+} // namespace Digikam
---
+++ libs/widgets/common/dlogoaction.h
@@ -0,0 +1,55 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2007-27-08
+ * Description : an tool bar action object to display logo
+ * 
+ * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef DLOGO_ACTION_H
+#define DLOGO_ACTION_H   
+
+// KDE includes.
+
+#include <kaction.h>
+
+// Local includes.
+
+#include "digikam_export.h"
+
+namespace Digikam 
+{
+
+class DIGIKAM_EXPORT DLogoAction : public KAction
+{
+  Q_OBJECT
+
+public:
+    
+    DLogoAction(QObject* parent, const char* name=0);
+               
+    virtual int plug(QWidget *widget, int index=-1);
+
+private slots:
+
+    void slotProcessURL(const QString&);
+};
+
+} // namespace Digikam
+
+#endif /* DLOGO_ACTION_H */
