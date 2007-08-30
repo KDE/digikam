39333ef75cee65fb93925391351ee4620e923e8c
Gilles Caulier <caulier.gilles@gmail.com>
2007-08-30 16:21:43 +0000

    digiKam from trunk (KDE4) : missing to force to selectAll items when DownloadAll or DownloadAndDeleteAll is used CCBUGS: 143934
    svn path=/trunk/extragear/graphics/digikam/; revision=706517

--- utilities/cameragui/cameraui.cpp
+++ utilities/cameragui/cameraui.cpp
@@ -1203,6 +1203,12 @@ void CameraUI::slotDownloadAndDeleteAll()
 
 void CameraUI::slotDownload(bool onlySelected, bool deleteAfter, Album *album)
 {
+    // See B.K.O #143934: force to select all items to prevent problem 
+    // when !renameCustomizer->useDefault() ==> iconItem->getDownloadName()
+    // can return an empty string in this case because it depends on selection.
+    if (!onlySelected)
+        d->view->slotSelectAll();
+
     QString   newDirName;
     IconItem* firstItem = d->view->firstItem();
     if (firstItem)
@@ -1292,12 +1298,6 @@ void CameraUI::slotDownload(bool onlySelected, bool deleteAfter, Album *album)
         downloadName                 = iconItem->getDownloadName();
         mtime                        = iconItem->itemInfo()->mtime;
 
-        // occurs if renameCustomizer->useDefault() and Download All is used.
-        // If !useDefault, downloadName depends on selection,
-        // so Download All is disabled! (see slotNewSelection)
-        if (downloadName.isEmpty())
-            downloadName = d->view->defaultDownloadName(iconItem);
-        
         KUrl u(url);
         QString errMsg;
         QDateTime dateTime;
@@ -1675,8 +1675,6 @@ void CameraUI::slotNewSelection(bool hasSelection)
 {
     if (!d->renameCustomizer->useDefault())
     {
-        // for customized names, the downloadNames depend on the selection.
-        // So do not allow Download All if there is a selection!
         d->downloadSelectedAction->setEnabled(hasSelection);
         d->downloadDelSelectedAction->setEnabled(hasSelection);
         d->deleteSelectedAction->setEnabled(hasSelection);
@@ -1684,9 +1682,6 @@ void CameraUI::slotNewSelection(bool hasSelection)
     }
     else
     {
-        // if useDefault, the name can easily be computed without selection context,
-        // so we can allow Download All.
-        // This is the easiest default for new users
         d->downloadSelectedAction->setEnabled(hasSelection);
         d->downloadDelSelectedAction->setEnabled(hasSelection);
         d->deleteSelectedAction->setEnabled(hasSelection);
