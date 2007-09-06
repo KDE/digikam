6e22e9dea573760d8b98fdc649129f46ebd14d3e
Gilles Caulier <caulier.gilles@gmail.com>
2007-09-06 10:00:58 +0000

    update
    svn path=/trunk/extragear/graphics/digikam/; revision=709001

--- digikam/welcomepageview.cpp
+++ digikam/welcomepageview.cpp
@@ -106,6 +106,7 @@ QString WelcomePageView::infoPage()
     newFeatures << i18n("Hardware handling using KDE4 Solid interface");
     newFeatures << i18n("XMP metadata support");
     newFeatures << i18n("A new camera interface");
+    newFeatures << i18n("A new tool to capture pictures from Camera");
     newFeatures << i18n("Supports the latest camera RAW files");
 
     QString featureItems;
