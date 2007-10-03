dccb9d20a6d9dcb15e2b2de62c19149e44ab6172
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-03 11:36:03 +0000

    fix warnings
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=720636

--- libs/dimg/loaders/pngloader.cpp
+++ libs/dimg/loaders/pngloader.cpp
@@ -597,7 +597,7 @@ bool PNGLoader::save(const QString& filePath, DImgLoaderObserver *observer)
     
     if (!profile_rawdata.isEmpty())
     {
-        png_set_iCCP(png_ptr, info_ptr, "icc", PNG_COMPRESSION_TYPE_BASE, profile_rawdata.data(), profile_rawdata.size());
+        png_set_iCCP(png_ptr, info_ptr, (png_charp)"icc", PNG_COMPRESSION_TYPE_BASE, profile_rawdata.data(), profile_rawdata.size());
     }    
 
     // -------------------------------------------------------------------
@@ -628,7 +628,7 @@ bool PNGLoader::save(const QString& filePath, DImgLoaderObserver *observer)
     libpngver.replace('\n', ' ');
     software.append(QString(" (%1)").arg(libpngver));
     png_text text;
-    text.key  = "Software";
+    text.key  = (png_charp)("Software");
     text.text = (char *)software.ascii();
 #ifdef ENABLE_DEBUG_MESSAGES
     DDebug() << "Writing PNG Embedded text: key=" << text.key << " text=" << text.text << endl;
@@ -688,12 +688,12 @@ bool PNGLoader::save(const QString& filePath, DImgLoaderObserver *observer)
                     profile = ba;
                 }
 
-                writeRawProfile(png_ptr, info_ptr, "exif", profile.data(), (png_uint_32) profile.size());
+                writeRawProfile(png_ptr, info_ptr, (png_charp)("exif"), profile.data(), (png_uint_32) profile.size());
                 break;
             }
             case(DImg::IPTC):
             {
-                writeRawProfile(png_ptr, info_ptr, "iptc", ba.data(), (png_uint_32) ba.size());
+                writeRawProfile(png_ptr, info_ptr, (png_charp)("iptc"), ba.data(), (png_uint_32) ba.size());
                 break;
             }
             default:
