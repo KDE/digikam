62534ba4e6f65487af58cfe0193a78a624c34259
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2007-10-20 15:23:21 +0000

    Add two classes in DatabasebaseFields: - DatabaseFields::Set allows to 'or' together fields from all five enums - DatabaseFields::Hash allows to build a QHash from single field   values of all five enums
    svn path=/trunk/extragear/graphics/digikam/; revision=727450

--- libs/database/databasefields.h
+++ libs/database/databasefields.h
@@ -128,6 +128,98 @@ Q_DECLARE_FLAGS(ImageComments, ImageCommentsField);
 Q_DECLARE_FLAGS(ImagePositions, ImagePositionsField);
 
 
+
+#define DATABASEFIELDS_SET_DECLARE_METHODS(Flag, variable) \
+    inline Flag &operator=(const Flag &f) { return variable.operator=(f); } \
+    inline Flag &operator|=(Flag f) { return variable.operator|=(f); } \
+    inline Flag &operator|=(Flag::enum_type f) { return variable.operator|=(f); } \
+    inline Flag &operator^=(Flag f) { return variable.operator^=(f); } \
+    inline Flag &operator^=(Flag::enum_type f) { return variable.operator^=(f); } \
+    inline Flag operator|(Flag f) const { return variable.operator|(f); } \
+    inline Flag operator|(Flag::enum_type f) const { return variable.operator|(f); } \
+    inline Flag operator^(Flag f) const { return variable.operator^(f); } \
+    inline Flag operator^(Flag::enum_type f) const { return variable.operator^(f); } \
+    inline Flag operator&(Flag::enum_type f) const { return variable.operator&(f); } \
+
+/**
+ * This class provides a set of all DatabasFields enums,
+ * without resorting to a QSet.
+*/
+class Set
+{
+public:
+
+    Set()
+    {
+        images = ImagesNone;
+        imageInformation = ImageInformationNone;
+        imageMetadata = ImageMetadataNone;
+        imageComments = ImageCommentsNone;
+        imagePositions = ImagePositionsNone;
+    }
+
+
+    DATABASEFIELDS_SET_DECLARE_METHODS(Images, images)
+    DATABASEFIELDS_SET_DECLARE_METHODS(ImageInformation, imageInformation)
+    DATABASEFIELDS_SET_DECLARE_METHODS(ImageMetadata, imageMetadata)
+    DATABASEFIELDS_SET_DECLARE_METHODS(ImageComments, imageComments)
+    DATABASEFIELDS_SET_DECLARE_METHODS(ImagePositions, imagePositions)
+
+private:
+    Images images;
+    ImageInformation imageInformation;
+    ImageMetadata imageMetadata;
+    ImageComments imageComments;
+    ImagePositions imagePositions;
+};
+
+
+
+#define DATABASEFIELDS_HASH_DECLARE_METHODS(Key, method) \
+    int remove(const Key &key) { return remove(method(key)); } \
+    T take(const Key &key) { return take(method(key)); } \
+    \
+    bool contains(const Key &key) const { return contains(method(key)); } \
+    const T value(const Key &key) const { return value(method(key)); } \
+    const T value(const Key &key, const T &defaultValue) const { return value(method(key), defaultValue); } \
+    T &operator[](const Key &key) { return operator[](method(key)); } \
+    const T operator[](const Key &key) const { return operator[](method(key)); } \
+    \
+    QList<T> values(const Key &key) const { return value(method(key)); } \
+    int count(const Key &key) const { return count(method(key)); }
+
+/**
+ * This class provides a hash on all DatabaseFields enums,
+ * allowing to use the enum values as independent keys.
+ * You can use the class like a normal QHash with the value type defined
+ * by you, and as keys the members of the DatabaseFields enums.
+ * You can only use single enum members as keys, not or'ed numbers.
+ */
+
+template <class T>
+class Hash : public QHash<int, T>
+{
+public:
+
+    static inline int uniqueKey(ImagesField f)            { return f + 0 * 256 + 0; }
+    static inline int uniqueKey(ImageInformationField f) { return f + 1 * 256 + 1; }
+    static inline int uniqueKey(ImageMetadataField f)    { return f + 2 * 256 + 2; }
+    static inline int uniqueKey(ImageCommentsField f)    { return f + 3 * 256 + 3; }
+    static inline int uniqueKey(ImagePositionsField f)    { return f + 4 * 256 + 4; }
+
+    // override relevant methods from QHash
+    DATABASEFIELDS_HASH_DECLARE_METHODS(ImagesField, uniqueKey);
+    DATABASEFIELDS_HASH_DECLARE_METHODS(ImageInformationField, uniqueKey);
+    DATABASEFIELDS_HASH_DECLARE_METHODS(ImageMetadataField, uniqueKey);
+    DATABASEFIELDS_HASH_DECLARE_METHODS(ImageCommentsField, uniqueKey);
+    DATABASEFIELDS_HASH_DECLARE_METHODS(ImagePositionsField, uniqueKey);
+
+private:
+    // make pure int methods private
+    DATABASEFIELDS_HASH_DECLARE_METHODS(int, int);
+};
+
+
 } // end of namespace DatabaseFields
 
 } // end of namespace Digikam
