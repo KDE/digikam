a8269e2f1d8e6c159e5c6d257e9707c9e37305f3
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-23 05:22:01 +0000

    fix credits
    svn path=/trunk/extragear/graphics/digikam/; revision=728363

--- digikam/albumlister.h
+++ digikam/albumlister.h
@@ -7,6 +7,8 @@
  * Description : Albums lister. 
  * 
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007 by Arnd Baecker <arnd dot baecker at web dot de> 
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
