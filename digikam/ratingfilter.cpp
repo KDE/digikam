0be2fb9fd530d66026a6712df872c9832ee7ad54
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-23 05:48:47 +0000

    add whatsthis
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=728367

--- digikam/ratingfilter.cpp
+++ digikam/ratingfilter.cpp
@@ -7,6 +7,7 @@
  * Description : a widget to filter album contents by rating
  * 
  * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007 by Arnd Baecker <arnd dot baecker at web dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -74,6 +75,10 @@ RatingFilter::RatingFilter(QWidget* parent)
     d->ratingTracker = new DTipTracker("", this);
     updateRatingTooltip();
     setMouseTracking(true);
+
+    QWhatsThis::add(this, i18n("Select here the rating value used to filter "
+                               "albums contents. Use contextual pop-up menu to "
+                               "set rating filter condition."));
 
     // To dispatch signal from parent widget with filter condition.
     connect(this, SIGNAL(signalRatingChanged(int)),
--- digikam/ratingfilter.h
+++ digikam/ratingfilter.h
@@ -7,6 +7,7 @@
  * Description : a widget to filter album contents by rating
  * 
  * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007 by Arnd Baecker <arnd dot baecker at web dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
