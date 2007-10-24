a504435e8c162a78b459745fd68793ecb51402d5
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-24 20:59:23 +0000

    fix icon
    svn path=/trunk/extragear/graphics/digikam/; revision=729002

--- digikam/digikamapp.cpp
+++ digikam/digikamapp.cpp
@@ -920,7 +920,7 @@ void DigikamApp::setupActions()
 
     // -----------------------------------------------------------
 
-    KAction *syncMetadataAction = new KAction(KIcon("compfile"),
+    KAction *syncMetadataAction = new KAction(KIcon("run-build-file"),
                                               i18n("Update Metadata Database..."), this);
     connect(syncMetadataAction, SIGNAL(triggered()), this, SLOT(slotSyncAllPicturesMetadata()));
     actionCollection()->addAction("sync_metadata", syncMetadataAction);
