444333289026b7d8617424c3107c2235378a3a96
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2007-11-02 18:46:05 +0000

    Add a class to access the data from the ImagePosition table. An ImagePosition object encapsulates the information for an image, allows to read it in several formats and to change the information in the database.
    (code not yet tested; requires latest change to libkexiv2)
    
    svn path=/trunk/extragear/graphics/digikam/; revision=732076

---
+++ libs/database/imageposition.cpp
@@ -0,0 +1,379 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2007-11-01
+ * Description : Access image position stored in database.
+ *
+ * Copyright (C) 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// Qt includes
+
+#include <QSharedData>
+
+// Local includes.
+
+#include "databaseaccess.h"
+#include "albumdb.h"
+#include "databasefields.h"
+#include "dmetadata.h"
+#include "metadatainfo.h"
+#include "imageposition.h"
+
+namespace Digikam
+{
+
+class ImagePositionPriv : public QSharedData
+{
+public:
+    ImagePositionPriv()
+    {
+        imageId         = -1;
+        latitudeNumber  = 0;
+        longitudeNumber = 0;
+        altitude        = 0;
+        orientation     = 0;
+        tilt            = 0;
+        roll            = 0;
+        empty           = true;
+        dirtyFields     = DatabaseFields::ImagePositionsNone;
+    }
+
+    qlonglong imageId;
+
+    QString latitude;
+    QString longitude;
+    double  latitudeNumber;
+    double  longitudeNumber;
+    double  altitude;
+    double  orientation;
+    double  tilt;
+    double  roll;
+    QString description;
+
+    bool    empty;
+
+    DatabaseFields::ImagePositions dirtyFields;
+};
+
+ImagePosition::ImagePosition()
+{
+}
+
+ImagePosition::ImagePosition(qlonglong imageId)
+{
+    d = new ImagePositionPriv;
+
+    d->imageId = imageId;
+
+    QVariantList values = DatabaseAccess().db()->getImagePosition(imageId);
+    if (values.size() == 9)
+    {
+        d->empty           = false;
+        d->latitude        = values[0].toString();
+        d->latitudeNumber  = values[1].toDouble();
+        d->longitude       = values[2].toString();
+        d->longitudeNumber = values[3].toDouble();
+        d->altitude        = values[4].toDouble();
+        d->orientation     = values[5].toDouble();
+        d->tilt            = values[6].toDouble();
+        d->roll            = values[7].toDouble();
+        d->description     = values[8].toString();
+    }
+}
+
+ImagePosition::ImagePosition(const ImagePosition &other)
+{
+    d = other.d;
+}
+
+ImagePosition::~ImagePosition()
+{
+    apply();
+}
+
+bool ImagePosition::isNull() const
+{
+    return !d;
+}
+
+bool ImagePosition::isEmpty() const
+{
+    return !d || d->empty;
+}
+
+QString ImagePosition::latitude() const
+{
+    if (!d)
+        return QString();
+
+    return d->latitude;
+}
+
+QString ImagePosition::longitude() const
+{
+    if (!d)
+        return QString();
+
+    return d->longitude;
+
+}
+
+double ImagePosition::latitudeNumber() const
+{
+    if (!d)
+        return 0;
+
+    return d->latitudeNumber;
+}
+
+double ImagePosition::longitudeNumber() const
+{
+    if (!d)
+        return 0;
+
+    return d->longitudeNumber;
+}
+
+QString ImagePosition::latitudeFormatted() const
+{
+    if (!d)
+        return QString();
+
+    return DMetadata::valueToString(d->latitude, MetadataInfo::Latitude);
+}
+
+QString ImagePosition::longitudeFormatted() const
+{
+    if (!d)
+        return QString();
+
+    return DMetadata::valueToString(d->longitude, MetadataInfo::Longitude);
+}
+
+bool ImagePosition::latitudeUserPresentableNumbers(int *degrees, int *minutes, double *seconds, char *directionReference)
+{
+    if (!d)
+        return false;
+
+    return DMetadata::convertToUserPresentableNumbers(d->latitude, degrees, minutes, seconds, directionReference);
+}
+
+bool ImagePosition::longitudeUserPresentableNumbers(int *degrees, int *minutes, double *seconds, char *directionReference)
+{
+    if (!d)
+        return false;
+
+    return DMetadata::convertToUserPresentableNumbers(d->longitude, degrees, minutes, seconds, directionReference);
+}
+
+double ImagePosition::altitude() const
+{
+    if (!d)
+        return 0;
+
+    return d->altitude;
+}
+
+QString ImagePosition::altitudeFormatted() const
+{
+    if (!d)
+        return QString();
+
+    return DMetadata::valueToString(d->altitude, MetadataInfo::Altitude);
+}
+
+double ImagePosition::orientation() const
+{
+    if (!d)
+        return 0;
+
+    return d->orientation;
+}
+
+double ImagePosition::tilt() const
+{
+    if (!d)
+        return 0;
+
+    return d->tilt;
+}
+
+double ImagePosition::roll() const
+{
+    if (!d)
+        return 0;
+
+    return d->roll;
+}
+
+QString ImagePosition::description() const
+{
+    if (!d)
+        return QString();
+
+    return d->description;
+}
+
+bool ImagePosition::setLatitude(const QString &latitude)
+{
+    if (!d)
+        return false;
+
+    double number;
+    if (!DMetadata::convertFromGPSCoordinateString(latitude, &number))
+        return false;
+    d->latitude = latitude;
+    d->latitudeNumber = number;
+    d->dirtyFields |= DatabaseFields::Latitude | DatabaseFields::LatitudeNumber;
+    return true;
+}
+
+bool ImagePosition::setLongitude(const QString longitude)
+{
+    if (!d)
+        return false;
+
+    double number;
+    if (!DMetadata::convertFromGPSCoordinateString(longitude, &number))
+        return false;
+    d->longitude = longitude;
+    d->longitudeNumber = number;
+    d->dirtyFields |= DatabaseFields::Longitude | DatabaseFields::LongitudeNumber;
+    return true;
+}
+
+bool ImagePosition::setLatitude(double latitudeNumber)
+{
+    if (!d)
+        return false;
+
+    QString string = DMetadata::convertToGPSCoordinateString(true, latitudeNumber);
+    if (string.isNull())
+        return false;
+    d->latitude = string;
+    d->latitudeNumber = latitudeNumber;
+    d->dirtyFields |= DatabaseFields::Latitude | DatabaseFields::LatitudeNumber;
+    return true;
+}
+
+bool ImagePosition::setLongitude(double longitudeNumber)
+{
+    if (!d)
+        return false;
+
+    QString string = DMetadata::convertToGPSCoordinateString(false, longitudeNumber);
+    if (string.isNull())
+        return false;
+    d->longitude = string;
+    d->longitudeNumber = longitudeNumber;
+    d->dirtyFields |= DatabaseFields::Longitude | DatabaseFields::LongitudeNumber;
+    return true;
+}
+
+void ImagePosition::setAltitude(double altitude)
+{
+    if (!d)
+        return;
+
+    d->altitude = altitude;
+    d->dirtyFields |= DatabaseFields::Altitude;
+}
+
+void ImagePosition::setOrientation(double orientation)
+{
+    if (!d)
+        return;
+
+    d->orientation = orientation;
+    d->dirtyFields |= DatabaseFields::PositionOrientation;
+}
+
+void ImagePosition::setTilt(double tilt)
+{
+    if (!d)
+        return;
+
+    d->tilt = tilt;
+    d->dirtyFields |= DatabaseFields::PositionTilt;
+}
+
+void ImagePosition::setRoll(double roll)
+{
+    if (!d)
+        return;
+
+    d->roll = roll;
+    d->dirtyFields |= DatabaseFields::PositionRoll;
+}
+
+void ImagePosition::setDescription(const QString &description)
+{
+    if (!d)
+        return;
+
+    d->description = description;
+    d->dirtyFields |= DatabaseFields::PositionDescription;
+}
+
+void ImagePosition::apply()
+{
+    if (!d)
+        return;
+
+    if (d->dirtyFields == DatabaseFields::ImagePositionsNone)
+        return;
+
+    QVariantList values;
+    if (d->dirtyFields & DatabaseFields::Latitude)
+        values << d->latitude;
+    if (d->dirtyFields & DatabaseFields::LatitudeNumber)
+        values << d->latitudeNumber;
+    if (d->dirtyFields & DatabaseFields::Longitude)
+        values << d->longitude;
+    if (d->dirtyFields & DatabaseFields::LongitudeNumber)
+        values << d->longitudeNumber;
+    if (d->dirtyFields & DatabaseFields::Altitude)
+        values << d->altitude;
+    if (d->dirtyFields & DatabaseFields::PositionOrientation)
+        values << d->orientation;
+    if (d->dirtyFields & DatabaseFields::PositionTilt)
+        values << d->tilt;
+    if (d->dirtyFields & DatabaseFields::PositionRoll)
+        values << d->roll;
+    if (d->dirtyFields & DatabaseFields::PositionDescription)
+        values << d->description;
+
+    if (d->empty)
+    {
+        DatabaseAccess().db()->addImagePosition(d->imageId, values, d->dirtyFields);
+        d->empty = false;
+    }
+    else
+    {
+        DatabaseAccess().db()->changeImagePosition(d->imageId, values, d->dirtyFields);
+    }
+    d->dirtyFields = DatabaseFields::ImagePositionsNone;
+}
+
+
+
+
+} // namespace Digikam
+
+
+
---
+++ libs/database/imageposition.h
@@ -0,0 +1,149 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2007-11-01
+ * Description : Access image position stored in database.
+ *
+ * Copyright (C) 2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef IMAGEPOSITION_H
+#define IMAGEPOSITION_H
+
+// Qt includes
+
+#include <QString>
+#include <QSharedDataPointer>
+
+// Local includes.
+
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class ImagePositionPriv;
+
+class DIGIKAM_EXPORT ImagePosition
+{
+public:
+
+    /**
+     * Creates a null ImagePosition object
+     */
+    ImagePosition();
+    /**
+     * Creates an ImagePosition object for the given image.
+     * The information is read from the database.
+     */
+    ImagePosition(qlonglong imageId);
+
+    ImagePosition(const ImagePosition &other);
+    ~ImagePosition();
+
+    bool isNull() const;
+    /**
+     * An object is empty if no entry exists in the ImagePosition
+     * table for the referenced image, or if the object is null.
+     * An empty object is empty even if values have been set;
+     * it becomes not empty after calling apply().
+     */
+    bool isEmpty() const;
+
+    /** Returns latitude/longitude in the format as described by
+     *  the XMP specification as "GPSCoordinate":
+     *  A Text value in the form ?DDD,MM,SSk? or ?DDD,MM.mmk?.
+     *  This provides lossless storage.
+     */
+    QString latitude() const;
+    QString longitude() const;
+    /**
+     * Returns latitude/longitude as a double in degrees.
+     * North and East have a positive sign, South and West negative.
+     * This provides high precision, with the usual floating point
+     * concerns, and possible problems finding the exact text form when
+     * converting _back_ to fractions.
+     */
+    double latitudeNumber() const;
+    double longitudeNumber() const;
+    /**
+     * Returns the latitude/longitude in a user-presentable version,
+     * in the form "30°45'55.123'' East"
+     */
+    QString latitudeFormatted() const;
+    QString longitudeFormatted() const;
+    /**
+     * Returns latitude/longitude as user-presentable numbers.
+     * This means that degrees and minutes are integer, the seconds fractional.
+     * Direction reference is 'N'/'S', 'E'/'W' resp.
+     * This is for the purpose of presenting to the user, there are no guarantees on precision.
+     * Returns true if the values have been changed.
+     */
+    bool latitudeUserPresentableNumbers(int *degrees, int *minutes, double *seconds, char *directionReference);
+    bool longitudeUserPresentableNumbers(int *degrees, int *minutes, double *seconds, char *directionReference);
+    /** The altitude in meters */
+    double altitude() const;
+    /**
+     * Returns the altitude formatted in a user-presentable way in the form "43.45m"
+     */
+    QString altitudeFormatted() const;
+    double orientation() const;
+    double tilt() const;
+    double roll() const;
+    QString description() const;
+
+    /**
+     * Sets the latitude/longitude from the GPSCoordinate string as described by XMP.
+     * Returns true if the format is accepted.
+     */
+    bool setLatitude(const QString &latitude);
+    bool setLongitude(const QString longitude);
+    /**
+     * Sets the latitude/longitude from a double floating point number,
+     * as described for latitudeNumber() above.
+     * Returns true if the value is valid and accepted.
+     */
+    bool setLatitude(double latitudeNumber);
+    bool setLongitude(double longitudeNumber);
+    /**
+     * Set the altitude in meters
+     */
+    void setAltitude(double altitude);
+    void setOrientation(double orientation);
+    void setTilt(double tilt);
+    void setRoll(double roll);
+    void setDescription(const QString &description);
+
+    /**
+     * Apply all changes made to this object.
+     * (Also called from desctructor)
+     */
+    void apply();
+
+private:
+
+    QSharedDataPointer<ImagePositionPriv> d;
+
+};
+
+
+} // namespace Digikam
+
+#endif // IMAGEPOSITION_H
+
+
+
