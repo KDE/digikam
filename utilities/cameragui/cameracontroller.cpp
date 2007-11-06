d419ed44f3fdea0990fd3712c6cd24238dd95ce0
Gilles Caulier <caulier.gilles@gmail.com>
2007-11-06 08:29:26 +0000

    fix i18n
    svn path=/trunk/extragear/graphics/digikam/; revision=733383

--- utilities/cameragui/cameracontroller.cpp
+++ utilities/cameragui/cameracontroller.cpp
@@ -399,7 +399,7 @@ void CameraController::executeCommand(CameraCommand *cmd)
             QString folder = cmd->map["folder"].toString();
             QString file   = cmd->map["file"].toString();
 
-            sendInfo(i18n("Getting thumbnail in progress..."));
+            sendInfo(i18n("Getting thumbnails..."));
 
             QImage thumbnail;
             d->camera->getThumbnail(folder, file, thumbnail);
