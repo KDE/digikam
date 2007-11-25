65528f3278825f8d214bda538256dd48a31c9047
Gilles Caulier <caulier.gilles@gmail.com>
2007-11-25 16:54:10 +0000

    new widget to preform text search
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=741415

---
+++ libs/widgets/common/searchtextbar.cpp
@@ -0,0 +1,100 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2007-11-25
+ * Description : a bar used to search a text somewhere.
+ * 
+ * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// Qt includes.
+
+#include <qlabel.h>
+#include <qlayout.h>
+#include <qtoolbutton.h>
+#include <qtooltip.h>
+
+// KDE includes.
+
+#include <kapplication.h>
+#include <kiconloader.h>
+#include <klocale.h>
+#include <klineedit.h>
+#include <kdialogbase.h>
+
+// Local includes
+
+#include "searchtextbar.h"
+#include "searchtextbar.moc"
+
+namespace Digikam
+{
+
+class SearchTextBarPriv
+{
+public:
+
+    SearchTextBarPriv()
+    {
+        searchEdit  = 0;
+        searchLabel = 0;
+        clearButton = 0;
+    }
+
+    QLabel      *searchLabel;
+
+    QToolButton *clearButton;
+
+    KLineEdit   *searchEdit;
+};
+
+SearchTextBar::SearchTextBar(QWidget *parent)
+                 : QWidget(parent, 0, Qt::WDestructiveClose)
+{
+    d = new SearchTextBarPriv;
+    setFocusPolicy(QWidget::NoFocus);
+
+    QHBoxLayout *hlay = new QHBoxLayout(this);
+
+    d->clearButton = new QToolButton(this);
+    d->clearButton->setAutoRaise(true);
+    d->clearButton->setIconSet(kapp->iconLoader()->loadIcon("locationbar_erase",
+                               KIcon::Toolbar, KIcon::SizeSmall));
+
+    d->searchLabel = new QLabel(i18n("Search:"), this);
+    d->searchEdit  = new KLineEdit(this);
+    d->searchEdit->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum));
+
+    hlay->setSpacing(KDialog::spacingHint());
+    hlay->setMargin(0);
+    hlay->addWidget(d->clearButton);
+    hlay->addWidget(d->searchLabel);
+    hlay->addWidget(d->searchEdit);
+
+    connect(d->clearButton, SIGNAL(clicked()),
+            d->searchEdit, SLOT(clear()));
+
+    connect(d->searchEdit, SIGNAL(textChanged(const QString&)),
+            this, SIGNAL(signalTextChanged(const QString&)));
+}
+
+SearchTextBar::~SearchTextBar()
+{
+    delete d;
+}
+
+}  // namespace Digikam
---
+++ libs/widgets/common/searchtextbar.h
@@ -0,0 +1,61 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2007-11-25
+ * Description : a bar used to search a text somewhere.
+ * 
+ * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef SEARCH_TEXT_BAR_H
+#define SEARCH_TEXT_BAR_H
+
+// Qt includes.
+
+#include <qwidget.h>
+#include <qstring.h>
+
+// Local includes.
+
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class SearchTextBarPriv;
+
+class DIGIKAM_EXPORT SearchTextBar : public QWidget
+{
+Q_OBJECT
+
+public:
+
+    SearchTextBar(QWidget *parent=0);
+    ~SearchTextBar();
+    
+signals:
+    
+    void signalTextChanged(const QString&);    
+        
+private :
+
+    SearchTextBarPriv* d;    
+};
+
+}  // namespace Digikam
+
+#endif /* SEARCH_TEXT_BAR_H */
