5e066c0aa32b1f61a02fe3c0c7296400ae459e56
Gilles Caulier <caulier.gilles@gmail.com>
2007-11-27 21:23:18 +0000

    polish
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=742341

--- digikam/searchresultsview.cpp
+++ digikam/searchresultsview.cpp
@@ -7,6 +7,7 @@
  * Description : search results view.
  * 
  * Copyright (C) 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -43,11 +44,10 @@ namespace Digikam
 {
 
 SearchResultsView::SearchResultsView(QWidget* parent)
-    : QIconView(parent)
+                 : QIconView(parent)
 {
-    m_listJob  = 0;
-    m_thumbJob = 0;
-
+    m_listJob     = 0;
+    m_thumbJob    = 0;
     m_libraryPath = AlbumManager::instance()->getLibraryPath();
     m_filter      = AlbumSettings::instance()->getAllFileFilter();
 
--- digikam/searchresultsview.h
+++ digikam/searchresultsview.h
@@ -7,6 +7,7 @@
  * Description : search results view.
  *
  * Copyright (C) 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
+ * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -59,23 +60,23 @@ public:
     void openURL(const KURL& url);
     void clear();
     
-private:
-
-    KIO::TransferJob*         m_listJob;
-    
-    QGuardedPtr<ThumbnailJob> m_thumbJob;
-    
-    QDict<QIconViewItem>      m_itemDict;
-    
-    QString                   m_libraryPath;
-    QString                   m_filter;
-
 private slots:
 
     void slotData(KIO::Job *job, const QByteArray &data);
     void slotResult(KIO::Job *job);
     void slotGotThumbnail(const KURL& url, const QPixmap& pix);
     void slotFailedThumbnail(const KURL& url);
+
+private:
+    
+    QString                   m_libraryPath;
+    QString                   m_filter;
+
+    QDict<QIconViewItem>      m_itemDict;
+
+    QGuardedPtr<ThumbnailJob> m_thumbJob;
+
+    KIO::TransferJob*         m_listJob;
 };
 
 }  // namespace Digikam
