10e2dd6c619d2e781417077ebb1d7d298d52e46c
Albert Astals Cid <tsdgeos@terra.es>
2007-11-29 19:58:17 +0000

    Use >> to get translators placeholders
    svn path=/trunk/extragear/graphics/digikam/; revision=743072

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $PREPARETIPS > tips.cpp
-$EXTRACTRC `find . -name \*.rc` > rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.h -o -name \*.cpp` -o $podir/digikam.pot
 rm -f tips.cpp
