d7f4eb318ac8723afff4051058be7708ccb90675
Gerhard Kulzer <gerhard@kulzer.net>
2007-12-01 08:26:47 +0000

    adding NoRAWFiles to mime filter, polish i18n (Mikolaj)
    svn path=/trunk/extragear/graphics/digikam/; revision=743523

--- digikam/mimefilter.cpp
+++ digikam/mimefilter.cpp
@@ -5,7 +5,7 @@
  *
  * Date        : 2007-10-22
  * Description : a widget to filter album contents by type mime
- * 
+ *
  * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -13,7 +13,7 @@
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -49,21 +49,22 @@ MimeFilter::MimeFilter(QWidget* parent)
 {
     d = new MimeFilterPriv;
     insertItem( AllFiles,    i18n("All Files") );
+    insertItem( NoRAWFiles,  i18n("No RAW files") );
     insertItem( JPGFiles,    i18n("JPEG Files") );
     insertItem( PNGFiles,    i18n("PNG files") );
     insertItem( TIFFiles,    i18n("TIFF files") );
     insertItem( RAWFiles,    i18n("RAW files") );
-    insertItem( MoviesFiles, i18n("Movies files") );
+    insertItem( MoviesFiles, i18n("Movie files") );
     insertItem( AudioFiles,  i18n("Audio files") );
 
-    setToolTip(i18n("Mime type filter"));
-    setWhatsThis(i18n("Select here the mime type pattern used to filter albums contents"));
+    setToolTip(i18n("Filter by file type"));
+    setWhatsThis(i18n("Select the file types (mime types) you want to show"));
 
     setMimeFilter(AllFiles);
 }
 
 MimeFilter::~MimeFilter()
-{    
+{
     delete d;
 }
 
