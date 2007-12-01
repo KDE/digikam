e42a9266dcb821420b0a02812ed8bab31f0c4e17
Gerhard Kulzer <gerhard@kulzer.net>
2007-12-01 08:27:11 +0000

    adding NoRAWFiles to mime filter, polish i18n (Mikolaj)
    svn path=/trunk/extragear/graphics/digikam/; revision=743524

--- digikam/mimefilter.h
+++ digikam/mimefilter.h
@@ -5,7 +5,7 @@
  *
  * Date        : 2007-10-22
  * Description : a widget to filter album contents by type mime
- * 
+ *
  * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -13,7 +13,7 @@
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -36,12 +36,13 @@ class MimeFilterPriv;
 class MimeFilter : public QComboBox
 {
     Q_OBJECT
-    
+
 public:
 
     enum TypeMimeFilter
     {
-        AllFiles = 0,  
+        AllFiles = 0,
+        NoRAWFiles,
         JPGFiles,
         PNGFiles,
         TIFFiles,
