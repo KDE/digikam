c3c5c03ec3f3f793e893482207e7584443d2d1f7
Gerhard Kulzer <gerhard@kulzer.net>
2007-12-22 16:02:37 +0000

    update to 0.9.3 release
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=751680

--- digikam/version.h
+++ digikam/version.h
@@ -5,26 +5,26 @@
  *
  * Date        : 2004-09-09
  * Description : digiKam release ID header.
- * 
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com> 
+ *
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef DIGIKAM_VERSION_H
 #define DIGIKAM_VERSION_H
 
-static const char digikam_version[] = "0.9.3-rc1";
+static const char digikam_version[] = "0.9.3";
 
 #endif // DIGIKAM_VERSION_H
 
