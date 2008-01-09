8fe3bccd6eb0b22512447513f481b604a5ced2ea
Gilles Caulier <caulier.gilles@gmail.com>
2008-01-09 09:54:06 +0000

    report the method used by KDirWatch on the console
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=758812

--- digikam/albummanager.cpp
+++ digikam/albummanager.cpp
@@ -341,6 +341,16 @@ void AlbumManager::startScan()
     d->dirWatch = new KDirWatch(this);
     connect(d->dirWatch, SIGNAL(dirty(const QString&)),
             this, SLOT(slotDirty(const QString&)));
+    
+    KDirWatch::Method m = d->dirWatch->internalMethod();
+    QString mName("FAM");
+    if (m == KDirWatch::DNotify)
+        mName = QString("DNotify");
+    else if (m == KDirWatch::Stat)
+        mName = QString("Stat");
+    else if (m == KDirWatch::INotify)
+        mName = QString("INotify");
+    DDebug() << "KDirWatch method = " << mName << endl;
 
     d->dirWatch->addDir(d->libraryPath);
 
