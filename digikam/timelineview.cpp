6224273733ec223d56a13b7dd542739bae46bf20
Gilles Caulier <caulier.gilles@gmail.com>
2008-01-27 17:21:43 +0000

    sync with KDE3 branch
    svn path=/trunk/extragear/graphics/digikam/; revision=767267

--- digikam/timelineview.cpp
+++ digikam/timelineview.cpp
@@ -344,7 +344,7 @@ void TimeLineView::setActive(bool val)
     {
         int totalCount = 0;
         DateRangeList list = d->timeLineWidget->selectedDateRange(totalCount);
-        if (!list.isEmpty())
+        if (list.isEmpty())
         {
             AlbumManager::instance()->setCurrentAlbum(0);
         }
