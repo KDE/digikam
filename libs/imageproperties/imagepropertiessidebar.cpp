5ae90c8fcb504658fe262808505149f9334221cd
Gilles Caulier <caulier.gilles@gmail.com>
2008-02-12 11:12:21 +0000

    polish
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=774026

--- libs/imageproperties/imagepropertiessidebar.cpp
+++ libs/imageproperties/imagepropertiessidebar.cpp
@@ -7,7 +7,7 @@
  * Description : simple image properties side bar (without support 
  *               of digiKam database).
  *
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -44,6 +44,7 @@
 #include "imagepropertiesmetadatatab.h"
 #include "imagepropertiescolorstab.h"
 #include "imagepropertiessidebar.h"
+#include "imagepropertiessidebar.moc"
 
 namespace Digikam
 {
@@ -147,6 +148,3 @@ void ImagePropertiesSideBar::slotChangedTab(QWidget* tab)
 }
 
 }  // NameSpace Digikam
-
-#include "imagepropertiessidebar.moc"
-
