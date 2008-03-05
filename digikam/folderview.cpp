7cbd4f007d80cf1f3fcbb81e35f79f0e5b2ec473
Gilles Caulier <caulier.gilles@gmail.com>
2008-03-05 09:08:06 +0000

    polish
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=782461

--- digikam/folderview.cpp
+++ digikam/folderview.cpp
@@ -353,14 +353,17 @@ void FolderView::slotThemeChanged()
     d->itemSelPix = ThemeEngine::instance()->listSelPixmap(w, h);
 
     QPalette plt(palette());
+
     QColorGroup cg(plt.active());
     cg.setColor(QColorGroup::Base, ThemeEngine::instance()->baseColor());
     cg.setColor(QColorGroup::Text, ThemeEngine::instance()->textRegColor());
     cg.setColor(QColorGroup::HighlightedText, ThemeEngine::instance()->textSelColor());
     cg.setColor(QColorGroup::Link, ThemeEngine::instance()->textSpecialRegColor());
     cg.setColor(QColorGroup::LinkVisited, ThemeEngine::instance()->textSpecialSelColor());
+
     plt.setActive(cg);
     plt.setInactive(cg);
+    plt.setDisabled(cg);
     setPalette(plt);
 
     viewport()->update();
