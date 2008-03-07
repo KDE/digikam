32a574f1c62504171ff109f46066c8f2032582ec
Gilles Caulier <caulier.gilles@gmail.com>
2008-03-07 10:07:19 +0000

    digiKam from trunk : backport commit #783161 CCBUGS: 153775
    svn path=/trunk/extragear/graphics/digikam/; revision=783164

--- utilities/cameragui/cameracontroller.cpp
+++ utilities/cameragui/cameracontroller.cpp
@@ -7,7 +7,7 @@
  * Description : digital camera controller
  * 
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com> 
+ * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com> 
  * Copyright (C) 2006-2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
@@ -672,6 +672,7 @@ void CameraController::slotCheckRename(const QString &folder, const QString &fil
 
             int result = dlg.exec();
             dest       = dlg.newDestUrl().path();
+            info       = QFileInfo(dest);
 
             switch (result)
             {
@@ -722,10 +723,10 @@ void CameraController::slotCheckRename(const QString &folder, const QString &fil
     {
         unlink(QFile::encodeName(temp));
         emit signalInfoMsg(i18n("Skipped file %1", file));
-        emit signalSkipped(folder, file);        
+        emit signalSkipped(folder, file);
         return;
     }
-    
+
     // move the file to the destination file
     if (rename(QFile::encodeName(temp), QFile::encodeName(dest)) != 0)
     {
