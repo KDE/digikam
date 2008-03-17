c156c1dac28e4632805da43e2c53ab7329ce1ff1
Gilles Caulier <caulier.gilles@gmail.com>
2008-03-17 10:01:52 +0000

    polish
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=786519

--- utilities/lighttable/lighttablebar.h
+++ utilities/lighttable/lighttablebar.h
@@ -6,7 +6,7 @@
  * Date        : 2007-04-11
  * Description : light table thumbs bar
  *
- * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -50,7 +50,7 @@ class DIGIKAM_EXPORT LightTableBar : public ThumbBarView
 
 public:
 
-    LightTableBar(QWidget* parent, int orientation=Vertical, bool exifRotate=false);
+    LightTableBar(QWidget* parent, int orientation=Vertical, bool exifRotate=true);
     ~LightTableBar();
 
     ImageInfo*    currentItemImageInfo() const;
