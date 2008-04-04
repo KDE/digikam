4c8893ad63b4a7a7b4e6ad44b7b998bb548d4c05
Gilles Caulier <caulier.gilles@gmail.com>
2008-04-04 11:40:18 +0000

    missing file
    svn path=/trunk/extragear/graphics/digikam/; revision=793561

---
+++ config-digikam.h.cmake
@@ -0,0 +1,5 @@
+/* Define to 1 if you have KDEPIM shared library installed */
+#cmakedefine HAVE_KDEPIMLIBS 1
+
+/* Define to 1 if you have Marble Widget shared library installed */
+#cmakedefine HAVE_MARBLEWIDGET 1
