304dae6d369b698d0225d42195aac14c8289aeab
Gilles Caulier <caulier.gilles@gmail.com>
2008-04-11 09:37:17 +0000

    fix broken signal
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=795740

--- digikam/imagepreviewview.cpp
+++ digikam/imagepreviewview.cpp
@@ -572,8 +572,8 @@ void ImagePreviewView::slotCornerButtonPressed()
     pan->setRegionSelection(r);
     pan->setMouseFocus();
 
-    connect(pan, SIGNAL(signalSelectionMoved(QRect, bool)),
-            this, SLOT(slotPanIconSelectionMoved(QRect, bool)));
+    connect(pan, SIGNAL(signalSelectionMoved(const QRect&, bool)),
+            this, SLOT(slotPanIconSelectionMoved(const QRect&, bool)));
 
     connect(pan, SIGNAL(signalHiden()),
             this, SLOT(slotPanIconHiden()));
