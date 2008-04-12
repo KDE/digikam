06d43a5985df17b865dbe511fc55274f5ff40b97
Gilles Caulier <caulier.gilles@gmail.com>
2008-04-12 21:04:04 +0000

    move LensFun library to root digiKam album
    svn path=/trunk/extragear/graphics/digikam/; revision=796155

--- config-digikam.h.cmake
+++ config-digikam.h.cmake
@@ -3,3 +3,6 @@
 
 /* Define to 1 if you have Marble Widget shared library installed */
 #cmakedefine HAVE_MARBLEWIDGET 1
+
+/* Define to 1 if you have LensFun shared library installed */
+#cmakedefine HAVE_LENSFUN 1
