f873a1a04d4a9468b5ad024b7314489581ca7dfd
Gilles Caulier <caulier.gilles@gmail.com>
2008-04-12 13:27:34 +0000

    use const ref
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=796073

--- utilities/imageeditor/canvas/canvas.cpp
+++ utilities/imageeditor/canvas/canvas.cpp
@@ -7,7 +7,7 @@
  * Description : image editor canvas management class
  * 
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -1282,7 +1282,7 @@ void Canvas::slotPanIconHiden()
     d->cornerButton->blockSignals(false);
 }
 
-void Canvas::slotPanIconSelectionMoved(QRect r, bool b)
+void Canvas::slotPanIconSelectionMoved(const QRect& r, bool b)
 {
     setContentsPos((int)(r.x()*d->zoom), (int)(r.y()*d->zoom));
 
@@ -1330,4 +1330,3 @@ void Canvas::slotSelectNone()
 }
 
 }  // namespace Digikam
-
--- utilities/imageeditor/canvas/canvas.h
+++ utilities/imageeditor/canvas/canvas.h
@@ -7,7 +7,7 @@
  * Description : image editor canvas management class
  *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -190,7 +190,7 @@ private slots:
     void slotImageSaved(const QString& filePath, bool success);
     void slotCornerButtonPressed();
     void slotZoomChanged(double);
-    void slotPanIconSelectionMoved(QRect, bool);
+    void slotPanIconSelectionMoved(const QRect&, bool);
     void slotPanIconHiden();
 
 private:
