8326c36d10490d812f1d667ea303a7418354aa91
Albert Astals Cid <tsdgeos@terra.es>
2008-04-28 18:52:40 +0000

    merge *rc and *ui extracting because at the moment there's no *ui and it is failing, but if i just remove it it will probably be forgotten to be readded when some *ui is added
    svn path=/trunk/extragear/graphics/digikam/; revision=802178

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 $PREPARETIPS > tips.cpp
-$EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.h -o -name \*.cpp` -o $podir/digikam.pot
 rm -f tips.cpp
 rm -f rc.cpp
