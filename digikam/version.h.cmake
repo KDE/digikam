19a9e421edc94eeb4c3234020990ba25add90d35
Gilles Caulier <caulier.gilles@gmail.com>
2008-05-14 10:44:13 +0000

    compile
    svn path=/trunk/extragear/graphics/digikam/; revision=807675

--- digikam/version.h.cmake
+++ digikam/version.h.cmake
@@ -45,7 +45,7 @@ static inline const QString digiKamVersion()
 
     QString digiKamVer  = QString(digikam_version);
     if (!svnVer.isEmpty() && !svnVer.startsWith("unknow") && !svnVer.startsWith("export"))
-        digiKamVer = i18nc("%1 is digiKam version, %2 is the svn revision", "%1 (rev.: %1)", digikam_version, svnVer));
+        digiKamVer = i18nc("%1 is digiKam version, %2 is the svn revision", "%1 (rev.: %1)", digikam_version, svnVer);
 
     return digiKamVer;
 }
