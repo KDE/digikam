001ddd266564bb0f3a57278b42c5f26bf9fd81bd
Gilles Caulier <caulier.gilles@gmail.com>
2008-05-14 13:49:23 +0000

    to be sure than all compile everywhere
    svn path=/trunk/extragear/graphics/digikam/; revision=807709

--- libs/jpegutils/jpegutils.h
+++ libs/jpegutils/jpegutils.h
@@ -5,7 +5,7 @@
  *
  * Date        : 2004-09-29
  * Description : perform lossless rotation/flip to JPEG file
- * 
+ *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
  * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
@@ -14,12 +14,12 @@
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef JPEGUTILS_H
@@ -32,12 +32,14 @@
 
 // Local includes
 
+#include "digikam_export.h"
+
 namespace Digikam
 {
 
 DIGIKAM_EXPORT bool loadJPEGScaled(QImage& image, const QString& path, int maximumSize);
 DIGIKAM_EXPORT bool exifRotate(const QString& file, const QString& documentName);
-DIGIKAM_EXPORT bool jpegConvert(const QString& src, const QString& dest, const QString& documentName, 
+DIGIKAM_EXPORT bool jpegConvert(const QString& src, const QString& dest, const QString& documentName,
                                 const QString& format=QString("PNG"));
 DIGIKAM_EXPORT bool isJpegImage(const QString& file);
 
