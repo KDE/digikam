2e8d4ddb4015242ac729f706e0c8971c2bcb4568
Gilles Caulier <caulier.gilles@gmail.com>
2008-05-15 11:34:45 +0000

    update
    svn path=/trunk/extragear/graphics/digikam/; revision=808002

--- digikam.lsm.cmake
+++ digikam.lsm.cmake
@@ -2,7 +2,7 @@ Begin3
 Title:          digiKam
 Version:        ${DIGIKAM_VERSION_STRING}
 Entered-date:   2008-08-01
-Description:    KDE Photo Management
+Description:    Manage your photographs like a professional with the power of open source
 Keywords:       Digital Still Camera, KDE, Graphics, Image Viewer, Image Editor, Light Table, Time Line, Digital Camera Interface, Gphoto, Digital Photo Management, Search, Rating, Tags, Comments, Exif, Iptc, Makenotes, Xmp, Geolocation, Batch process, Raw workflow 
 Author:         Gilles Caulier <caulier dot gilles at gmail dot com>, Marcel Wiesweg <marcel dot wiesweg at gmx dot de>, Francisco J. Cruz <fj dot cruz at supercable dot es>, Joern Ahrens <joern dot ahrens at kdemail dot net>, Tom Albers <tomalbers at kde.nl>, Renchi Raju <renchi at pooh.tam.uiuc.edu>, Ralf Holzer <kde at ralfhoelzer.com>
 Home-page:      http://www.digikam.org
