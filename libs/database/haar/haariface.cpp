591faca3bc149b03aac923cc88663c4f7f127143
Gilles Caulier <caulier.gilles@gmail.com>
2008-05-17 08:40:52 +0000

    make public method. give credits to Marcel of course (:=)))
    svn path=/trunk/extragear/graphics/digikam/; revision=808641

--- libs/database/haar/haariface.cpp
+++ libs/database/haar/haariface.cpp
@@ -8,6 +8,7 @@
  *
  * Copyright (C) 2003 by Ricardo Niederberger Cabral <nieder at mail dot ru>
  * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -69,12 +70,10 @@ static const float s_haar_weights[2][6][3] =
         { 0.32,  0.07,  0.38 }}
 };
 
-
-
 HaarIface::HaarIface()
 {
     m_data = new Haar::ImageData();
-    m_bin = new Haar::WeightBin();
+    m_bin  = new Haar::WeightBin();
 }
 
 HaarIface::~HaarIface()
--- libs/database/haar/haariface.h
+++ libs/database/haar/haariface.h
@@ -8,6 +8,7 @@
  *
  * Copyright (C) 2003 by Ricardo Niederberger Cabral <nieder at mail dot ru>
  * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -48,6 +49,9 @@ public:
 
     HaarIface();
     ~HaarIface();
+
+    bool addImage(const QString& filename);
+    bool addImage(const QImage& image);
 
 private:
 
@@ -121,9 +125,6 @@ private:
 
     QImage      loadQImage(const QString &filename);
     void        fillPixelData(const QImage &image, Haar::ImageData *data);
-
-    bool        addImage(const QString& filename);
-    bool        addImage(const QImage& image);
 
     void        queryImgData(Haar::Idx* sig1, Haar::Idx* sig2, Haar::Idx* sig3,
                              double* avgl, int numres, int sketch);
