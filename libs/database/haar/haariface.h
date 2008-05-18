744a0e4f3b6e2d8471de6f8de28447293a111c66
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2008-05-18 20:23:31 +0000

    Remove internal definitions, create public API
    svn path=/trunk/extragear/graphics/digikam/; revision=809384

--- libs/database/haar/haariface.h
+++ libs/database/haar/haariface.h
@@ -39,9 +39,15 @@
 
 #include "haar.h"
 
+class QImage;
+
 namespace Digikam
 {
 
+class DImage;
+
+class HaarIfacePriv;
+class SignatureData;
 class HaarIface
 {
 
@@ -50,86 +56,33 @@ public:
     HaarIface();
     ~HaarIface();
 
-    bool addImage(const QString& filename);
-    bool addImage(const QImage& image);
+    static int preferredSize();
 
-private:
+    bool indexImage(const QString& filename);
+    bool indexImage(const QString& filename, const QImage &image);
+    bool indexImage(const QString& filename, const DImg &image);
+    bool indexImage(qlonglong imageid, const QImage &image);
+    bool indexImage(qlonglong imageid, const DImg &image);
 
-    /** signature structure 
-    */
-    typedef struct sigStruct_
+    enum SketchType
     {
-        long int  id;               // picture id
-        Haar::Idx sig1[HAAR_NUM_COEFS];  // Y positions with largest magnitude
-        Haar::Idx sig2[HAAR_NUM_COEFS];  // I positions with largest magnitude
-        Haar::Idx sig3[HAAR_NUM_COEFS];  // Q positions with largest magnitude
-        double    avgl[3];          // YIQ for position [0,0]
-        double    score;            // used when doing queries
-
-        // image properties extracted when opened for the first time
-        int       width;            // in pixels
-        int       height;           // in pixels
-
-        bool operator< (const sigStruct_& right) const 
-        {
-            return score < (right.score);
-        }
-    } sigStruct;
-
-    struct cmpf
-    {
-        bool operator()(const long int s1, const long int s2) const
-        {
-            return s1<s2;
-        }
+        ScannedSketch = 0,
+        PaintedSketch = 1
     };
 
-    typedef std::map<const long int, sigStruct*, cmpf>::iterator sigIterator;
-    typedef std::list<long int>                                  long_list;
-    typedef long_list::iterator                                  long_listIterator;
-    typedef std::priority_queue<sigStruct>                       priqueue;
-    typedef std::list<long_list>                                 long_list_2;       // a list of lists
-    typedef std::map<const long int, sigStruct*, cmpf>           sigMap;
-
-    sigMap        m_sigs;
-
-    unsigned char m_imgBin[16384];
-
-    /** Lists of picture ids, indexed by [color-channel][sign][position], i.e.,
-        R=0/G=1/B=2, pos=0/neg=1, (i*HAAR_NUM_PIXELS+j)
-    */
-    long_list     m_imgbuckets[3][2][16384];
-
-    /** results priority queue; largest at top 
-    */
-    priqueue      m_pqResults;
-
-    /** current result waiting to be returned 
-    */
-    sigStruct     m_curResult;
-
-    /** number of results found 
-    */
-    int           m_numres;
-
 private:
 
-    Haar::ImageData *m_data;
-    Haar::WeightBin *m_bin;
-
-    //void        initImgBin();
-    //void        freeSigs();
-
-    //int         getImageWidth(long int id);
-    //int         getImageHeight(long int id);
+    HaarIfacePriv *d;
 
     QImage      loadQImage(const QString &filename);
-    void        fillPixelData(const QImage &image, Haar::ImageData *data);
+    void        fillPixelData(const QImage &image);
+    void        fillPixelData(const DImg &image);
 
-    void        queryImgData(Haar::Idx* sig1, Haar::Idx* sig2, Haar::Idx* sig3,
-                             double* avgl, int numres, int sketch);
-    bool        queryImgFile(const QString& filename, int numres, int sketch);
+    bool indexImage(qlonglong imageid);
+    QList<qlonglong> bestMatches(SignatureData *data, int numberOfResults, SketchType type);
+    QList<qlonglong> bestMatchesForFile(const QString& filename, int numberOfResults, SketchType type);
 
+#if 0
     /*
     long_list   queryImgDataForThres(sigMap* tsigs, Haar::Idx* sig1, Haar::Idx* sig2, Haar::Idx* sig3,
                                      double* avgl, float thresd, int sketch);
@@ -146,7 +99,6 @@ private:
     //double      calcDiff(long int id1, long int id2);
 
     // TODO: Marcel, these methods can be removed.
-#if 0
     int         loadDB(char* filename);
     int         saveDB(char* filename);
     int         resetDB();
