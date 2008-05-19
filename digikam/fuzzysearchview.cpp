df331832c6181339cd1acc04c4b1e2648dbecff7
Gilles Caulier <caulier.gilles@gmail.com>
2008-05-19 11:40:57 +0000

    digiKam from trunk : new left sidebar tab to perform Fuzzy search. Not yet suitable
    svn path=/trunk/extragear/graphics/digikam/; revision=809768

---
+++ digikam/fuzzysearchview.cpp
@@ -0,0 +1,150 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2008-05-19
+ * Description : Fuzzy search sidebar tab contents.
+ *
+ * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt includes.
+
+#include <QFrame>
+#include <QLayout>
+#include <QPushButton>
+
+// KDE include.
+
+#include <khbox.h>
+#include <klocale.h>
+#include <kconfig.h>
+#include <kdialog.h>
+#include <kiconloader.h>
+#include <kapplication.h>
+#include <kstandarddirs.h>
+#include <kmessagebox.h>
+
+// Local includes.
+
+#include "album.h"
+#include "albummanager.h"
+#include "ddebug.h"
+#include "sketchwidget.h"
+#include "fuzzysearchview.h"
+#include "fuzzysearchview.moc"
+
+namespace Digikam
+{
+
+class FuzzySearchViewPriv
+{
+
+public:
+
+    FuzzySearchViewPriv()
+    {
+        sketchWidget = 0;
+    }
+
+    SketchWidget *sketchWidget;
+};
+
+FuzzySearchView::FuzzySearchView(QWidget *parent)
+               : QFrame(parent)
+{
+    d = new FuzzySearchViewPriv;
+    setAttribute(Qt::WA_DeleteOnClose);
+    setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
+    setLineWidth(1);
+
+    QGridLayout *grid = new QGridLayout(this);
+
+    // ---------------------------------------------------------------
+
+    d->sketchWidget = new SketchWidget(this);
+
+    // ---------------------------------------------------------------
+
+    grid->addWidget(d->sketchWidget, 0, 0, 1, 2);
+    grid->setRowStretch(1, 10);
+    grid->setMargin(KDialog::spacingHint());
+    grid->setSpacing(KDialog::spacingHint());
+}
+
+FuzzySearchView::~FuzzySearchView()
+{
+    writeConfig();
+    delete d;
+}
+
+void FuzzySearchView::readConfig()
+{
+/*    KSharedConfig::Ptr config = KGlobal::config();
+    KConfigGroup group        = config->group(QString("TimeLine SideBar"));
+
+    d->timeUnitCB->setCurrentIndex(group.readEntry("Histogram TimeUnit", (int)TimeLineWidget::Month));
+    slotTimeUnitChanged(d->timeUnitCB->currentIndex());
+
+    int id = group.readEntry("Histogram Scale", (int)TimeLineWidget::LinScale);
+    if ( d->scaleBG->button( id ) )
+       d->scaleBG->button( id )->setChecked(true);
+    slotScaleChanged(d->scaleBG->checkedId());
+
+    QDateTime now = QDateTime::currentDateTime();
+    d->timeLineWidget->setCursorDateTime(group.readEntry("Cursor Position", now));
+    d->timeLineWidget->setCurrentIndex(d->timeLineWidget->indexForCursorDateTime());*/
+}
+
+void FuzzySearchView::writeConfig()
+{
+/*
+    KSharedConfig::Ptr config = KGlobal::config();
+    KConfigGroup group        = config->group(QString("TimeLine SideBar"));
+    group.writeEntry("Histogram TimeUnit", d->timeUnitCB->currentIndex());
+    group.writeEntry("Histogram Scale", d->scaleBG->checkedId());
+    group.writeEntry("Cursor Position", d->timeLineWidget->cursorDateTime());
+    group.sync();*/
+}
+
+void FuzzySearchView::setActive(bool val)
+{
+/*    if (d->timeLineFolderView->selectedItem()) 
+    {
+        d->timeLineFolderView->setActive(val);
+    }
+    else if (val)
+    {
+        int totalCount = 0;
+        DateRangeList list = d->timeLineWidget->selectedDateRange(totalCount);
+        if (list.isEmpty())
+        {
+            AlbumManager::instance()->setCurrentAlbum(0);
+        }
+        else
+        {
+            AlbumList sList = AlbumManager::instance()->allSAlbums();
+            for (AlbumList::iterator it = sList.begin(); it != sList.end(); ++it)
+            {
+                SAlbum* salbum = (SAlbum*)(*it);
+                if (salbum->title() == d->timeLineFolderView->currentTimeLineSearchName())
+                    AlbumManager::instance()->setCurrentAlbum(salbum);
+            }
+        }
+    }*/
+}
+
+}  // NameSpace Digikam
