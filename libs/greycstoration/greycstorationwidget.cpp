143e8081f4f52a1c211c1f053dfa8edfda305d1b
Gilles Caulier <caulier.gilles@gmail.com>
2008-06-06 04:16:05 +0000

    typo
    svn path=/trunk/extragear/graphics/digikam/; revision=817459

--- libs/greycstoration/greycstorationwidget.cpp
+++ libs/greycstoration/greycstorationwidget.cpp
@@ -6,7 +6,7 @@
  * Date        : 2007-09-13
  * Description : Greycstoration settings widgets
  *
- * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -165,8 +165,7 @@ GreycstorationWidget::GreycstorationWidget(QTabWidget *parent)
     d->iterationInput = new KIntNumInput(d->generalPage);
     d->iterationInput->setRange(1, 5000, 1);
     d->iterationInput->setSliderEnabled(true);
-    d->iterationInput->setWhatsThis( i18n("<p>Sets the number of times the filter is applied to the "
-                                             "the image."));
+    d->iterationInput->setWhatsThis( i18n("<p>Sets the number of times the filter is applied to the image."));
 
     d->alphaLabel = new QLabel(i18n("Noise:"), d->generalPage);
     d->alphaInput = new KDoubleNumInput(d->generalPage);
