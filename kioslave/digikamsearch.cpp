d6228d5b049c45967c90f5f5698745ae2648908c
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2008-06-14 17:02:36 +0000

    Support rebuilding the Duplicate search albums from the ioslave. Necessary information is passed via KIO metadata.
    svn path=/trunk/extragear/graphics/digikam/; revision=820533

--- kioslave/digikamsearch.cpp
+++ kioslave/digikamsearch.cpp
@@ -43,6 +43,7 @@
 #include "databaseaccess.h"
 #include "databaseurl.h"
 #include "albumdb.h"
+#include "haariface.h"
 #include "digikamsearch.h"
 
 kio_digikamsearch::kio_digikamsearch(const QByteArray &pool_socket,
@@ -57,6 +58,8 @@ kio_digikamsearch::~kio_digikamsearch()
 
 void kio_digikamsearch::special(const QByteArray& data)
 {
+    bool duplicates = (metaData("duplicates") == "true");
+
     KUrl    kurl;
     int     listingType = 0;
 
@@ -67,32 +70,88 @@ void kio_digikamsearch::special(const QByteArray& data)
 
     kDebug() << "kio_digikamsearch::special " << kurl;
 
-    Digikam::DatabaseUrl dbUrl(kurl);
-    Digikam::DatabaseAccess::setParameters(dbUrl);
-
-    int id = dbUrl.searchId();
-    Digikam::SearchInfo info = Digikam::DatabaseAccess().db()->getSearchInfo(id);
-
-    Digikam::ImageLister lister;
-
-    if (listingType == 0)
+    if (!duplicates)
     {
-        // send data every 200 images to be more responsive
-        Digikam::ImageListerSlaveBasePartsSendingReceiver receiver(this, 200);
-        if (info.type == Digikam::DatabaseSearch::HaarSearch)
-            lister.listHaarSearch(&receiver, info.query);
+        Digikam::DatabaseUrl dbUrl(kurl);
+        Digikam::DatabaseAccess::setParameters(dbUrl);
+
+        int id = dbUrl.searchId();
+        Digikam::SearchInfo info = Digikam::DatabaseAccess().db()->getSearchInfo(id);
+
+        Digikam::ImageLister lister;
+
+        if (listingType == 0)
+        {
+            // send data every 200 images to be more responsive
+            Digikam::ImageListerSlaveBasePartsSendingReceiver receiver(this, 200);
+            if (info.type == Digikam::DatabaseSearch::HaarSearch)
+                lister.listHaarSearch(&receiver, info.query);
+            else
+                lister.listSearch(&receiver, info.query);
+            if (!receiver.hasError)
+                receiver.sendData();
+        }
         else
-            lister.listSearch(&receiver, info.query);
-        if (!receiver.hasError)
-            receiver.sendData();
+        {
+            Digikam::ImageListerSlaveBaseReceiver receiver(this);
+            // fast mode: limit results to 500
+            lister.listSearch(&receiver, info.query, 500);
+            if (!receiver.hasError)
+                receiver.sendData();
+        }
     }
     else
     {
-        Digikam::ImageListerSlaveBaseReceiver receiver(this);
-        // fast mode: limit results to 500
-        lister.listSearch(&receiver, info.query, 500);
-        if (!receiver.hasError)
-            receiver.sendData();
+        QString idsString = metaData("albumids");
+        QString thresholdString = metaData("threshold");
+
+        // get albums to scan
+        QStringList idsStringList = idsString.split(",");
+        QList<int> albumIds;
+        foreach(const QString &idString, idsStringList)
+        {
+            bool ok;
+            int albumId = idString.toInt(&ok);
+            if (ok)
+                albumIds << albumId;
+        }
+
+        if (albumIds.isEmpty())
+        {
+            kDebug() << "No album ids passed for duplicates search";
+            error(KIO::ERR_INTERNAL, "No album ids passed");
+            return;
+        }
+
+        // get info about threshold
+        bool ok;
+        double threshold = thresholdString.toDouble(&ok);
+        if (!ok)
+            threshold = 0.4;
+
+        // route progress info to KIOSlave facilities
+        class DuplicatesProgressObserver : public Digikam::HaarProgressObserver
+        {
+            public:
+                DuplicatesProgressObserver(KIO::SlaveBase *slave) : m_slave(slave) {}
+
+                virtual void totalNumberToScan(int number)
+                {
+                    m_slave->totalSize(number);
+                }
+                virtual void processedNumber(int number)
+                {
+                    m_slave->processedSize(number);
+                }
+
+            private:
+                KIO::SlaveBase *m_slave;
+        };
+        DuplicatesProgressObserver observer(this);
+
+        // rebuild the duplicate albums
+        Digikam::HaarIface iface;
+        iface.rebuildDuplicatesAlbums(albumIds, threshold, &observer);
     }
 
     finished();
