a0f84693c501212c02a7895093bdd7797a7b06bf
Gilles Caulier <caulier.gilles@gmail.com>
2008-06-18 06:45:13 +0000

    Fix First run dialog message
    svn path=/trunk/extragear/graphics/digikam/; revision=821683

--- digikam/firstrun.h
+++ digikam/firstrun.h
@@ -6,14 +6,14 @@
  * Date        : 2006-04-25
  * Description : a widget to use in first run dialog
  *
- * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -44,19 +44,15 @@ class DIGIKAM_EXPORT FirstRunWidget : public QWidget
     Q_OBJECT
 
 public:
-    
-    FirstRunWidget( QWidget* parent = 0 );
+
+    FirstRunWidget(QWidget* parent = 0);
     ~FirstRunWidget();
 
 public:
-    
+
     QLabel        *m_pixLabel;
     KUrlRequester *m_path;
-    
-protected slots:
-    
-    virtual void languageChange();
-    
+
 private:
 
     QLabel *m_textLabel1;
