0a31a9c9e9e36f41b5216168332aa990289d86f5
Gilles Caulier <caulier.gilles@gmail.com>
2008-07-10 14:57:18 +0000

    fix gcc 4.3.1 warnings
    svn path=/trunk/extragear/graphics/digikam/; revision=830464

--- imageplugins/hotpixels/hotpixelfixer.cpp
+++ imageplugins/hotpixels/hotpixelfixer.cpp
@@ -5,25 +5,25 @@
  *
  * Date        : 2005-03-27
  * Description : Threaded image filter to fix hot pixels
- * 
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2005-2006 by Unai Garro <ugarro at users dot sourceforge dot net>
- * 
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // C++ includes. 
- 
+
 #include <cmath>
 #include <cstdlib>
 
@@ -63,7 +63,7 @@ HotPixelFixer::HotPixelFixer(Digikam::DImg *orgImage, QObject *parent, const QLi
     m_hpList              = hpList;
     m_interpolationMethod = interpolationMethod;
     mWeightList.clear();
-    
+
     initFilter();
 }
 
@@ -71,7 +71,7 @@ HotPixelFixer::~HotPixelFixer()
 {
 }
 
-void HotPixelFixer::filterImage(void)
+void HotPixelFixer::filterImage()
 {
     QList <HotPixel>::ConstIterator it;
     QList <HotPixel>::ConstIterator end(m_hpList.end()); 
@@ -80,7 +80,7 @@ void HotPixelFixer::filterImage(void)
         HotPixel hp = *it;
         interpolate(m_orgImage, hp, m_interpolationMethod);
     }
-        
+
     m_destImage = m_orgImage;
 }
 
@@ -90,7 +90,7 @@ void HotPixelFixer::interpolate (Digikam::DImg &img, HotPixel &hp, int method)
     const int xPos = hp.x();
     const int yPos = hp.y();
     bool sixtBits  = img.sixteenBit();
-    
+
     // Interpolate pixel.
     switch (method)
     {
@@ -98,14 +98,14 @@ void HotPixelFixer::interpolate (Digikam::DImg &img, HotPixel &hp, int method)
         {
             // We implement the bidimendional one first.
             // TODO: implement the rest of directions (V & H) here
-        
+
             //case twodim:
             // {
             int sum_weight = 0;
             double vr = 0.0, vg = 0.0, vb = 0.0;
             int x, y;
             Digikam::DColor col;
-        
+
             for (x = xPos; x < xPos+hp.width(); ++x)
             {
                 if (validPoint(img,QPoint(x,yPos-1)))
@@ -125,7 +125,7 @@ void HotPixelFixer::interpolate (Digikam::DImg &img, HotPixel &hp, int method)
                     ++sum_weight;
                 }
             }
-        
+
             for (y = yPos; y < hp.height(); ++y)
             {
                 if (validPoint(img,QPoint(xPos-1,y)))
@@ -145,14 +145,13 @@ void HotPixelFixer::interpolate (Digikam::DImg &img, HotPixel &hp, int method)
                     ++sum_weight;
                 }
             }
-        
+
             if (sum_weight > 0)
             {
                 vr /= (double)sum_weight;
                 vg /= (double)sum_weight;
                 vb /= (double)sum_weight;
-    
-                
+
                 for (x = 0; x < hp.width(); ++x)
                 for (y = 0; y < hp.height(); ++y)
                 if (validPoint(img,QPoint(xPos+x,yPos+y)))
@@ -164,32 +163,32 @@ void HotPixelFixer::interpolate (Digikam::DImg &img, HotPixel &hp, int method)
             }
             break;
         }
-    
+
         case LINEAR_INTERPOLATION:
             weightPixels(img, hp, LINEAR_INTERPOLATION, TWODIM_DIRECTION, sixtBits ? 65535: 255);
             break;
-    
+
         case QUADRATIC_INTERPOLATION:
             weightPixels(img, hp, QUADRATIC_INTERPOLATION, TWODIM_DIRECTION, sixtBits ? 65535 : 255);
             break;
-    
+
         case CUBIC_INTERPOLATION:
             weightPixels(img, hp, CUBIC_INTERPOLATION, TWODIM_DIRECTION, sixtBits ? 65535 : 255);
             break;
-    } 
+    }
 }
 
 void HotPixelFixer::weightPixels (Digikam::DImg &img, HotPixel &px, int method, Direction dir, int maxComponent)
 {
     //TODO: implement direction here too
-        
+
     for (int iComp = 0; iComp < 3; ++iComp)
     {
         // Obtain weight data block.  
-    
+
         Weights w;
         int polynomeOrder=-1;
-        
+
         switch (method)
         {
             case AVERAGE_INTERPOLATION:  // Gilles: to prevent warnings from compiler.
@@ -202,30 +201,30 @@ void HotPixelFixer::weightPixels (Digikam::DImg &img, HotPixel &px, int method, 
                 break;
             case CUBIC_INTERPOLATION:
                 polynomeOrder=3;
-                break;        
+                break;
         }
         if (polynomeOrder<0) return;
-        
+
         // In the one-dimensional case, the width must be 1,
         // and the size must be stored in height 
-        
+
         w.setWidth(dir == TWODIM_DIRECTION ? px.width() : 1);
         w.setHeight(dir == HORIZONTAL_DIRECTION ? px.width() : px.height());
         w.setPolynomeOrder(polynomeOrder);
         w.setTwoDim(dir == TWODIM_DIRECTION);
-    
+
         //TODO: check this, it must not recalculate existing calculated weights
         //for now I don't think it is finding the duplicates fine, so it uses
         //the previous one always...
-        
+
         //if (mWeightList.find(w)==mWeightList.end())
         //{
             w.calculateWeights();
-            
+
         //    mWeightList.append(w);
-            
+
         //}
-     
+
         // Calculate weighted pixel sum.  
         for (int y = 0; y<px.height(); ++y)
         {
@@ -235,15 +234,15 @@ void HotPixelFixer::weightPixels (Digikam::DImg &img, HotPixel &px, int method, 
                 {
                     double sum_weight = 0.0, v = 0.0;
                     size_t i;
-            
-                    for (i = 0; i < w.positions().count(); ++i)
+
+                    for (i = 0; i < (size_t)w.positions().count(); ++i)
                     {
                         // In the one-dimensional case, only the y coordinate is used.  
                         const int xx = px.x()+(dir == VERTICAL_DIRECTION ? x : 
                                   dir== HORIZONTAL_DIRECTION ? w.positions()[i].y() : w.positions()[i].x());
                         const int yy = px.y()+(dir == HORIZONTAL_DIRECTION ? y : 
                                   w.positions()[i].y());
-            
+
                         if (validPoint (img,QPoint(xx, yy)))
                         {
                             //TODO: check this. I think it is broken
@@ -260,15 +259,15 @@ void HotPixelFixer::weightPixels (Digikam::DImg &img, HotPixel &px, int method, 
                             {
                                 weight=w[i][y][x];
                             }
-                                    
+
                             if (iComp==0) v += weight * img.getPixelColor(xx, yy).red();
                             else if (iComp==1) v += weight * img.getPixelColor(xx, yy).green();
                             else v += weight * img.getPixelColor(xx, yy).blue();
-                            
+
                             sum_weight += weight;
                         }
-                    } 
-                        
+                    }
+
                     Digikam::DColor color=img.getPixelColor(px.x()+x,px.y()+y);
                     int component;
                     if (fabs (v) <= DBL_MIN)
@@ -284,15 +283,15 @@ void HotPixelFixer::weightPixels (Digikam::DImg &img, HotPixel &px, int method, 
                         component=maxComponent;
                     else
                         component=0;
-                    
+
                     if (iComp==0) color.setRed(component);
                     else if (iComp==1) color.setGreen(component);
                     else color.setBlue(component);
 
-                    
+
                     img.setPixelColor(px.x()+x,px.y()+y,color);
-                } 
-            } 
+                }
+            }
         }
     }
 }
--- imageplugins/hotpixels/hotpixelfixer.h
+++ imageplugins/hotpixels/hotpixelfixer.h
@@ -5,10 +5,10 @@
  *
  * Date        : 2005-03-27
  * Description : Threaded image filter to fix hot pixels
- * 
+ *
  * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2005-2006 by Unai Garro <ugarro at users dot sourceforge dot net>
- * 
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
@@ -19,7 +19,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef HOTPIXELFIXER_H
@@ -49,7 +49,7 @@ class HotPixelFixer : public Digikam::DImgThreadedFilter
 {
 
 public:
-    
+
     enum InterpolationMethod 
     {
         AVERAGE_INTERPOLATION   = 0,
@@ -57,39 +57,39 @@ public:
         QUADRATIC_INTERPOLATION = 2,
         CUBIC_INTERPOLATION     = 3
     };
-    
+
     enum Direction
     {
         TWODIM_DIRECTION     = 0,
         VERTICAL_DIRECTION   = 1,
-        HORIZONTAL_DIRECTION = 2    
+        HORIZONTAL_DIRECTION = 2
     };
 
 public:
-        
+
     HotPixelFixer(Digikam::DImg *orgImage, QObject *parent, 
                   const QList<HotPixel>& hpList, int interpolationMethod);
     ~HotPixelFixer();
 
 private: 
 
-    virtual void filterImage(void);
-    
+    virtual void filterImage();
+
     void interpolate (Digikam::DImg &img,HotPixel &hp, int method);
     void weightPixels (Digikam::DImg &img, HotPixel &px, int method, 
                        Direction dir, int maxComponent);
-    
+
     inline bool validPoint(Digikam::DImg &img, QPoint p)
     {
         return (p.x()>=0 && p.y()>=0 && p.x()<(long) img.width() && p.y()<(long) img.height());
     };
-    
+
     QList <Weights> mWeightList;
 
 private: 
 
     int             m_interpolationMethod;
-       
+
     QList<HotPixel> m_hpList;
 };
 
