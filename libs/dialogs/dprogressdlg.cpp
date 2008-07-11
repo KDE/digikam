715fb0e624028d9a6ea9a99240249dee44cc5299
Gilles Caulier <caulier.gilles@gmail.com>
2008-07-11 14:45:58 +0000

    revert. Marcel, there is a little problem when you play with Dprogress dialog at the startup with ScanController (disable Splash to see what i mean) The progress bar still to 100% during few second before to start to zero and show real progress. CCMAIL: marcel.wiesweg@gmx.de
    svn path=/trunk/extragear/graphics/digikam/; revision=830961

--- libs/dialogs/dprogressdlg.cpp
+++ libs/dialogs/dprogressdlg.cpp
@@ -5,7 +5,7 @@
  *
  * Date        : 2006-30-08
  * Description : a progress dialog for digiKam
- * 
+ *
  * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -13,12 +13,12 @@
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // Qt includes.
@@ -130,8 +130,6 @@ DProgressDlg::DProgressDlg(QWidget *parent, const QString &caption)
 
     connect(this, SIGNAL(cancelClicked()),
             this, SLOT(slotCancel()));
-            
-    reset();
 }
 
 DProgressDlg::~DProgressDlg()
