d75b9f55b2d0b70ac62303fc69f6691988df0884
Gilles Caulier <caulier.gilles@gmail.com>
2008-07-28 11:34:10 +0000

    Set SHIFT+CTRL+T shortcut to InsertText to prevent conflict with image editor thumbbar shortcut
    svn path=/trunk/extragear/graphics/digikam/; revision=838640

--- imageplugins/inserttext/imageplugin_inserttext.cpp
+++ imageplugins/inserttext/imageplugin_inserttext.cpp
@@ -5,24 +5,24 @@
  *
  * Date        : 2005-02-14
  * Description : a plugin to insert a text over an image.
- * 
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * 
+ *
+ * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // KDE includes.
-  
+
 #include <klocale.h>
 #include <kgenericfactory.h>
 #include <klibloader.h>
@@ -44,7 +44,7 @@ ImagePlugin_InsertText::ImagePlugin_InsertText(QObject *parent, const QVariantLi
                       : Digikam::ImagePlugin(parent, "ImagePlugin_InsertText")
 {
     m_insertTextAction = new KAction(KIcon("insert-text"), i18n("Insert Text..."), this);
-    m_insertTextAction->setShortcut(QKeySequence(Qt::CTRL+Qt::Key_T));
+    m_insertTextAction->setShortcut(QKeySequence(Qt::SHIFT+Qt::CTRL+Qt::Key_T));
 
     connect(m_insertTextAction, SIGNAL(triggered(bool) ),
             this, SLOT(slotInsertText()));
@@ -52,7 +52,7 @@ ImagePlugin_InsertText::ImagePlugin_InsertText(QObject *parent, const QVariantLi
     actionCollection()->addAction("imageplugin_inserttext", m_insertTextAction );
 
     setXMLFile("digikamimageplugin_inserttext_ui.rc");
-    
+
     DDebug() << "ImagePlugin_InsertText plugin loaded" << endl;
 }
 
