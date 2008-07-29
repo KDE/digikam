1aa2b1cd8167d053c79d1bbe91a3abefee8a58e5
Gilles Caulier <caulier.gilles@gmail.com>
2008-07-29 14:07:37 +0000

    Bug fix: prevent crash with JPEG200 loader if an exception is generated by Jasper toolkit.
    svn path=/trunk/extragear/graphics/digikam/; revision=839206

--- libs/dimg/loaders/jp2kloader.h
+++ libs/dimg/loaders/jp2kloader.h
@@ -5,15 +5,15 @@
  *
  * Date        : 2006-06-14
  * Description : A JPEG2000 IO file for DImg framework
- * 
- * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com> 
+ *
+ * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -26,7 +26,7 @@
 
 // C ansi includes.
 
-extern "C" 
+extern "C"
 {
 #include <jasper/jasper.h>
 }
@@ -46,7 +46,7 @@ class DIGIKAM_EXPORT JP2KLoader : public DImgLoader
 public:
 
     JP2KLoader(DImg* image);
-    
+
     bool load(const QString& filePath, DImgLoaderObserver *observer);
     bool save(const QString& filePath, DImgLoaderObserver *observer);
 
