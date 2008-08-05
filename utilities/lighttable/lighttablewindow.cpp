16fd083815cc633443804f056ad885653a215715
Andi Clemens <andi.clemens@gmx.net>
2008-08-05 20:22:14 +0000

    backport commit #842714 from KDE4
    CCBUGS:167571
    
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=842717

--- utilities/lighttable/lighttablewindow.cpp
+++ utilities/lighttable/lighttablewindow.cpp
@@ -732,13 +732,13 @@ void LightTableWindow::slotItemSelected(ImageInfo* info)
         {
             if (!curr->prev())
             {
-                d->backwardAction->setEnabled(false);
+//                d->backwardAction->setEnabled(false);
                 d->firstAction->setEnabled(false);
             }
 
             if (!curr->next())
             {
-                d->forwardAction->setEnabled(false);
+//                d->forwardAction->setEnabled(false);
                 d->lastAction->setEnabled(false);
             }
 
@@ -1595,15 +1595,27 @@ void LightTableWindow::slotToggleOnSyncPreview(bool t)
 void LightTableWindow::slotBackward()
 {
     ThumbBarItem* curr = d->barView->currentItem();
-    if (curr && curr->prev())
-        d->barView->setSelected(curr->prev());
+    ThumbBarItem* last = d->barView->lastItem();
+    if (curr)
+    {
+        if (curr->prev())
+            d->barView->setSelected(curr->prev());
+        else
+            d->barView->setSelected(last);
+    }
 }
 
 void LightTableWindow::slotForward()
 {
     ThumbBarItem* curr = d->barView->currentItem();
-    if (curr && curr->next())
-        d->barView->setSelected(curr->next());
+    ThumbBarItem* first = d->barView->firstItem();
+    if (curr)
+    {
+        if (curr->next())
+            d->barView->setSelected(curr->next());
+        else
+            d->barView->setSelected(first);
+    }
 }
 
 void LightTableWindow::slotFirst()
