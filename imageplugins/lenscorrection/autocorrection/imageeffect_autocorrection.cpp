ff1c9cc36cda739e64b2ed2cb96ed338e1e8acc5
Andi Clemens <andi.clemens@gmx.net>
2008-08-17 14:33:26 +0000

    compile
    svn path=/trunk/extragear/graphics/digikam/; revision=848374

--- imageplugins/lenscorrection/autocorrection/imageeffect_autocorrection.cpp
+++ imageplugins/lenscorrection/autocorrection/imageeffect_autocorrection.cpp
@@ -97,7 +97,7 @@ ImageEffect_AutoCorrection::ImageEffect_AutoCorrection(QWidget* parent)
     // -------------------------------------------------------------
 
     m_showGrid   = new QCheckBox(i18n("Show grid"), m_settingsWidget);
-    m_showGrid->setWhatsThis(i18n("Set this option to visualize correction grid to be applied.";
+    m_showGrid->setWhatsThis(i18n("Set this option to visualize correction grid to be applied."));
 
     m_filterCCA  = new QCheckBox(i18n("Chromatic Aberration"), m_settingsWidget);
     m_filterCCA->setWhatsThis(i18n("Chromatic aberration is easily recognised as color fringes "
