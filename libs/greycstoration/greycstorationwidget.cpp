0530ae5cd9a4510b10d2731bdfd40d3c14a91efa
Andi Clemens <andi.clemens@gmx.net>
2008-08-19 07:29:21 +0000

    Continue to replace K*NumInput objects by R*NumInput objects...
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=849191

--- libs/greycstoration/greycstorationwidget.cpp
+++ libs/greycstoration/greycstorationwidget.cpp
@@ -35,12 +35,17 @@
 
 #include <kdialog.h>
 #include <klocale.h>
-#include <knuminput.h>
+
+// LibKDcraw includes.
+
+#include <libkdcraw/rnuminput.h>
 
 // Local includes.
 
 #include "greycstorationwidget.h"
 #include "greycstorationwidget.moc"
+
+using namespace KDcrawIface;
 
 namespace Digikam
 {
@@ -104,18 +109,18 @@ public:
 
     QTabWidget      *parent;
 
-    KDoubleNumInput *amplitudeInput;
-    KDoubleNumInput *sharpnessInput;
-    KDoubleNumInput *anisotropyInput;
-    KDoubleNumInput *alphaInput;
-    KDoubleNumInput *sigmaInput;
-    KDoubleNumInput *gaussianPrecInput;
-    KDoubleNumInput *dlInput;
-    KDoubleNumInput *daInput;
+    RDoubleNumInput *amplitudeInput;
+    RDoubleNumInput *sharpnessInput;
+    RDoubleNumInput *anisotropyInput;
+    RDoubleNumInput *alphaInput;
+    RDoubleNumInput *sigmaInput;
+    RDoubleNumInput *gaussianPrecInput;
+    RDoubleNumInput *dlInput;
+    RDoubleNumInput *daInput;
 
-    KIntNumInput    *iterationInput;
-    KIntNumInput    *tileInput;
-    KIntNumInput    *btileInput;    
+    RIntNumInput    *iterationInput;
+    RIntNumInput    *tileInput;
+    RIntNumInput    *btileInput;
 };
 
 GreycstorationWidget::GreycstorationWidget(QTabWidget *parent)
@@ -125,15 +130,15 @@ GreycstorationWidget::GreycstorationWidget(QTabWidget *parent)
     d->parent = parent;
 
     // -------------------------------------------------------------
-    
+
     d->generalPage     = new QWidget( parent );
     QGridLayout* grid1 = new QGridLayout(d->generalPage, 6, 2, KDialog::spacingHint());
     parent->addTab( d->generalPage, i18n("General") );
-    
+
     d->sharpnessLabel = new QLabel(i18n("Detail preservation:"), d->generalPage);
-    d->sharpnessInput = new KDoubleNumInput(d->generalPage);
+    d->sharpnessInput = new RDoubleNumInput(d->generalPage);
     d->sharpnessInput->setPrecision(2);
-    d->sharpnessInput->setRange(0.01, 1.0, 0.1, true);
+    d->sharpnessInput->setRange(0.01, 1.0, 0.1);
     QWhatsThis::add( d->sharpnessInput, i18n("<p>Preservation of details to set the sharpening level "
                                              "of the small features in the target image. "
                                              "Higher values leave details sharp."));
@@ -141,18 +146,18 @@ GreycstorationWidget::GreycstorationWidget(QTabWidget *parent)
     grid1->addMultiCellWidget(d->sharpnessInput, 0, 0, 1, 1);
 
     d->anisotropyLabel = new QLabel(i18n("Anisotropy:"), d->generalPage);
-    d->anisotropyInput = new KDoubleNumInput(d->generalPage);
+    d->anisotropyInput = new RDoubleNumInput(d->generalPage);
     d->anisotropyInput->setPrecision(2);
-    d->anisotropyInput->setRange(0.0, 1.0, 0.1, true);
+    d->anisotropyInput->setRange(0.0, 1.0, 0.1);
     QWhatsThis::add( d->anisotropyInput, i18n("<p>Anisotropic (directional) modifier of the details. "
                                               "Keep it small for Gaussian noise."));
     grid1->addMultiCellWidget(d->anisotropyLabel, 1, 1, 0, 0);
     grid1->addMultiCellWidget(d->anisotropyInput, 1, 1, 1, 1);
 
     d->amplitudeLabel = new QLabel(i18n("Smoothing:"), d->generalPage);
-    d->amplitudeInput = new KDoubleNumInput(d->generalPage);
+    d->amplitudeInput = new RDoubleNumInput(d->generalPage);
     d->amplitudeInput->setPrecision(2);
-    d->amplitudeInput->setRange(0.01, 500.0, 0.1, true);
+    d->amplitudeInput->setRange(0.01, 500.0, 0.1);
     QWhatsThis::add( d->amplitudeInput, i18n("<p>Total smoothing power: if the Detail Factor sets the relative "
                                              "smoothing and the Anisotropy Factor the direction, "
                                              "the Smoothing Factor sets the overall effect."));
@@ -160,77 +165,77 @@ GreycstorationWidget::GreycstorationWidget(QTabWidget *parent)
     grid1->addMultiCellWidget(d->amplitudeInput, 2, 2, 1, 1);
 
     d->sigmaLabel = new QLabel(i18n("Regularity:"), d->generalPage);
-    d->sigmaInput = new KDoubleNumInput(d->generalPage);
+    d->sigmaInput = new RDoubleNumInput(d->generalPage);
     d->sigmaInput->setPrecision(2);
-    d->sigmaInput->setRange(0.0, 10.0, 0.1, true);
+    d->sigmaInput->setRange(0.0, 10.0, 0.1);
     QWhatsThis::add( d->sigmaInput, i18n("<p>This value controls the evenness of smoothing to the image. "
                                          "Do not use a high value here, or the "
                                          "target image will be completely blurred."));
     grid1->addMultiCellWidget(d->sigmaLabel, 3, 3, 0, 0);
     grid1->addMultiCellWidget(d->sigmaInput, 3, 3, 1, 1);
-    
+
     d->iterationLabel = new QLabel(i18n("Iterations:"), d->generalPage);
-    d->iterationInput = new KIntNumInput(d->generalPage);
-    d->iterationInput->setRange(1, 5000, 1, true);
+    d->iterationInput = new RIntNumInput(d->generalPage);
+    d->iterationInput->setRange(1, 5000, 1);
     QWhatsThis::add( d->iterationInput, i18n("<p>Sets the number of times the filter is applied to "
                                              "the image."));
     grid1->addMultiCellWidget(d->iterationLabel, 4, 4, 0, 0);
     grid1->addMultiCellWidget(d->iterationInput, 4, 4, 1, 1);
 
     d->alphaLabel = new QLabel(i18n("Noise:"), d->generalPage);
-    d->alphaInput = new KDoubleNumInput(d->generalPage);
+    d->alphaInput = new RDoubleNumInput(d->generalPage);
     d->alphaInput->setPrecision(2);
-    d->alphaInput->setRange(0.01, 1.0, 0.1, true);
+    d->alphaInput->setRange(0.01, 1.0, 0.1);
     QWhatsThis::add( d->alphaInput, i18n("<p>Sets the noise scale."));
     grid1->addMultiCellWidget(d->alphaLabel, 5, 5, 0, 0);
     grid1->addMultiCellWidget(d->alphaInput, 5, 5, 1, 1);
     grid1->setRowStretch(6, 10);
-    
+
     // -------------------------------------------------------------
-    
+
     d->advancedPage    = new QWidget( parent );
     QGridLayout* grid2 = new QGridLayout(d->advancedPage, 6, 2, KDialog::spacingHint());
     parent->addTab( d->advancedPage, i18n("Advanced Settings") );
-    
+
     d->daLabel = new QLabel(i18n("Angular step:"), d->advancedPage);
-    d->daInput = new KDoubleNumInput(d->advancedPage);
+    d->daInput = new RDoubleNumInput(d->advancedPage);
     d->daInput->setPrecision(2);
-    d->daInput->setRange(0.0, 90.0, 1.0, true);
+    d->daInput->setRange(0.0, 90.0, 1.0);
     QWhatsThis::add( d->daInput, i18n("<p>Set here the angular integration step (in degrees) "
                                       "analogous to anisotropy."));
     grid2->addMultiCellWidget(d->daLabel, 0, 0, 0, 0);
     grid2->addMultiCellWidget(d->daInput, 0, 0, 1, 1);
 
     d->dlLabel = new QLabel(i18n("Integral step:"), d->advancedPage);
-    d->dlInput = new KDoubleNumInput(d->advancedPage);
+    d->dlInput = new RDoubleNumInput(d->advancedPage);
     d->dlInput->setPrecision(2);
-    d->dlInput->setRange(0.0, 1.0, 0.1, true);
+    d->dlInput->setRange(0.0, 1.0, 0.1);
     QWhatsThis::add( d->dlInput, i18n("<p>Set here the spatial integral step."));
     grid2->addMultiCellWidget(d->dlLabel, 1, 1, 0, 0);
     grid2->addMultiCellWidget(d->dlInput, 1, 1, 1, 1);
 
     d->gaussianPrecLabel = new QLabel(i18n("Gaussian:"), d->advancedPage);
-    d->gaussianPrecInput = new KDoubleNumInput(d->advancedPage);
+    d->gaussianPrecInput = new RDoubleNumInput(d->advancedPage);
     d->gaussianPrecInput->setPrecision(2);
-    d->gaussianPrecInput->setRange(0.01, 20.0, 0.01, true);
+    d->gaussianPrecInput->setRange(0.01, 20.0, 0.01);
     QWhatsThis::add( d->gaussianPrecInput, i18n("<p>Set here the precision of the Gaussian function."));
     grid2->addMultiCellWidget(d->gaussianPrecLabel, 2, 2, 0, 0);
     grid2->addMultiCellWidget(d->gaussianPrecInput, 2, 2, 1, 1);
 
     d->tileLabel = new QLabel(i18n("Tile size:"), d->advancedPage);
-    d->tileInput = new KIntNumInput(d->advancedPage);
-    d->tileInput->setRange(0, 2000, 1, true);
+    d->tileInput = new RIntNumInput(d->advancedPage);
+    d->tileInput->setRange(0, 2000, 1);
     QWhatsThis::add( d->tileInput, i18n("<p>Sets the tile size."));
     grid2->addMultiCellWidget(d->tileLabel, 3, 3, 0, 0);
     grid2->addMultiCellWidget(d->tileInput, 3, 3, 1, 1);
 
     d->btileLabel = new QLabel(i18n("Tile border:"), d->advancedPage);
-    d->btileInput = new KIntNumInput(d->advancedPage);
-    d->btileInput->setRange(1, 20, 1, true);
+    d->btileInput = new RIntNumInput(d->advancedPage);
+    d->btileInput->setRange(1, 20, 1);
     QWhatsThis::add( d->btileInput, i18n("<p>Sets the size of each tile border."));
     grid2->addMultiCellWidget(d->btileLabel, 4, 4, 0, 0);
     grid2->addMultiCellWidget(d->btileInput, 4, 4, 1, 1);
-    
+
     d->interpolationLabel = new QLabel(i18n("Interpolation:"), d->advancedPage);
     d->interpolationBox   = new QComboBox(false, d->advancedPage);
     d->interpolationBox->insertItem( i18n("Nearest Neighbor"), GreycstorationSettings::NearestNeighbor );
@@ -240,7 +245,7 @@ GreycstorationWidget::GreycstorationWidget(QTabWidget *parent)
                                                "desired image quality."));
     grid2->addMultiCellWidget(d->interpolationLabel, 5, 5, 0, 0);
     grid2->addMultiCellWidget(d->interpolationBox, 5, 5, 1, 1);
-    
+
     d->fastApproxCBox = new QCheckBox(i18n("Fast approximation"), d->advancedPage);
     QWhatsThis::add( d->fastApproxCBox, i18n("<p>Enable fast approximation when rendering images."));
     grid2->addMultiCellWidget(d->fastApproxCBox, 6, 6, 0, 1);
@@ -264,18 +269,18 @@ void GreycstorationWidget::setSettings(GreycstorationSettings settings)
     blockSignals(true);
     d->fastApproxCBox->setChecked(settings.fastApprox);
     d->interpolationBox->setCurrentItem(settings.interp);
-    d->amplitudeInput->setValue(settings.amplitude);
-    d->sharpnessInput->setValue(settings.sharpness);
-    d->anisotropyInput->setValue(settings.anisotropy);
-    d->alphaInput->setValue(settings.alpha);
-    d->sigmaInput->setValue(settings.sigma);
-    d->gaussianPrecInput->setValue(settings.gaussPrec);
-    d->dlInput->setValue(settings.dl);
-    d->daInput->setValue(settings.da);
-    d->iterationInput->setValue(settings.nbIter);
-    d->tileInput->setValue(settings.tile);
-    d->btileInput->setValue(settings.btile);
-    blockSignals(false);    
+    d->amplitudeInput->setDefaultValue(settings.amplitude);
+    d->sharpnessInput->setDefaultValue(settings.sharpness);
+    d->anisotropyInput->setDefaultValue(settings.anisotropy);
+    d->alphaInput->setDefaultValue(settings.alpha);
+    d->sigmaInput->setDefaultValue(settings.sigma);
+    d->gaussianPrecInput->setDefaultValue(settings.gaussPrec);
+    d->dlInput->setDefaultValue(settings.dl);
+    d->daInput->setDefaultValue(settings.da);
+    d->iterationInput->setDefaultValue(settings.nbIter);
+    d->tileInput->setDefaultValue(settings.tile);
+    d->btileInput->setDefaultValue(settings.btile);
+    blockSignals(false);
 }
 
 GreycstorationSettings GreycstorationWidget::getSettings()
@@ -294,18 +299,18 @@ GreycstorationSettings GreycstorationWidget::getSettings()
     settings.da         = d->daInput->value();
     settings.nbIter     = d->iterationInput->value();
     settings.tile       = d->tileInput->value();
-    settings.btile      = d->btileInput->value();   
+    settings.btile      = d->btileInput->value();
 
-    return settings; 
+    return settings;
 }
 
 bool GreycstorationWidget::loadSettings(QFile& file, const QString& header)
 {
     QTextStream stream( &file );
-    
+
     if (stream.readLine() != header)
         return false;
-    
+
     blockSignals(true);
 
     GreycstorationSettings settings;
@@ -321,7 +326,7 @@ bool GreycstorationWidget::loadSettings(QFile& file, const QString& header)
     settings.da         = stream.readLine().toDouble();
     settings.nbIter     = stream.readLine().toInt();
     settings.tile       = stream.readLine().toInt();
-    settings.btile      = stream.readLine().toInt();   
+    settings.btile      = stream.readLine().toInt();
     setSettings(settings);
 
     blockSignals(false);
@@ -331,21 +336,21 @@ bool GreycstorationWidget::loadSettings(QFile& file, const QString& header)
 void GreycstorationWidget::saveSettings(QFile& file, const QString& header)
 {
     GreycstorationSettings settings = getSettings();
-    QTextStream stream( &file );        
-    stream << header << "\n";    
-    stream << settings.fastApprox << "\n";    
-    stream << settings.interp << "\n";    
-    stream << settings.amplitude << "\n";    
-    stream << settings.sharpness << "\n";    
-    stream << settings.anisotropy << "\n";    
-    stream << settings.alpha << "\n";    
-    stream << settings.sigma << "\n";    
-    stream << settings.gaussPrec << "\n";    
-    stream << settings.dl << "\n";    
-    stream << settings.da << "\n";    
-    stream << settings.nbIter << "\n";    
-    stream << settings.tile << "\n";    
-    stream << settings.btile << "\n";    
+    QTextStream stream( &file );
+    stream << header << "\n";
+    stream << settings.fastApprox << "\n";
+    stream << settings.interp << "\n";
+    stream << settings.amplitude << "\n";
+    stream << settings.sharpness << "\n";
+    stream << settings.anisotropy << "\n";
+    stream << settings.alpha << "\n";
+    stream << settings.sigma << "\n";
+    stream << settings.gaussPrec << "\n";
+    stream << settings.dl << "\n";
+    stream << settings.da << "\n";
+    stream << settings.nbIter << "\n";
+    stream << settings.tile << "\n";
+    stream << settings.btile << "\n";
 }
 
 } // NameSpace Digikam
