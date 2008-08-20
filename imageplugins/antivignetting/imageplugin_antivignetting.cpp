a13073fe8a2efb87f90e5972bef4702c2a0eea2b
Andi Clemens <andi.clemens@gmx.net>
2008-08-20 11:49:37 +0000

    Action for "Anti-Vignetting" has been renamed from "Vignetting" to "Vignetting Correction...", the old title was somehow confusing.
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=849885

--- imageplugins/antivignetting/imageplugin_antivignetting.cpp
+++ imageplugins/antivignetting/imageplugin_antivignetting.cpp
@@ -4,9 +4,9 @@
  * http://www.digikam.org
  *
  * Date        : 2004-12-25
- * Description : a digiKam image plugin to reduce 
+ * Description : a digiKam image plugin to reduce
  *               vignetting on an image.
- * 
+ *
  * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -14,16 +14,16 @@
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // KDE includes.
-  
+
 #include <klocale.h>
 #include <kgenericfactory.h>
 #include <klibloader.h>
@@ -43,12 +43,12 @@ K_EXPORT_COMPONENT_FACTORY( digikamimageplugin_antivignetting,
 ImagePlugin_AntiVignetting::ImagePlugin_AntiVignetting(QObject *parent, const char*, const QStringList &)
                           : Digikam::ImagePlugin(parent, "ImagePlugin_AntiVignetting")
 {
-    m_antivignettingAction = new KAction(i18n("Vignetting..."), "antivignetting", 0, 
+    m_antivignettingAction = new KAction(i18n("Vignetting Correction..."), "antivignetting", 0,
                                  this, SLOT(slotAntiVignetting()),
                                  actionCollection(), "imageplugin_antivignetting");
 
-    setXMLFile("digikamimageplugin_antivignetting_ui.rc");                
-    
+    setXMLFile("digikamimageplugin_antivignetting_ui.rc");
+
     DDebug() << "ImagePlugin_AntiVignetting plugin loaded" << endl;
 }
 
