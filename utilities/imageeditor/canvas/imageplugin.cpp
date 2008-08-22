9198321c96d6446e35899c1d1395ddca86e2110b
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-22 10:38:12 +0000

    missing to commit new image plugin api
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=850796

--- utilities/imageeditor/canvas/imageplugin.cpp
+++ utilities/imageeditor/canvas/imageplugin.cpp
@@ -5,25 +5,29 @@
  *
  * Date        : 2004-06-04
  * Description : image plugins interface for image editor
- * 
+ *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
-
+ * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // Local includes.
 
+#include "editortool.h"
+#include "editortooliface.h"
 #include "imageplugin.h"
+#include "imageplugin.moc"
 
 namespace Digikam
 {
@@ -35,7 +39,7 @@ ImagePlugin::ImagePlugin(QObject *parent, const char* name)
 }
 
 ImagePlugin::~ImagePlugin()
-{    
+{
 }
 
 void ImagePlugin::setEnabledSelectionActions(bool)
@@ -48,13 +52,28 @@ void ImagePlugin::setEnabledActions(bool)
 
 void ImagePlugin::setParentWidget(QWidget* parent)
 {
-    m_parentWidget = parent;    
+    m_parentWidget = parent;
 }
 
-QWidget* ImagePlugin::parentWidget()
+QWidget* ImagePlugin::parentWidget() const
 {
     return m_parentWidget;
 }
 
-}  // namespace Digikam
+void ImagePlugin::loadTool(EditorTool* tool)
+{
+    EditorToolIface::editorToolIface()->loadTool(tool);
 
+    connect(tool, SIGNAL(okClicked()),
+            this, SLOT(slotToolDone()));
+
+    connect(tool, SIGNAL(cancelClicked()),
+            this, SLOT(slotToolDone()));
+}
+
+void ImagePlugin::slotToolDone()
+{
+    EditorToolIface::editorToolIface()->unLoadTool();
+}
+
+}  // namespace Digikam
