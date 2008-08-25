de977b2cf05bef02cec209d886c79e81b1af3b83
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-25 10:19:46 +0000

    add enableButton()
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=852083

--- utilities/imageeditor/editor/editortoolsettings.cpp
+++ utilities/imageeditor/editor/editortoolsettings.cpp
@@ -213,4 +213,10 @@ KPushButton* EditorToolSettings::button(int buttonCode) const
     return 0;
 }
 
+void EditorToolSettings::enableButton(int buttonCode, bool state)
+{
+    KPushButton *btn = button(buttonCode);
+    if (btn) btn->setEnabled(state);
+}
+
 } // NameSpace Digikam
