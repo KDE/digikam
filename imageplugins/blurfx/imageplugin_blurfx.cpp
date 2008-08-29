f0ff53877a95970580333e1514f36e6e6f2b3e63
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-29 12:04:44 +0000

    compile
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=854328

--- imageplugins/blurfx/imageplugin_blurfx.cpp
+++ imageplugins/blurfx/imageplugin_blurfx.cpp
@@ -5,20 +5,20 @@
  *
  * Date        : 2005-02-09
  * Description : a plugin to apply Blur FX to images
- * 
- * Copyright 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * Copyright 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // KDE includes.
@@ -28,6 +28,7 @@
 #include <klibloader.h>
 #include <kaction.h>
 #include <kcursor.h>
+#include <kapplication.h>
 
 // Local includes.
 
@@ -45,9 +46,9 @@ ImagePlugin_BlurFX::ImagePlugin_BlurFX(QObject *parent, const char*, const QStri
     m_blurfxAction = new KAction(i18n("Blur Effects..."), "blurfx", 0, 
                          this, SLOT(slotBlurFX()),
                          actionCollection(), "imageplugin_blurfx");
-                
-    setXMLFile( "digikamimageplugin_blurfx_ui.rc" );    
-        
+
+    setXMLFile( "digikamimageplugin_blurfx_ui.rc" );
+
     DDebug() << "ImagePlugin_BlurFX plugin loaded" << endl;
 }
 
@@ -62,6 +63,6 @@ void ImagePlugin_BlurFX::setEnabledActions(bool enable)
 
 void ImagePlugin_BlurFX::slotBlurFX()
 {
-    DigikamBlurFXImagesPlugin::ImageEffect_BlurFX dlg(parentWidget());
+    DigikamBlurFXImagesPlugin::ImageEffect_BlurFX dlg(kapp->activeWindow());
     dlg.exec();
 }
