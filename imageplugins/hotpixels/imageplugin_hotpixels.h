48e57bbc27ec816f29c30d2e333db7f782256c69
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-29 12:14:10 +0000

    compile
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=854336

--- imageplugins/hotpixels/imageplugin_hotpixels.h
+++ imageplugins/hotpixels/imageplugin_hotpixels.h
@@ -6,21 +6,21 @@
  * Date        : 2005-03-27
  * Description : a digiKam image plugin for fixing dots produced by
  *               hot/stuck/dead pixels from a CCD.
- * 
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2005-2006 by Unai Garro <ugarro at users dot sourceforge dot net>
- * 
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef IMAGEPLUGIN_HOTPIXELS_H
@@ -34,7 +34,7 @@
 class DIGIKAMIMAGEPLUGINS_EXPORT ImagePlugin_HotPixels : public Digikam::ImagePlugin
 {
     Q_OBJECT
-    
+
 public:
 
     ImagePlugin_HotPixels(QObject *parent, const char* name,
@@ -42,7 +42,7 @@ public:
     ~ImagePlugin_HotPixels();
 
     void setEnabledActions(bool enable);
-        
+
 private slots:
 
     void slotHotPixels();
@@ -51,5 +51,5 @@ private:
 
     KAction *m_hotpixelsAction;
 };
-    
+
 #endif /* IMAGEPLUGIN_HOTPIXELS_H */
