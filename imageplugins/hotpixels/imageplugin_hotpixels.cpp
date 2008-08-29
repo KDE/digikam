ae52a2992582145f6a5431f22f80e0bb6490fa55
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-29 13:55:35 +0000

    compile
    svn path=/trunk/extragear/graphics/digikam/; revision=854452

--- imageplugins/hotpixels/imageplugin_hotpixels.cpp
+++ imageplugins/hotpixels/imageplugin_hotpixels.cpp
@@ -6,21 +6,21 @@
  * Date        : 2005-03-27
  * Description : a digiKam image plugin for fixing dots produced by
  *               hot/stuck/dead pixels from a CCD.
- * 
+ *
  * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2005-2006 by Unai Garro <ugarro at users dot sourceforge dot net>
- * 
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // KDE includes.
@@ -31,6 +31,7 @@
 #include <kaction.h>
 #include <kactioncollection.h>
 #include <kcursor.h>
+#include <kapplication.h>
 
 // Local includes.
 
@@ -67,6 +68,6 @@ void ImagePlugin_HotPixels::setEnabledActions(bool enable)
 
 void ImagePlugin_HotPixels::slotHotPixels()
 {
-    DigikamHotPixelsImagesPlugin::ImageEffect_HotPixels dlg(parentWidget());
+    DigikamHotPixelsImagesPlugin::ImageEffect_HotPixels dlg(kapp->activeWindow());
     dlg.exec();
 }
