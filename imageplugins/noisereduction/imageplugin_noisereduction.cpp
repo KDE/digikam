c7f53197e08f2e68cd44116fcce4b2503e6debf0
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-29 12:18:18 +0000

    compile
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=854338

--- imageplugins/noisereduction/imageplugin_noisereduction.cpp
+++ imageplugins/noisereduction/imageplugin_noisereduction.cpp
@@ -5,20 +5,20 @@
  *
  * Date        : 2004-08-24
  * Description : a plugin to reduce CCD noise.
- * 
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * 
+ *
+ * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // KDE includes.
@@ -28,6 +28,7 @@
 #include <klibloader.h>
 #include <kaction.h>
 #include <kcursor.h>
+#include <kapplication.h>
 
 // Local includes.
 
@@ -45,8 +46,8 @@ ImagePlugin_NoiseReduction::ImagePlugin_NoiseReduction(QObject *parent, const ch
     m_noiseReductionAction = new KAction(i18n("Noise Reduction..."), "noisereduction", 0,
                              this, SLOT(slotNoiseReduction()),
                              actionCollection(), "imageplugin_noisereduction");
-                
-    setXMLFile("digikamimageplugin_noisereduction_ui.rc");                
+
+    setXMLFile("digikamimageplugin_noisereduction_ui.rc");
 
     DDebug() << "ImagePlugin_NoiseReduction plugin loaded" << endl;
 }
@@ -62,8 +63,6 @@ void ImagePlugin_NoiseReduction::setEnabledActions(bool enable)
 
 void ImagePlugin_NoiseReduction::slotNoiseReduction()
 {
-    DigikamNoiseReductionImagesPlugin::ImageEffect_NoiseReduction dlg(parentWidget());
+    DigikamNoiseReductionImagesPlugin::ImageEffect_NoiseReduction dlg(kapp->activeWindow());
     dlg.exec();
 }
-
-
