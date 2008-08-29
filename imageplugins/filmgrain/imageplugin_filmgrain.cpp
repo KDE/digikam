d935cd837e16be36edfd3c67074252d911fcae60
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-29 12:12:45 +0000

    compile
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=854334

--- imageplugins/filmgrain/imageplugin_filmgrain.cpp
+++ imageplugins/filmgrain/imageplugin_filmgrain.cpp
@@ -6,20 +6,20 @@
  * Date        : 2004-10-01
  * Description : a digiKam image editor plugin for add film 
  *               grain on an image.
- * 
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * 
+ *
+ * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // KDE includes.
@@ -29,6 +29,7 @@
 #include <klibloader.h>
 #include <kaction.h>
 #include <kcursor.h>
+#include <kapplication.h>
 
 // Local includes.
 
@@ -46,9 +47,9 @@ ImagePlugin_FilmGrain::ImagePlugin_FilmGrain(QObject *parent, const char*, const
     m_filmgrainAction = new KAction(i18n("Add Film Grain..."), "filmgrain", 0, 
                             this, SLOT(slotFilmGrain()),
                             actionCollection(), "imageplugin_filmgrain");
-                
-    setXMLFile( "digikamimageplugin_filmgrain_ui.rc" );                                
-    
+
+    setXMLFile( "digikamimageplugin_filmgrain_ui.rc" );
+
     DDebug() << "ImagePlugin_FilmGrain plugin loaded" << endl;
 }
 
@@ -63,6 +64,6 @@ void ImagePlugin_FilmGrain::setEnabledActions(bool enable)
 
 void ImagePlugin_FilmGrain::slotFilmGrain()
 {
-    DigikamFilmGrainImagesPlugin::ImageEffect_FilmGrain dlg(parentWidget());
+    DigikamFilmGrainImagesPlugin::ImageEffect_FilmGrain dlg(kapp->activeWindow());
     dlg.exec();
 }
