97a8a32dd094f52f992ab874a0463493a3dd65b7
Gilles Caulier <caulier.gilles@gmail.com>
2008-08-30 14:18:37 +0000

    dispatch busy state to settings widget
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=854882

--- utilities/imageeditor/editor/editortool.cpp
+++ utilities/imageeditor/editor/editortool.cpp
@@ -155,6 +155,11 @@ void EditorTool::setToolSettings(EditorToolSettings *settings)
             this, SLOT(slotEffect()));
 }
 
+void EditorTool::setBusy(bool state)
+{
+    d->settings->setBusy(state);
+}
+
 void EditorTool::readSettings()
 {
     d->settings->readSettings();
