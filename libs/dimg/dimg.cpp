8ba1b2b676d1ade1fcbcb542fcd56c324ab16514
Gilles Caulier <caulier.gilles@gmail.com>
2008-09-08 10:01:41 +0000

    digiKam from trunk : prepare the future : compile with libkdcraw 0.4.0 which use libRAW instead dcraw.c
    svn path=/trunk/extragear/graphics/digikam/; revision=858534

--- libs/dimg/dimg.cpp
+++ libs/dimg/dimg.cpp
@@ -7,20 +7,20 @@
  * Description : digiKam 8/16 bits image management API
  *
  * Copyright (C) 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com> 
- * Copyright (C) 2006-2007 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com> 
+ * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // C ANSI includes.
@@ -36,7 +36,7 @@ extern "C"
 
 // Qt includes.
 
-#include <QtCore>        
+#include <QtCore> 
 #include <QFile>
 #include <QFileInfo>
 #include <QMap>
@@ -45,8 +45,12 @@ extern "C"
 
 // LibKDcraw includes.
 
-#include <libkdcraw/dcrawbinary.h>
+#include <libkdcraw/version.h>
 #include <libkdcraw/kdcraw.h>
+
+#if KDCRAW_VERSION < 0x000400
+#include <libkdcraw/dcrawbinary.h>
+#endif
 
 // Local includes.
 
@@ -507,7 +511,11 @@ DImg::FORMAT DImg::fileFormat(const QString& filePath)
         return NONE;
     }
 
+#if KDCRAW_VERSION < 0x000400
     QString rawFilesExt(KDcrawIface::DcrawBinary::instance()->rawFiles());
+#else
+    QString rawFilesExt(KDcrawIface::KDcraw::rawFiles());
+#endif
     QString ext = fileInfo.suffix().toUpper();
 
     if (!ext.isEmpty())
--- libs/dimg/dimg.h
+++ libs/dimg/dimg.h
@@ -7,8 +7,8 @@
  * Description : digiKam 8/16 bits image management API
  *
  * Copyright (C) 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>  
- * Copyright (C) 2006-2007 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>  
+ * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
