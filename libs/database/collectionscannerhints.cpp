138d1bc637ea79d362b47ac789db3039725b1321
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2008-09-17 09:09:46 +0000

    Container classes to contain descriptions of move or copy operations on albums or images. Source and destination is stored, so that the originating image can be deduced when the newly created destination is scanned.
    svn path=/trunk/extragear/graphics/digikam/; revision=861809

---
+++ libs/database/collectionscannerhints.cpp
@@ -0,0 +1,255 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2008-09-09
+ * Description : Hint data containers for the collection scanner
+ * 
+ * Copyright (C) 2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+// Qt includes.
+
+#include <QHash>
+#include <QList>
+#include <QDBusArgument>
+
+// Local includes.
+
+#include "collectionscannerhints.h"
+
+namespace Digikam
+{
+
+CollectionScannerHints::Album::Album()
+    : albumRootId(0), albumId(0)
+{
+}
+
+CollectionScannerHints::Album::Album(int albumRootId, int albumId)
+    : albumRootId(albumRootId), albumId(albumId)
+{
+}
+
+bool CollectionScannerHints::Album::isNull() const
+{
+    return albumRootId == 0 || albumId == 0;
+}
+
+bool CollectionScannerHints::Album::operator==(const Album &other) const
+{
+    return albumRootId == other.albumRootId || albumId == other.albumId;
+}
+
+uint CollectionScannerHints::Album::qHash() const
+{
+    return ::qHash(albumRootId) ^ ::qHash(albumId);
+}
+
+
+CollectionScannerHints::DstPath::DstPath()
+    : albumRootId(0)
+{
+}
+
+CollectionScannerHints::DstPath::DstPath(int albumRootId, const QString &relativePath)
+    : albumRootId(albumRootId), relativePath(relativePath)
+{
+}
+
+bool CollectionScannerHints::DstPath::isNull() const
+{
+    return albumRootId == 0 || relativePath.isNull();
+}
+
+bool CollectionScannerHints::DstPath::operator==(const DstPath &other) const
+{
+    return albumRootId == other.albumRootId || relativePath == other.relativePath;
+}
+
+uint CollectionScannerHints::DstPath::qHash() const
+{
+    return ::qHash(albumRootId) ^ ::qHash(relativePath);
+}
+
+
+CollectionScannerHints::Item::Item()
+    : id(0)
+{
+}
+
+CollectionScannerHints::Item::Item(qlonglong id)
+    : id(id)
+{
+}
+
+bool CollectionScannerHints::Item::isNull() const
+{
+    return id == 0;
+}
+
+bool CollectionScannerHints::Item::operator==(const Item &other) const
+{
+    return id == other.id;
+}
+
+uint CollectionScannerHints::Item::qHash() const
+{
+    return ::qHash(id);
+}
+
+
+AlbumCopyMoveHint::AlbumCopyMoveHint()
+{
+}
+
+AlbumCopyMoveHint::AlbumCopyMoveHint(int srcAlbumRootId, int srcAlbum,
+                                     int dstAlbumRootId, const QString &dstRelativePath)
+    : m_src(srcAlbumRootId, srcAlbum),
+      m_dst(dstAlbumRootId, dstRelativePath)
+{
+}
+
+int AlbumCopyMoveHint::albumRootIdSrc() const
+{
+    return m_src.albumRootId;
+}
+
+int AlbumCopyMoveHint::albumIdSrc() const
+{
+    return m_src.albumId;
+}
+
+bool AlbumCopyMoveHint::isSrcAlbum(int albumRootId, int albumId) const
+{
+    return m_src.albumRootId == albumRootId && m_src.albumId == albumId;
+}
+
+int AlbumCopyMoveHint::albumRootIdDst() const
+{
+    return m_dst.albumRootId;
+}
+
+QString AlbumCopyMoveHint::relativePathDst() const
+{
+    return m_dst.relativePath;
+}
+
+bool AlbumCopyMoveHint::isDstAlbum(int albumRootId, const QString &relativePath) const
+{
+    return m_dst.albumRootId == albumRootId && m_dst.relativePath == relativePath;
+}
+
+uint AlbumCopyMoveHint::qHash() const
+{
+    return ::qHash(m_src.albumRootId) ^ ::qHash(m_src.albumId)
+         ^ ::qHash(m_dst.albumRootId) ^ ::qHash(m_dst.relativePath);
+}
+
+AlbumCopyMoveHint &AlbumCopyMoveHint::operator<<(const QDBusArgument &argument)
+{
+    argument.beginStructure();
+    argument >> m_src.albumRootId >> m_src.albumId
+             >> m_dst.albumRootId >> m_dst.relativePath;
+    argument.endStructure();
+    return *this;
+}
+
+const AlbumCopyMoveHint &AlbumCopyMoveHint::operator>>(QDBusArgument &argument) const
+{
+    argument.beginStructure();
+    argument << m_src.albumRootId << m_src.albumId
+             << m_dst.albumRootId << m_dst.relativePath;
+    argument.endStructure();
+    return *this;
+}
+
+
+ItemCopyMoveHint::ItemCopyMoveHint()
+{
+}
+
+ItemCopyMoveHint::ItemCopyMoveHint(QList<qlonglong> srcIds, int dstItemRootId, int dstAlbumId, QStringList dstNames)
+    : m_srcIds(srcIds),
+      m_dst(dstItemRootId, dstAlbumId),
+      m_dstNames(dstNames)
+{
+}
+
+QList<qlonglong> ItemCopyMoveHint::srcIds() const
+{
+    return m_srcIds;
+}
+
+bool ItemCopyMoveHint::isSrcId(qlonglong id) const
+{
+    return m_srcIds.contains(id);
+}
+
+int ItemCopyMoveHint::albumRootIdDst() const
+{
+    return m_dst.albumRootId;
+}
+
+int ItemCopyMoveHint::albumIdDst() const
+{
+    return m_dst.albumId;
+}
+
+bool ItemCopyMoveHint::isDstAlbum(int albumRootId, int albumId) const
+{
+    return m_dst.albumRootId == albumRootId && m_dst.albumId == albumId;
+}
+
+QStringList ItemCopyMoveHint::dstNames() const
+{
+    return m_dstNames;
+}
+
+QString ItemCopyMoveHint::dstName(qlonglong id) const
+{
+    if (m_dstNames.isEmpty())
+        return QString();
+    int index = m_srcIds.indexOf(id);
+    return m_dstNames[index];
+}
+
+
+ItemCopyMoveHint &ItemCopyMoveHint::operator<<(const QDBusArgument &argument)
+{
+    argument.beginStructure();
+    argument >> m_srcIds
+             >> m_dst.albumRootId >> m_dst.albumId
+             >> m_dstNames;
+    argument.endStructure();
+    return *this;
+}
+
+const ItemCopyMoveHint &ItemCopyMoveHint::operator>>(QDBusArgument &argument) const
+{
+    argument.beginStructure();
+    argument << m_srcIds
+             << m_dst.albumRootId << m_dst.albumId
+             << m_dstNames;
+    argument.endStructure();
+    return *this;
+}
+
+
+}
+
+
+
---
+++ libs/database/collectionscannerhints.h
@@ -0,0 +1,179 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2008-09-09
+ * Description : Hint data containers for the collection scanner
+ * 
+ * Copyright (C) 2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef COLLECTIONSCANNERHINTS_H
+#define COLLECTIONSCANNERHINTS_H
+
+// Qt includes.
+
+#include <QList>
+
+// Local includes.
+
+#include "dbusutilities.h"
+#include "digikam_export.h"
+
+class QDBusArgument;
+
+namespace Digikam
+{
+
+namespace CollectionScannerHints
+{
+
+class DIGIKAM_EXPORT Album
+{
+public:
+    Album();
+    Album(int albumRootId, int albumId);
+
+    bool isNull() const;
+    uint qHash() const;
+    bool operator==(const Album &other) const;
+
+    int albumRootId;
+    int albumId;
+};
+
+class DIGIKAM_EXPORT DstPath
+{
+public:
+    DstPath();
+    DstPath(int albumRootId, const QString &relativePath);
+
+    bool isNull() const;
+    uint qHash() const;
+    bool operator==(const DstPath &other) const;
+
+    int albumRootId;
+    QString relativePath;
+};
+
+class DIGIKAM_EXPORT Item
+{
+public:
+    Item();
+    Item(qlonglong id);
+
+    bool isNull() const;
+    uint qHash() const;
+    bool operator==(const Item &other) const;
+
+    qlonglong id;
+};
+
+inline uint qHash(const Album &src) { return src.qHash(); }
+inline uint qHash(const DstPath &dst) { return dst.qHash(); }
+inline uint qHash(const Item &item) { return item.qHash(); }
+
+}
+
+
+class DIGIKAM_EXPORT AlbumCopyMoveHint
+{
+public:
+
+    /** An AlbumCopyMoveHint describes an existing album
+     *  and a destination to which this album is expected to be
+     *  copied, moved or renamed.
+     */
+
+    AlbumCopyMoveHint();
+    AlbumCopyMoveHint(int srcAlbumRootId, int srcAlbum,
+                      int dstAlbumRootId, const QString &dstRelativePath);
+
+    int albumRootIdSrc() const;
+    int albumIdSrc() const;
+    bool isSrcAlbum(int albumRootId, int albumId) const;
+    CollectionScannerHints::Album src() const { return m_src; }
+
+    int albumRootIdDst() const;
+    QString relativePathDst() const;
+    bool isDstAlbum(int albumRootId, const QString &relativePath) const;
+    CollectionScannerHints::DstPath dst() const { return m_dst; }
+
+    uint qHash() const;
+
+    bool operator==(const CollectionScannerHints::Album &src) { return src == m_src; }
+    bool operator==(const CollectionScannerHints::DstPath &dst) { return dst == m_dst; }
+
+    AlbumCopyMoveHint &operator<<(const QDBusArgument &argument);
+    const AlbumCopyMoveHint &operator>>(QDBusArgument &argument) const;
+
+    operator const CollectionScannerHints::Album &() const { return m_src; }
+    operator const CollectionScannerHints::DstPath &() const { return m_dst; }
+
+protected:
+
+    CollectionScannerHints::Album m_src;
+    CollectionScannerHints::DstPath m_dst;
+};
+
+class DIGIKAM_EXPORT ItemCopyMoveHint
+{
+public:
+
+    /** An ItemCopyMoveHint describes a list of existing items that will
+     *  be copied, moved or renamed to an album given by album root id and album id.
+     *  In the new album, the items will have the filenames given in dstNames.
+     */
+
+    ItemCopyMoveHint();
+    ItemCopyMoveHint(QList<qlonglong> srcIds, int dstAlbumRootId, int albumId, QStringList dstNames);
+
+    QList<qlonglong> srcIds() const;
+    bool isSrcId(qlonglong id) const;
+
+    int albumRootIdDst() const;
+    int albumIdDst() const;
+    bool isDstAlbum(int albumRootId, int albumId) const;
+    CollectionScannerHints::Album dst() const { return m_dst; }
+    QStringList dstNames() const;
+    QString dstName(qlonglong id) const;
+
+    bool operator==(const CollectionScannerHints::Album &dst) { return dst == m_dst; }
+
+    ItemCopyMoveHint &operator<<(const QDBusArgument &argument);
+    const ItemCopyMoveHint &operator>>(QDBusArgument &argument) const;
+
+    operator const CollectionScannerHints::Album&() const { return m_dst; }
+
+protected:
+
+    QList<qlonglong> m_srcIds;
+    CollectionScannerHints::Album m_dst;
+    QStringList m_dstNames;
+};
+
+
+inline uint qHash(const Digikam::AlbumCopyMoveHint &hint) { return hint.qHash(); }
+
+} // end of namespace
+
+DECLARE_METATYPE_FOR_DBUS(Digikam::AlbumCopyMoveHint);
+DECLARE_METATYPE_FOR_DBUS(Digikam::ItemCopyMoveHint);
+
+#endif
+
+
+
