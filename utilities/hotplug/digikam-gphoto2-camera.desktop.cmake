a320a9a325976e9dadeb5fc4aef6fd29ff5fabb2
Script Kiddy <scripty@kde.org>
2008-09-20 12:08:53 +0000

    SVN_SILENT made messages (.desktop file)
    svn path=/trunk/extragear/graphics/digikam/; revision=862932

--- utilities/hotplug/digikam-gphoto2-camera.desktop.cmake
+++ utilities/hotplug/digikam-gphoto2-camera.desktop.cmake
@@ -2,9 +2,27 @@
 Exec=@digikamhelperdir@/digikam-camera detect %u
 Icon=digikam
 Name=digiKam Detect and Download
-Name[de]=Bilder mit digiKam herunterladen
-Name[fr]=Télécharger les photos avec digiKam
-Name[tr]=digiKam ile resimler göster
+Name[ca]=Detecta i descarrega amb el digiKam
+Name[de]=digiKam - Entdecken und herunterladen
+Name[el]=digiKam ανίχνευση και λήψη
+Name[et]=Fotode tuvastamine ja allalaadimine digiKamiga
+Name[fi]=Tunnista kamera ja lataa kuvat digiKamilla
+Name[gl]=Detección e Descarga de digiKam
+Name[it]=Rileva e scarica con digiKam
+Name[ja]=digiKam 検出とダウンロード
+Name[km]=រក​ឃើញ និង​ទាញយក digiKam
+Name[lv]=digiKam noteicējs un ielādētājs
+Name[nds]=digiKam - Opdecken un daalladen
+Name[nl]=Detecteren en downloaden
+Name[pl]=Wykrywanie i pobieranie za pomocą digiKam
+Name[pt]=Detectar e Transferir com o digiKam
+Name[pt_BR]=Detectar e Transferir com o digiKam
+Name[sv]=Digikam detektering och nerladdning
+Name[th]=ตรวจสอบและดาวน์โหลดด้วย digiKam
+Name[tr]=digiKam Belirle ve İndir
+Name[uk]=digiKam - виявлення і звантаження
+Name[x-test]=xxdigiKam Detect and Downloadxx
+Name[zh_CN]=digiKam 探测和下载
 
 [Desktop Entry]
 Encoding=UTF-8
--- utilities/hotplug/digikam-mount-and-download.desktop.cmake
+++ utilities/hotplug/digikam-mount-and-download.desktop.cmake
@@ -2,9 +2,28 @@
 Exec=@digikamhelperdir@/digikam-camera storage %u
 Icon=digikam
 Name=Download Photos with digiKam
-Name[de]=Bilder mit digiKam herunterladen
-Name[fr]=Télécharger les photos avec digiKam
-Name[tr]=digiKam ile resimleri indir
+Name[ca]=Descàrrega de fotos amb el digiKam
+Name[de]=Fotos mit digiKam herunterladen
+Name[el]=Λήψη φωτογραφιών με το digiKam
+Name[et]=Fotode allalaadimine digiKamiga
+Name[fi]=Lataa valokuvat digiKamilla
+Name[gl]=Descargar Fotos con digiKam
+Name[it]=Scarica foto con digiKam
+Name[ja]=digiKam で写真をダウンロード
+Name[km]=ទាញយក​រូបថត​ដោយ digiKam
+Name[lv]=Ielādēt fotogrāfijas ar digiKam
+Name[nds]=Fotos mit digiKam daalladen
+Name[nl]=Foto's met digiKam downloaden
+Name[pl]=Pobierz zdjęcia za pomocą digiKam
+Name[pt]=Obter Fotos com o digiKam
+Name[pt_BR]=Obter Fotos com o digiKam
+Name[ro]=Descărcere fotografii cu digiKam
+Name[sv]=Ladda ner foton med Digikam
+Name[th]=ดาวน์โหลดภาพถ่ายด้วย digiKam
+Name[tr]=digiKam ile Fotoğrafları İndir
+Name[uk]=Завантажити фотографії програмою digiKam
+Name[x-test]=xxDownload Photos with digiKamxx
+Name[zh_CN]=用 digiKam 下载照片
 
 [Desktop Entry]
 Encoding=UTF-8
