2d5aa707e0a382cf59479722cfb239d4e7d53620
Gilles Caulier <caulier.gilles@gmail.com>
2008-09-23 14:37:26 +0000

    clean up Exiv2 XMP sdk properly
    svn path=/trunk/extragear/graphics/digikam/; revision=863928

--- digikam/main.cpp
+++ digikam/main.cpp
@@ -173,9 +173,11 @@ int main(int argc, char *argv[])
 
     KTipDialog::showMultiTip(0, tipsFiles, false);
 
-    return app.exec();
+    int ret = app.exec();
 
 #if KEXIV2_VERSION >= 0x000300
     KExiv2Iface::KExiv2::cleanupExiv2();
 #endif
+
+    return ret;
 }
