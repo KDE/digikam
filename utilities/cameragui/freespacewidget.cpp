9f05362f4f3fac1167fc51fa540c2219ae852aa4
Gilles Caulier <caulier.gilles@gmail.com>
2008-10-03 11:24:25 +0000

    fix depreciate.
    svn path=/trunk/extragear/graphics/digikam/; revision=867389

--- utilities/cameragui/freespacewidget.cpp
+++ utilities/cameragui/freespacewidget.cpp
@@ -6,7 +6,7 @@
  * Date        : 2007-08-31
  * Description : a widget to display free space for a mount-point.
  *
- * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -37,8 +37,7 @@
 #include <kdebug.h>
 #include <kurl.h>
 #include <klocale.h>
-#include <kdiskfreespace.h>
-#include <kmountpoint.h>
+#include <kdiskfreespaceinfo.h>
 #include <kio/global.h>
 #include <kiconloader.h>
 
@@ -285,21 +284,11 @@ void FreeSpaceWidget::paintEvent(QPaintEvent*)
 
 void FreeSpaceWidget::slotTimeout()
 {
-    KMountPoint::List list = KMountPoint::currentMountPoints();
-    KMountPoint::Ptr mp    = list.findByPath(d->path);
-    if (mp)
+    KDiskFreeSpaceInfo info = KDiskFreeSpaceInfo::freeSpaceInfo(d->path);
+    if(info.isValid())
     {
-        KDiskFreeSpace *job = new KDiskFreeSpace;
-        connect(job, SIGNAL(foundMountPoint(QString, quint64, quint64, quint64)),
-                this, SLOT(slotAvailableFreeSpace(QString, quint64, quint64, quint64)));
-        job->readDF(mp->mountPoint());
+        setInformations(info.size(), info.used(), info.available(), info.mountPoint());
     }
-}
-
-void FreeSpaceWidget::slotAvailableFreeSpace(QString mountPoint, quint64 kBSize,
-                                             quint64 kBUsed, quint64 kBAvail)
-{
-    setInformations(kBSize, kBUsed, kBAvail, mountPoint);
 }
 
 }  // namespace Digikam
--- utilities/cameragui/freespacewidget.h
+++ utilities/cameragui/freespacewidget.h
@@ -5,20 +5,20 @@
  *
  * Date        : 2007-08-31
  * Description : a widget to display free space for a mount-point.
- * 
- * Copyright (C) 2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef FREESPACEWIDGET_H
@@ -51,7 +51,7 @@ public:
         UMSCamera,
         GPhotoCamera
     };
-    
+
 public:
 
     FreeSpaceWidget(QWidget* parent, int width);
@@ -59,7 +59,7 @@ public:
 
     void          setEstimatedDSizeKb(unsigned long dSize);
     unsigned long estimatedDSizeKb();
-    
+
     bool          isValid();
     int           percentUsed();
     unsigned long kBSize();
@@ -71,7 +71,7 @@ public:
     void setMode(FreeSpaceMode mode);
     void setPath(const QString& path);
     void setInformations(unsigned long kBSize,
-                         unsigned long kBUsed, unsigned long kBAvail, 
+                         unsigned long kBUsed, unsigned long kBAvail,
                          const QString& mountPoint);
 
 protected:
@@ -82,8 +82,7 @@ protected:
 private slots:
 
     void slotTimeout();
-    void slotAvailableFreeSpace(QString mountPoint, quint64 kBSize, quint64 kBUsed, quint64 kBAvail);
-    
+
 private:
 
     FreeSpaceWidgetPriv* d;
