3a1165ffe0929196b8cd911764c541eb08be0e48
Gilles Caulier <caulier.gilles@gmail.com>
2008-10-08 09:50:11 +0000

    New digiKam Cmake flag to disable Gphoto2 support. This will be used especailly unde Win32 port where Gphoto2 do not exist. This patch only set a flag in config-digikam.h. We must use it in source code to disable GPhoto2 code relevant. CCMAIL: ps_ml@gmx.de
    svn path=/trunk/extragear/graphics/digikam/; revision=869139

--- config-digikam.h.cmake
+++ config-digikam.h.cmake
@@ -6,3 +6,7 @@
 
 /* Define to 1 if you have LensFun shared library installed */
 #cmakedefine HAVE_LENSFUN 1
+
+/* Define to 1 if GPhoto2 shared library support is enabled */
+#cmakedefine ENABLE_GPHOTO2 1
+
