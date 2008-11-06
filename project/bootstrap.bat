f85735bbf03c59785ec4c16bebd5f8f790e27393
Gilles Caulier <caulier.gilles@gmail.com>
2008-11-06 09:02:23 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=880705

--- project/bootstrap.bat
+++ project/bootstrap.bat
@@ -1,11 +1,13 @@
 @echo off
+
 rem KDE4 must be installed into C:\KDE4
 set KDEDIRS=C:\KDE4
 set PATH=%PATH%;%KDEDIRS%\bin
 set QT_PLUGIN_PATH=%KDEDIR%\plugins
+set KDE4_INSTALL_DIR=%KDEDIRS%
 
 rem clean up CMake cache
 del -f CMakeCache.txt
 
 rem configure CMake env.
-cmake -G "MinGW Makefiles" . -DCMAKE_LIBRARY_PATH=%KDEDIRS%/lib -DENABLE_GPHOTO2=OFF -DCMAKE_INSTALL_PREFIX=C:/KDE4
+cmake -G "MinGW Makefiles" . -DCMAKE_LIBRARY_PATH=%KDEDIRS%/lib -DENABLE_GPHOTO2=OFF -DCMAKE_INSTALL_PREFIX=%KDE4_INSTALL_DIR%
