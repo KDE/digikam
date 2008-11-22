fcc29e9b6b4966479e191b331107f62e6c63607d
Patrick Spendrin <ps_ml@gmx.de>
2008-11-22 02:05:49 +0000

    adjust copyright notice
    svn path=/trunk/extragear/graphics/digikam/; revision=887481

--- libs/jpegutils/jpegwin.cpp
+++ libs/jpegutils/jpegwin.cpp
@@ -3,10 +3,10 @@
  * This file is a part of digiKam project
  * http://www.digikam.org
  *
- * Date        : 2005-11-01
- * Description : a PNG image loader for DImg framework.
+ * Date        : 2008-11-22
+ * Description : some workaround functions to read jpeg files without relying on libjpeg
  * 
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com> 
+ * Copyright (C) 2008 Patrick Spendrin <ps_ml@gmx.de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
