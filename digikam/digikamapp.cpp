acf12896eb4a786454826abc0f12de6c6340a1ea
Andi Clemens <andi.clemens@gmx.net>
2008-12-11 09:45:08 +0000

    do not display tooltip from statuszoombar in fullscreen mode
    svn path=/trunk/extragear/graphics/digikam/; revision=895645

--- digikam/digikamapp.cpp
+++ digikam/digikamapp.cpp
@@ -2294,14 +2294,16 @@ void DigikamApp::slotThumbSizeChanged(int size)
 {
     d->statusZoomBar->setZoomSliderValue(size);
     d->statusZoomBar->setZoomTrackerText(i18n("Size: %1", size));
-    d->statusZoomBar->triggerZoomTrackerToolTip();
+    if (!d->fullScreen)
+        d->statusZoomBar->triggerZoomTrackerToolTip();
 }
 
 void DigikamApp::slotZoomChanged(double zoom, int size)
 {
     d->statusZoomBar->setZoomSliderValue(size);
     d->statusZoomBar->setZoomTrackerText(i18n("zoom: %1%", (int)(zoom*100.0)));
-    d->statusZoomBar->triggerZoomTrackerToolTip();
+    if (!d->fullScreen)
+        d->statusZoomBar->triggerZoomTrackerToolTip();
 }
 
 void DigikamApp::slotTogglePreview(bool t)
