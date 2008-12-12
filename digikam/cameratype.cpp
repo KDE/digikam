5c49745a1d5fdaf13dcb452f2b86286431777a8b
Gilles Caulier <caulier.gilles@gmail.com>
2008-12-12 22:39:19 +0000

    Very nice : in copy constructor, the d private insternal class is never created. This can give a beautifull crash (:=)))...
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=896241

--- digikam/cameratype.cpp
+++ digikam/cameratype.cpp
@@ -7,7 +7,7 @@
  * Description : Camera settings container.
  * 
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -76,13 +76,9 @@ CameraType::CameraType(const QString& title, const QString& model,
     d->valid      = true;    
 }
 
-CameraType::~CameraType()
-{
-    delete d;
-}
-
 CameraType::CameraType(const CameraType& ctype)
 {
+    d = new CameraTypePrivate;
     d->title      = ctype.d->title;
     d->model      = ctype.d->model;
     d->port       = ctype.d->port;
@@ -90,6 +86,11 @@ CameraType::CameraType(const CameraType& ctype)
     d->action     = ctype.d->action;
     d->lastAccess = ctype.d->lastAccess;
     d->valid      = ctype.d->valid;
+}
+
+CameraType::~CameraType()
+{
+    delete d;
 }
 
 CameraType& CameraType::operator=(const CameraType& ctype)
