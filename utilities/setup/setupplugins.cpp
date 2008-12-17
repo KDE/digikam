14383aeb8f0ca482502779ca6e50fce4d65650f5
Gilles Caulier <caulier.gilles@gmail.com>
2008-12-17 07:48:35 +0000

    no need to  display libkipi version here
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=897925

--- utilities/setup/setupplugins.cpp
+++ utilities/setup/setupplugins.cpp
@@ -2,23 +2,23 @@
  *
  * This file is a part of digiKam project
  * http://www.digikam.org
- * 
+ *
  * Date        : 2004-01-02
  * Description : setup Kipi plugins tab.
- * 
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifdef HAVE_CONFIG_H
@@ -70,23 +70,18 @@ SetupPlugins::SetupPlugins(QWidget* parent )
             : QWidget(parent)
 {
     d = new SetupPluginsPriv;
-    QVBoxLayout *layout = new QVBoxLayout( parent );
-    
-    QHBoxLayout *hlay = new QHBoxLayout(layout);
-    d->pluginsNumber = new QLabel(parent);
-    
-    QLabel *KipiVersion = new QLabel(i18n("Kipi library version: %1").arg(kipi_version), parent);
-    KipiVersion->setAlignment ( Qt::AlignRight | Qt::AlignVCenter );
-    
-    hlay->addWidget(d->pluginsNumber, 1);
-    hlay->addStretch(1);
-    hlay->addWidget(KipiVersion, 1);
-    
+    QVBoxLayout *layout = new QVBoxLayout(parent);
+    d->pluginsNumber    = new QLabel(parent);
+    d->pluginsNumber->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+
     d->kipiConfig = KIPI::PluginLoader::instance()->configWidget( parent );
-    QString pluginsListHelp = i18n("<p>A list of available Kipi plugins "
-                                    "appears below.");
-    QWhatsThis::add( d->kipiConfig, pluginsListHelp);
-    layout->addWidget( d->kipiConfig );
+    QString pluginsListHelp = i18n("<p>A list of available Kipi plugins appears below.");
+    QWhatsThis::add(d->kipiConfig, pluginsListHelp);
+
+    layout->addWidget(d->pluginsNumber);
+    layout->addWidget(d->kipiConfig);
+    layout->setMargin(0);
+    layout->setSpacing(KDialog::spacingHint());
 }
 
 SetupPlugins::~SetupPlugins()
@@ -107,4 +102,3 @@ void SetupPlugins::applyPlugins()
 }
 
 }  // namespace Digikam
-
