066b74ffe28e6424c622e1e87957bd55e4c4e039
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2008-12-30 18:49:28 +0000

    Add a desktop file intended for the Plasma Device Notifier applet. May need some testing.
    CCBUG: 178435
    
    svn path=/trunk/extragear/graphics/digikam/; revision=903521

---
+++ utilities/hotplug/digikam-opencamera.desktop.cmake
@@ -0,0 +1,39 @@
+[Desktop Entry]
+X-KDE-Solid-Predicate=[ StorageVolume.ignored == false OR Camera.supportedDrivers == 'gphoto' ]
+Type=Service
+Actions=open;
+
+[Desktop Action open]
+# %f gives StorageAccess.filePath, %d gives Block.device, %i gives UDI
+Exec=@digikamhelperdir@/digikam-camera downloadFromUdi %i
+Icon=digikam
+Name=Download Photos with digiKam
+Name[ca]=Descàrrega de fotos amb el digiKam
+Name[cs]=Stáhnout fotografie pomocí digiKamu
+Name[da]=Download fotografier med digiKam
+Name[de]=Fotos mit digiKam herunterladen
+Name[el]=Λήψη φωτογραφιών με το digiKam
+Name[et]=Fotode allalaadimine digiKamiga
+Name[fi]=Lataa valokuvat digiKamilla
+Name[fr]=Télécharger des photos avec Digikam
+Name[gl]=Descargar Fotos con digiKam
+Name[is]=Hala niður myndum með digiKam
+Name[it]=Scarica foto con digiKam
+Name[ja]=digiKam で写真をダウンロード
+Name[km]=ទាញយក​រូបថត​ដោយ digiKam
+Name[lv]=Ielādēt fotogrāfijas ar digiKam
+Name[nb]=Last ned fotografier med digiKam
+Name[nds]=Fotos mit digiKam daalladen
+Name[nl]=Foto's met digiKam downloaden
+Name[pa]=ਡਿਜਿਕੈਮ ਨਾਲ ਫੋਟੋ ਡਾਊਨਲੋਡ ਕਰੋ
+Name[pl]=Pobierz zdjęcia za pomocą digiKam
+Name[pt]=Obter Fotos com o digiKam
+Name[pt_BR]=Baixar as fotos com o digiKam
+Name[ro]=Descărcere fotografii cu digiKam
+Name[sv]=Ladda ner foton med Digikam
+Name[th]=ดาวน์โหลดภาพถ่ายด้วย digiKam
+Name[tr]=Fotoğrafları DigiKam ile İndir
+Name[uk]=Завантажити фотографії програмою digiKam
+Name[x-test]=xxDownload Photos with digiKamxx
+Name[zh_CN]=用 digiKam 下载照片
+
