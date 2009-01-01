7ae6b17b2a03502ce7cdae50a7e300b5371ec5fa
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-01 16:53:13 +0000

    a new year
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=904214

--- digikam/daboutdata.h
+++ digikam/daboutdata.h
@@ -6,8 +6,8 @@
  * Date        : 2008-08-08
  * Description : digiKam about data.
  *
- * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2008 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2008-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2009 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -117,7 +117,7 @@ static inline const char* themeDesignerDescription()
 
 static inline const char* copyright()
 {
-    return I18N_NOOP("(c) 2002-2008, digiKam developers team");
+    return I18N_NOOP("(c) 2002-2009, digiKam developers team");
 }
 
 static inline const char* webProjectUrl()
