7d1cb8b542661769d1bb1b0134f03c286e40a367
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-03 19:38:26 +0000

    folder view and item highlightment : fix crash when item is deleted. BUG: 179534
    svn path=/trunk/extragear/graphics/digikam/; revision=905140

--- digikam/folderview.cpp
+++ digikam/folderview.cpp
@@ -7,7 +7,7 @@
  * Description : implementation of folder view.
  *
  * Copyright (C) 2005-2006 by Joern Ahrens <joern.ahrens@kdemail.net>
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -209,6 +209,21 @@ void FolderView::slotIconSizeChanged()
 {
     d->itemHeight = qMax(AlbumThumbnailLoader::instance()->thumbnailSize() + 2*itemMargin(), fontMetrics().height());
     slotThemeChanged();
+}
+
+void FolderView::takeItem(Q3ListViewItem* item)
+{
+    if (d->toolTipItem == item)
+    {
+        d->toolTipItem = 0;
+        d->toolTipTimer->stop();
+        slotToolTip();
+    }
+
+    if (d->itemHighlighted == item)
+        d->itemHighlighted = 0;
+
+    Q3ListView::takeItem(item);
 }
 
 void FolderView::leaveEvent(QEvent* e)
--- digikam/folderview.h
+++ digikam/folderview.h
@@ -7,7 +7,7 @@
  * Description : implementation of folder view.
  *
  * Copyright (C) 2005-2006 by Joern Ahrens <joern.ahrens@kdemail.net>
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -92,6 +92,7 @@ public:
     QPixmap  itemBasePixmapSelected() const;
 
     virtual void collapseView(CollapseMode mode = RestoreCurrentAlbum);
+    void takeItem(Q3ListViewItem* item);
 
 protected:
 
