49058d76c9e91da422124e4039be89823e461be9
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-05 12:13:29 +0000

    missing to remove old lines with previous commit CCBUGS: 179653
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=905929

--- digikam/albumiconviewfilter.cpp
+++ digikam/albumiconviewfilter.cpp
@@ -6,7 +6,7 @@
  * Date        : 2007-11-27
  * Description : a bar to filter album contents
  *
- * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -159,9 +159,6 @@ void AlbumIconViewFilter::slotItemsFilterMatch(bool match)
 
     if (d->ratingFilter->rating() != 0 || d->ratingFilter->ratingFilterCondition() != AlbumLister::GreaterEqualCondition)
         filtersList.append(i18n("<br/><nobr><i>Rating</i></nobr>"));
-
-    if (d->ratingFilter->rating() != 0)
-        filtersList.append(i18n("<br><nobr><i>Rating</i></nobr>"));
 
     if (AlbumLister::instance()->tagFiltersIsActive())
         filtersList.append(i18n("<br><nobr><i>Tags</i></nobr>"));
