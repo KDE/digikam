a14cc2e0bf337c47cf9004f3e7287bd992ec2dd3
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-16 12:43:04 +0000

    do not use alpha blending in image properties BUG: 179898
    svn path=/trunk/extragear/graphics/digikam/; revision=911898

--- libs/imageproperties/imagepropertiestxtlabel.h
+++ libs/imageproperties/imagepropertiestxtlabel.h
@@ -7,7 +7,7 @@
  * Description : simple text labels to display image
  *               properties meta infos
  *
- * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -46,15 +46,17 @@ public:
         : QLabel(parent)
     {
         setText(name);
-        setFont(KGlobalSettings::smallestReadableFont());
+        QFont fnt = KGlobalSettings::smallestReadableFont();
+        fnt.setItalic(true);
+        setFont(fnt);
         setAlignment(Qt::AlignRight | Qt::AlignTop);
         setWordWrap(false);
-
+/*
         QPalette palette  = this->palette();
         QColor foreground = palette.color(QPalette::Foreground);
-        foreground.setAlpha(128);
+        foreground.setAlpha(0);
         palette.setColor(QPalette::Foreground, foreground);
-        setPalette(palette);
+        setPalette(palette);*/
     };
 
     ~DTextLabelName(){};
