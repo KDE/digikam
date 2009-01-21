5e77ce1928fd575c6c7d7bb1a11527f78d0de4bb
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-21 08:38:43 +0000

    new panasonic tag for lens description
    svn path=/trunk/extragear/graphics/digikam/; revision=914474

--- libs/dmetadata/dmetadata.cpp
+++ libs/dmetadata/dmetadata.cpp
@@ -6,8 +6,8 @@
  * Date        : 2006-02-23
  * Description : image metadata interface
  *
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -564,6 +564,7 @@ QString DMetadata::getLensDescription() const
     lensExifTags.append("Exif.Nikon3.LensData");     // Nikon Cameras Makernote.
     lensExifTags.append("Exif.Minolta.LensID");      // Minolta Cameras Makernote.
     lensExifTags.append("Exif.Pentax.LensType");     // Pentax Cameras Makernote.
+    lensExifTags.append("Exif.Panasonic.0x0051");    // Panasonic Cameras Makernote.
     lensExifTags.append("Exif.Panasonic.0x0310");    // Panasonic Cameras Makernote.
     lensExifTags.append("Exif.Sigma.LensRange");     // Sigma Cameras Makernote.
     lensExifTags.append("Exif.Photo.0xFDEA");        // Nonstandard Exif tag set by Camera Raw.
--- libs/dmetadata/dmetadata.h
+++ libs/dmetadata/dmetadata.h
@@ -6,8 +6,8 @@
  * Date        : 2006-02-23
  * Description : image metadata interface
  *
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
