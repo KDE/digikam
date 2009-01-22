c954668e1c696c6a7ea2bfdb6e74ac6ede7d3c44
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-22 17:23:00 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=915276

--- utilities/imageeditor/editor/imageiface.cpp
+++ utilities/imageeditor/editor/imageiface.cpp
@@ -7,7 +7,7 @@
  * Description : image data interface for image plugins
  *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -178,7 +178,10 @@ uchar* ImageIface::getPreviewImage() const
             im = new DImg(w, h, s, a, data, true);
             delete [] data;
 
-            if (!im || im->isNull())
+            if (!im)
+                return 0;
+
+            if (im->isNull())
             {
                 delete im;
                 return 0;
