36e0c37ea278ecca06102b306da92ced5f69e68c
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-23 12:07:43 +0000

    yse right button code for Discard action BUG: 181610
    svn path=/trunk/extragear/graphics/digikam/; revision=915530

--- libs/imageproperties/imagedescedittab.cpp
+++ libs/imageproperties/imagedescedittab.cpp
@@ -7,8 +7,8 @@
  * Description : Captions, Tags, and Rating properties editor
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2003-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Copyright (C) 2003-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -455,16 +455,16 @@ void ImageDescEditTab::slotChangingItems()
         }
 
         bool alwaysApply = false;
-        int returnCode = KMessageBox::createKMessageBox
-                         (dialog, QMessageBox::Information,
-                          text, QStringList(),
-                          i18n("Always apply changes without confirmation"),
-                          &alwaysApply, KMessageBox::Notify);
+        int returnCode   = KMessageBox::createKMessageBox(dialog, 
+                                        QMessageBox::Information,
+                                        text, QStringList(),
+                                        i18n("Always apply changes without confirmation"),
+                                        &alwaysApply, KMessageBox::Notify);
 
         if (alwaysApply)
             AlbumSettings::instance()->setApplySidebarChangesDirectly(true);
 
-        if (returnCode == KDialog::User1)
+        if (returnCode == KDialog::No)
             return;
         // otherwise apply
     }
--- libs/imageproperties/imagedescedittab.h
+++ libs/imageproperties/imagedescedittab.h
@@ -7,8 +7,8 @@
  * Description : Captions, Tags, and Rating properties editor
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2003-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Copyright (C) 2003-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
