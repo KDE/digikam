5b7ed3a473e950900f6dbcd404c36432f59a6f62
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-26 08:00:35 +0000

    polish
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=916832

--- utilities/imageeditor/tools/imageresize.h
+++ utilities/imageeditor/tools/imageresize.h
@@ -2,23 +2,23 @@
  *
  * This file is a part of digiKam project
  * http://www.digikam.org
- * 
+ *
  * Date        : 2005-04-07
  * Description : a tool to resize an image
- * 
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * Copyright (C) 2005-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef IMAGE_RESIZE_H
@@ -49,11 +49,11 @@ public:
 
     ImageResize(QWidget* parent);
     ~ImageResize();
-       
+
 protected:
 
     void closeEvent(QCloseEvent *e);
-    
+
 private:
 
     void customEvent(QCustomEvent *event);
@@ -68,14 +68,14 @@ private slots:
     void slotUser3();
     void processCImgURL(const QString&);
     void slotValuesChanged();
-    void readUserSettings();        
-    void slotRestorationToggled(bool);    
+    void readUserSettings();
+    void slotRestorationToggled(bool);
 
 private:
 
     ImageResizePriv *d;
 };
-    
+
 }  // NameSpace Digikam
 
 #endif /* IMAGE_RESIZE_H */
