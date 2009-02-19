44f6fd767a5571c6036e3370584601fe86a646ae
Gilles Caulier <caulier.gilles@gmail.com>
2009-02-19 09:42:30 +0000

    polish
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=928258

--- imageplugins/perspective/matrix.h
+++ imageplugins/perspective/matrix.h
@@ -6,21 +6,21 @@
  * Date        : 2005-02-17
  * Description : a matrix implementation for image
  *               perspective adjustment.
- * 
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2007 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
- * 
+ *
+ * Copyright (C) 2005-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 #ifndef IMAGEEFFECT_PERSPECTIVE_MATRIX_H
--- imageplugins/perspective/perspectivewidget.h
+++ imageplugins/perspective/perspectivewidget.h
@@ -6,8 +6,8 @@
  * Date  : 2005-01-18
  * Description : a widget class to edit perspective.
  *
- * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2005-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -90,7 +90,7 @@ public slots:
 signals:
 
     void signalPerspectiveChanged(QRect newSize, float topLeftAngle, float topRightAngle,
-                                   float bottomLeftAngle, float bottomRightAngle);
+                                  float bottomLeftAngle, float bottomRightAngle);
 
 protected:
 
