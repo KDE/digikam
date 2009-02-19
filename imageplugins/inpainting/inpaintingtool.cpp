8b2c141aef87f3f6270731be5ac3984f0cea1870
Gilles Caulier <caulier.gilles@gmail.com>
2009-02-19 14:05:53 +0000

    sync with trunk BUG: 184731
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=928401

--- imageplugins/inpainting/inpaintingtool.cpp
+++ imageplugins/inpainting/inpaintingtool.cpp
@@ -7,7 +7,7 @@
  * Description : a digiKam image editor plugin to inpaint
  *               a photograph
  *
- * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -67,6 +67,7 @@
 
 #include "daboutdata.h"
 #include "ddebug.h"
+#include "dimgthreadedfilter.h"
 #include "imageiface.h"
 #include "imagewidget.h"
 #include "editortoolsettings.h"
@@ -337,14 +338,15 @@ void InPaintingTool::prepareFinal()
 {
     if (!m_isComputed)
     {
-//FIXME        setProgressVisible(true);
         prepareEffect();
     }
     else
     {
-        putFinalData();
-        kapp->restoreOverrideCursor();
-//FIXME        accept();
+        Digikam::DImgThreadedFilter::EventData *eventData = new Digikam::DImgThreadedFilter::EventData();
+        eventData->progress  = 100;
+        eventData->starting  = false;
+        eventData->success   = true;
+        QApplication::postEvent(this, new QCustomEvent(QEvent::User, eventData));
     }
 }
 
