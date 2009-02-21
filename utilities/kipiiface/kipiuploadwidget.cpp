d8dbea67e4c0fdab95d3aa12156e42afdce149ed
Gilles Caulier <caulier.gilles@gmail.com>
2009-02-21 20:48:44 +0000

    set minimumheight
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=929665

--- utilities/kipiiface/kipiuploadwidget.cpp
+++ utilities/kipiiface/kipiuploadwidget.cpp
@@ -7,7 +7,7 @@
  * Description : a widget to select an image collection
  *               to upload new items using digiKam album folder views
  *
- * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009 by Luka Renko <lure at kubuntu dot org>
  *
  * This program is free software; you can redistribute it
@@ -86,12 +86,12 @@ KipiUploadWidget::KipiUploadWidget(KipiInterface* iface, QWidget *parent)
     d->albumsView->setDropIndicatorShown(false);
     d->albumsView->setAcceptDrops(false);
     d->albumsView->header()->hide();
+    d->albumsView->setMinimumHeight(250);
 
     d->searchBar      = new SearchTextBar(this, "KipiUploadWidgetSearchBar");
 
-    d->newAlbumBtn    = new KPushButton(
-                        KGuiItem(i18n("&New Album"), "albumfolder-new",
-                                 i18n("Create new album")), this);
+    d->newAlbumBtn    = new KPushButton(KGuiItem(i18n("&New Album"), "albumfolder-new",
+                                                 i18n("Create new album")), this);
 
     QGridLayout *grid = new QGridLayout(this);
     grid->addWidget(d->albumsView,   0, 0, 10, 5);
