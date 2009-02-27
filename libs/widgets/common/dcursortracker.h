f97afd8bda1bf687a4528b047f0d67888d71a8f6
Andi Clemens <andi.clemens@gmx.net>
2009-02-27 06:56:08 +0000

    update copyright information
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=932653

--- libs/widgets/common/dcursortracker.h
+++ libs/widgets/common/dcursortracker.h
@@ -8,6 +8,7 @@
  *               Tool tip content is displayed without delay.
  *
  * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C)      2009 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
