8011b29caefcfe5977267b1367a2e459198a37f3
Andi Clemens <andi.clemens@gmx.net>
2009-03-07 14:26:06 +0000

    fix copyright
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=936411

--- libs/widgets/common/dcursortracker.cpp
+++ libs/widgets/common/dcursortracker.cpp
@@ -7,7 +7,7 @@
  * Description : A tool tip widget which follows cursor movements.
  *               Tool tip content is displayed without delay.
  *
- * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C)      2009 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
