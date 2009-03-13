51b935c0d99af574e19ac5f4ea12a4095a87caed
Gilles Caulier <caulier.gilles@gmail.com>
2009-03-13 11:52:21 +0000

    backport commit #938858 from KDE3 branch
    svn path=/trunk/extragear/graphics/digikam/; revision=938867

--- libs/greycstoration/greycstorationiface.cpp
+++ libs/greycstoration/greycstorationiface.cpp
@@ -6,7 +6,7 @@
  * Date        : 2007-12-03
  * Description : Greycstoration interface.
  *
- * Copyright (C) 2007-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -79,7 +79,7 @@ public:
     {
         mode               = GreycstorationIface::Restore;
         gfact              = 1.0;
-        computationThreads = 2;
+        computationThreads = 1;     // Use only one thread. See B.K.O #186642 for details.
     }
 
     float                  gfact;
