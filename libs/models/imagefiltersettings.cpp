3e8d3cf615b3c8bbbbb24c6acc81942710043fe4
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-03-14 21:49:25 +0000

    A dedicated value class to hold filter settings as used traditionally. Comes with public member vars, setter methods for convenience and the matches() method.
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=939449

---
+++ libs/models/imagefiltersettings.cpp
@@ -0,0 +1,301 @@
+/* ============================================================
+*
+* This file is a part of digiKam project
+* http://www.digikam.org
+*
+* Date        : 2009-03-05
+* Description : Filter values for use with ImageFilterModel
+*
+* Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+*
+* This program is free software; you can redistribute it
+* and/or modify it under the terms of the GNU General
+* Public License as published by the Free Software Foundation;
+* either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* ============================================================ */
+
+#include "imagefiltersettings.h"
+
+// Qt includes
+
+#include <QDateTime>
+
+// KDE includes
+
+// Local includes
+
+#include "imageinfo.h"
+
+namespace Digikam
+{
+
+ImageFilterSettings::ImageFilterSettings()
+{
+    untaggedFilter = false;
+    ratingFilter   = 0;
+    mimeTypeFilter = MimeFilter::AllFiles;
+    ratingCond     = ImageFilterSettings::GreaterEqualCondition;
+    matchingCond   = ImageFilterSettings::OrCondition;
+}
+
+void ImageFilterSettings::setDayFilter(const QList<QDateTime>& days)
+{
+    dayFilter.clear();
+
+    for (QList<QDateTime>::const_iterator it = days.constBegin(); it != days.constEnd(); ++it)
+        dayFilter.insert(*it, true);
+}
+
+bool ImageFilterSettings::isFilteringByTags() const
+{
+    if (!tagFilter.isEmpty() || untaggedFilter)
+        return true;
+
+    return false;
+}
+
+bool ImageFilterSettings::isFilteringByText() const
+{
+    if (!textFilterSettings.text.isEmpty())
+        return true;
+
+    return false;
+}
+
+bool ImageFilterSettings::isFiltering() const
+{
+    return !dayFilter.isEmpty() || !tagFilter.isEmpty() || !textFilterSettings.text.isEmpty()
+            || untaggedFilter || ratingFilter!=-1;
+}
+
+void ImageFilterSettings::setTagFilter(const QList<int>& tags, MatchingCondition matchingCond,
+                                       bool showUnTagged)
+{
+    tagFilter      = tags;
+    matchingCond   = matchingCond;
+    untaggedFilter = showUnTagged;
+}
+
+void ImageFilterSettings::setRatingFilter(int rating, RatingCondition ratingCond)
+{
+    ratingFilter = rating;
+    ratingCond   = ratingCond;
+}
+
+void ImageFilterSettings::setMimeTypeFilter(int mime)
+{
+    mimeTypeFilter = (MimeFilter::TypeMimeFilter)mime;
+}
+
+void ImageFilterSettings::setTextFilter(const SearchTextSettings& settings)
+{
+    textFilterSettings = settings;
+}
+
+void ImageFilterSettings::setTagNames(const QHash<int, QString> &hash)
+{
+    tagNameHash = hash;
+}
+
+void ImageFilterSettings::setAlbumNames(const QHash<int, QString> &hash)
+{
+    albumNameHash = hash;
+}
+
+bool ImageFilterSettings::matches(const ImageInfo &info, bool *foundText) const
+{
+    if (!isFiltering())
+        return true;
+
+    bool match = false;
+
+    if (!tagFilter.isEmpty())
+    {
+        QList<int> tagIds = info.tagIds();
+        QList<int>::const_iterator it;
+
+        if (matchingCond == OrCondition)
+        {
+            for (it = tagFilter.begin(); it != tagFilter.end(); ++it)
+            {
+                if (tagIds.contains(*it))
+                {
+                    match = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // AND matching condition...
+
+            for (it = tagFilter.begin(); it != tagFilter.end(); ++it)
+            {
+                if (!tagIds.contains(*it))
+                    break;
+            }
+
+            if (it == tagFilter.end())
+                match = true;
+        }
+
+        match |= (untaggedFilter && tagIds.isEmpty());
+    }
+    else if (untaggedFilter)
+    {
+        match = info.tagIds().isEmpty();
+    }
+    else
+    {
+        match = true;
+    }
+
+    if (!dayFilter.isEmpty())
+    {
+        match &= dayFilter.contains(QDateTime(info.dateTime().date(), QTime()));
+    }
+
+    //-- Filter by rating ---------------------------------------------------------
+
+    if (ratingFilter >= 0)
+    {
+        // for now we treat -1 (no rating) just like a rating of 0.
+        int rating = info.rating();
+        if (rating == -1)
+            rating = 0;
+
+        if (ratingCond == GreaterEqualCondition)
+        {
+            // If the rating is not >=, i.e it is <, then it does not match.
+            if (rating < ratingFilter)
+            {
+                match = false;
+            }
+        }
+        else if (ratingCond == EqualCondition)
+        {
+            // If the rating is not =, i.e it is !=, then it does not match.
+            if (rating != ratingFilter)
+            {
+                match = false;
+            }
+        }
+        else
+        {
+            // If the rating is not <=, i.e it is >, then it does not match.
+            if (rating > ratingFilter)
+            {
+                match = false;
+            }
+        }
+    }
+
+    // -- Filter by mime type -----------------------------------------------------
+
+    switch(mimeTypeFilter)
+    {
+        // info.format is a standardized string: Only one possibility per mime type
+        case MimeFilter::ImageFiles:
+        {
+            if (info.category() != DatabaseItem::Image)
+                match = false;
+            break;
+        }
+        case MimeFilter::JPGFiles:
+        {
+            if (info.format() != "JPG")
+                match = false;
+            break;
+        }
+        case MimeFilter::PNGFiles:
+        {
+            if (info.format() != "PNG")
+                match = false;
+            break;
+        }
+        case MimeFilter::TIFFiles:
+        {
+            if (info.format() != "TIFF")
+                match = false;
+            break;
+        }
+        case MimeFilter::DNGFiles:
+        {
+            if (info.format() != "RAW-DNG")
+                match = false;
+            break;
+        }
+        case MimeFilter::NoRAWFiles:
+        {
+            if (info.format().startsWith("RAW"))
+                match = false;
+            break;
+        }
+        case MimeFilter::RAWFiles:
+        {
+            if (!info.format().startsWith("RAW"))
+                match = false;
+            break;
+        }
+        case MimeFilter::MoviesFiles:
+        {
+            if (info.category() != DatabaseItem::Video)
+                match = false;
+            break;
+        }
+        case MimeFilter::AudioFiles:
+        {
+            if (info.category() != DatabaseItem::Audio)
+                match = false;
+            break;
+        }
+        default:        // All Files: do nothing...
+            break;
+    }
+
+    //-- Filter by text -----------------------------------------------------------
+
+    if (!textFilterSettings.text.isEmpty())
+    {
+        bool textMatch = false;
+
+        // Image name
+        if (info.name().contains(textFilterSettings.text, textFilterSettings.caseSensitive))
+        {
+            textMatch = true;
+        }
+
+        // Image comment
+        if (info.comment().contains(textFilterSettings.text, textFilterSettings.caseSensitive))
+            textMatch = true;
+
+        // Tag names
+        foreach (int id, info.tagIds())
+        {
+            if (tagNameHash.value(id).contains(textFilterSettings.text, textFilterSettings.caseSensitive))
+                textMatch = true;
+        }
+
+        // Album names
+        if (albumNameHash.value(info.albumId()).contains(textFilterSettings.text, textFilterSettings.caseSensitive))
+            textMatch = true;
+
+        match &= textMatch;
+        if (foundText)
+            *foundText = textMatch;
+    }
+
+    return match;
+}
+
+}
+
+
+
---
+++ libs/models/imagefiltersettings.h
@@ -0,0 +1,120 @@
+/* ============================================================
+*
+* This file is a part of digiKam project
+* http://www.digikam.org
+*
+* Date        : 2009-03-05
+* Description : Filter values for use with ImageFilterModel
+*
+* Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+*
+* This program is free software; you can redistribute it
+* and/or modify it under the terms of the GNU General
+* Public License as published by the Free Software Foundation;
+* either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* ============================================================ */
+
+#ifndef IMAGEFILTERSETTINGS_H
+#define IMAGEFILTERSETTINGS_H
+
+// Qt includes.
+
+#include <QHash>
+#include <QList>
+#include <QMap>
+#include <QString>
+
+// Local includes.
+
+#include "searchtextbar.h"
+#include "mimefilter.h"
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class ImageInfo;
+
+class DIGIKAM_EXPORT ImageFilterSettings
+{
+public:
+
+    ImageFilterSettings();
+
+    /**
+     *  Returns true if the given ImageInfo matches the filter criteria.
+     *  Optionally, foundText is set to true if it matched by text search.
+     */
+    bool matches(const ImageInfo &info, bool *foundText = 0) const;
+
+   /// --- Tags filter ---
+
+    /// Possible logical matching condition used to sort tags id.
+    enum MatchingCondition
+    {
+        OrCondition,
+        AndCondition
+    };
+
+    bool                            untaggedFilter;
+    QList<int>                      tagFilter;
+    MatchingCondition               matchingCond;
+
+    void setTagFilter(const QList<int>& tags, MatchingCondition matchingCond,
+                      bool showUnTagged = false);
+
+   /// --- Rating filter ---
+
+    /// Possible conditions used to filter rating: >=, =, <=
+    enum RatingCondition
+    {
+        GreaterEqualCondition,
+        EqualCondition,
+        LessEqualCondition
+    };
+
+    int                             ratingFilter;
+    RatingCondition                 ratingCond;
+
+    void setRatingFilter(int rating, RatingCondition ratingCond);
+
+   /// --- Date filter ---
+
+    QMap<QDateTime, bool>           dayFilter;
+    void setDayFilter(const QList<QDateTime>& days);
+
+
+   /// --- Text filter ---
+
+    SearchTextSettings              textFilterSettings;
+    void setTextFilter(const SearchTextSettings& settings);
+
+    /// Helpers for text search: Set these if you want to search album or tag names with text search
+    QHash<int, QString> tagNameHash;
+    QHash<int, QString> albumNameHash;
+    void setTagNames(const QHash<int, QString> &tagNameHash);
+    void setAlbumNames(const QHash<int, QString> &albumNameHash);
+
+   /// --- Mime filter ---
+
+    MimeFilter::TypeMimeFilter      mimeTypeFilter;
+    void setMimeTypeFilter(int mimeTypeFilter);
+
+    /// Returns if the tag is a filter criteria
+    bool isFilteringByTags() const;
+    /// Returns if the text (including comment) is a filter criteria
+    bool isFilteringByText() const;
+    /// Returns if images will be filtered by these criteria at all
+    bool isFiltering() const;
+};
+
+}
+
+#endif
