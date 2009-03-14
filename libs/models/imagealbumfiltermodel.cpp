fa2cb3e7e4afa280ef0359e252638504393bc193
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-03-14 21:49:42 +0000

    Matching ImageAlbumModel, this is ImageAlbumFilterModel, an ImageFilterModel with minor additions to support AlbumManager and Albums.
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=939451

---
+++ libs/models/imagealbumfiltermodel.cpp
@@ -0,0 +1,130 @@
+/* ============================================================
+*
+* This file is a part of digiKam project
+* http://www.digikam.org
+*
+* Date        : 2009-03-11
+* Description : Qt item model for database entries, using AlbumManager
+*
+* Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+*
+* This program is free software; you can redistribute it
+* and/or modify it under the terms of the GNU General
+* Public License as published by the Free Software Foundation;
+* either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* ============================================================ */
+
+#include "imagealbumfiltermodel.h"
+#include "imagealbumfiltermodel.moc"
+
+// Qt includes.
+
+// KDE includes.
+
+// Local includes.
+
+#include "imagefiltermodelpriv.h"
+#include "album.h"
+#include "albummanager.h"
+#include "databaseaccess.h"
+#include "databasechangesets.h"
+#include "databasewatch.h"
+#include "imagealbummodel.h"
+
+namespace Digikam
+{
+
+class ImageAlbumFilterModelPrivate : public ImageFilterModelPrivate
+{
+public:
+
+    QHash<int, QString> tagNamesHash;
+    QHash<int, QString> albumNamesHash;
+
+    bool albumChange(Album *album);
+};
+
+ImageAlbumFilterModel::ImageAlbumFilterModel(QObject *parent)
+    : ImageFilterModel(*new ImageAlbumFilterModelPrivate, parent)
+{
+}
+
+ImageAlbumFilterModel::~ImageAlbumFilterModel()
+{
+}
+
+void ImageAlbumFilterModel::setSourceImageModel(ImageAlbumModel* model)
+{
+    /*
+    Q_D(ImageAlbumFilterModel)
+    if (dynamic_cast<IoslaveImageModel*>(d->imageModel))
+    {
+        disconnect(d->imageModel, SIGNAL(listedAlbumChanged(Album*)),
+                   this, SLOT(slotListedAlbumChanged(Album *)));
+    }
+
+    connect(model, SIGNAL(listedAlbumChanged(Album*)),
+            this, SLOT(slotListedAlbumChanged(Album *)));
+            */
+
+    ImageFilterModel::setSourceImageModel(model);
+}
+
+/*
+void ImageAlbumFilterModel::slotListedAlbumChanged(Album *album)
+{
+    Q_D(ImageAlbumFilterModel)
+    d->currentAlbum = album;
+}
+*/
+
+void ImageAlbumFilterModel::setImageFilterSettings(const ImageFilterSettings &s)
+{
+    Q_D(ImageAlbumFilterModel);
+    ImageFilterSettings settings(s);
+    settings.setAlbumNames(d->albumNamesHash);
+    settings.setTagNames(d->tagNamesHash);
+    ImageFilterModel::setImageFilterSettings(settings);
+}
+
+void ImageAlbumFilterModel::albumChange(Album *album)
+{
+    Q_D(ImageAlbumFilterModel);
+    if (album->type() == Album::PHYSICAL)
+    {
+        d->albumNamesHash = AlbumManager::instance()->albumTitles();
+        if (d->filter.isFilteringByText())
+            setImageFilterSettings(d->filter);
+    }
+    else if (album->type() == Album::TAG)
+    {
+        d->tagNamesHash = AlbumManager::instance()->tagNames();
+        if (d->filter.isFilteringByText() || d->filter.isFilteringByTags())
+            setImageFilterSettings(d->filter);
+    }
+}
+
+void ImageAlbumFilterModel::slotAlbumRenamed(Album *album)
+{
+    albumChange(album);
+}
+
+void ImageAlbumFilterModel::slotAlbumAdded(Album *album)
+{
+    albumChange(album);
+}
+
+void ImageAlbumFilterModel::slotAlbumAboutToBeDeleted(Album *album)
+{
+    albumChange(album);
+}
+
+}
+
---
+++ libs/models/imagealbumfiltermodel.h
@@ -0,0 +1,75 @@
+/* ============================================================
+*
+* This file is a part of digiKam project
+* http://www.digikam.org
+*
+* Date        : 2009-03-11
+* Description : Qt item model for database entries, using AlbumManager
+*
+* Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+*
+* This program is free software; you can redistribute it
+* and/or modify it under the terms of the GNU General
+* Public License as published by the Free Software Foundation;
+* either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* ============================================================ */
+
+#ifndef IMAGEALBUMFILTERMODEL_H
+#define IMAGEALBUMFILTERMODEL_H
+
+// Qt includes.
+
+// Local includes.
+
+#include "imagefiltermodel.h"
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class Album;
+class ImageAlbumModel;
+class ImageAlbumFilterModelPrivate;
+
+class DIGIKAM_EXPORT ImageAlbumFilterModel : public ImageFilterModel
+{
+    Q_OBJECT
+
+public:
+
+    ImageAlbumFilterModel(QObject *parent = 0);
+    ~ImageAlbumFilterModel();
+
+    virtual void setImageFilterSettings(const ImageFilterSettings &settings);
+
+    void setSourceImageModel(ImageAlbumModel* model);
+
+
+protected slots:
+
+    //void slotListedAlbumChanged(Album *album);
+    void slotAlbumRenamed(Album *album);
+    void slotAlbumAdded(Album *album);
+    void slotAlbumAboutToBeDeleted(Album *album);
+
+private:
+
+    void albumChange(Album *album);
+
+    Q_DECLARE_PRIVATE(ImageAlbumFilterModel)
+
+};
+
+}
+
+#endif
+
+
+
