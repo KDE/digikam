df4ab742014fb2dc944ca5aad471c8a58a0052df
Gilles Caulier <caulier.gilles@gmail.com>
2009-03-15 22:03:23 +0000

    sync with trunk
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=939881

--- libs/imageproperties/worldmapwidget.h
+++ libs/imageproperties/worldmapwidget.h
@@ -6,7 +6,7 @@
  * Date        : 2006-02-20
  * Description : a widget to display GPS info on a world map
  *
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
