d452d5a2d190d85e8a74feb74546e55f1eb2940c
Gilles Caulier <caulier.gilles@gmail.com>
2009-03-15 22:02:36 +0000

    compile
    svn path=/trunk/extragear/graphics/digikam/; revision=939880

--- libs/imageproperties/worldmapwidget.cpp
+++ libs/imageproperties/worldmapwidget.cpp
@@ -6,7 +6,7 @@
  * Date        : 2006-02-20
  * Description : a widget to display GPS info on a world map
  *
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -156,7 +156,11 @@ void WorldMapWidget::setGPSPositions(const GPSInfoList& list)
     {
         d->marbleWidget->setHome(lng, lat);
         d->marbleWidget->centerOn(lng, lat);
+#if MARBLE_VERSION >= 0x00800
         d->marbleWidget->addPlacemarkData(kmlDocument.toString());
+#else
+        d->marbleWidget->addPlaceMarkData(kmlDocument.toString());
+#endif
     }
 #else // MARBLE_VERSION
 
