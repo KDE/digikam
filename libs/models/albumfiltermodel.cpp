79c38b7d8ddc810c0fbc836e681cb99736cc3d01
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-03-27 12:01:03 +0000

    Add a dedicated QSortFilterProxyModel to sort albums in natural order, filter according to SearchTextSettings. Has some convenience methods to access source model.
    svn path=/branches/extragear/graphics/digikam/0.11/; revision=945376

---
+++ libs/models/albumfiltermodel.cpp
@@ -0,0 +1,161 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-03-24
+ * Description : Qt Model for Albums - filter model
+ *
+ * Copyright (C) 2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "albumfiltermodel.h"
+#include "albumfiltermodel.moc"
+
+// Qt includes.
+
+#include <QSortFilterProxyModel>
+
+// KDE includes.
+
+#include <kstringhandler.h>
+
+// Local includes.
+
+#include "albummodel.h"
+
+namespace Digikam
+{
+
+AlbumFilterModel::AlbumFilterModel(QObject *parent)
+    : QSortFilterProxyModel(parent)
+{
+    setDynamicSortFilter(true);
+}
+
+void AlbumFilterModel::setSearchTextSettings(const SearchTextSettings& settings)
+{
+    m_settings = settings;
+    invalidateFilter();
+    emit filterChanged();
+}
+
+bool AlbumFilterModel::isFiltering() const
+{
+    return !m_settings.text.isEmpty();
+}
+
+SearchTextSettings AlbumFilterModel::searchTextSettings() const
+{
+    return m_settings;
+}
+
+void AlbumFilterModel::setSourceAlbumModel(AbstractAlbumModel *source)
+{
+    setSourceModel(source);
+}
+
+void AlbumFilterModel::setSourceModel(QAbstractItemModel* model)
+{
+    // made it protected, only setSourceAlbumModel is public
+    QSortFilterProxyModel::setSourceModel(model);
+}
+
+AbstractAlbumModel *AlbumFilterModel::sourceAlbumModel() const
+{
+    return static_cast<AbstractAlbumModel*>(sourceModel());
+}
+
+Album *AlbumFilterModel::albumForIndex(const QModelIndex &index) const
+{
+    return sourceAlbumModel()->albumForIndex(mapToSource(index));
+}
+
+QModelIndex AlbumFilterModel::indexForAlbum(Album *album) const
+{
+    return mapFromSource(sourceAlbumModel()->indexForAlbum(album));
+}
+
+QModelIndex AlbumFilterModel::rootAlbumIndex() const
+{
+    return mapFromSource(sourceAlbumModel()->rootAlbumIndex());
+}
+
+AlbumFilterModel::MatchResult AlbumFilterModel::matches(const QModelIndex &index) const
+{
+    return matches(sourceAlbumModel()->albumForIndex(index));
+}
+
+AlbumFilterModel::MatchResult AlbumFilterModel::matches(Album *album) const
+{
+    if (!album)
+        return NoMatch;
+
+    PAlbum *palbum = dynamic_cast<PAlbum*>(album);
+
+    if (album->isRoot() || (palbum && palbum->isAlbumRoot()))
+        return SpecialMatch;
+
+    if (album->title().contains(m_settings.text, m_settings.caseSensitive))
+        return TitleMatch;
+
+    // check if any of the parents match the search
+    Album *parent = album->parent();
+    PAlbum* pparent = palbum ? static_cast<PAlbum*>(parent) : 0;
+
+    while (parent && !(parent->isRoot() || (pparent && pparent->isAlbumRoot()) ) )
+    {
+        if (parent->title().contains(m_settings.text, m_settings.caseSensitive))
+            return ParentMatch;
+
+        parent = parent->parent();
+    }
+
+    // check if any of the children match the search
+    AlbumIterator it(album);
+    while (it.current())
+    {
+        if ((*it)->title().contains(m_settings.text, m_settings.caseSensitive))
+            return ChildMatch;
+        ++it;
+    }
+
+    return NoMatch;
+}
+
+bool AlbumFilterModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
+{
+    QModelIndex index = sourceAlbumModel()->index(source_row, 0, source_parent);
+    Album *album = sourceAlbumModel()->albumForIndex(index);
+    if (!album)
+        return false;
+    MatchResult result = matches(album);
+    return result;
+}
+
+bool AlbumFilterModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
+{
+    QVariant valLeft = left.data(sortRole());
+    QVariant valRight = right.data(sortRole());
+
+    if (valLeft.type() == QVariant::String && valRight.type() == QVariant::String)
+        return KStringHandler::naturalCompare(valLeft.toString(), valRight.toString(), sortCaseSensitivity()) < 0;
+    else
+        return QSortFilterProxyModel::lessThan(left, right);
+}
+
+
+}
+
---
+++ libs/models/albumfiltermodel.h
@@ -0,0 +1,97 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-03-24
+ * Description : Qt Model for Albums - filter model
+ *
+ * Copyright (C) 2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef ALBUMFILTERMODEL_H
+#define ALBUMFILTERMODEL_H
+
+// Qt includes.
+
+#include <QSortFilterProxyModel>
+
+// Local includes.
+
+#include "albummodel.h"
+#include "searchtextbar.h"
+
+namespace Digikam
+{
+
+class AlbumFilterModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+public:
+
+    AlbumFilterModel(QObject *parent = 0);
+
+    void setSourceAlbumModel(AbstractAlbumModel *source);
+
+    AbstractAlbumModel *sourceAlbumModel() const;
+
+    Album *albumForIndex(const QModelIndex &index) const;
+    QModelIndex indexForAlbum(Album *album) const;
+    QModelIndex rootAlbumIndex() const;
+
+    /// Returns if the set search text settings will result in any filtering
+    bool isFiltering() const;
+    SearchTextSettings searchTextSettings() const;
+
+    /** Returns if the filter matches this album (same logic as filterAcceptsRow).
+        An album matches if the search text settings are found in a parent album's title,
+        in the album's title or in a child album's title, or if it is a special album (root)
+        that is never filtered out. */
+    enum MatchResult
+    {
+        /// Can use as bool value if match/no match only is needed
+        NoMatch = 0,
+        TitleMatch,
+        ParentMatch,
+        ChildMatch,
+        SpecialMatch
+    };
+    MatchResult matches(Album *album) const;
+    MatchResult matches(const QModelIndex &index) const;
+
+public Q_SLOTS:
+
+    void setSearchTextSettings(const SearchTextSettings& settings);
+
+Q_SIGNALS:
+
+    void filterChanged();
+
+protected:
+
+    // use setSourceAlbumModel please
+    virtual void setSourceModel(QAbstractItemModel* model);
+
+    bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const;
+    bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
+
+    SearchTextSettings m_settings;
+};
+
+}
+
+#endif
+
