fee280723a3c6118521022f4c80b18448192c468
Andi Clemens <andi.clemens@gmx.net>
2009-03-29 22:01:01 +0000

    fix copyright headers
    svn path=/trunk/extragear/graphics/digikam/; revision=946644

--- libs/database/imagescanner.cpp
+++ libs/database/imagescanner.cpp
@@ -6,7 +6,7 @@
  * Date        : 2007-09-19
  * Description : Scanning of a single image
  *
- * Copyright (C) 2007-2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2007-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
