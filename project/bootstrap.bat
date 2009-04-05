467fcf9188ad158ba0bb3d65e24c9af371a7a37d
Gilles Caulier <caulier.gilles@gmail.com>
2009-04-05 09:53:39 +0000

    add codeblock IDE support
    svn path=/trunk/extragear/graphics/digikam/; revision=949407

--- project/bootstrap.bat
+++ project/bootstrap.bat
@@ -7,7 +7,14 @@ set QT_PLUGIN_PATH=%KDEDIRS%\plugins
 set KDE4_INSTALL_DIR=%KDEDIRS%
 
 rem clean up CMake cache
-del -f CMakeCache.txt
+del /F "CMakeCache.txt"
 
 rem configure CMake env.
-cmake -G "MinGW Makefiles" . -DCMAKE_LIBRARY_PATH=%KDEDIRS%/lib -DENABLE_GPHOTO2=OFF -DCMAKE_INSTALL_PREFIX=%KDE4_INSTALL_DIR%
+
+rem We will work on command line using MinGW compiler
+rem set MAKEFILES_TYPE="MinGW Makefiles"
+
+rem We will work with CodeBlock IDE using MinGW compiler
+set MAKEFILES_TYPE="CodeBlocks - MinGW Makefiles"
+
+cmake -G %MAKEFILES_TYPE% . -DCMAKE_LIBRARY_PATH=%KDEDIRS%/lib -DENABLE_GPHOTO2=OFF -DCMAKE_INSTALL_PREFIX=%KDE4_INSTALL_DIR%
