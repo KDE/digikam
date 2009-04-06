9f6875dfd0ed4972ba3e09c99d3abefef82fe8fb
Andi Clemens <andi.clemens@gmx.net>
2009-04-06 07:42:56 +0000

    fix copyright header
    svn path=/trunk/extragear/graphics/digikam/; revision=949894

--- imageplugins/freerotation/freerotationtool.h
+++ imageplugins/freerotation/freerotationtool.h
@@ -7,7 +7,8 @@
  * Description : a digiKam image editor plugin to process image
  *               free rotation.
  *
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009      by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
