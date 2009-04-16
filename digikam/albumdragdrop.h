50651116bdd914cd3e94bdc1cc70e4b862188a43
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-04-16 18:06:34 +0000

    Add an interface to handle drag and drop in album tree model/views. The built-in d'n'd support falls short in quite a few fields, it is not fine-grained enough for our needs, decides too much on its own and allows dropping in between items which we absolutely do not want. We can use most of it but override the important parts.
    A drag and drop handler will do stuff that is on view's terrain like
    displaying context menus, but it will access model's data and backend
    d'n'd is more models' area. So we have this class in between the two,
    a bit like a delegate.
    
    svn path=/trunk/extragear/graphics/digikam/; revision=955001

---
+++ digikam/albumdragdrop.h
@@ -0,0 +1,57 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-04-16
+ * Description : Qt Model for Albums - drag and drop handling
+ *
+ * Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef ALBUMDRAGDROP_H
+#define ALBUMDRAGDROP_H
+
+// Qt includes
+
+// Local includes
+
+#include "albummodeldragdrophandler.h"
+#include "albummodel.h"
+
+
+namespace Digikam
+{
+
+class AlbumDragDropHandler : public AlbumModelDragDropHandler
+{
+    Q_OBJECT
+
+public:
+
+    AlbumDragDropHandler(AlbumModel *model);
+
+    AlbumModel *model() const { return static_cast<AlbumModel*>(m_model); }
+
+    virtual bool dropEvent(QAbstractItemView *view, QDropEvent *e, const QModelIndex &droppedOn);
+    virtual Qt::DropAction accepts(const QMimeData *data, const QModelIndex &dropIndex);
+    virtual QStringList mimeTypes() const;
+    virtual QMimeData *createMimeData(const QList<Album*>);
+};
+
+}
+
+#endif
+
