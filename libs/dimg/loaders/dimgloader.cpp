98e6cec78941e9b5d92cf2b7c86f423b8561ecaf
Gilles Caulier <caulier.gilles@gmail.com>
2009-04-17 06:37:26 +0000

    digiKam 0.11.0 : Color Management : use SRGB D60 color profile intead sRGB D50 for standard JPEG file if no profile is embeded by camera. CCBUGS: 189250
    svn path=/trunk/extragear/graphics/digikam/; revision=955139

--- libs/dimg/loaders/dimgloader.cpp
+++ libs/dimg/loaders/dimgloader.cpp
@@ -7,7 +7,7 @@
  * Description : DImg image loader interface
  *
  * Copyright (C) 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2005-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -198,7 +198,7 @@ bool DImgLoader::checkExifWorkingColorSpace()
         case DMetadata::WORKSPACE_SRGB:
         {
             kDebug(50003) << "Exif color-space tag is sRGB. Using default sRGB ICC profile." << endl;
-            m_image->getICCProfilFromFile(directory + QString("srgb.icm"));
+            m_image->getICCProfilFromFile(directory + QString("srgb-d65.icm"));
             return true;
             break;
         }
