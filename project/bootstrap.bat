3b987f3ce457157679f3dd956de5f22e3696f11e
Gilles Caulier <caulier.gilles@gmail.com>
2009-04-19 21:48:19 +0000

    update
    svn path=/trunk/extragear/graphics/digikam/; revision=956350

--- project/bootstrap.bat
+++ project/bootstrap.bat
@@ -1,20 +1,14 @@
 @echo off
 
-rem KDE4 must be installed into C:\KDE4
-set KDEDIRS=C:\KDE4
+set KDEDIRS=E:\kde4-mingw
 set PATH=%PATH%;%KDEDIRS%\bin
-set QT_PLUGIN_PATH=%KDEDIRS%\plugins
+set QT_PLUGIN_PATH=%KDEDIR%\plugins
 set KDE4_INSTALL_DIR=%KDEDIRS%
+set QT_INSTALL_DIR=%KDEDIRS%
 
 rem clean up CMake cache
-del /F "CMakeCache.txt"
+del /F CMakeCache.txt
 
 rem configure CMake env.
-
-rem We will work on command line using MinGW compiler
-rem set MAKEFILES_TYPE="MinGW Makefiles"
-
-rem We will work with CodeBlock IDE using MinGW compiler
-set MAKEFILES_TYPE="CodeBlocks - MinGW Makefiles"
-
-cmake -G %MAKEFILES_TYPE% . -DCMAKE_LIBRARY_PATH=%KDEDIRS%/lib -DENABLE_GPHOTO2=OFF -DCMAKE_INSTALL_PREFIX=%KDE4_INSTALL_DIR%
+cmake -G "MinGW Makefiles" . -DCMAKE_LIBRARY_PATH=%KDEDIRS%/lib -DENABLE_GPHOTO2=OFF -DCMAKE_INSTALL_PREFIX=%KDE4_INSTALL_DIR% 
+rem cmake -G "Visual Studio 9 2008" . -DEXPAT_LIBRARY=%KDEDIRS%/lib/libexpat.lib  -DCMAKE_LIBRARY_PATH=%KDEDIRS%/lib -DENABLE_GPHOTO2=OFF -DCMAKE_INSTALL_PREFIX=%KDE4_INSTALL_DIR% 
