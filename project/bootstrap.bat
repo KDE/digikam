0b6823cd57741d22e26b5f462975fddd77011ae3
Gilles Caulier <caulier.gilles@gmail.com>
2009-04-24 07:35:48 +0000

    update
    svn path=/trunk/extragear/graphics/digikam/; revision=958512

--- project/bootstrap.bat
+++ project/bootstrap.bat
@@ -1,10 +1,15 @@
 @echo off
 
-set KDEDIRS=E:\kde4-mingw
-set PATH=%PATH%;%KDEDIRS%\bin
-set QT_PLUGIN_PATH=%KDEDIR%\plugins
+set KDEDIRS=C:\kde4-mingw
+set PATH=%PATH%;%KDEDIRS%\bin;%KDEDIRS%\plugins
 set KDE4_INSTALL_DIR=%KDEDIRS%
+set KDEDIRS=%KDEDIRS%
+set KDEROOT=%KDEDIRS%
+set QTMAKESPEC=%KDEDIRS%\mkspecs\win32-g++
+set QT_PLUGIN_PATH=%KDEDIRS%\plugins
+set QTDIR=%KDEDIRS%
 set QT_INSTALL_DIR=%KDEDIRS%
+
 
 rem clean up CMake cache
 del /F CMakeCache.txt
