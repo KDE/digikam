e45d06eb3bf034c2d80bd8f745b71eba025f0ed9
Gilles Caulier <caulier.gilles@gmail.com>
2009-04-25 17:04:22 +0000

    typo
    svn path=/trunk/extragear/graphics/digikam/; revision=959159

--- imageplugins/contentawareresizing/contentawareresizetool.cpp
+++ imageplugins/contentawareresizing/contentawareresizetool.cpp
@@ -262,7 +262,7 @@ ContentAwareResizeTool::ContentAwareResizeTool(QObject *parent)
     d->funcInput->addItem(i18n("Absolute value"));
     d->funcInput->setDefaultIndex(ContentAwareResizeToolPriv::Abs);
     d->funcInput->setWhatsThis(i18n("This option allows you to choose a gradient function. This function is used "
-                                    "to know wich pixels should be removed."));
+                                    "to know which pixels should be removed."));
 
     // -------------------------------------------------------------
 
