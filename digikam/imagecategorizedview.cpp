045a997e7b75d804605a894ac0bf2c45b472bf30
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-04-25 15:59:23 +0000

    Working on various features: - tool tip support - QListView settings - watch flags for ImageModel - item activation handlers (need to check if single/double click   activation settings in Qt honour KDE settings) - methods to access current, selected and all image infos - scrollTo item support - context menu handler
    svn path=/trunk/extragear/graphics/digikam/; revision=959105

--- digikam/imagecategorizedview.cpp
+++ digikam/imagecategorizedview.cpp
@@ -26,18 +26,45 @@
 
 // Qt includes
 
+#include <QHelpEvent>
+
 // KDE includes
 
 // Local includes
 
+#include "databasefields.h"
+#include "imagealbummodel.h"
 #include "imagealbumfiltermodel.h"
 #include "imagethumbnailmodel.h"
 #include "imagedelegate.h"
 #include "imagecategorydrawer.h"
+#include "itemviewtooltip.h"
 #include "thumbnailloadthread.h"
+#include "tooltipfiller.h"
 
 namespace Digikam
 {
+
+class ImageItemViewToolTip : public ItemViewToolTip
+{
+public:
+
+    ImageItemViewToolTip(ImageCategorizedView *view)
+        : ItemViewToolTip(view)
+    {
+    }
+
+    ImageCategorizedView *view() const
+    { return static_cast<ImageCategorizedView*>(ItemViewToolTip::view()); }
+
+protected:
+
+    virtual QString tipContents()
+    {
+        ImageInfo info = ImageModel::retrieveImageInfo(currentIndex());
+        return ToolTipFiller::imageInfoTipContents(info);
+    }
+};
 
 class ImageCategorizedViewPriv
 {
@@ -45,6 +72,12 @@ public:
 
     ImageCategorizedViewPriv()
     {
+        model          = 0;
+        filterModel    = 0;
+        delegate       = 0;
+        categoryDrawer = 0;
+        toolTip        = 0;
+        scrollToItemId = 0;
     }
 
     ImageAlbumModel         *model;
@@ -52,27 +85,70 @@ public:
 
     ImageDelegate           *delegate;
     ImageCategoryDrawer     *categoryDrawer;
+    ImageItemViewToolTip    *toolTip;
 
     ThumbnailSize            thumbnailSize;
+    qlonglong                scrollToItemId;
 };
 
 ImageCategorizedView::ImageCategorizedView(QWidget *parent)
     : KCategorizedView(parent), d(new ImageCategorizedViewPriv)
 {
+    setSpacing(10);
+    setViewMode(QListView::IconMode);
+    setLayoutDirection(Qt::LeftToRight);
+    setFlow(QListView::LeftToRight);
+    setResizeMode(QListView::Adjust);
+    setMovement(QListView::Static);
+    setWrapping(true);
+
+    setSelectionMode(QAbstractItemView::ExtendedSelection);
+
+    setDragEnabled(true);
+    setEditTriggers(QAbstractItemView::NoEditTriggers);
+    viewport()->setAcceptDrops(true);
+    setMouseTracking(true);
+
     d->model = new ImageAlbumModel(this);
     d->model->setThumbnailLoadThread(ThumbnailLoadThread::defaultIconViewThread());
     d->filterModel = new ImageAlbumFilterModel(this);
-    d->filterModel->setSourceModel(d->model);
+    d->filterModel->setSourceImageModel(d->model);
+
+    // set flags that we want to get dataChanged() signals for
+    DatabaseFields::Set watchFlags;
+    watchFlags |= DatabaseFields::Name | DatabaseFields::FileSize | DatabaseFields::ModificationDate;
+    watchFlags |= DatabaseFields::Rating | DatabaseFields::CreationDate | DatabaseFields::Orientation |
+                  DatabaseFields::Width | DatabaseFields::Height;
+    watchFlags |= DatabaseFields::Comment;
+    d->model->setWatchFlags(watchFlags);
 
     d->delegate = new ImageDelegate(this);
     setItemDelegate(d->delegate);
     setCategoryDrawer(d->delegate->categoryDrawer());
 
+    d->toolTip = new ImageItemViewToolTip(this);
+
     setModel(d->filterModel);
+
+    connect(d->model, SIGNAL(signalImageInfosAdded(const QList<ImageInfo> &)),
+            this, SLOT(slotImageInfosAdded()));
+
+    connect(this, SIGNAL(activated(const QModelIndex &)),
+            this, SLOT(slotActivated(const ImageInfo &)));
+    /*
+    if (KGlobalSettings::singleClick()) {
+        connect(this, SIGNAL(clicked(const QModelIndex&)),
+                controller, SLOT(triggerItem(const QModelIndex&)));
+    } else {
+        connect(this, SIGNAL(doubleClicked(const QModelIndex&)),
+                controller, SLOT(triggerItem(const QModelIndex&)));
+    }
+    */
 }
 
 ImageCategorizedView::~ImageCategorizedView()
 {
+    delete d->toolTip;
     delete d;
 }
 
@@ -86,21 +162,99 @@ ImageAlbumFilterModel *ImageCategorizedView::imageFilterModel() const
     return d->filterModel;
 }
 
-ImageDelegate *delegate() const
+ImageDelegate *ImageCategorizedView::delegate() const
 {
     return d->delegate;
 }
+
+ImageInfo ImageCategorizedView::currentInfo() const
+{
+    return d->filterModel->imageInfo(currentIndex());
+}
+
+QList<ImageInfo> ImageCategorizedView::selectedImageInfos() const
+{
+    return d->filterModel->imageInfos(selectedIndexes());
+}
+
+QList<ImageInfo> ImageCategorizedView::selectedImageInfosCurrentFirst() const
+{
+    QList<QModelIndex> indexes = selectedIndexes();
+    QModelIndex current = currentIndex();
+    QList<ImageInfo> infos;
+    foreach (const QModelIndex &index, indexes)
+    {
+        ImageInfo info = d->filterModel->imageInfo(index);
+        if (index == current)
+            infos.prepend(info);
+        else
+            infos.append(info);
+    }
+    return infos;
+}
+
+QList<ImageInfo> ImageCategorizedView::imageInfos() const
+{
+    return d->filterModel->imageInfosSorted();
+}
+
+KUrl::List ImageCategorizedView::urls() const
+{
+    QList<ImageInfo> infos = imageInfos();
+    KUrl::List urls;
+    foreach (const ImageInfo &info, infos)
+        urls << info.fileUrl();
+    return urls;
+}
+
+KUrl::List ImageCategorizedView::selectedUrls() const
+{
+    QList<ImageInfo> infos = selectedImageInfos();
+    KUrl::List urls;
+    foreach (const ImageInfo &info, infos)
+        urls << info.fileUrl();
+    return urls;
+}
+
 
 ThumbnailSize ImageCategorizedView::thumbnailSize() const
 {
     return d->model->thumbnailSize();
 }
 
+void ImageCategorizedView::setThumbnailSize(int size)
+{
+    setThumbnailSize(size);
+}
+
 void ImageCategorizedView::setThumbnailSize(const ThumbnailSize &size)
 {
     d->model->setThumbnailSize(size);
     d->delegate->setThumbnailSize(size);
-    viewport()->update();
+    //viewport()->update();
+}
+
+void ImageCategorizedView::scrollToWhenAvailable(qlonglong imageId)
+{
+    d->scrollToItemId = imageId;
+}
+
+void ImageCategorizedView::scrollToStoredItem()
+{
+    if (d->scrollToItemId)
+    {
+        if (d->model->hasImage(d->scrollToItemId))
+        {
+            scrollTo(d->filterModel->indexForImageId(d->scrollToItemId));
+            d->scrollToItemId = 0;
+        }
+    }
+}
+
+void ImageCategorizedView::slotImageInfosAdded()
+{
+    if (d->scrollToItemId)
+        scrollToStoredItem();
 }
 
 void ImageCategorizedView::slotThemeChanged()
@@ -113,10 +267,35 @@ void ImageCategorizedView::slotSetupChanged()
     viewport()->update();
 }
 
-void ImageCategorizedView::changeEvent(QEvent * event);
+void ImageCategorizedView::slotActivated(const QModelIndex &index)
 {
-    if (event->type == QEvent::FontChange)
-        d->delegate->setDefaultViewOptions(viewOptions());
+    ImageInfo info = d->filterModel->imageInfo(index);
+    if (!info.isNull())
+        activated(info);
+}
+
+void ImageCategorizedView::activated(const ImageInfo &)
+{
+}
+
+void ImageCategorizedView::contextMenuEvent(QContextMenuEvent* event)
+{
+    QModelIndex index = indexAt(event->pos());
+    if (index.isValid())
+    {
+        ImageInfo info = d->filterModel->imageInfo(index);
+        showContextMenu(event, info);
+    }
+    else
+        showContextMenu(event);
+}
+
+void ImageCategorizedView::showContextMenu(QContextMenuEvent *, const ImageInfo &)
+{
+}
+
+void ImageCategorizedView::showContextMenu(QContextMenuEvent *)
+{
 }
 
 bool ImageCategorizedView::viewportEvent(QEvent *event)
@@ -126,7 +305,18 @@ bool ImageCategorizedView::viewportEvent(QEvent *event)
         case QEvent::FontChange:
             d->delegate->setDefaultViewOptions(viewOptions());
             break;
-        case QEvent::helpEvent:
+        case QEvent::ToolTip:
+        {
+            QHelpEvent *he = static_cast<QHelpEvent*>(event);
+            const QModelIndex index = indexAt(he->pos());
+            if (!index.isValid())
+                break;
+            QStyleOptionViewItem option = viewOptions();
+            option.rect = visualRect(index);
+            option.state |= (index == currentIndex() ? QStyle::State_HasFocus : QStyle::State_None);
+            d->toolTip->show(he, option, index);
+            return true;
+        }
         default:
             break;
     }
--- digikam/imagecategorizedview.h
+++ digikam/imagecategorizedview.h
@@ -32,6 +32,7 @@
 
 // Local includes
 
+#include "imageinfo.h"
 #include "thumbnailsize.h"
 
 namespace Digikam
@@ -56,22 +57,51 @@ public:
 
     ImageDelegate *delegate() const;
 
+    ImageInfo currentInfo() const;
+    QList<ImageInfo> selectedImageInfos() const;
+    QList<ImageInfo> selectedImageInfosCurrentFirst() const;
+    QList<ImageInfo> imageInfos() const;
+
+    KUrl::List urls() const;
+    KUrl::List selectedUrls() const;
+
+    ThumbnailSize thumbnailSize() const;
+    void setThumbnailSize(const ThumbnailSize &size);
+
+    /** Scroll the view to the given item when it becomes available */
+    void scrollToWhenAvailable(qlonglong imageId);
+
+public Q_SLOTS:
+
+    void setThumbnailSize(int size);
+
 protected Q_SLOTS:
 
     void slotThemeChanged();
     void slotSetupChanged();
 
+    void slotImageInfosAdded();
+
+    void slotActivated(const QModelIndex &index);
+
 protected:
 
-    void changeEvent(QEvent * event);
+    /// Reimplement these in a subclass
+    virtual void activated(const ImageInfo &info);
+    virtual void showContextMenu(QContextMenuEvent *event, const ImageInfo &info);
+    virtual void showContextMenu(QContextMenuEvent *event);
+
     bool viewportEvent(QEvent *event);
+    void contextMenuEvent(QContextMenuEvent* event);
 
 private:
+
+    void scrollToStoredItem();
 
     ImageCategorizedViewPriv* const d;
 };
 
 }
 
-#endif 
+#endif
 
