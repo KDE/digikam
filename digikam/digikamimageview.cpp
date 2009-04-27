147e16349159c4dfd3ab48279c557902be304ef5
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-04-27 13:08:39 +0000

    Add stub for a ImageCategorizedView derived class that can implement all the non-view-UI functionality of AlbumIconView (manipulating images, drag and drop, context menu etc.)
    svn path=/trunk/extragear/graphics/digikam/; revision=959872

---
+++ digikam/digikamimageview.cpp
@@ -0,0 +1,80 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-04-24
+ * Description : Qt item view for images
+ *
+ * Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "digikamimageview.h"
+#include "digikamimageview.moc"
+
+// Qt includes
+
+// KDE includes
+
+#include <kdebug.h>
+
+// Local includes
+
+#include "imagealbumfiltermodel.h"
+
+namespace Digikam
+{
+
+class DigikamImageViewPriv
+{
+public:
+    DigikamImageViewPriv()
+    {
+    }
+};
+
+DigikamImageView::DigikamImageView(QWidget *parent)
+               : ImageCategorizedView(parent), d(new DigikamImageViewPriv)
+{
+    imageFilterModel()->setCategorizationMode(ImageFilterModel::CategoryByAlbum);
+}
+
+DigikamImageView::~DigikamImageView()
+{
+    delete d;
+}
+
+void DigikamImageView::activated(const ImageInfo &info)
+{
+}
+
+void DigikamImageView::showContextMenu(QContextMenuEvent *event, const ImageInfo &info)
+{
+}
+
+void DigikamImageView::showContextMenu(QContextMenuEvent *event)
+{
+}
+
+void DigikamImageView::copy()
+{
+}
+
+void DigikamImageView::paste()
+{
+}
+
+
+} // namespace Digikam
---
+++ digikam/digikamimageview.h
@@ -0,0 +1,66 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-04-24
+ * Description : Qt item view for images
+ *
+ * Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAMIMAGEVIEW_H
+#define DIGIKAMIMAGEVIEW_H
+
+// Qt includes
+
+// KDE includes
+
+// Local includes
+
+#include "imagecategorizedview.h"
+
+namespace Digikam
+{
+
+class DigikamImageViewPriv;
+
+class DigikamImageView : public ImageCategorizedView
+{
+    Q_OBJECT
+
+public:
+
+    DigikamImageView(QWidget *parent = 0);
+    ~DigikamImageView();
+
+public Q_SLOTS:
+
+protected:
+
+    virtual void activated(const ImageInfo &info);
+    virtual void showContextMenu(QContextMenuEvent *event, const ImageInfo &info);
+    virtual void showContextMenu(QContextMenuEvent *event);
+    virtual void copy();
+    virtual void paste();
+
+private:
+
+    DigikamImageViewPriv* const d;
+};
+
+} // namespace Digikam
+
+#endif /* DIGIKAMIMAGEVIEW_H */
