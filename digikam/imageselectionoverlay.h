09f010afcedfa9b528036758b967ca0a48b553c8
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-04-30 17:53:26 +0000

    The ImageSelectionOverlay is an ImageDelegateOverlay that displays an ItemViewHoverButton with the select/deselect icon in the upper corner of the item's visual rect and adjusts the view's selection.
    svn path=/trunk/extragear/graphics/digikam/; revision=961791

---
+++ digikam/imageselectionoverlay.h
@@ -0,0 +1,78 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-04-30
+ * Description : Qt item view mouse hover button
+ *
+ * Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef IMAGESELECTIONOVERLAY_H
+#define IMAGESELECTIONOVERLAY_H
+
+// Qt includes
+
+#include <QAbstractButton>
+#include <QAbstractItemView>
+
+// KDE includes
+
+// Local includes
+
+#include "itemviewhoverbutton.h"
+#include "imagedelegateoverlay.h"
+
+namespace Digikam
+{
+
+class ImageSelectionOverlayButton : public ItemViewHoverButton
+{
+public:
+
+    ImageSelectionOverlayButton(QAbstractItemView *parentView);
+    virtual QSize sizeHint() const;
+
+protected:
+
+    virtual QPixmap icon();
+    virtual void updateToolTip();
+};
+
+class ImageSelectionOverlay : public HoverWidgetDelegateOverlay
+{
+    Q_OBJECT
+
+public:
+
+    ImageSelectionOverlay(QObject *parent);
+    virtual void setActive(bool active);
+
+protected:
+
+    virtual ItemViewHoverButton *createButton();
+    virtual void updateButton(const QModelIndex &index);
+
+protected Q_SLOTS:
+
+    void slotClicked(bool checked);
+    void slotSelectionChanged(const QItemSelection&, const QItemSelection&);
+};
+
+
+} // namespace Digikam
+
+#endif /* IMAGESELECTIONOVERLAY_H */
