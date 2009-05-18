0deb8ef09bb94d8a06140e4e242646d1db942ff6
Andi Clemens <andi.clemens@gmx.net>
2009-05-18 12:09:59 +0000

    fix layout
    svn path=/trunk/extragear/graphics/digikam/; revision=969448

--- imageplugins/noisereduction/noisereductiontool.cpp
+++ imageplugins/noisereduction/noisereductiontool.cpp
@@ -6,7 +6,7 @@
  * Date        : 2004-08-24
  * Description : a plugin to reduce CCD noise.
  *
- * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -56,14 +56,14 @@
 
 // Local includes
 
-#include "version.h"
 #include "daboutdata.h"
+#include "dexpanderbox.h"
 #include "dimg.h"
+#include "editortoolsettings.h"
 #include "imageiface.h"
 #include "imagepanelwidget.h"
-#include "editortoolsettings.h"
 #include "noisereduction.h"
-#include "dexpanderbox.h"
+#include "version.h"
 
 using namespace KDcrawIface;
 using namespace Digikam;
@@ -263,16 +263,16 @@ NoiseReductionTool::NoiseReductionTool(QObject* parent)
     grid2->setRowStretch(4, 10);
     grid2->setMargin(m_gboxSettings->spacingHint());
     grid2->setSpacing(m_gboxSettings->spacingHint());
-    
+
     DExpanderBox * m_expanderBox = new DExpanderBox;
     m_expanderBox->addItem(firstPage, SmallIcon("noisereduction"), i18n("Details"),
                            QString("DetailsContainer"), true);
     m_expanderBox->addItem(secondPage, SmallIcon("noisereduction"), i18n("Advanced settings"),
                            QString("AdvancedSettingsContainer"), true);
     m_expanderBox->addStretch();
-    
+
     grid->addWidget(m_expanderBox, 0, 0, 1, 1);
-    grid->setRowStretch(1, 10);
+    grid->setRowStretch(0, 10);
     grid->setMargin(m_gboxSettings->spacingHint());
     grid->setSpacing(m_gboxSettings->spacingHint());
 
