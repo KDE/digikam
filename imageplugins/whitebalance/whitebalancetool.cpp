3774a62fb5dc4373f9d67f394035cdcbaf82febd
Andi Clemens <andi.clemens@gmx.net>
2009-05-19 17:53:31 +0000

    fix copyright header
    svn path=/trunk/extragear/graphics/digikam/; revision=970160

--- imageplugins/whitebalance/whitebalancetool.cpp
+++ imageplugins/whitebalance/whitebalancetool.cpp
@@ -7,8 +7,8 @@
  * Description : a digiKam image editor plugin to correct
  *               image white balance
  *
- * Copyright (C) 2005-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2008 by Guillaume Castagnino <casta at xwing dot info>
+ * Copyright (C) 2008-2009 by Guillaume Castagnino <casta at xwing dot info>
+ * Copyright (C) 2005-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
