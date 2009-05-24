59707b4ed98c0b54fae3b82ae37dc0e18d24416b
Gilles Caulier <caulier.gilles@gmail.com>
2009-05-24 14:04:45 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=972294

--- libs/threadimageio/thumbnailbasic.cpp
+++ libs/threadimageio/thumbnailbasic.cpp
@@ -7,8 +7,8 @@
  * Description : Loader for thumbnails
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2003-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2008 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Copyright (C) 2003-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
