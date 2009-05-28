f15f712f54ae71a5abbc8850b5915ebde739e06f
Andi Clemens <andi.clemens@gmx.net>
2009-05-28 15:53:29 +0000

    I guess the plural needs to be used here... at least it sounds better :-)
    svn path=/trunk/extragear/graphics/digikam/; revision=974154

--- libs/database/dbstatdlg.h
+++ libs/database/dbstatdlg.h
@@ -4,7 +4,7 @@
  * http://www.digikam.org
  *
  * Date        : 2009-05-28
- * Description : database statistic dialog
+ * Description : database statistics dialog
  *
  * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
