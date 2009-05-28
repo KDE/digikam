0cc16458f1863c81d2d2a6691f95f9507b3b6bbb
Gilles Caulier <caulier.gilles@gmail.com>
2009-05-28 07:49:00 +0000

    backport commit #973904 from trunk CCBUGS: 169213
    svn path=/branches/extragear/kde3/graphics/digikam/; revision=973914

--- digikam/albumfolderview.cpp
+++ digikam/albumfolderview.cpp
@@ -7,7 +7,7 @@
  * Description : Albums folder view.
  *
  * Copyright (C) 2005-2006 by Joern Ahrens <joern.ahrens@kdemail.net>
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
@@ -785,7 +785,6 @@ void AlbumFolderView::albumNew(AlbumFolderViewItem *item)
             item->setOpen(true);
 
         ensureItemVisible(newItem);
-        setSelected(newItem, true);
     }
 }
 
