344dc65896fc49b100996ce356ada142a1a524c7
Andi Clemens <andi.clemens@gmx.net>
2009-05-31 08:26:37 +0000

    update copyright header
    svn path=/trunk/extragear/graphics/digikam/; revision=975782

--- imageplugins/freerotation/freerotation.h
+++ imageplugins/freerotation/freerotation.h
@@ -5,22 +5,23 @@
  *
  * Date        : 2005-07-18
  * Description : Free rotation threaded image filter.
- * 
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * 
+ *
+ * Copyright (C) 2004-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
-  
+
 #ifndef FREE_ROTATION_H
 #define FREE_ROTATION_H
 
@@ -40,33 +41,33 @@ class FreeRotation : public Digikam::DImgThreadedFilter
 {
 
 public:
-    
+
     FreeRotation(Digikam::DImg *orgImage, QObject *parent=0, double angle=0.0,
-                 bool antialiasing=true, int autoCrop=NoAutoCrop, QColor backgroundColor=Qt::black, 
+                 bool antialiasing=true, int autoCrop=NoAutoCrop, QColor backgroundColor=Qt::black,
                  int orgW=0, int orgH=0);
-    
+
     ~FreeRotation(){};
-    
+
     QSize getNewSize(void){ return m_newSize; };
 
 public:
-    
-    enum AutoCropTypes 
+
+    enum AutoCropTypes
     {
         NoAutoCrop=0,
         WidestArea,
         LargestArea
     };
-        
-private:  
+
+private:
 
     virtual void filterImage(void);
-           
+
     inline int setPosition (int Width, int X, int Y)
     {
-       return (Y *Width*4 + 4*X); 
+       return (Y *Width*4 + 4*X);
     };
-    
+
     inline bool isInside (int Width, int Height, int X, int Y)
     {
        bool bIsWOk = ((X < 0) ? false : (X >= Width ) ? false : true);
@@ -74,20 +75,20 @@ private:
        return (bIsWOk && bIsHOk);
     };
 
-private:  
+private:
 
     bool   m_antiAlias;
-    
+
     int    m_autoCrop;
     int    m_orgW;
     int    m_orgH;
-    
+
     double m_angle;
-    
+
     QSize  m_newSize;
- 
+
     QColor m_backgroundColor;
-};    
+};
 
 }  // namespace DigikamFreeRotationImagesPlugin
 
