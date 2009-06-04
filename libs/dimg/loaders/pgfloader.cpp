b38fb10cacfbbed08d30f1533298955b985bf210
Gilles Caulier <caulier.gilles@gmail.com>
2009-06-04 18:45:27 +0000

    first version of PGF writter
    svn path=/trunk/extragear/graphics/digikam/; revision=977590

--- libs/dimg/loaders/pgfloader.cpp
+++ libs/dimg/loaders/pgfloader.cpp
@@ -132,7 +132,6 @@ bool PGFLoader::load(const QString& filePath, DImgLoaderObserver *observer)
         return false;
 #endif
 
-    // Create stream object and image
     CPGFFileStream stream(fd);
     CPGFImage      pgf;
     int            colorModel = DImg::COLORMODELUNKNOWN;
@@ -197,7 +196,8 @@ bool PGFLoader::load(const QString& filePath, DImgLoaderObserver *observer)
             data = new uchar[width*height*4];  // 8 bits/color/pixel
 
         pgf.Read(0, CallbackForLibPGF, this);
-        pgf.GetBitmap(m_sixteenBit ? width*8 : width*4, (UINT8*)data, m_sixteenBit ? 64 : 32, channelMap, CallbackForLibPGF, this);
+        pgf.GetBitmap(m_sixteenBit ? width*8 : width*4, (UINT8*)data, m_sixteenBit ? 64 : 32,
+                      channelMap, CallbackForLibPGF, this);
 
         if (observer)
             observer->progressInfo(m_image, 1.0);
@@ -209,6 +209,11 @@ bool PGFLoader::load(const QString& filePath, DImgLoaderObserver *observer)
         imageSetAttribute("originalColorModel", colorModel);
         imageSetAttribute("originalBitDepth", bitDepth);
 
+#ifdef WIN32
+        CloseHandle(fd);
+#else
+        close(fd);
+#endif
         return true;
     }
     catch(IOException& e)
@@ -248,228 +253,69 @@ bool PGFLoader::save(const QString& filePath, DImgLoaderObserver *observer)
     }
 #endif
 
-    // create stream object
-    CPGFFileStream stream(fd);
-
-    CPGFImage pgf;
-/*
-    // -------------------------------------------------------------------
-    // Initialize JPEG 2000 API.
-
-    register long  i, x, y;
-    unsigned long  number_components;
-
-    jas_image_t          *jp2_image   = 0;
-    jas_stream_t         *jp2_stream  = 0;
-    jas_matrix_t         *pixels[4];
-    jas_image_cmptparm_t  component_info[4];
-
-    int init = jas_init();
-    if (init != 0)
+    try
     {
-        kDebug(50003) << "Unable to init JPEG2000 decoder" << endl;
-        return false;
+        int channelMap[] = { 0, 1, 2, 3 };
+        QVariant qualityAttr = imageGetAttribute("quality");
+        int quality          = qualityAttr.isValid() ? qualityAttr.toInt() : 90;
+
+        if (quality < 0)
+            quality = 90;
+        if (quality > 100)
+            quality = 100;
+
+        // NOTE: to have a lossless compression use quality=100.
+        int rate= quality / 100.0F;
+
+        kDebug(50003) << "PGF quality: " << quality << endl;
+        kDebug(50003) << "PGF rate " << rate << endl;
+
+        CPGFFileStream stream(fd);
+        CPGFImage      pgf;
+        PGFHeader      header;
+        header.width    = imageWidth();
+        header.height   = imageHeight();
+        header.bpp      = imageBitsDepth();
+        header.channels = imageHasAlpha() ? 4 : 3;
+        header.quality  = rate;
+        header.mode     = imageHasAlpha() ? ImageModeRGBA : ImageModeRGBColor;
+        header.background.rgbtBlue = header.background.rgbtGreen = header.background.rgbtRed = 0;
+        pgf.SetHeader(header);
+
+        pgf.ImportBitmap(imageBytesDepth()*imageWidth(), (UINT8*)imageData(), imageBitsDepth(),
+                         channelMap, CallbackForLibPGF, this);
+
+        UINT32 nWrittenBytes = 0;
+        pgf.Write(&stream, 0, CallbackForLibPGF, &nWrittenBytes, this);
+
+#ifdef WIN32
+        CloseHandle(fd);
+#else
+        close(fd);
+#endif
+
+        if (observer)
+            observer->progressInfo(m_image, 1.0);
+
+        imageSetAttribute("savedformat", "PGF");
+        saveMetadata(filePath);
+
+        return true;
     }
-
-    jp2_stream = jas_stream_fopen(QFile::encodeName(filePath), "wb");
-    if (jp2_stream == 0)
+    catch(IOException& e)
     {
-        kDebug(50003) << "Unable to open JPEG2000 stream" << endl;
-        return false;
-    }
+        int err = e.error;
+        if (err >= AppError) err -= AppError;
+        kDebug(50003) << "Error: Opening and saving PGF image failed (" << err << ")!" << endl;
 
-    number_components = imageHasAlpha() ? 4 : 3;
-
-    for (i = 0 ; i < (long)number_components ; ++i)
-    {
-        component_info[i].tlx    = 0;
-        component_info[i].tly    = 0;
-        component_info[i].hstep  = 1;
-        component_info[i].vstep  = 1;
-        component_info[i].width  = imageWidth();
-        component_info[i].height = imageHeight();
-        component_info[i].prec   = imageBitsDepth();
-        component_info[i].sgnd   = false;
-    }
-
-    jp2_image = jas_image_create(number_components, component_info, JAS_CLRSPC_UNKNOWN);
-    if (jp2_image == 0)
-    {
-        jas_stream_close(jp2_stream);
-        kDebug(50003) << "Unable to create JPEG2000 image" << endl;
-        return false;
-    }
-
-    if (observer)
-        observer->progressInfo(m_image, 0.1F);
-
-    // -------------------------------------------------------------------
-    // Check color space.
-
-    if (number_components >= 3 )    // RGB & RGBA
-    {
-        // Alpha Channel
-        if (number_components == 4 )
-            jas_image_setcmpttype(jp2_image, 3, JAS_IMAGE_CT_OPACITY);
-
-        jas_image_setclrspc(jp2_image, JAS_CLRSPC_SRGB);
-        jas_image_setcmpttype(jp2_image, 0, JAS_IMAGE_CT_COLOR(JAS_CLRSPC_CHANIND_RGB_R));
-        jas_image_setcmpttype(jp2_image, 1, JAS_IMAGE_CT_COLOR(JAS_CLRSPC_CHANIND_RGB_G));
-        jas_image_setcmpttype(jp2_image, 2, JAS_IMAGE_CT_COLOR(JAS_CLRSPC_CHANIND_RGB_B));
-    }
-
-    // -------------------------------------------------------------------
-    // Set ICC color profile.
-
-    // FIXME : doesn't work yet!
-
-    jas_cmprof_t  *cm_profile  = 0;
-    jas_iccprof_t *icc_profile = 0;
-
-    QByteArray profile_rawdata = m_image->getICCProfil();
-
-    icc_profile = jas_iccprof_createfrombuf((uchar*)profile_rawdata.data(), profile_rawdata.size());
-    if (icc_profile != 0)
-    {
-        cm_profile = jas_cmprof_createfromiccprof(icc_profile);
-        if (cm_profile != 0)
-        {
-            jas_image_setcmprof(jp2_image, cm_profile);
-        }
-    }
-
-    // -------------------------------------------------------------------
-    // Convert to JPEG 2000 pixels.
-
-    for (i = 0 ; i < (long)number_components ; ++i)
-    {
-        pixels[i] = jas_matrix_create(1, (unsigned int)imageWidth());
-        if (pixels[i] == 0)
-        {
-            for (x = 0 ; x < i ; ++x)
-            jas_matrix_destroy(pixels[x]);
-
-            jas_image_destroy(jp2_image);
-            kDebug(50003) << "Error encoding JPEG2000 image data : Memory Allocation Failed" << endl;
-            return false;
-        }
-    }
-
-    unsigned char* data = imageData();
-    unsigned char* pixel;
-    unsigned short r, g, b, a=0;
-    uint           checkpoint = 0;
-
-    for (y = 0 ; y < (long)imageHeight() ; ++y)
-    {
-        if (observer && y == (long)checkpoint)
-        {
-            checkpoint += granularity(observer, imageHeight(), 0.8F);
-            if (!observer->continueQuery(m_image))
-            {
-                jas_image_destroy(jp2_image);
-                for (i = 0 ; i < (long)number_components ; ++i)
-                    jas_matrix_destroy(pixels[i]);
-
-                jas_cleanup();
-
-                return false;
-            }
-            observer->progressInfo(m_image, 0.1 + (0.8 * ( ((float)y)/((float)imageHeight()) )));
-        }
-
-        for (x = 0 ; x < (long)imageWidth() ; ++x)
-        {
-            pixel = &data[((y * imageWidth()) + x) * imageBytesDepth()];
-
-            if ( imageSixteenBit() )        // 16 bits image.
-            {
-                b = (unsigned short)(pixel[0]+256*pixel[1]);
-                g = (unsigned short)(pixel[2]+256*pixel[3]);
-                r = (unsigned short)(pixel[4]+256*pixel[5]);
-
-                if (imageHasAlpha())
-                    a = (unsigned short)(pixel[6]+256*pixel[7]);
-            }
-            else                            // 8 bits image.
-            {
-                b = (unsigned short)pixel[0];
-                g = (unsigned short)pixel[1];
-                r = (unsigned short)pixel[2];
-
-                if (imageHasAlpha())
-                    a = (unsigned short)(pixel[3]);
-            }
-
-            jas_matrix_setv(pixels[0], x, r);
-            jas_matrix_setv(pixels[1], x, g);
-            jas_matrix_setv(pixels[2], x, b);
-
-            if (number_components > 3)
-                jas_matrix_setv(pixels[3], x, a);
-        }
-
-        for (i = 0 ; i < (long)number_components ; ++i)
-        {
-            int ret = jas_image_writecmpt(jp2_image, (short) i, 0, (unsigned int)y,
-                                          (unsigned int)imageWidth(), 1, pixels[i]);
-            if (ret != 0)
-            {
-                kDebug(50003) << "Error encoding JPEG2000 image data" << endl;
-
-                jas_image_destroy(jp2_image);
-                for (i = 0 ; i < (long)number_components ; ++i)
-                    jas_matrix_destroy(pixels[i]);
-
-                jas_cleanup();
-                return false;
-            }
-        }
-    }
-
-    QVariant qualityAttr = imageGetAttribute("quality");
-    int quality          = qualityAttr.isValid() ? qualityAttr.toInt() : 90;
-
-    if (quality < 0)
-        quality = 90;
-    if (quality > 100)
-        quality = 100;
-
-    QString     rate;
-    QTextStream ts( &rate, QIODevice::WriteOnly );
-
-    // NOTE: to have a lossless compression use quality=100.
-    // jp2_encode()::optstr:
-    // - rate=#B => the resulting file size is about # bytes
-    // - rate=0.0 .. 1.0 => the resulting file size is about the factor times
-    //                      the uncompressed size
-    ts << "rate=" << ( quality / 100.0F );
-
-    kDebug(50003) << "JPEG2000 quality: " << quality << endl;
-    kDebug(50003) << "JPEG2000 " << rate << endl;
-
-    int ret = jp2_encode(jp2_image, jp2_stream, rate.toUtf8().data());
-    if (ret != 0)
-    {
-        kDebug(50003) << "Unable to encode JPEG2000 image" << endl;
-
-        jas_image_destroy(jp2_image);
-        jas_stream_close(jp2_stream);
-        for (i = 0 ; i < (long)number_components ; ++i)
-            jas_matrix_destroy(pixels[i]);
-
-        jas_cleanup();
+#ifdef WIN32
+        CloseHandle(fd);
+#else
+        close(fd);
+#endif
 
         return false;
     }
-*/
-    if (observer)
-        observer->progressInfo(m_image, 1.0);
-
-    imageSetAttribute("savedformat", "PGF");
-    saveMetadata(filePath);
-
-    return false;
 }
 
 bool PGFLoader::hasAlpha() const
