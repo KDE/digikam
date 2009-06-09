c9cdf7b653a2735ddea75101692be3a0c4a7c0cd
Andi Clemens <andi.clemens@gmx.net>
2009-06-09 09:46:39 +0000

    Add initial tests for ManualRenameInput widget. Right now they do nothing, tests need to be written.
    svn path=/trunk/extragear/graphics/digikam/; revision=979218

---
+++ tests/manualrenameinputtest.cpp
@@ -0,0 +1,38 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : Jun 9, 2009
+ * Description : a test for the ManualRenameInput widget
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// KDE includes
+
+#include <qtest_kde.h>
+
+// Local includes
+
+#include "manualrenameinputtest.h"
+
+QTEST_KDEMAIN_CORE(ManualRenameInputTest)
+
+void ManualRenameInputTest::testNumberToken()
+{
+    // dummy check
+    QVERIFY(1);
+}
---
+++ tests/manualrenameinputtest.h
@@ -0,0 +1,40 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : Jun 9, 2009
+ * Description : a test for the ManualRenameInput widget
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef MANUALRENAMEINPUTTEST_H_
+#define MANUALRENAMEINPUTTEST_H_
+
+// Qt includes
+
+#include <QtCore/QObject>
+
+class ManualRenameInputTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+
+    void testNumberToken();
+};
+
+#endif /* MANUALRENAMEINPUTTEST_H_ */
