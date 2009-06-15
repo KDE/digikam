eb0867c3f89197195badab244803283dd6a86120
Gilles Caulier <caulier.gilles@gmail.com>
2009-06-15 19:21:07 +0000

    compile under MSVC 9
    svn path=/trunk/extragear/graphics/digikam/; revision=982396

--- libs/3rdparty/libpgf/PGFplatform.h
+++ libs/3rdparty/libpgf/PGFplatform.h
@@ -25,6 +25,8 @@
 #define PGF_PGFPLATFORM_H
 
 #include <cassert>
+#include <cmath>
+#include <cstdlib>
 
 //-------------------------------------------------------------------------------
 // ROI support
