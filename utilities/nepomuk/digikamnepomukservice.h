7b0e0fb17f6d053f63624d0c13273cac33f17287
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-06-23 21:17:53 +0000

    Nepomuk coding sprint results: Initial revision of a Digikam <-> Nepomuk synchronisation bridge. For now this is implemented as a Nepomuk service running independently from the main digikam instance. Syncs rating, comment and tags; tags is one-way currently, syncing tags from Nepomuk to digikam is awaiting implementation. Changes in digikam are immediately pushed to Nepomuk by listening to digikam's DBus signals. After first start, a full sync of all entries is triggered. The initial sync is recorded in the database. Changes in Nepomuk are registered by listening to global change signals. After a timed delay a Sparql query retrieves all relevant changes that occurred since the last check. The time is recorded in the database. The database file path is read from the digikam configuration file. If an instance is running, it is queried directly. If the database file changes, the service is informed by digikam. (This does not scale well to several instances running different DBs, but digikam does not support this scenario anyway) Both syncing directions are independently configurable. Default is false for now; we should consider defaulting to true.
    Note: I am running KDE4.3beta2 at the moment. If there is code not compiling with
    KDE4.2, we must add a conditional switch to CMake.
    
    CCMAIL: digikam-devel@kde.org trueg@kde.org
    
    svn path=/trunk/extragear/graphics/digikam/; revision=986000

---
+++ utilities/nepomuk/digikamnepomukservice.h
@@ -0,0 +1,126 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-06-19
+ * Description : Service to sync digikam and nepomuk storages
+ *
+ * Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAMNEPOMUKSERVICE_H
+#define DIGIKAMNEPOMUKSERVICE_H
+
+// Qt includes
+
+// KDE includes
+
+#include <ksharedconfig.h>
+#include <kurl.h>
+#include <Nepomuk/Service>
+
+// Local includes
+
+class KJob;
+namespace KIO { class Job; }
+namespace Soprano { class Statement; }
+
+namespace Digikam
+{
+
+class DatabaseParameters;
+class ImageInfo;
+class ImageChangeset;
+class ImageTagChangeset;
+class TagChangeset;
+class NepomukServicePriv;
+
+class NepomukService : public Nepomuk::Service
+{
+    Q_OBJECT
+    Q_CLASSINFO( "D-Bus Interface", "org.kde.digikam.DigikamNepomukService" )
+
+public:
+
+    NepomukService(QObject* parent, const QVariantList&);
+    ~NepomukService();
+
+public Q_SLOTS:
+
+    /** Sets the digikam database to watch.
+     *  The parameter is the url() of a KUrl which contains the database parameters
+     *  serialized by DatabaseParameters.
+     */
+    Q_SCRIPTABLE void databaseChanged();
+    Q_SCRIPTABLE void enableSyncToDigikam(bool syncToDigikam);
+    Q_SCRIPTABLE void enableSyncToNepomuk(bool syncToNepomuk);
+
+protected Q_SLOTS:
+
+    void slotImageChange(const ImageChangeset& changeset);
+    void slotImageTagChange(const ImageTagChangeset& changeset);
+    void slotTagChange(const TagChangeset& changeset);
+
+    void slotStatementAdded(const Soprano::Statement& statement);
+    void slotStatementRemoved(const Soprano::Statement& statement);
+
+    void syncNepomukToDigikam();
+    void fullSyncDigikamToNepomuk();
+
+    void cleanIgnoreList();
+
+    void slotFullSyncJobResult(KJob *job);
+    void slotFullSyncJobData(KIO::Job*, const QByteArray& data);
+
+protected:
+
+    void connectToDatabase();
+
+    enum SyncToNepomukSettings
+    {
+        SyncNothing = 0,
+        SyncRating = 0x1,
+        SyncHasNoRating = 0x2,
+        SyncComment = 0x4
+    };
+
+    void readConfig();
+    void syncToNepomuk(const QList<qlonglong> &imageid, SyncToNepomukSettings syncSettings);
+    void syncToNepomuk(const QList<ImageInfo> &infos, SyncToNepomukSettings syncSettings);
+    void syncTagsToNepomuk(const QList<qlonglong> &imageIds, const QList<int> &tagIds, bool addOrRemove);
+    void syncRatingToDigikam(const KUrl::List &filePaths, const QList<int> &ratings);
+    void syncCommentToDigikam(const KUrl::List &filePaths, const QStringList &ratings);
+    void syncTagsToDigikam(const KUrl::List &filePaths, const QList<QUrl> &tags);
+    void pushTagsToNepomuk(const QList<ImageInfo> &imageInfos);
+
+    QDateTime lastSyncToDigikam();
+    void markAsSyncedToDigikam();
+    bool hasSyncToNepomuk();
+    void markAsSyncedToNepomuk();
+    void checkTagList();
+    DatabaseParameters databaseParameters();
+    KSharedConfig::Ptr digikamConfig();
+
+private:
+
+    NepomukServicePriv* const d;
+};
+
+} // namespace Digikam
+
+#endif // DIGIKAMNEPOMUKSERVICE_H
+
+
