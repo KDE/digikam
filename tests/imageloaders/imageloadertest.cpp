e358bfe66ee5b5baa403b953f5f069569f04ac67
Andi Clemens <andi.clemens@gmx.net>
2009-06-25 16:46:59 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=987138

--- tests/imageloaders/imageloadertest.cpp
+++ tests/imageloaders/imageloadertest.cpp
@@ -56,6 +56,7 @@ void ImageLoaderTest::testLoadPNG()
     QCOMPARE((uint)src.height(),    dimg.height());
     QCOMPARE(src.hasAlphaChannel(), dimg.hasAlpha());
 
+    // compare image data
     dst = dimg.convertToPixmap().toImage().convertToFormat(src.format());
     QCOMPARE(src, dst);
 }
@@ -76,6 +77,7 @@ void ImageLoaderTest::testLoadJPG()
     QCOMPARE((uint)src.height(),    dimg.height());
     QCOMPARE(src.hasAlphaChannel(), dimg.hasAlpha());
 
+    // compare image data
     dst = dimg.convertToPixmap().toImage().convertToFormat(src.format());
     QCOMPARE(src, dst);
 }
