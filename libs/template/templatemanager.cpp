954d317aa3848e044ef3cf714c25b57ebda201d9
Gilles Caulier <caulier.gilles@gmail.com>
2009-06-26 04:29:45 +0000

    credits
    svn path=/trunk/extragear/graphics/digikam/; revision=987439

--- libs/template/templatemanager.cpp
+++ libs/template/templatemanager.cpp
@@ -7,6 +7,7 @@
  * Description : metadata template manager.
  *
  * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
