99143d56dfa9e12460c998c998e8c52435c2d474
Gilles Caulier <caulier.gilles@gmail.com>
2009-07-16 03:20:41 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=997580

--- libs/template/altlangstredit.cpp
+++ libs/template/altlangstredit.cpp
@@ -368,7 +368,7 @@ void AltLangStrEdit::slotDeleteValue()
 void AltLangStrEdit::slotSelectionChanged(int index)
 {
     QString lang = d->languageCB->currentText();
-    d->valueEdit->setSpellCheckingLanguage(lang); 
+    d->valueEdit->setSpellCheckingLanguage(lang.replace("-", "_")); 
     d->valueEdit->blockSignals(true);
 
     if (!d->languageCB->itemIcon(index).isNull())
