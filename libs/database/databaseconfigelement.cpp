729d750f26763d89cd36efc9e899e03b595d9a63
Gilles Caulier <caulier.gilles@gmail.com>
2009-07-22 08:05:30 +0000

    fix coding style. Never use tabs in source code. Missing header. Please use digiKam header text. CCMAIL·: hamsi2k@freenet.de
    svn path=/branches/extragear/graphics/digikam/1.0-databaseext/; revision=1000854

--- libs/database/databaseconfigelement.cpp
+++ libs/database/databaseconfigelement.cpp
@@ -1,19 +1,42 @@
-/*
- * databaseconfigelement.cpp
+/* ============================================================
  *
- *  Created on: 27.06.2009
- *      Author: mueller
- */
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-06-27
+ * Description : Database element configuration
+ *
+ * Copyright (C) 2009 by Holger Foerster <hamsi2k at freenet dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
-#include <databaseconfigelement.h>
+// Local includes.
 
-namespace Digikam{
-databaseconfigelement::databaseconfigelement() {
-	// TODO Auto-generated constructor stub
+#include "databaseconfigelement.h"
+
+namespace Digikam
+{
+
+databaseconfigelement::databaseconfigelement()
+{
+    // TODO Auto-generated constructor stub
 
 }
 
-databaseconfigelement::~databaseconfigelement() {
-	// TODO Auto-generated destructor stub
+databaseconfigelement::~databaseconfigelement()
+{
+    // TODO Auto-generated destructor stub
 }
-}
+
+} // namespace Digikam
--- libs/database/databaseconfigelement.h
+++ libs/database/databaseconfigelement.h
@@ -1,12 +1,30 @@
-/*
- * databaseconfigelement.h
+/* ============================================================
  *
- *  Created on: 27.06.2009
- *      Author: mueller
- */
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-06-27
+ * Description : Database element configuration
+ *
+ * Copyright (C) 2009 by Holger Foerster <hamsi2k at freenet dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #ifndef DATABASECONFIGELEMENT_H_
 #define DATABASECONFIGELEMENT_H_
+
+// Qt includes.
 
 #include <QtGlobal>
 #include <QMap>
@@ -16,34 +34,41 @@
 
 #include "digikam_export.h"
 
-namespace Digikam{
-            class DIGIKAM_EXPORT databaseActionElement {
-            public:
-                QString m_Mode;
-                int     m_Order;
-                QString m_Statement;
-        };
+namespace Digikam
+{
 
-        class DIGIKAM_EXPORT databaseAction {
-                public:
-                    QString m_Name;
-		    QString m_Mode;
-                    QList<databaseActionElement> m_DBActionElements;
-        };
+class DIGIKAM_EXPORT databaseActionElement
+{
+    public:
+        QString m_Mode;
+        int     m_Order;
+        QString m_Statement;
+};
 
-	class DIGIKAM_EXPORT databaseconfigelement {
-	public:
-		databaseconfigelement();
-		virtual ~databaseconfigelement();
+class DIGIKAM_EXPORT databaseAction
+{
+    public:
+        QString m_Name;
+        QString m_Mode;
+        QList<databaseActionElement> m_DBActionElements;
+};
 
-		QString m_DatabaseID;
-		QString m_HostName;
-		QString m_Port;
-		QString m_ConnectOptions;
-		QString m_DatabaseName;
-		QString m_UserName;
-		QString m_Password;
-                QMap<QString, databaseAction> m_SQLStatements;
-	};
-}
+class DIGIKAM_EXPORT databaseconfigelement
+{
+public:
+    databaseconfigelement();
+    virtual ~databaseconfigelement();
+
+    QString m_DatabaseID;
+    QString m_HostName;
+    QString m_Port;
+    QString m_ConnectOptions;
+    QString m_DatabaseName;
+    QString m_UserName;
+    QString m_Password;
+    QMap<QString, databaseAction> m_SQLStatements;
+};
+
+} // namespace Digikam
+
 #endif /* DATABASECONFIGELEMENT_H_ */
