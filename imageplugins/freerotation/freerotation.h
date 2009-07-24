ed9500c1a31efeb03edd4df6304aa4aba9501950
Andi Clemens <andi.clemens@gmx.net>
2009-07-24 20:26:45 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=1002040

--- imageplugins/freerotation/freerotation.h
+++ imageplugins/freerotation/freerotation.h
@@ -7,7 +7,7 @@
  * Description : Free rotation threaded image filter.
  *
  * Copyright (C) 2004-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2009      by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
