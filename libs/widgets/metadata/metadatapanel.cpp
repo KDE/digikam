38433696f30ca0fa29ef7321b6f1e079b2b44359
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2009-07-30 13:50:14 +0000

    Make Metadata panel just add itself at the current position, not requiring to take the second to fifth tab.
    svn path=/trunk/extragear/graphics/digikam/; revision=1004565

--- libs/widgets/metadata/metadatapanel.cpp
+++ libs/widgets/metadata/metadatapanel.cpp
@@ -215,19 +215,19 @@ MetadataPanel::MetadataPanel(KTabWidget* tab)
 
     d->exifViewerConfig   = new MetadataSelectorView(d->tab);
     d->exifViewerConfig->setDefaultFilter(d->defaultExifFilter);
-    d->tab->insertTab(1, d->exifViewerConfig, i18n("EXIF viewer"));
+    d->tab->addTab(d->exifViewerConfig, i18n("EXIF viewer"));
 
     d->mknoteViewerConfig = new MetadataSelectorView(d->tab);
     d->mknoteViewerConfig->setDefaultFilter(d->defaultMknoteFilter);
-    d->tab->insertTab(2, d->mknoteViewerConfig, i18n("Makernotes viewer"));
+    d->tab->addTab(d->mknoteViewerConfig, i18n("Makernotes viewer"));
 
     d->iptcViewerConfig   = new MetadataSelectorView(d->tab);
     d->iptcViewerConfig->setDefaultFilter(d->defaultIptcFilter);
-    d->tab->insertTab(3, d->iptcViewerConfig, i18n("IPTC viewer"));
+    d->tab->addTab(d->iptcViewerConfig, i18n("IPTC viewer"));
 
     d->xmpViewerConfig    = new MetadataSelectorView(d->tab);
     d->xmpViewerConfig->setDefaultFilter(d->defaultXmpFilter);
-    d->tab->insertTab(4, d->xmpViewerConfig, i18n("XMP viewer"));
+    d->tab->addTab(d->xmpViewerConfig, i18n("XMP viewer"));
 
 #if KEXIV2_VERSION < 0x010000
     d->tab->setTabBarHidden(true);
@@ -290,7 +290,7 @@ void MetadataPanel::applySettings()
 #endif
 }
 
-void MetadataPanel::slotTabChanged(int index)
+void MetadataPanel::slotTabChanged(int)
 {
     DMetadata meta;
     kapp->setOverrideCursor(Qt::WaitCursor);
@@ -298,52 +298,41 @@ void MetadataPanel::slotTabChanged(int index)
     KSharedConfig::Ptr config = KGlobal::config();
     KConfigGroup group        = config->group("Image Properties SideBar");
 
-    switch(index)
-    {
+    QWidget *tab = d->tab->currentWidget();
 #if KEXIV2_VERSION >= 0x010000
-        case 1:
+    if (tab == d->exifViewerConfig)
+    {
+        if (!d->exifViewerConfig->itemsCount())
         {
-            if (!d->exifViewerConfig->itemsCount())
-            {
-                d->exifViewerConfig->setTagsMap(meta.getStdExifTagsList());
-                d->exifViewerConfig->setcheckedTagsList(group.readEntry("EXIF Tags Filter", d->exifViewerConfig->defaultFilter()));
-            }
-            break;
+            d->exifViewerConfig->setTagsMap(meta.getStdExifTagsList());
+            d->exifViewerConfig->setcheckedTagsList(group.readEntry("EXIF Tags Filter", d->exifViewerConfig->defaultFilter()));
         }
-
-        case 2:
-        {
-            if (!d->mknoteViewerConfig->itemsCount())
-            {
-                d->mknoteViewerConfig->setTagsMap(meta.getMakernoteTagsList());
-                d->mknoteViewerConfig->setcheckedTagsList(group.readEntry("MAKERNOTE Tags Filter", d->mknoteViewerConfig->defaultFilter()));
-            }
-            break;
-        }
-
-        case 3:
-        {
-            if (!d->iptcViewerConfig->itemsCount())
-            {
-                d->iptcViewerConfig->setTagsMap(meta.getIptcTagsList());
-                d->iptcViewerConfig->setcheckedTagsList(group.readEntry("IPTC Tags Filter", d->iptcViewerConfig->defaultFilter()));
-            }
-            break;
-        }
-
-        case 4:
-        {
-            if (!d->xmpViewerConfig->itemsCount())
-            {
-                d->xmpViewerConfig->setTagsMap(meta.getXmpTagsList());
-                d->xmpViewerConfig->setcheckedTagsList(group.readEntry("XMP Tags Filter", d->xmpViewerConfig->defaultFilter()));
-            }
-            break;
-        }
-#endif
-        default:
-            break;
     }
+    else if (tab == d->mknoteViewerConfig)
+    {
+        if (!d->mknoteViewerConfig->itemsCount())
+        {
+            d->mknoteViewerConfig->setTagsMap(meta.getMakernoteTagsList());
+            d->mknoteViewerConfig->setcheckedTagsList(group.readEntry("MAKERNOTE Tags Filter", d->mknoteViewerConfig->defaultFilter()));
+        }
+    }
+    else if (tab == d->iptcViewerConfig)
+    {
+        if (!d->iptcViewerConfig->itemsCount())
+        {
+            d->iptcViewerConfig->setTagsMap(meta.getIptcTagsList());
+            d->iptcViewerConfig->setcheckedTagsList(group.readEntry("IPTC Tags Filter", d->iptcViewerConfig->defaultFilter()));
+        }
+    }
+    else if (tab == d->xmpViewerConfig)
+    {
+        if (!d->xmpViewerConfig->itemsCount())
+        {
+            d->xmpViewerConfig->setTagsMap(meta.getXmpTagsList());
+            d->xmpViewerConfig->setcheckedTagsList(group.readEntry("XMP Tags Filter", d->xmpViewerConfig->defaultFilter()));
+        }
+    }
+#endif
     kapp->restoreOverrideCursor();
 }
 
