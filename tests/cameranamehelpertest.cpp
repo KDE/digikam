d52853b95cfa24de43ac062ba6978611d78306da
Andi Clemens <andi.clemens@gmx.net>
2009-08-01 20:24:45 +0000

    add cameranamehelper test, not functional yet
    svn path=/trunk/extragear/graphics/digikam/; revision=1005710

---
+++ tests/cameranamehelpertest.cpp
@@ -0,0 +1,38 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-08-01
+ * Description : a test for the CameraNameHelper
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "cameranamehelpertest.h"
+#include "cameranamehelpertest.moc"
+
+// KDE includes
+
+#include <qtest_kde.h>
+
+// Local includes
+
+#include "cameranamehelper.h"
+
+using namespace Digikam;
+
+QTEST_KDEMAIN(CameraNameHelperTest, GUI)
+
