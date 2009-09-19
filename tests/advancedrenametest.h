29377ece0d4d92943faa3eede64572cfe6cd3785
Andi Clemens <andi.clemens@gmx.net>
2009-09-19 10:47:12 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=1025649

--- tests/advancedrenametest.h
+++ tests/advancedrenametest.h
@@ -4,7 +4,7 @@
  * http://www.digikam.org
  *
  * Date        : 2009-06-09
- * Description : a test for the AdvancedRenameWidget
+ * Description : a test for the AdvancedRename utility
  *
  * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
  *
