1d439eae277c4b5f6938e73967a30935dc1f51a3
Gilles Caulier <caulier.gilles@gmail.com>
2009-10-05 18:10:25 +0000

    use digiKam header
    svn path=/trunk/extragear/graphics/digikam/; revision=1031666

--- imageplugins/localcontrast/ToneMappingParameters.h
+++ imageplugins/localcontrast/ToneMappingParameters.h
@@ -1,23 +1,25 @@
-/*
-        LDR ToneMapper
-
-        Copyright (C) 2009 Nasca Octavian Paul
-        Author: Nasca Octavian Paul
-
-        This program is free software: you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation, either version 2 of the License, or
-        (at your option) any later version.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-        along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- */
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-08-09
+ * Description : LDR ToneMapper <http://zynaddsubfx.sourceforge.net/other/tonemapping>.
+ *
+ * Copyright (C) 2009 by Nasca Octavian Paul <zynaddsubfx at yahoo dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
 #ifndef TONE_MAPPING_PARAMETERS_H
 #define TONE_MAPPING_PARAMETERS_H
