563bec58b2a0d5d16b6ff1eadcbadd4e6dc373b7
Gilles Caulier <caulier.gilles@gmail.com>
2009-10-08 07:30:46 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=1032559

--- imageplugins/localcontrast/ToneMappingParameters.cpp
+++ imageplugins/localcontrast/ToneMappingParameters.cpp
@@ -49,10 +49,10 @@ ToneMappingParameters::ToneMappingParameters()
 
     for (int i=0 ; i < TONEMAPPING_MAX_STAGES ; i++)
     {
-        stage[i].enabled = (i==0);
+        stage[i].enabled = (i == 0);
         stage[i].power   = 30.0;
         stage[i].blur    = 80;
-    };
+    }
 
     unsharp_mask.enabled   = false;
     unsharp_mask.power     = 30;
@@ -114,94 +114,95 @@ REALTYPE ToneMappingParameters::get_unsharp_mask_blur()
 
 void ToneMappingParameters::save_parameters(const char *filename)
 {
-    FILE *f = fopen(filename,"w");
+    FILE *f = fopen(filename, "w");
     if (!f) return;
-    fprintf(f,"Tonemapping_by_PAUL\n");
-    fprintf(f,"1\n");//version
+    fprintf(f, "Tonemapping_by_PAUL\n");
+    fprintf(f, "1\n");//version
 
-    fprintf(f,"info_fast_mode %d\n",info_fast_mode);
-    fprintf(f,"low_saturation %d\n",low_saturation);
-    fprintf(f,"high_saturation %d\n",high_saturation);
-    fprintf(f,"stretch_contrast %d\n",stretch_contrast);
-    fprintf(f,"function_id %d\n",function_id);
+    fprintf(f, "info_fast_mode %d\n", info_fast_mode);
+    fprintf(f, "low_saturation %d\n", low_saturation);
+    fprintf(f, "high_saturation %d\n", high_saturation);
+    fprintf(f, "stretch_contrast %d\n", stretch_contrast);
+    fprintf(f, "function_id %d\n", function_id);
 
-    for (int i=0;i<TONEMAPPING_MAX_STAGES;i++)
+    for (int i=0 ; i < TONEMAPPING_MAX_STAGES ; i++)
     {
-        fprintf(f,"STAGE %d\n", i);
-        fprintf(f,"enabled %d\n",stage[i].enabled);
-        fprintf(f,"power %g\n", stage[i].power);
-        fprintf(f,"blur %g\n",stage[i].blur);
+        fprintf(f, "STAGE %d\n", i);
+        fprintf(f, "enabled %d\n",stage[i].enabled);
+        fprintf(f, "power %g\n", stage[i].power);
+        fprintf(f, "blur %g\n",stage[i].blur);
     }
 
-    fprintf(f,"unsharp_mask_enabled %d\n",unsharp_mask.enabled);
-    fprintf(f,"unsharp_mask_power %g\n",unsharp_mask.power);
-    fprintf(f,"unsharp_mask_blur %g\n",unsharp_mask.blur);
-    fprintf(f,"unsharp_mask_threshold %d\n",unsharp_mask.threshold);
+    fprintf(f, "unsharp_mask_enabled %d\n",unsharp_mask.enabled);
+    fprintf(f, "unsharp_mask_power %g\n",unsharp_mask.power);
+    fprintf(f, "unsharp_mask_blur %g\n",unsharp_mask.blur);
+    fprintf(f, "unsharp_mask_threshold %d\n",unsharp_mask.threshold);
 
     fclose(f);
 }
 
 bool ToneMappingParameters::load_parameters(const char *filename)
 {
-    FILE *f = fopen(filename,"r");
+    FILE *f = fopen(filename, "r");
     if (!f) return false;
 
-    const int max_line=1024;
+    const int max_line = 1024;
     char line[max_line];
-    line[0]=0;
+    line[0] = 0;
 
-    fgets(line,max_line,f);
+    fgets(line, max_line, f);
     if (feof(f)) return false;
-    if (strstr(line,"Tonemapping_by_PAUL")!=line) return false;
-    fgets(line,max_line,f);//version
-    int current_stage=0;
+    if (strstr(line, "Tonemapping_by_PAUL") != line) return false;
+    fgets(line, max_line, f);//version
+    int current_stage = 0;
 
     while (!feof(f))
     {
-        for (int i=0;i<max_line;i++) line[i]=0;
+        for (int i=0 ; i < max_line ; i++)
+            line[i] = 0;
+
         fgets(line,max_line-1,f);
 
         if (strlen(line)<3) continue;
 
-        int space=0;
-        for (int i=0;i<max_line;i++)
+        int space = 0;
+        for (int i=0 ; i < max_line ; i++)
         {
-            if (line[i]==' ')
+            if (line[i] == ' ')
             {
-                line[i]=0;
-                space=i+1;
+                line[i] = 0;
+                space = i+1;
                 break;
-            };
-        };
+            }
+        }
 
-        const char *par=line;
-        const char *sval=&line[space];
-        int ipar=atoi(sval);
-        REALTYPE fpar=atof(sval);
+        const char *par  = line;
+        const char *sval = &line[space];
+        int ipar         = atoi(sval);
+        REALTYPE fpar    = atof(sval);
 
-        if (strstr(par,"info_fast_mode")==par) info_fast_mode=ipar;
+        if (strstr(par, "info_fast_mode") == par) info_fast_mode = ipar;
+        if (strstr(par, "low_saturation") == par) low_saturation = ipar;
+        if (strstr(par, "high_saturation") == par) high_saturation = ipar;
+        if (strstr(par, "stretch_contrast") == par) stretch_contrast = ipar;
+        if (strstr(par, "function_id") == par) function_id = ipar;
 
-        if (strstr(par,"low_saturation")==par) low_saturation=ipar;
-        if (strstr(par,"high_saturation")==par) high_saturation=ipar;
-        if (strstr(par,"stretch_contrast")==par) stretch_contrast=ipar;
-        if (strstr(par,"function_id")==par) function_id=ipar;
-
-        if (strstr(par,"STAGE")==par)
+        if (strstr(par, "STAGE") == par)
         {
-            if (ipar<0) ipar=0;
-            if (ipar>(TONEMAPPING_MAX_STAGES-1)) ipar=TONEMAPPING_MAX_STAGES-1;
-            current_stage=ipar;
-        };
+            if (ipar < 0) ipar = 0;
+            if (ipar > (TONEMAPPING_MAX_STAGES-1)) ipar = TONEMAPPING_MAX_STAGES-1;
+            current_stage = ipar;
+        }
 
-        if (strstr(par,"enabled")==par) stage[current_stage].enabled=ipar;
-        if (strstr(par,"power")==par) stage[current_stage].power=fpar;
-        if (strstr(par,"blur")==par) stage[current_stage].blur=fpar;
+        if (strstr(par, "enabled") == par) stage[current_stage].enabled = ipar;
+        if (strstr(par, "power") == par) stage[current_stage].power = fpar;
+        if (strstr(par, "blur") == par) stage[current_stage].blur = fpar;
 
-        if (strstr(par,"unsharp_mask_enabled")==par) unsharp_mask.enabled=ipar;
-        if (strstr(par,"unsharp_mask_power")==par) unsharp_mask.power=fpar;
-        if (strstr(par,"unsharp_mask_blur")==par) unsharp_mask.blur=fpar;
-        if (strstr(par,"unsharp_mask_threshold")==par) unsharp_mask.threshold=ipar;
-    };
+        if (strstr(par, "unsharp_mask_enabled") == par) unsharp_mask.enabled = ipar;
+        if (strstr(par, "unsharp_mask_power") == par) unsharp_mask.power = fpar;
+        if (strstr(par, "unsharp_mask_blur") == par) unsharp_mask.blur = fpar;
+        if (strstr(par, "unsharp_mask_threshold") == par) unsharp_mask.threshold = ipar;
+    }
 
     fclose(f);
 
