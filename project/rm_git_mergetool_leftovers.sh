e667f8de3334dc29aab0b9382dda2434eff78ee9
Andi Clemens <andi.clemens@gmx.net>
2009-10-13 09:01:00 +0000

    Add more checks and proper return values
    svn path=/trunk/extragear/graphics/digikam/; revision=1034601

--- project/rm_git_mergetool_leftovers.sh
+++ project/rm_git_mergetool_leftovers.sh
@@ -2,17 +2,26 @@
 
 # Use this script to clean up your working dir before building the API documentation
 
+E_ERROR=1
+
 function usage() {
     echo "usage: " $0 "<path>"
 }
 
 if [ $# -ne 1 ]; then
     usage
-    exit 0
+    exit $E_ERROR
 fi
 
-find $1 -type f -name "*BASE*" \
-                -or -name "*REMOTE*" \
-                -or -name "*LOCAL*" \
-                -or -name "*.orig" \
+if [ -d $1 ]; then
+    find $1 -type f -name "*BASE*" \
+        -or -name "*REMOTE*" \
+        -or -name "*LOCAL*" \
+        -or -name "*.orig" \
         -exec rm {} \;
+    exit $?
+else
+    echo "'$1' is not a valid directory. Make sure you have permissions to access this location."
+    exit $E_ERROR
+fi
+
