aa1a8e6549a641fa905f94064798e6a58cd3e390
Gilles Caulier <caulier.gilles@gmail.com>
2009-10-15 12:31:19 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=1035586

--- utilities/searchwindow/searchutilities.cpp
+++ utilities/searchwindow/searchutilities.cpp
@@ -6,7 +6,7 @@
  * Date        : 2008-03-14
  * Description : User interface for searches
  *
- * Copyright (C) 2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2008-2009 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -362,7 +362,7 @@ void CustomStepsDoubleSpinBox::setSuggestedInitialValue(double initialValue)
 void CustomStepsDoubleSpinBox::setSingleSteps(double smaller, double larger)
 {
     m_smallerStep = smaller;
-    m_largerStep = larger;
+    m_largerStep  = larger;
 }
 
 void CustomStepsDoubleSpinBox::setInvertStepping(bool invert)
@@ -493,7 +493,7 @@ void CustomStepsIntSpinBox::setSuggestedInitialValue(int initialValue)
 void CustomStepsIntSpinBox::setSingleSteps(int smaller, int larger)
 {
     m_smallerStep = smaller;
-    m_largerStep = larger;
+    m_largerStep  = larger;
 }
 
 void CustomStepsIntSpinBox::setInvertStepping(bool invert)
