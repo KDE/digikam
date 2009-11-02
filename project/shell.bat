a76edc8f3eb926c6719866e1f5e6bb502da4b04e
Gilles Caulier <caulier.gilles@gmail.com>
2009-11-02 11:06:52 +0000

    svn path=/trunk/extragear/graphics/digikam/; revision=1043797

---
+++ project/shell.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set KDEDIRS=E:\kde4-mingw
+set PATH=%PATH%;%KDEDIRS%\bin;%KDEDIRS%\plugins
+set KDE4_INSTALL_DIR=%KDEDIRS%
+set KDEDIRS=%KDEDIRS%
+set KDEROOT=%KDEDIRS%
+set QTMAKESPEC=%KDEDIRS%\mkspecs\win32-g++
+set QT_PLUGIN_PATH=%KDEDIRS%\plugins
+set QTDIR=%KDEDIRS%
+set QT_INSTALL_DIR=%KDEDIRS%
+
+
+C:\mc\mc.exe
