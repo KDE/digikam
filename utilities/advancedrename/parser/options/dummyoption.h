054ecbc8b0de4ebf1eaa43bd751800156196002c
Andi Clemens <andi.clemens@gmx.net>
2009-11-17 12:14:23 +0000

    Add a dummyoption class with some explanation in the source code to add new options for the AdvancedRename utility
    svn path=/trunk/extragear/graphics/digikam/; revision=1050486

---
+++ utilities/advancedrename/parser/options/dummyoption.h
@@ -0,0 +1,55 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 200X-XX-XX
+ * Description : an option to provide <FILL IN PURPOSE> information to the parser
+ *
+ * Copyright (C) 2009 by <YOUR NAME AND MAIL ADRESS>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DUMMYOPTION_H
+#define DUMMYOPTION_H
+
+// Qt includes
+
+#include <QObject>
+#include <QString>
+
+// Local includes
+
+#include "option.h"
+
+namespace Digikam
+{
+
+class DummyOption : public Option
+{
+    Q_OBJECT
+
+public:
+
+    DummyOption();
+    ~DummyOption() {};
+
+protected:
+
+    virtual void parseOperation(const QString& parseString, ParseInformation& info, ParseResults& results);
+};
+
+} // namespace Digikam
+
+#endif /* DUMMYOPTION_H */
