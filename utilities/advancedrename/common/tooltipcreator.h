9ef3d8489068c7da560c46caedf8f16d006368a7
Andi Clemens <andi.clemens@gmx.net>
2009-11-26 14:24:05 +0000

    Add TooltipCreator class to avoid macros in AdvancedRenameWidget::createToolTip()
    svn path=/trunk/extragear/graphics/digikam/; revision=1054688

---
+++ utilities/advancedrename/common/tooltipcreator.h
@@ -0,0 +1,60 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-09-14
+ * Description : a class to build the tooltip for a renameparser and its options
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef TOOLTIPCREATOR_H
+#define TOOLTIPCREATOR_H
+
+// Qt includes
+
+#include <QString>
+
+namespace Digikam
+{
+
+class Parser;
+
+class TooltipCreator
+{
+public:
+
+    TooltipCreator(Parser* parser);
+    virtual ~TooltipCreator();
+
+    QString tooltip();
+
+private:
+
+    QString markOption(const QString& str);
+    QString createHeader(const QString& str);
+
+    template <class T>
+    QString createEntries(QList<T*> &data);
+
+private:
+
+    Parser* const parser;
+};
+
+} // namespace Digikam
+
+#endif /* TOOLTIPCREATOR_H */
