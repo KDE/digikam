8c3f55820024ec04116044edd0ac9d3cfdd317f3
Johannes Wienke <languitar@semipol.de>
2010-02-04 22:30:05 +0000

    Enable root tag in ImageDescEditTab. It's not checkable now but the check box is still drawn.
    svn path=/trunk/extragear/graphics/digikam/; revision=1085329

--- libs/imageproperties/imagedescedittab.cpp
+++ libs/imageproperties/imagedescedittab.cpp
@@ -183,9 +183,10 @@ ImageDescEditTab::ImageDescEditTab(QWidget *parent)
                                      "',' can be used to create more than one hierarchy at the same time."));
 
     d->tagModificationHelper = new TagModificationHelper(this, this);
-    d->tagModel = new TagModel(AbstractAlbumModel::IgnoreRootAlbum, this);
+    d->tagModel = new TagModel(AbstractAlbumModel::IncludeRootAlbum, this);
     d->tagModel->setCheckable(true);
     d->tagModel->setTristate(true);
+    d->tagModel->setRootCheckable(false);
     d->tagCheckView = new TagCheckView(captionTagsArea, d->tagModel);
 
     KHBox *tagsSearch  = new KHBox(captionTagsArea);
