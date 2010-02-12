a2208312c89dff1138af73667d27a3c33f148038
Gilles Caulier <caulier.gilles@gmail.com>
2010-02-12 08:43:07 +0000

    new folder dedicated to host multithreaded auto color correction algorithms
    svn path=/trunk/extragear/graphics/digikam/; revision=1089029

---
+++ libs/dimg/filters/auto/autolevelsfilter.cpp
@@ -0,0 +1,125 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2005-24-01
+ * Description : auto levels image filter.
+ *
+ * Copyright (C) 2005-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "autolevelsfilter.h"
+
+// C++ includes
+
+#include <cstdio>
+#include <cmath>
+
+// KDE includes
+
+#include <kdebug.h>
+
+// Local includes
+
+#include "dimg.h"
+#include "imagehistogram.h"
+#include "imagelevels.h"
+
+namespace Digikam
+{
+
+AutoLevelsFilter::AutoLevelsFilter(DImg* orgImage, QObject* parent)
+                : DImgThreadedFilter(orgImage, parent, "AutoLevelsFilter")
+{
+    initFilter();
+}
+
+AutoLevelsFilter::AutoLevelsFilter(uchar* data, uint width, uint height, bool sixteenBit)
+                : DImgThreadedFilter()
+{
+    autoLevelsCorrectionImage(data, width, height, sixteenBit);
+}
+
+AutoLevelsFilter::~AutoLevelsFilter()
+{
+}
+
+void AutoLevelsFilter::filterImage()
+{
+    autoLevelsCorrectionImage(m_orgImage.bits(), m_orgImage.width(), m_orgImage.height(), m_orgImage.sixteenBit());
+    m_destImage = m_orgImage;
+}
+
+/** Performs histogram auto correction of levels.
+    This method maximizes the tonal range in the Red,
+    Green, and Blue channels. It search the image shadow and highlight
+    limit values and adjust the Red, Green, and Blue channels
+    to a full histogram range.*/
+void AutoLevelsFilter::autoLevelsCorrectionImage(uchar* data, int w, int h, bool sixteenBit)
+{
+    if (!data || !w || !h)
+    {
+       kWarning() << ("DImgImageFilters::autoLevelsCorrectionImage: no image data available!");
+       return;
+    }
+
+    postProgress(10);
+    uchar* desData;
+
+    // Create the new empty destination image data space.
+    if (sixteenBit)
+       desData = new uchar[w*h*8];
+    else
+       desData = new uchar[w*h*4];
+
+    postProgress(20);
+
+    // Create an histogram of the current image.
+    ImageHistogram* histogram = new ImageHistogram(data, w, h, sixteenBit);
+    histogram->calculate();
+    postProgress(30);
+
+    // Create an empty instance of levels to use.
+    ImageLevels* levels = new ImageLevels(sixteenBit);
+    postProgress(40);
+
+    // Initialize an auto levels correction of the histogram.
+    levels->levelsAuto(histogram);
+    postProgress(50);
+
+    // Calculate the LUT to apply on the image.
+    levels->levelsLutSetup(AlphaChannel);
+    postProgress(60);
+
+    // Apply the lut to the image.
+    levels->levelsLutProcess(data, desData, w, h);
+    postProgress(70);
+
+    if (sixteenBit)
+       memcpy (data, desData, w*h*8);
+    else
+       memcpy (data, desData, w*h*4);
+
+    postProgress(80);
+
+    delete [] desData;
+    delete histogram;
+    delete levels;
+
+    postProgress(90);
+}
+
+}  // namespace Digikam
---
+++ libs/dimg/filters/auto/autolevelsfilter.h
@@ -0,0 +1,58 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2005-24-01
+ * Description : auto levels image filter.
+ *
+ * Copyright (C) 2005-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef AUTOLEVELSFILTER_H
+#define AUTOLEVELSFILTER_H
+
+// Local includes
+
+#include "digikam_export.h"
+#include "dimgthreadedfilter.h"
+#include "globals.h"
+
+using namespace Digikam;
+
+namespace Digikam
+{
+
+class DImg;
+class AutoLevelsFilterPriv;
+
+class DIGIKAM_EXPORT AutoLevelsFilter : public DImgThreadedFilter
+{
+
+public:
+
+    AutoLevelsFilter(DImg* orgImage, QObject* parent=0);
+    AutoLevelsFilter(uchar* data, uint width, uint height, bool sixteenBit);
+    virtual ~AutoLevelsFilter();
+
+private:
+
+    void filterImage();
+    void autoLevelsCorrectionImage(uchar* data, int w, int h, bool sixteenBit);
+};
+
+}  // namespace Digikam
+
+#endif /* AUTOLEVELSFILTER_H */
