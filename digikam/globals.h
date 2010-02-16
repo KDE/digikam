d3ea6de4efbdce280a82665b57b6b665dbc6ab1e
Gilles Caulier <caulier.gilles@gmail.com>
2010-02-16 12:08:25 +0000

    more globals macro here
    svn path=/trunk/extragear/graphics/digikam/; revision=1090949

--- digikam/globals.h
+++ digikam/globals.h
@@ -7,6 +7,7 @@
  * Description : global macros, variables and flags
  *
  * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2009-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -28,6 +29,8 @@
 #define CLAMP0255(a)   qMin(qMax(a,0), 255)
 #define CLAMP065535(a) qMin(qMax(a,0), 65535)
 #define CLAMP(x,l,u)   ((x)<(l)?(l):((x)>(u)?(u):(x)))
+#define MAX3(a, b, c) (qMax(qMax(a,b),b))
+#define MIN3(a, b, c) (qMin(qMin(a,b),b))
 
 namespace Digikam
 {
