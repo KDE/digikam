ffa07a867a9b2444bcdf6424ea9794e255784b96
Gilles Caulier <caulier.gilles@gmail.com>
2010-03-01 15:45:25 +0000

    implement CurvesSettings widget
    svn path=/trunk/extragear/graphics/digikam/; revision=1097537

---
+++ libs/dimg/filters/curves/curvessettings.cpp
@@ -0,0 +1,281 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-03-01
+ * Description : Curves settings view.
+ *
+ * Copyright (C) 2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "curvessettings.moc"
+
+// Qt includes
+
+#include <QString>
+#include <QButtonGroup>
+#include <QFile>
+#include <QFrame>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QRegExp>
+#include <QTextStream>
+#include <QToolButton>
+#include <QVBoxLayout>
+
+// KDE includes
+
+#include <kdebug.h>
+#include <kurl.h>
+#include <kdialog.h>
+#include <klocale.h>
+#include <kapplication.h>
+#include <kfiledialog.h>
+#include <kglobal.h>
+#include <kglobalsettings.h>
+#include <kmessagebox.h>
+#include <kstandarddirs.h>
+#include <kcombobox.h>
+#include <kseparator.h>
+#include <kiconloader.h>
+
+// LibKDcraw includes
+
+#include <libkdcraw/rcombobox.h>
+#include <libkdcraw/rnuminput.h>
+
+// Local includes
+
+#include "rexpanderbox.h"
+#include "colorgradientwidget.h"
+
+using namespace KDcrawIface;
+
+namespace Digikam
+{
+
+class CurvesSettingsPriv
+{
+
+public:
+
+    CurvesSettingsPriv() :
+        histoSegments(0),
+        configCurveEntry("AdjustCurves"),
+        curvesBox(0)
+        {}
+
+    int           histoSegments;
+
+    const QString configCurveEntry;
+    
+    CurvesBox*    curvesBox;
+};
+
+CurvesSettings::CurvesSettings(QWidget* parent, DImg* img)
+              : QWidget(parent),
+                d(new CurvesSettingsPriv)
+{
+    d->histoSegments  = img->sixteenBit() ? 65535 : 255;
+    QGridLayout* grid = new QGridLayout(parent);
+    d->curvesBox      = new CurvesBox(256, 256, img->bits(), img->width(), img->height(), img->sixteenBit());
+    d->curvesBox->enableGradients(true);
+    d->curvesBox->enableControlWidgets(true);
+
+    grid->addWidget(d->curvesBox, 0, 0, 1, 1);
+    grid->setRowStretch(1, 10);
+    grid->setMargin(KDialog::spacingHint());
+    grid->setSpacing(KDialog::spacingHint());
+
+    // -------------------------------------------------------------
+
+    connect(d->curvesBox, SIGNAL(signalCurvesChanged()),
+            this, SIGNAL(signalSettingsChanged()));
+
+    connect(d->curvesBox, SIGNAL(signalChannelReset(int)),
+            this, SIGNAL(signalChannelReset(int)));
+
+    connect(d->curvesBox, SIGNAL(signalPickerChanged(int)),
+            this, SIGNAL(signalSettingsChanged()));
+}
+
+CurvesSettings::~CurvesSettings()
+{
+    delete d;
+}
+
+void CurvesSettings::setChannel(ChannelType channel)
+{
+    d->curvesBox->setChannel(channel);
+}
+
+void CurvesSettings::setScale(HistogramScale type)
+{
+    d->curvesBox->setScale(type);
+}
+
+void CurvesSettings::slotSpotColorChanged(const DColor& color)
+{
+    DColor sc = color;
+
+    switch (d->curvesBox->picker())
+    {
+        case CurvesBox::BlackTonal:
+        {
+            // Black tonal curves point.
+            d->curvesBox->curves()->setCurvePoint(LuminosityChannel, 1,
+                    QPoint(qMax(qMax(sc.red(), sc.green()), sc.blue()), 42*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(RedChannel, 1, QPoint(sc.red(), 42*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(GreenChannel, 1, QPoint(sc.green(), 42*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(BlueChannel, 1, QPoint(sc.blue(), 42*d->histoSegments/256));
+            d->curvesBox->resetPickers();
+            break;
+        }
+        case CurvesBox::GrayTonal:
+        {
+            // Gray tonal curves point.
+            d->curvesBox->curves()->setCurvePoint(LuminosityChannel, 8,
+                    QPoint(qMax(qMax(sc.red(), sc.green()), sc.blue()), 128*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(RedChannel, 8, QPoint(sc.red(), 128*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(GreenChannel, 8, QPoint(sc.green(), 128*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(BlueChannel, 8, QPoint(sc.blue(), 128*d->histoSegments/256));
+            d->curvesBox->resetPickers();
+            break;
+        }
+        case CurvesBox::WhiteTonal:
+        {
+            // White tonal curves point.
+            d->curvesBox->curves()->setCurvePoint(LuminosityChannel, 15,
+                    QPoint(qMax(qMax(sc.red(), sc.green()), sc.blue()), 213*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(RedChannel, 15, QPoint(sc.red(), 213*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(GreenChannel, 15, QPoint(sc.green(), 213*d->histoSegments/256));
+            d->curvesBox->curves()->setCurvePoint(BlueChannel, 15, QPoint(sc.blue(), 213*d->histoSegments/256));
+            d->curvesBox->resetPickers();
+            break;
+        }
+        default:
+        {
+            d->curvesBox->setCurveGuide(color);
+            return;
+        }
+    }
+
+    // Calculate Red, green, blue curves.
+
+    for (int i = LuminosityChannel ; i <= BlueChannel ; ++i)
+       d->curvesBox->curves()->curvesCalculateCurve(i);
+
+    d->curvesBox->repaint();
+    
+    emit signalSpotColorChanged();
+}
+
+CurvesContainer CurvesSettings::settings() const
+{
+    CurvesContainer prm;
+    prm.lumCurveVals   = d->curvesBox->curves()->getCurveValues(LuminosityChannel);
+    prm.redCurveVals   = d->curvesBox->curves()->getCurveValues(RedChannel);
+    prm.greenCurveVals = d->curvesBox->curves()->getCurveValues(GreenChannel);
+    prm.blueCurveVals  = d->curvesBox->curves()->getCurveValues(BlueChannel);
+    prm.alphaCurveVals = d->curvesBox->curves()->getCurveValues(AlphaChannel);
+    return prm;
+}
+
+void CurvesSettings::setSettings(const CurvesContainer& settings)
+{
+    blockSignals(true);
+    d->curvesBox->curves()->setCurveValues(LuminosityChannel, settings.lumCurveVals);
+    d->curvesBox->curves()->setCurveValues(RedChannel,        settings.redCurveVals);
+    d->curvesBox->curves()->setCurveValues(GreenChannel,      settings.greenCurveVals);
+    d->curvesBox->curves()->setCurveValues(BlueChannel,       settings.blueCurveVals);
+    d->curvesBox->curves()->setCurveValues(AlphaChannel,      settings.alphaCurveVals);
+    blockSignals(false);
+}
+
+void CurvesSettings::resetToDefault()
+{
+    blockSignals(true);
+    d->curvesBox->resetChannels();
+    d->curvesBox->resetPickers();
+    blockSignals(false);
+}
+
+CurvesContainer CurvesSettings::defaultSettings() const
+{
+    CurvesContainer prm;
+/*
+    prm.black       = d->blackInput->defaultValue();
+    prm.exposition  = d->mainExposureInput->defaultValue() + d->fineExposureInput->defaultValue();
+    prm.temperature = d->temperatureInput->defaultValue();
+    prm.green       = d->greenInput->defaultValue();
+    prm.dark        = d->darkInput->defaultValue();
+    prm.gamma       = d->gammaInput->defaultValue();
+    prm.saturation  = d->saturationInput->defaultValue();
+*/
+    return prm;
+}
+
+void CurvesSettings::readSettings(KConfigGroup& group)
+{
+    d->curvesBox->reset();
+    d->curvesBox->readCurveSettings(group, d->configCurveEntry);
+    d->curvesBox->update();
+}
+
+void CurvesSettings::writeSettings(KConfigGroup& group)
+{
+    d->curvesBox->writeCurveSettings(group, d->configCurveEntry);
+}
+
+void CurvesSettings::loadSettings()
+{
+    KUrl loadCurvesFile;
+
+    loadCurvesFile = KFileDialog::getOpenUrl(KGlobalSettings::documentPath(),
+                                             QString( "*" ), kapp->activeWindow(),
+                                             QString( i18n("Select Gimp Curves File to Load")) );
+    if ( loadCurvesFile.isEmpty() )
+       return;
+
+    if ( d->curvesBox->curves()->loadCurvesFromGimpCurvesFile( loadCurvesFile ) == false )
+    {
+        KMessageBox::error(kapp->activeWindow(),
+                           i18n("Cannot load from the Gimp curves text file."));
+        return;
+    }
+}
+
+void CurvesSettings::saveAsSettings()
+{
+    KUrl saveCurvesFile;
+
+    saveCurvesFile = KFileDialog::getSaveUrl(KGlobalSettings::documentPath(),
+                                             QString( "*" ), kapp->activeWindow(),
+                                             QString( i18n("Gimp Curves File to Save")) );
+    if ( saveCurvesFile.isEmpty() )
+       return;
+
+    if ( d->curvesBox->curves()->saveCurvesToGimpCurvesFile( saveCurvesFile ) == false )
+    {
+        KMessageBox::error(kapp->activeWindow(),
+                           i18n("Cannot save to the Gimp curves text file."));
+        return;
+    }
+}
+
+}  // namespace Digikam
