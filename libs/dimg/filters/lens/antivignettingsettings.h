369b6e00494e898d68a1a51f1d79c483f6f044bf
Gilles Caulier <caulier.gilles@gmail.com>
2010-03-15 12:15:41 +0000

    start to implement Antivignetting settings view
    svn path=/trunk/extragear/graphics/digikam/; revision=1103571

---
+++ libs/dimg/filters/lens/antivignettingsettings.h
@@ -0,0 +1,74 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-03-15
+ * Description : Anti vignetting settings view.
+ *
+ * Copyright (C) 2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef ANTIVIGNETTINGSETTINGS_H
+#define ANTIVIGNETTINGSETTINGS_H
+
+// Local includes
+
+#include <QWidget>
+
+// KDE includes
+
+#include <kconfig.h>
+
+// Local includes
+
+#include "digikam_export.h"
+#include "antivignettingfilter.h"
+
+namespace Digikam
+{
+
+class AntiVignettingSettingsPriv;
+
+class DIGIKAM_EXPORT AntiVignettingSettings : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    AntiVignettingSettings(QWidget* parent);
+    ~AntiVignettingSettings();
+
+    AntiVignettingContainer defaultSettings() const;
+    void resetToDefault();
+
+    AntiVignettingContainer settings() const;
+    void setSettings(const AntiVignettingContainer& settings);
+
+    void readSettings(KConfigGroup& group);
+    void writeSettings(KConfigGroup& group);
+
+Q_SIGNALS:
+
+    void signalSettingsChanged();
+
+private:
+
+    AntiVignettingSettingsPriv* const d;
+};
+
+}  // namespace Digikam
+
+#endif /* ANTIVIGNETTINGSETTINGS_H */
