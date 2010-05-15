e44215a07335d951675fb3e0050049a12d5fb043
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-05-15 16:02:00 +0000

    An ImagePreviewView replacement built ontop QGv
    svn path=/branches/extragear/graphics/digikam/gsoc-2010/; revision=1127149

---
+++ digikam/imagepreviewviewv2.cpp
@@ -0,0 +1,363 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2006-21-12
+ * Description : a embedded view to show the image preview widget.
+ *
+ * Copyright (C) 2006-2010 Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "imagepreviewviewv2.moc"
+
+// Qt includes
+
+#include <QCursor>
+#include <QGraphicsSceneContextMenuEvent>
+#include <QToolBar>
+
+// KDE includes
+
+#include <kaction.h>
+#include <kactionmenu.h>
+#include <kapplication.h>
+#include <kcursor.h>
+#include <kdialog.h>
+#include <kiconloader.h>
+#include <klocale.h>
+#include <kmenu.h>
+#include <kdebug.h>
+#include <kmimetype.h>
+#include <kmimetypetrader.h>
+
+// LibKIPI includes
+
+#include <libkipi/plugin.h>
+#include <libkipi/pluginloader.h>
+
+// Local includes
+
+#include "albumsettings.h"
+#include "albumwidgetstack.h"
+#include "contextmenuhelper.h"
+#include "digikamapp.h"
+#include "dimg.h"
+#include "dimgpreviewitem.h"
+#include "dpopupmenu.h"
+#include "imageinfo.h"
+#include "metadatamanager.h"
+#include "ratingpopupmenu.h"
+#include "tagspopupmenu.h"
+#include "themeengine.h"
+
+namespace Digikam
+{
+
+class ImagePreviewViewItem : public DImgPreviewItem
+{
+public:
+
+    ImagePreviewViewItem(ImagePreviewViewV2 *view) : m_view(view)
+    {
+    }
+
+    void contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+    {
+        m_view->showContextMenu(m_info, event);
+    }
+
+    void setImageInfo(const ImageInfo& info)
+    {
+        m_info = info;
+        setPath(info.filePath());
+    }
+    
+    ImageInfo imageInfo() const
+    {
+        return m_info;
+    }
+
+protected:
+
+    ImagePreviewViewV2 *m_view;
+    ImageInfo m_info;
+};
+
+class ImagePreviewViewV2Priv
+{
+public:
+
+    ImagePreviewViewV2Priv()
+    {
+        item                 = 0;
+        stack                = 0;
+        toolBar              = 0;
+        back2AlbumAction     = 0;
+        prevAction           = 0;
+        nextAction           = 0;
+        rotLeftAction        = 0;
+        rotRightAction       = 0;
+    }
+
+    ImagePreviewViewItem *item;
+
+    QAction*           back2AlbumAction;
+    QAction*           prevAction;
+    QAction*           nextAction;
+    QAction*           rotLeftAction;
+    QAction*           rotRightAction;
+
+    QToolBar*          toolBar;
+
+    AlbumWidgetStack*  stack;
+};
+
+ImagePreviewViewV2::ImagePreviewViewV2(AlbumWidgetStack* parent)
+                : GraphicsDImgView(parent), d(new ImagePreviewViewV2Priv)
+{
+    d->item = new ImagePreviewViewItem(this);
+    setItem(d->item);
+
+    connect(d->item, SIGNAL(loaded()),
+            this, SLOT(imageLoaded()));
+
+    connect(d->item, SIGNAL(loadingFailed()),
+            this, SLOT(imageLoadingFailed()));
+
+    installPanIcon();
+
+    // ------------------------------------------------------------
+
+    d->stack            = parent;
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    // ------------------------------------------------------------
+
+    d->back2AlbumAction = new QAction(SmallIcon("folder-image"),        i18n("Back to Album"),                  this);
+    d->prevAction       = new QAction(SmallIcon("go-previous"),         i18nc("go to previous image", "Back"),  this);
+    d->nextAction       = new QAction(SmallIcon("go-next"),             i18nc("go to next image", "Forward"),   this);
+    d->rotLeftAction    = new QAction(SmallIcon("object-rotate-left"),  i18nc("@info:tooltip", "Rotate Left"),  this);
+    d->rotRightAction   = new QAction(SmallIcon("object-rotate-right"), i18nc("@info:tooltip", "Rotate Right"), this);
+
+    d->toolBar = new QToolBar(this);
+    d->toolBar->addAction(d->prevAction);
+    d->toolBar->addAction(d->nextAction);
+    d->toolBar->addAction(d->back2AlbumAction);
+    d->toolBar->addAction(d->rotLeftAction);
+    d->toolBar->addAction(d->rotRightAction);
+
+    connect(d->prevAction, SIGNAL(triggered()),
+            this, SIGNAL(toPreviousImage()));
+
+    connect(d->nextAction, SIGNAL(triggered()),
+            this, SIGNAL(toNextImage()));
+
+    connect(d->back2AlbumAction, SIGNAL(triggered()),
+            this, SIGNAL(signalBack2Album()));
+
+    connect(d->rotLeftAction, SIGNAL(triggered()),
+            this, SLOT(slotRotateLeft()));
+
+    connect(d->rotRightAction, SIGNAL(triggered()),
+            this, SLOT(slotRotateRight()));
+
+    // ------------------------------------------------------------
+
+    connect(this, SIGNAL(toNextImage()),
+            this, SIGNAL(signalNextItem()));
+
+    connect(this, SIGNAL(toPreviousImage()),
+            this, SIGNAL(signalPrevItem()));
+
+    connect(this, SIGNAL(activated()),
+            this, SIGNAL(signalBack2Album()));
+
+    connect(ThemeEngine::instance(), SIGNAL(signalThemeChanged()),
+            this, SLOT(slotThemeChanged()));
+
+    connect(AlbumSettings::instance(), SIGNAL(setupChanged()),
+            this, SLOT(slotSetupChanged()));
+}
+
+ImagePreviewViewV2::~ImagePreviewViewV2()
+{
+    delete d;
+}
+
+void ImagePreviewViewV2::reload()
+{
+    previewItem()->reload();
+}
+
+void ImagePreviewViewV2::imageLoaded()
+{
+    d->stack->setPreviewMode(AlbumWidgetStack::PreviewImageMode);
+    d->stack->previewLoaded();
+    emit signalPreviewLoaded(true);
+    d->rotLeftAction->setEnabled(true);
+    d->rotRightAction->setEnabled(true);
+}
+
+void ImagePreviewViewV2::imageLoadingFailed()
+{
+    d->stack->setPreviewMode(AlbumWidgetStack::PreviewImageMode);
+    d->stack->previewLoaded();
+    emit signalPreviewLoaded(false);
+    d->rotLeftAction->setEnabled(false);
+    d->rotRightAction->setEnabled(false);
+}
+
+void ImagePreviewViewV2::setImageInfo(const ImageInfo& info, const ImageInfo& previous, const ImageInfo& next)
+{
+    d->item->setImageInfo(info);
+
+    d->prevAction->setEnabled(!previous.isNull());
+    d->nextAction->setEnabled(!next.isNull());
+
+    d->item->setPreloadPaths(QStringList() << next.filePath() << previous.filePath());
+}
+
+ImageInfo ImagePreviewViewV2::getImageInfo() const
+{
+    return d->item->imageInfo();
+}
+
+void ImagePreviewViewV2::showContextMenu(const ImageInfo& info, QGraphicsSceneContextMenuEvent *event)
+{
+    if (info.isNull())
+        return;
+
+    event->accept();
+
+    QList<qlonglong> idList;
+    idList << info.id();
+    KUrl::List selectedItems;
+    selectedItems << info.fileUrl();
+
+    // --------------------------------------------------------
+
+    DPopupMenu popmenu(this);
+    ContextMenuHelper cmhelper(&popmenu);
+
+    cmhelper.addAction(d->prevAction, true);
+    cmhelper.addAction(d->nextAction, true);
+    cmhelper.addAction(d->back2AlbumAction);
+    cmhelper.addGotoMenu(idList);
+    popmenu.addSeparator();
+    // --------------------------------------------------------
+    cmhelper.addAction("image_edit");
+    cmhelper.addServicesMenu(selectedItems);
+    cmhelper.addKipiActions(idList);
+    popmenu.addSeparator();
+    // --------------------------------------------------------
+    cmhelper.addAction("image_find_similar");
+    cmhelper.addActionLightTable();
+    cmhelper.addQueueManagerMenu();
+    popmenu.addSeparator();
+    // --------------------------------------------------------
+    cmhelper.addActionItemDelete(this, SIGNAL(signalDeleteItem()));
+    popmenu.addSeparator();
+    // --------------------------------------------------------
+    cmhelper.addAssignTagsMenu(idList);
+    cmhelper.addRemoveTagsMenu(idList);
+    popmenu.addSeparator();
+    // --------------------------------------------------------
+    cmhelper.addRatingMenu();
+
+    // special action handling --------------------------------
+
+    connect(&cmhelper, SIGNAL(signalAssignTag(int)),
+            this, SLOT(slotAssignTag(int)));
+
+    connect(&cmhelper, SIGNAL(signalRemoveTag(int)),
+            this, SLOT(slotRemoveTag(int)));
+
+    connect(&cmhelper, SIGNAL(signalAssignRating(int)),
+            this, SLOT(slotAssignRating(int)));
+
+    connect(&cmhelper, SIGNAL(signalAddToExistingQueue(int)),
+            this, SIGNAL(signalAddToExistingQueue(int)));
+
+    connect(&cmhelper, SIGNAL(signalGotoTag(int)),
+            this, SIGNAL(signalGotoTagAndItem(int)));
+
+    connect(&cmhelper, SIGNAL(signalGotoAlbum(const ImageInfo&)),
+            this, SIGNAL(signalGotoAlbumAndItem(const ImageInfo&)));
+
+    connect(&cmhelper, SIGNAL(signalGotoDate(const ImageInfo&)),
+            this, SIGNAL(signalGotoDateAndItem(const ImageInfo&)));
+
+    cmhelper.exec(event->screenPos());
+}
+
+void ImagePreviewViewV2::slotAssignTag(int tagID)
+{
+    MetadataManager::instance()->assignTag(d->item->imageInfo(), tagID);
+}
+
+void ImagePreviewViewV2::slotRemoveTag(int tagID)
+{
+    MetadataManager::instance()->removeTag(d->item->imageInfo(), tagID);
+}
+
+void ImagePreviewViewV2::slotAssignRating(int rating)
+{
+    MetadataManager::instance()->assignRating(d->item->imageInfo(), rating);
+}
+
+void ImagePreviewViewV2::slotThemeChanged()
+{
+    QPalette plt(palette());
+    plt.setColor(backgroundRole(), ThemeEngine::instance()->baseColor());
+    setPalette(plt);
+}
+
+void ImagePreviewViewV2::slotSetupChanged()
+{
+    previewItem()->setLoadFullImageSize(AlbumSettings::instance()->getPreviewLoadFullImageSize());
+    previewItem()->setExifRotate(AlbumSettings::instance()->getExifRotate());
+}
+
+void ImagePreviewViewV2::slotRotateLeft()
+{
+    KActionMenu* action = dynamic_cast<KActionMenu*>(ContextMenuHelper::kipiRotateAction());
+    if (action)
+    {
+        QList<QAction*> list = action->menu()->actions();
+        foreach(QAction* ac, list)
+        {
+            if (ac->objectName() == QString("rotate_ccw"))
+                ac->trigger();
+        }
+    }
+}
+
+void ImagePreviewViewV2::slotRotateRight()
+{
+    KActionMenu* action = dynamic_cast<KActionMenu*>(ContextMenuHelper::kipiRotateAction());
+    if (action)
+    {
+        QList<QAction*> list = action->menu()->actions();
+        foreach(QAction* ac, list)
+        {
+            if (ac->objectName() == QString("rotate_cw"))
+                ac->trigger();
+        }
+    }
+}
+
+}  // namespace Digikam
---
+++ digikam/imagepreviewviewv2.h
@@ -0,0 +1,104 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2006-21-12
+ * Description : a embedded view to show the image preview widget.
+ *
+ * Copyright (C) 2006-2010 Gilles Caulier  <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef IMAGEPREVIEWVIEWV2_H
+#define IMAGEPREVIEWVIEWV2_H
+
+// Local includes
+
+#include "graphicsdimgview.h"
+#include "imageinfo.h"
+#include "digikam_export.h"
+
+class QPixmap;
+
+namespace Digikam
+{
+
+class AlbumWidgetStack;
+class ImagePreviewViewV2Priv;
+class LoadingDescription;
+
+class ImagePreviewViewV2 : public GraphicsDImgView
+{
+    Q_OBJECT
+
+public:
+
+    ImagePreviewViewV2(AlbumWidgetStack* parent);
+    ~ImagePreviewViewV2();
+
+    void setImageInfo(const ImageInfo& info = ImageInfo(),
+                      const ImageInfo& previous = ImageInfo(),
+                      const ImageInfo& next = ImageInfo());
+
+    ImageInfo getImageInfo() const;
+
+    void reload();
+    void setImagePath(const QString& path=QString());
+    void setPreviousNextPaths(const QString& previous, const QString& next);
+
+    void showContextMenu(const ImageInfo& info, QGraphicsSceneContextMenuEvent *event);
+
+Q_SIGNALS:
+
+    void signalNextItem();
+    void signalPrevItem();
+    void signalDeleteItem();
+    void signalEditItem();
+    void signalPreviewLoaded(bool success);
+    void signalBack2Album();
+    void signalSlideShow();
+    void signalInsert2LightTable();
+    void signalInsert2QueueMgr();
+    void signalFindSimilar();
+    void signalAddToExistingQueue(int);
+
+    void signalGotoAlbumAndItem(const ImageInfo&);
+    void signalGotoDateAndItem(const ImageInfo&);
+    void signalGotoTagAndItem(int);
+
+private Q_SLOTS:
+
+    void imageLoaded();
+    void imageLoadingFailed();
+
+    void slotAssignTag(int tagID);
+    void slotRemoveTag(int tagID);
+    void slotAssignRating(int rating);
+
+    void slotThemeChanged();
+    void slotSetupChanged();
+
+    void slotRotateLeft();
+    void slotRotateRight();
+
+private:
+
+    ImagePreviewViewV2Priv* const d;
+};
+
+}  // namespace Digikam
+
+#endif /* IMAGEPREVIEWVIEW_H */
