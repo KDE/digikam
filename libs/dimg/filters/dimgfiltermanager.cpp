bc03139ab691f2605cd896607cef64b2fb5fe1ae
Martin Klapetek <martin.klapetek@gmail.com>
2010-06-28 16:09:30 +0000

    Basic infrastructure for recreating filters from image history. This manager will keep track of all present filters and will be able to recreate them. The generator class will advertise particular filter to the manager
    svn path=/branches/extragear/graphics/digikam/gsoc-2010/; revision=1143719

---
+++ libs/dimg/filters/dimgfiltermanager.cpp
@@ -0,0 +1,151 @@
+/* ============================================================
+ * 
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-06-24
+ * Description : manager for filters (registering, creating etc)
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt includes
+
+#include <QMap>
+
+// KDE includes
+
+#include <kdebug.h>
+#include <kglobal.h>
+
+// Local includes
+
+#include "dimgfiltergenerator.h"
+#include "bcgfilter.h"
+#include "dimgfiltermanager.h"
+
+
+namespace Digikam
+{
+
+class DImgFilterManagerPriv
+{
+public:
+
+    DImgFilterManagerPriv()
+    {
+    }
+
+    ~DImgFilterManagerPriv()
+    {
+        foreach (DImgFilterGenerator* gen, builtinGenerators)
+            delete gen;
+    }
+
+    void setupBuiltinGenerators();
+
+    QMap<QString, DImgFilterGenerator*> filterMap;
+
+    QList<DImgFilterGenerator*> builtinGenerators;
+};
+
+void DImgFilterManagerPriv::setupBuiltinGenerators()
+{
+    builtinGenerators
+    << new BasicDImgFilterGenerator<BCGFilter>();
+}
+
+class DImgFilterManagerCreator { public: DImgFilterManager object; };
+K_GLOBAL_STATIC(DImgFilterManagerCreator, creator)
+
+DImgFilterManager* DImgFilterManager::instance()
+{
+    return &creator->object;
+}
+
+DImgFilterManager::DImgFilterManager()
+            : d(new DImgFilterManagerPriv)
+{
+    d->setupBuiltinGenerators();
+    foreach (DImgFilterGenerator* gen, d->builtinGenerators)
+        addGenerator(gen);
+}
+
+DImgFilterManager::~DImgFilterManager()
+{
+    delete d;
+}
+
+void DImgFilterManager::addGenerator(DImgFilterGenerator *generator)
+{
+    foreach (const QString& id, generator->supportedFilters())
+    {
+        if (d->filterMap.contains(id))
+        {
+            kError() << "Attempt to register filter identifier" << id << "twice. Ignoring.";
+            continue;
+        }
+        d->filterMap[id] = generator;
+    }
+}
+
+void DImgFilterManager::removeGenerator(DImgFilterGenerator *generator)
+{
+    QMap<QString, DImgFilterGenerator*>::iterator it;
+    for (it = d->filterMap.begin(); it != d->filterMap.end(); )
+    {
+        if (it.value() == generator)
+            it = d->filterMap.erase(it);
+        else
+            ++it;
+    }
+}
+
+QStringList DImgFilterManager::supportedFilters()
+{
+    return d->filterMap.keys();
+}
+
+QList<int> DImgFilterManager::supportedVersions(const QString& filterIdentifier)
+{
+    DImgFilterGenerator *gen = d->filterMap.value(filterIdentifier);
+    if (gen)
+        return gen->supportedVersions(filterIdentifier);
+    return QList<int>();
+}
+
+bool DImgFilterManager::isSupported(const QString& filterIdentifier)
+{
+    return d->filterMap.contains(filterIdentifier);
+}
+
+bool DImgFilterManager::isSupported(const QString& filterIdentifier, int version)
+{
+    DImgFilterGenerator *gen = d->filterMap.value(filterIdentifier);
+    if (gen)
+        return gen->isSupported(filterIdentifier, version);
+    return false;
+}
+
+DImgThreadedFilter *DImgFilterManager::createFilter(const QString& filterIdentifier, int version)
+{
+    DImgFilterGenerator *gen = d->filterMap.value(filterIdentifier);
+    if (gen)
+        return gen->createFilter(filterIdentifier, version);
+    return 0;
+}
+
+}
+
---
+++ libs/dimg/filters/dimgfiltermanager.h
@@ -0,0 +1,71 @@
+/* ============================================================
+ * 
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-06-24
+ * Description : manager for filters (registering, creating etc)
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIMGFILTERMANAGER_H
+#define DIMGFILTERMANAGER_H
+
+#include <QStringList>
+#include <QList>
+#include <QString>
+
+#include "dimgfiltergenerator.h"
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class DImgFilterManagerPriv;
+
+class DIGIKAM_EXPORT DImgFilterManager : public DImgFilterGenerator
+{
+public:
+
+    static DImgFilterManager *instance();
+
+    virtual QStringList supportedFilters();
+    virtual QList<int> supportedVersions(const QString& filterIdentifier);
+
+    virtual bool isSupported(const QString& filterIdentifier);
+    virtual bool isSupported(const QString& filterIdentifier, int version);
+
+    DImgThreadedFilter *createFilter(const QString& filterIdentifier, int version);
+
+    /**
+     * Registers all filter provided by this generator.
+     */
+    void addGenerator(DImgFilterGenerator *generator);
+    void removeGenerator(DImgFilterGenerator *generator);
+
+private:
+
+    friend class DImgFilterManagerCreator;
+    DImgFilterManager();
+    ~DImgFilterManager();
+
+    DImgFilterManagerPriv *const d;
+
+};
+
+}
+
+#endif
