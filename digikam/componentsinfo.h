d311ec00691cd3dd4eaa2a06d834d7abe503f141
Gilles Caulier <caulier.gilles@gmail.com>
2010-06-29 09:38:38 +0000

    compile
    svn path=/branches/extragear/graphics/digikam/gsoc-2010/; revision=1143995

--- digikam/componentsinfo.h
+++ digikam/componentsinfo.h
@@ -74,9 +74,9 @@ static inline void showDigikamComponentsInfo()
 #endif /* HAVE_GPHOTO2 */
 
     list.insert(i18n("LibKipi"),    KIPI::Interface::version());
-    list.insert(i18n("LibOpenCV"),  KFace::Face::LibOpenCVVersion());
-    list.insert(i18n("Libface"),    KFace::Face::LibFaceVersion());
-    list.insert(i18n("LibKface"),   KFace::Face::version());
+    list.insert(i18n("LibOpenCV"),  KFaceIface::Face::LibOpenCVVersion());
+    list.insert(i18n("Libface"),    KFaceIface::Face::LibFaceVersion());
+    list.insert(i18n("LibKface"),   KFaceIface::Face::version());
 
     // Database Backend information
 
