768619d11791233e7baf1d797a205b0ce9559f38
Gilles Caulier <caulier.gilles@gmail.com>
2010-07-19 08:38:47 +0000

    backport from GoSC branch
    svn path=/trunk/extragear/graphics/digikam/; revision=1151640

--- utilities/setup/setupmisc.cpp
+++ utilities/setup/setupmisc.cpp
@@ -7,7 +7,7 @@
  * Description : mics configuration setup tab
  *
  * Copyright (C) 2004 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2005-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -78,36 +78,36 @@ public:
 SetupMisc::SetupMisc(QWidget* parent)
          : QScrollArea(parent), d(new SetupMiscPriv)
 {
-    QWidget *panel = new QWidget(viewport());
+    QWidget* panel = new QWidget(viewport());
     setWidget(panel);
     setWidgetResizable(true);
 
-    QVBoxLayout *layout               = new QVBoxLayout(panel);
+    QVBoxLayout* layout               = new QVBoxLayout(panel);
     d->showTrashDeleteDialogCheck     = new QCheckBox(i18n("Confirm when moving items to the &trash."), panel);
     d->showPermanentDeleteDialogCheck = new QCheckBox(i18n("Confirm when permanently deleting items."), panel);
     d->sidebarApplyDirectlyCheck      = new QCheckBox(i18n("Do not confirm when applying changes in the &right sidebar."), panel);
     d->showSplashCheck                = new QCheckBox(i18n("&Show splash screen at startup."), panel);
     d->scanAtStart                    = new QCheckBox(i18n("&Scan for new items at startup (makes startup slower.)"), panel);
 
-    KHBox *tabStyleHbox = new KHBox(panel);
+    KHBox* tabStyleHbox = new KHBox(panel);
     d->sidebarTypeLabel = new QLabel(i18n("Sidebar tab title:"), tabStyleHbox);
     d->sidebarType      = new KComboBox(tabStyleHbox);
     d->sidebarType->addItem(i18n("Only For Active Tab"), 0);
     d->sidebarType->addItem(i18n("For All Tabs"),        1);
     d->sidebarType->setToolTip(i18n("Set this option to configure how sidebar tab titles are visible."));
 
-    KHBox *stringComparisonHbox  = new KHBox(panel);
+    KHBox* stringComparisonHbox  = new KHBox(panel);
     d->stringComparisonTypeLabel = new QLabel(i18n("String comparison type:"), stringComparisonHbox);
     d->stringComparisonType      = new KComboBox(stringComparisonHbox);
     d->stringComparisonType->addItem(i18nc("method to compare strings", "Natural"), AlbumSettings::Natural);
     d->stringComparisonType->addItem(i18nc("method to compare strings", "Normal"),  AlbumSettings::Normal);
-    d->stringComparisonType->setToolTip(i18n("Sets the way in which strings are compared inside digiKam. "
+    d->stringComparisonType->setToolTip(i18n("<qt>Sets the way in which strings are compared inside digiKam. "
                     "This eg. influences the sorting of the tree views.<br/>"
                     "<b>Natural</b> tries to compare strings in a way that regards some normal conventions "
                     "and will eg. result in sorting numbers naturally even if they have a different number of digits.<br/>"
                     "<b>Normal</b> uses a more technical approach. "
                     "Use this style if you eg. want to entitle albums with ISO dates (201006 or 20090523) "
-                    "and the albums should be sorted according to these dates."));
+                    "and the albums should be sorted according to these dates.</qt>"));
 
     // --------------------------------------------------------
 
