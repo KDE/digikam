7f0501dc7ab47aaafffb714a8cf710446d1bd996
Aditya Jitesh Bhatt <adityabhatt1991@gmail.com>
2010-07-23 16:18:50 +0000

    License
    svn path=/branches/extragear/graphics/digikam/; revision=1153606

--- digikam/imagepreviewviewv2.h
+++ digikam/imagepreviewviewv2.h
@@ -8,7 +8,8 @@
  *
  * Copyright (C) 2006-2010 Gilles Caulier  <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
- *
+ * Copyright (C) 2010 by Aditya Bhatt <adityabhatt1991 at gmail dot com>
+ * 
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
