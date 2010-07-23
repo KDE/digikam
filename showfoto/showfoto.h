a50f3bd7b88a8855d0fc69e51121a3a2ff638a94
Gilles Caulier <caulier.gilles@gmail.com>
2010-07-23 16:27:08 +0000

    sync with trunk rev #1153599
    svn path=/branches/extragear/graphics/digikam/; revision=1153608

--- showfoto/showfoto.h
+++ showfoto/showfoto.h
@@ -6,11 +6,11 @@
  * Date        : 2004-11-22
  * Description : stand alone digiKam image editor GUI
  *
+ * Copyright (C) 2004-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2010 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
  * Copyright (C) 2005-2006 by Tom Albers <tomalbers@kde.nl>
- * Copyright (C) 2004-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2006-2009 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
- * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -115,6 +115,7 @@ private Q_SLOTS:
 
     void slotDeleteCurrentItemResult(KJob*);
 
+    void slotPrepareToLoad();
     void slotLoadingStarted(const QString& filename);
     void slotLoadingFinished(const QString& filename, bool success);
     void slotSavingStarted(const QString& filename);
