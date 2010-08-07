897d472b2058b2de9e7f538b93b5455650f709a1
Aditya Jitesh Bhatt <adityabhatt1991@gmail.com>
2010-08-07 15:50:34 +0000

    Much-needed documentation for FaceIface, at last.
    svn path=/branches/extragear/graphics/digikam/; revision=1160288

--- libs/database/face/faceiface.h
+++ libs/database/face/faceiface.h
@@ -54,12 +54,47 @@ public:
     FaceIface();
     ~FaceIface();
 
+    /**
+     * Tells if the image has been scanned for faces or not
+     */
     bool                hasBeenScanned(qlonglong imageid);
+    
+    /**
+     * Detects faces from the image and returns a list of faces
+     * @param image The DImg , from which face rectangles will be cropped out
+     * @param imageid The image id from the database
+     * @return A list of faces found in the given image. With cropped face images.
+     */
     QList<Face>         findAndTagFaces(DImg& image, qlonglong imageid);
+    
+    /**
+     * Reads rect tags in the specified image and returns a list of faces, read from these tags.
+     * Very fast compared to findAndTagFaces. It is "read-only".
+     */
     QList<Face>         findFacesFromTags(DImg& image, qlonglong imageid);
+    
+    /**
+     * Unassigns all face tags from the image and sets it's scanned property to false.
+     */
     void                forgetFaceTags(qlonglong imageid);
+    
+    /**
+     * Returns a list of all tag rectangles for the image. Unlike findAndTagFaces, this does not take a DImg, 
+     * because it returns only a QRect, not a Face, so no need of cropping a face rectangle.
+     */
     QList<QRect>        getTagRects(qlonglong imageid);
+    
+    /**
+     * Returns a list of image ids of all images in the DB which have a specified person within.
+     * @param tagId The person's id. Or tag id for the person tag. Same thing.
+     * @param repeat If repeat is specified as true, then if person with id tagId is found n times in an image, 
+     * that image's id will be pushed into the returned list n times. Useful for thumbnailing as I see it.
+     */
     QList<qlonglong>    imagesWithPerson(int tagId, bool repeat = false);
+    
+    /** 
+     * Returns the number of faces present in an image.
+     */
     int                 numberOfFaces(qlonglong imageid);
     
     QString rectToString(const QRect& rect)        const;
