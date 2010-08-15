1217228d7a7384d5a01fe9e9f8b1cabd5f4791a5
Gilles Caulier <caulier.gilles@gmail.com>
2010-08-15 09:50:21 +0000

    no need a dot at end of slogan
    svn path=/trunk/extragear/graphics/digikam/; revision=1163886

--- digikam/daboutdata.h
+++ digikam/daboutdata.h
@@ -44,7 +44,7 @@ static inline KLocalizedString digiKamSloganFormated()
                   "Please translate using short words else the slogan can be truncated.",
                   "<qt><font color=\"white\">"
                   "<b>Manage</b> your <b>photographs</b> like <b>a professional</b> "
-                  "with the power of <b>open source</b>."
+                  "with the power of <b>open source</b>"
                   "</font></qt>"
                  );
 }
@@ -52,7 +52,7 @@ static inline KLocalizedString digiKamSloganFormated()
 static inline KLocalizedString digiKamSlogan()
 {
     return ki18n("Manage your photographs like a professional, "
-                 "with the power of open source.");
+                 "with the power of open source");
 }
 
 static inline KLocalizedString copyright()
