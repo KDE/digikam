d58aa498723f64a6fb55dabbe4010cf329c50c2c
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-08-26 18:54:07 +0000

    A class to encapsulate tag region descriptions in the database. Use QXmlStreamReader/Writer to parse and create XML. Store a rectangle as a single XML tag (<?xml ...> stripped) in SVG style
    svn path=/branches/extragear/graphics/digikam/; revision=1168484

---
+++ libs/database/tagregion.cpp
@@ -0,0 +1,109 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-08-26
+ * Description : Tag region formatting
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "tagregion.h"
+
+// Qt includes
+
+#include <QXmlStreamReader>
+#include <QXmlStreamWriter>
+
+// Local includes
+
+
+namespace Digikam
+{
+
+TagRegion::TagRegion(const QString& descriptor)
+    : m_value(descriptor), m_type(Invalid)
+{
+    QString xmlStartDocument = "<?xml version=\"1.0\"?>";
+    QXmlStreamReader reader(xmlStartDocument + descriptor);
+    if (reader.readNextStartElement())
+    {
+        if (reader.name() == "rect")
+        {
+            QRect r(reader.attributes().value("x").toString().toInt(),
+                    reader.attributes().value("y").toString().toInt(),
+                    reader.attributes().value("width").toString().toInt(),
+                    reader.attributes().value("height").toString().toInt());
+
+            if (r.isValid())
+            {
+                m_value = r;
+                m_type  = Rect;
+            }
+        }
+    }
+}
+
+TagRegion::TagRegion(const QRect& rect)
+    : m_value(rect), m_type(Rect)
+{
+}
+
+TagRegion::Type TagRegion::type() const
+{
+    return m_type;
+}
+
+bool TagRegion::isValid() const
+{
+    return m_type != Invalid;
+}
+
+QString TagRegion::toXml() const
+{
+    if (m_type == Invalid)
+        return QString();
+
+    QString output;
+    QXmlStreamWriter writer(&output);
+    writer.writeStartDocument();
+    int lengthOfHeader = output.length();
+
+    if (m_type == Rect)
+    {
+        QRect rect = m_value.toRect();
+        writer.writeStartElement("rect");
+        writer.writeAttribute("x", QString::number(rect.x()));
+        writer.writeAttribute("y", QString::number(rect.y()));
+        writer.writeAttribute("width", QString::number(rect.width()));
+        writer.writeAttribute("height", QString::number(rect.height()));
+        writer.writeEndElement();
+    }
+
+    // cut off the <?xml> tag at start of document
+    return output.mid(lengthOfHeader);
+}
+
+QRect TagRegion::toRect() const
+{
+    if (m_type == Rect)
+        return m_value.toRect();
+    return QRect();
+}
+
+
+}
+
---
+++ libs/database/tagregion.h
@@ -0,0 +1,76 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-08-26
+ * Description : Tag region formatting
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel.wiesweg@gmx.de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef TAGREGION_H
+#define TAGREGION_H
+
+// Qt includes
+
+#include <QRect>
+#include <QString>
+#include <QVariant>
+
+// Local includes
+
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class DIGIKAM_DATABASE_EXPORT TagRegion
+{
+public:
+
+    /**
+     * Use this small class to convert between the formatted
+     * textual representation of a tag region in the database
+     * and the corresponding object.
+     */
+
+    /// Construct with the textual descriptor
+    explicit TagRegion(const QString& descriptor);
+    /// Construct with the region
+    explicit TagRegion(const QRect& rect);
+
+    enum Type
+    {
+        Invalid,
+        Rect
+    };
+
+    Type type() const;
+    bool isValid() const;
+
+    QString toXml() const;
+    QRect toRect() const;
+
+protected:
+
+    QVariant m_value;
+    Type     m_type;
+};
+
+}
+
+#endif
+
