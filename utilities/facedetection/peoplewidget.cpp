db423b438a475db5a32b19f2526d1a8926547d28
Gilles Caulier <caulier.gilles@gmail.com>
2010-09-03 08:15:06 +0000

    polish
    svn path=/branches/extragear/graphics/digikam/; revision=1171241

--- utilities/facedetection/peoplewidget.cpp
+++ utilities/facedetection/peoplewidget.cpp
@@ -49,7 +49,7 @@ public:
 };
 
 PeopleWidget::PeopleWidget(QWidget* parent, int numPeople)
-            :QWidget(parent), d(new PeopleWidgetPriv)
+            : QWidget(parent), d(new PeopleWidgetPriv)
 {
     d->peopleCount->setNum(numPeople);
     d->label->setText("Number of People");
@@ -58,6 +58,14 @@ PeopleWidget::PeopleWidget(QWidget* parent, int numPeople)
 PeopleWidget::~PeopleWidget()
 {
     delete d;
+}
+
+int PeopleWidget::cursorInfo(QString& /*infoDate*/)
+{
+}
+
+void PeopleWidget::setCurrentIndex(int /*index*/)
+{
 }
 
 }; // Namespace Digikam
