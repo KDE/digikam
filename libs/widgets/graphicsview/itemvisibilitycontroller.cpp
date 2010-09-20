fe89459f208073e90e7bf8494d2e31b657a2fa10
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-09-20 12:25:01 +0000

    Add a generic controller for controlling visibility of items or elements within items where showing/hiding shall be animated, visibility is controlled by a higher-level parameter and a lower-level state.
    svn path=/branches/extragear/graphics/digikam/; revision=1177509

---
+++ libs/widgets/graphicsview/itemvisibilitycontroller.cpp
@@ -0,0 +1,303 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-09-20
+ * Description : Managing visibility state with animations
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "itemvisibilitycontroller.h"
+
+// Qt includes
+
+#include <QParallelAnimationGroup>
+#include <QPropertyAnimation>
+
+// KDE includes
+
+#include <kdebug.h>
+
+// Local includes
+
+namespace Digikam
+{
+
+ItemVisibilityControllerPropertyObject::ItemVisibilityControllerPropertyObject(QObject *parent)
+    : QObject(parent)
+{
+    m_opacity = 0;
+    m_visible = false;
+}
+
+qreal ItemVisibilityControllerPropertyObject::opacity() const
+{
+    return m_opacity;
+}
+
+void ItemVisibilityControllerPropertyObject::setOpacity(qreal opacity)
+{
+    m_opacity = opacity;
+    emit opacityChanged();
+}
+
+bool ItemVisibilityControllerPropertyObject::isVisible() const
+{
+    return m_visible;
+}
+
+void ItemVisibilityControllerPropertyObject::setVisible(bool visible)
+{
+    m_visible = visible;
+    visibleChanged();
+}
+
+// ---
+
+AnimatedVisibility::AnimatedVisibility(QObject *parent)
+    : ItemVisibilityControllerPropertyObject(parent)
+{
+    m_controller = new ItemVisibilityController(ItemVisibilityController::SingleItem, this);
+    m_controller->setItem(this);
+}
+
+ItemVisibilityController *AnimatedVisibility::controller() const
+{
+    return m_controller;
+}
+
+// ---
+
+class ItemVisibilityController::ItemVisibilityControllerPriv
+{
+public:
+
+    ItemVisibilityControllerPriv(ItemVisibilityController *q) : q(q)
+    {
+        animation      = 0;
+        state          = ItemVisibilityController::Hidden;
+        visible        = false;
+        shallBeShown   = true;
+    }
+
+    ItemVisibilityController::ItemType type;
+    QAbstractAnimation*                animation;
+    ItemVisibilityController::State    state;
+    bool                               visible;
+    bool                               shallBeShown;
+    QList<QObject*>                    items;
+
+    void                       transitionToVisible(bool show);
+    void                       setVisibleProperty(bool value);
+    void                       syncProperties(QObject *o);
+    QPropertyAnimation        *createAnimation();
+
+    QPropertyAnimation        *propertyAnimation() const { return static_cast<QPropertyAnimation*>(animation); }
+    QParallelAnimationGroup   *animationGroup() const { return static_cast<QParallelAnimationGroup*>(animation); }
+
+    ItemVisibilityController* const q;
+};
+
+
+ItemVisibilityController::ItemVisibilityController(ItemType type, QObject *parent)
+    : QObject(parent), d(new ItemVisibilityControllerPriv(this))
+{
+    d->type = type;
+}
+
+ItemVisibilityController::~ItemVisibilityController()
+{
+    delete d->animation;
+    delete d;
+}
+
+void ItemVisibilityController::setItem(QObject *object)
+{
+    if (d->type == ItemGroup)
+    {
+        clear();
+        addItem(object);
+        return;
+    }
+
+    if (!d->propertyAnimation())
+    {
+        d->animation = d->createAnimation();
+    }
+
+    d->propertyAnimation()->setTargetObject(object);
+
+    d->syncProperties(object);
+    d->items << object;
+}
+
+void ItemVisibilityController::addItem(QObject *object)
+{
+    if (d->type == SingleItem)
+    {
+        setItem(object);
+        return;
+    }
+
+    if (!d->animationGroup())
+    {
+        d->animation = new  QParallelAnimationGroup(this);
+
+        connect(d->animationGroup(), SIGNAL(finished()),
+                this, SLOT(animationFinished()));
+    }
+
+    QPropertyAnimation* anim = d->createAnimation();
+    anim->setTargetObject(object);
+    d->animationGroup()->addAnimation(anim);
+
+    d->syncProperties(object);
+    d->items << object;
+}
+
+void ItemVisibilityController::clear()
+{
+    if (d->type == ItemGroup)
+    {
+        if (d->animationGroup())
+            d->animationGroup()->clear();
+    }
+    else
+    {
+        delete d->animation;
+        d->animation = 0;
+    }
+    d->state   = Hidden;
+    d->visible = false;
+}
+
+bool ItemVisibilityController::shallBeShown() const
+{
+    return d->shallBeShown;
+}
+
+bool ItemVisibilityController::isVisible() const
+{
+    return d->visible;
+}
+
+ItemVisibilityController::State ItemVisibilityController::state() const
+{
+    return d->state;
+}
+
+QAbstractAnimation *ItemVisibilityController::animation() const
+{
+    return d->animation;
+}
+
+void ItemVisibilityController::setShallBeShown(bool shallBeShown)
+{
+    d->shallBeShown = shallBeShown;
+    d->transitionToVisible(d->shallBeShown && d->visible);
+}
+
+void ItemVisibilityController::show()
+{
+    setVisible(true);
+}
+
+void ItemVisibilityController::hide()
+{
+    setVisible(false);
+}
+
+void ItemVisibilityController::setVisible(bool visible)
+{
+    d->visible = visible;
+    d->transitionToVisible(d->shallBeShown && d->visible);
+}
+
+QPropertyAnimation *ItemVisibilityController::ItemVisibilityControllerPriv::createAnimation()
+{
+    QPropertyAnimation *anim = new QPropertyAnimation(q);
+    anim->setPropertyName("opacity");
+    anim->setStartValue(0);
+    anim->setEndValue(1.0);
+    anim->setDuration(200);
+    anim->setEasingCurve(QEasingCurve::InOutQuad);
+    return anim;
+}
+
+void ItemVisibilityController::ItemVisibilityControllerPriv::setVisibleProperty(bool value)
+{
+    foreach (QObject *o, items)
+    {
+        o->setProperty("visible", value);
+    }
+}
+
+void ItemVisibilityController::ItemVisibilityControllerPriv::syncProperties(QObject *o)
+{
+    if (state == Visible || state == FadingIn)
+    {
+        o->setProperty("visible", true);
+        o->setProperty("opacity", 1.0);
+    }
+    else
+    {
+        o->setProperty("visible", false);
+        o->setProperty("opacity", 0);
+    }
+}
+
+void ItemVisibilityController::ItemVisibilityControllerPriv::transitionToVisible(bool show)
+{
+    //kDebug() << "state" << state << "show" << show << items.size();
+
+    if (show)
+    {
+        if (state == Visible || state == FadingIn)
+            return;
+        if (state == Hidden)
+            setVisibleProperty(true);
+        state = FadingIn;
+    }
+    else
+    {
+        if (state == Hidden || state == FadingOut)
+            return;
+        state = FadingOut;
+    }
+
+    QAbstractAnimation::Direction direction = show ? QAbstractAnimation::Forward : QAbstractAnimation::Backward;
+    animation->setDirection(direction);
+    animation->start();
+}
+
+void ItemVisibilityController::animationFinished()
+{
+    if (d->state == FadingOut)
+    {
+        d->setVisibleProperty(false);
+        d->state = Hidden;
+    }
+    else
+    {
+        d->state = Visible;
+    }
+}
+
+
+} // namespace Digikam
+
+
---
+++ libs/widgets/graphicsview/itemvisibilitycontroller.h
@@ -0,0 +1,192 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-09-20
+ * Description : Managing visibility state with animations
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef ITEMVISIBILITYCONTROLLER_H
+#define ITEMVISIBILITYCONTROLLER_H
+
+// Qt includes
+
+#include <QAbstractAnimation>
+
+// KDE includes
+
+// Local includes
+
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class DIGIKAM_EXPORT ItemVisibilityController : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool  shallBeShown READ shallBeShown WRITE setShallBeShown)
+    Q_PROPERTY(bool  visible READ isVisible WRITE setVisible)
+    Q_ENUMS(State)
+
+public:
+
+    /**
+     *  This class handles complex visibility situations for items.
+     *  There is a 3-tiered approach:
+     *  1) shallBeShown determines if the items shall at any time be shown.
+     *     If it is false, items will never be shown.
+     *     Default is true, so you can ignore this setting.
+     *  2) visible determines if the items shall be shown now.
+     *     Only takes effect if shallBeShown is true.
+     *     Default is false: Initially, controlled items are hidden.
+     *  3) Opacitiy and individual item visibility:
+     *     When showing, items are first set to individually visible,
+     *     then their opacity is increased from 0 to 1.
+     *     When hiding, opacity is first decreased from 1 to 0,
+     *     then they are set individually to hidden.
+     *  Different types of items can be handled:
+     *  - a group of items with an "opacity" and "visible" property
+     *  - a single item with an "opacity" and "visible" property
+     *  - a proxy object with these properties (see above)
+     */
+
+    enum State
+    {
+        Hidden,
+        FadingIn,
+        Visible,
+        FadingOut
+    };
+
+    enum ItemType
+    {
+        SingleItem,
+        ItemGroup
+    };
+
+    ItemVisibilityController(ItemType type = SingleItem, QObject *parent = 0);
+    ~ItemVisibilityController();
+
+    /**
+     * Set or add objects. The given objects shall provide
+     * an "opacity" and a "visible" property.
+     * You can, for convenience, use a ItemVisibilityControllerPropertyObject
+     * as a value container, if your items do not provide these properties directly.
+     * No ownership is taken, so the objects should live as long as this object
+     * is used.
+     */
+    /// Call this for SingleItem animations
+    void setItem(QObject *object);
+    /// Call this for ItemGroup animations
+    void addItem(QObject *object);
+
+    bool  shallBeShown() const;
+    bool  isVisible() const;
+    State state() const;
+
+    /// Remove all animations
+    void clear();
+
+    /**
+     * Returns the property used for the animation.
+     * If the item is a SingleItem, it will be a QPropertyAnimation.
+     * If it's an ItemGroup, it will be a QParallelAnimationGroup.
+     * If animations are switched off globally, this can be 0.
+     */
+    QAbstractAnimation *animation() const;
+
+public Q_SLOTS:
+
+    void setShallBeShown(bool shallBeShown);
+    void show();
+    void hide();
+    void setVisible(bool visible);
+
+protected Q_SLOTS:
+
+    void animationFinished();
+
+public: // internal
+
+
+private:
+
+    class ItemVisibilityControllerPriv;
+    ItemVisibilityControllerPriv* const d;
+};
+
+class DIGIKAM_EXPORT ItemVisibilityControllerPropertyObject : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(qreal opacity READ opacity WRITE setOpacity NOTIFY opacityChanged)
+    Q_PROPERTY(bool  visible READ isVisible WRITE setVisible NOTIFY visibleChanged)
+
+public:
+
+    /**
+     *  You can use this object as a container providing the properties
+     *  set by ItemVisibilityController.
+     *  Connect to the signals accordingly, e.g. to trigger a repaint.
+     */
+
+    ItemVisibilityControllerPropertyObject(QObject *parent = 0);
+
+    qreal opacity() const;
+    void setOpacity(qreal opacity);
+
+    bool isVisible() const;
+    void setVisible(bool visible);
+
+Q_SIGNALS:
+
+    void opacityChanged();
+    void visibleChanged();
+
+protected:
+
+    qreal m_opacity;
+    qreal m_visible;
+};
+
+class DIGIKAM_EXPORT AnimatedVisibility : public ItemVisibilityControllerPropertyObject
+{
+    Q_OBJECT
+
+public:
+
+    /** A convenience class:
+     *  The property object brings its own controller.
+     *  Ready to use: Just construct an object and connect to the signals.
+     *  Please note the difference between controller()->setVisible() and setVisible():
+     *  You want to call the controller's method!
+     */
+
+    AnimatedVisibility(QObject *parent = 0);
+
+    ItemVisibilityController *controller() const;
+
+protected:
+
+    ItemVisibilityController *m_controller;
+};
+
+} // namespace Digikam
+
+#endif // ITEMVISIBILITYCONTROLLER_H
+
