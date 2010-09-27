e66b83a4bf2de6f446e1313ed19bb6368c5f7da0
Gilles Caulier <caulier.gilles@gmail.com>
2010-09-27 11:56:35 +0000

    include Lensfun library in digiKam as 3rd party. The current lensfun code come from trunk repository (0.2.5.1) If lensfun is found on your computer, this one will be used intead. This lensfun inclusion in digiKam core is mandatory to provide lens auto correction under Windows and MacOS-X where LensFun package do not exist
    svn path=/trunk/extragear/graphics/digikam/; revision=1180214

---
+++ libs/3rdparty/lensfun/include/config-lensfun.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_LENSFUN_H
+#define CONFIG_LENSFUN_H
+
+#define CONF_PACKAGE "lensfun"
+
+/* Define to 1 if GNU GCC is detected */
+#define CONF_COMPILER_GCC 
+
+/* Define path to install lens database XML files */
+#define CONF_DATADIR "/usr/share/apps/digikam/lensfun"
+
+#endif // CONFIG_LENSFUN_H
