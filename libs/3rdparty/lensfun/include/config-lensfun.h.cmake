f26b4052bfcf3b6b726889f58114f77ba59c9cfb
Gilles Caulier <caulier.gilles@gmail.com>
2010-09-28 10:27:49 +0000

    fix compile with MSVC
    svn path=/trunk/extragear/graphics/digikam/; revision=1180612

--- libs/3rdparty/lensfun/include/config-lensfun.h.cmake
+++ libs/3rdparty/lensfun/include/config-lensfun.h.cmake
@@ -3,10 +3,13 @@
 
 #define CONF_PACKAGE "lensfun"
 
-/* Define to 1 if GNU GCC is detected */
-#define CONF_COMPILER_GCC ${CMAKE_COMPILER_IS_GNUCXX}
-
 /* Define path to install lens database XML files */
 #define CONF_DATADIR "${KDE4_DATA_INSTALL_DIR}/digikam/lensfun"
+
+#ifdef Q_CC_MSVC
+#define PLATFORM_WINDOWS 1
+#elseifdef QCC_GNU
+#define CONF_COMPILER_GCC 1
+#endif
 
 #endif // CONFIG_LENSFUN_H
