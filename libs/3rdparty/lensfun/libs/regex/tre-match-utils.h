157ce74ecd1ceab2327788ca6afc7d348543bc5d
Gilles Caulier <caulier.gilles@gmail.com>
2010-09-28 09:46:24 +0000

    MSVC don't know inline for C ansi source code. It use __inline
    svn path=/trunk/extragear/graphics/digikam/; revision=1180547

--- libs/3rdparty/lensfun/libs/regex/tre-match-utils.h
+++ libs/3rdparty/lensfun/libs/regex/tre-match-utils.h
@@ -171,10 +171,15 @@
        && (pos == 0 || next_c == L'\0'					      \
 	   || IS_WORD_CHAR(prev_c) != IS_WORD_CHAR(next_c))))
 
+#ifndef __GNUC__
+#define INLINE inline
+#else
+#define INLINE __inline /* For MSVC compiler. */
+#endif
 
 
 /* Returns 1 if `t1' wins `t2', 0 otherwise. */
-inline static int
+INLINE static int
 tre_tag_order(int num_tags, tre_tag_direction_t *tag_directions,
 	      int *t1, int *t2)
 {
@@ -200,7 +205,7 @@ tre_tag_order(int num_tags, tre_tag_direction_t *tag_directions,
   return 0;
 }
 
-inline static int
+INLINE static int
 tre_neg_char_classes_match(tre_ctype_t *classes, tre_cint_t wc, int icase)
 {
   DPRINT(("neg_char_classes_test: %p, %d, %d\n", classes, wc, icase));
