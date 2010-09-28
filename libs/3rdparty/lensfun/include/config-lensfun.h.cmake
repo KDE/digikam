ca318f8ed89edc2dd4ce4642c7801968d50c8006
Gilles Caulier <caulier.gilles@gmail.com>
2010-09-28 10:40:33 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=1180618

--- libs/3rdparty/lensfun/include/config-lensfun.h.cmake
+++ libs/3rdparty/lensfun/include/config-lensfun.h.cmake
@@ -1,11 +1,35 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-09-27
+ * Description : lensfun config header
+ *
+ * Copyright (C) 2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef CONFIG_LENSFUN_H
 #define CONFIG_LENSFUN_H
 
 #define CONF_PACKAGE "lensfun"
 
+/* We don't need to export Lensfun library symbol */
 #define CONF_LENSFUN_STATIC 1
 
-/* Define path to install lens database XML files */
+/* Define path where are installed lens database XML files */
 #define CONF_DATADIR "${KDE4_DATA_INSTALL_DIR}/digikam/lensfun"
 
 #endif // CONFIG_LENSFUN_H
