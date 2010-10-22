be2d778f889104b90b33d02b572a9b1df6c42e28
Gilles Caulier <caulier.gilles@gmail.com>
2010-10-22 13:00:40 +0000

    add info about liblqr
    svn path=/trunk/extragear/graphics/digikam/; revision=1188453

--- libs/dialogs/libsinfodlg.cpp
+++ libs/dialogs/libsinfodlg.cpp
@@ -129,6 +129,19 @@ LibsInfoDlg::LibsInfoDlg(QWidget *parent)
     list.insert(i18n("LibLensFun"),                  i18n("external shared library"));
 #endif // USE_EXT_LIBLENSFUN
 
+#ifndef USE_EXT_LIBLQR
+#   ifdef HAVE_GLIB2
+    list.insert(i18n("LibLqr"),                      i18n("internal library",
+                                                          LF_VERSION_MAJOR,
+                                                          LF_VERSION_MINOR,
+                                                          LF_VERSION_MICRO,
+                                                          LF_VERSION_BUGFIX));
+#   endif // HAVE_GLIB2
+#else
+    list.insert(i18n("LibLqr"),                      i18n("external shared library"));
+#endif // USE_EXT_LIBLQR
+
+
     list.insert(i18n("LibPNG"),                      QString(PNG_LIBPNG_VER_STRING));
     list.insert(i18n("LibTIFF"),                     QString(TIFFLIB_VERSION_STR).replace('\n', ' '));
     list.insert(i18n("LibJPEG"),                     QString::number(JPEG_LIB_VERSION));
