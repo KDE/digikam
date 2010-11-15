d81915187501c03fd2aeb093db8ab91eabd70d94
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-11-15 10:49:59 +0000

    Add a test for reproducible actions. Add test images for the "auto" filters (test is passed for those).
    To prepare a test case:
    - we have defined an original image, which is supplied             
    - in digikam, load the original and apply one or several filters
    - store as a new version, as PNG for lossless(!) compression
    - commit the file to the test images directory
    
    The test does the following:
    - load the original image
    - for each remaining image in the directory:
       - load the image
       - read the history, extract the applied filter actions
       - apply the actions to the original image using a FilterActionFilter
       - compare pixel-by-pixel the new result to the stored result
       - fail if not identical
    
    If filters are changed and older versions are not supported,
    simply a new testcase should be produced, replacing the old one
    (even better if the filter still supports the old version)
    
    
    
    svn path=/branches/extragear/graphics/digikam/; revision=1197299

---
+++ tests/dimagefilteractiontest.cpp
@@ -0,0 +1,134 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-11-13
+ * Description : a test for applying FilterActions
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "dimagefilteractiontest.h"
+
+// Qt includes
+
+#include <QPixmap>
+#include <QLabel>
+#include <QDialog>
+#include <QHBoxLayout>
+
+// KDE includes
+
+#include <qtest_kde.h>
+
+// Local includes
+
+#include "dimg.h"
+#include "filteractionfilter.h"
+
+using namespace Digikam;
+
+QTEST_KDEMAIN(DImageFilterActionTest, /*No*/GUI)
+//QTEST_MAIN(DImageFilterActionTest)
+
+void DImageFilterActionTest::testActions()
+{
+    QStringList files = QDir(imagePath()).entryList(QDir::Files);
+    files.removeOne(originalImage());
+
+    DImg original(imagePath() + originalImage());
+    QVERIFY(!original.isNull());
+
+    foreach (const QString& fileName, files)
+    {
+        DImg ref(imagePath() + fileName);
+        QVERIFY(!ref.isNull());
+        DImageHistory history = ref.getImageHistory();
+
+        FilterActionFilter filter;
+        filter.setFilterActions(history.allActions());
+        QVERIFY(filter.isReproducible() || filter.isComplexAction());
+
+        filter.setupFilter(original.copy());
+        filter.startFilterDirectly();
+        qDebug() << filter.filterActions().size();
+
+        DImg img = filter.getTargetImage();
+
+        QVERIFY(ref.size() == img.size());
+
+        bool isEqual = true;
+        DImg diff(ref.width(), ref.height(), ref.sixteenBit());
+        diff.fill(DColor(Qt::black));
+        for (uint x=0; x<ref.width(); x++)
+        {
+            for (uint y=0; y<ref.height(); y++)
+            {
+                DColor cref = ref.getPixelColor(x,y);
+                DColor cres = img.getPixelColor(x,y);
+                if (cref.red() != cres.red() || cref.green() != cres.green() || cref.blue() != cres.blue())
+                {
+                    //qDebug() << x << y;
+                    diff.setPixelColor(x,y, DColor(Qt::white));
+                    isEqual = false;
+                }
+            }
+        }
+        if (!isEqual)
+        {
+            showDiff(original, ref, img, diff);
+        }
+        QVERIFY(isEqual);
+    }
+}
+
+void DImageFilterActionTest::showDiff(const Digikam::DImg& orig, const Digikam::DImg& ref,
+                                      const Digikam::DImg& result, const DImg& diff)
+{
+    QDialog d;
+    QLabel l1, l2, l3, l4;
+    l1.setPixmap(orig.convertToPixmap());
+    l2.setPixmap(ref.convertToPixmap());
+    l3.setPixmap(result.convertToPixmap());
+    l4.setPixmap(diff.convertToPixmap());
+    QHBoxLayout layout(&d);
+    layout.addWidget(&l1);
+    layout.addWidget(&l2);
+    layout.addWidget(&l3);
+    layout.addWidget(&l4);
+    d.setLayout(&layout);
+    d.exec();
+}
+
+void DImageFilterActionTest::initTestCase()
+{
+}
+
+void DImageFilterActionTest::cleanupTestCase()
+{
+}
+
+QString DImageFilterActionTest::originalImage()
+{
+    // picture taken by me, downscaled to 100x66 and metadata stripped off
+    return "DSC00636.JPG";
+}
+
+QString DImageFilterActionTest::imagePath()
+{
+    return QString(KDESRCDIR) + "filteractiontestimages";
+}
+
