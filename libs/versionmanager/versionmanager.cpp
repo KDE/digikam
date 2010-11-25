ed3e1d5b62d3fea00fd32ff35b3b36ccbe405a73
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-11-25 14:57:49 +0000

    Extend the version manager, giving it the power to make the decisions when saving a versioned image.
    - the VersionNamingScheme is an interface to implement the naming
      scheme of saved files.
      Implemented is only the current "_v....jpg" scheme with some
      adjustments to reduce dimensionality (editing v3 -> v3v1? No, v4)
    - VersionManagerSettings is a container for relevant settings.
      Still some TODO here.
    - VersionFileOperation is a structure giving the result from the VersionManager:
      it can tell where to save which files and versions
    - the VersionManager itself is no longer a singleton, but a normal
      class where some virtual methods can be overridden.
      ImageWindow will have such a class, and give it the settings from AlbumSettings
    
    
    svn path=/branches/extragear/graphics/digikam/; revision=1200708

--- libs/versionmanager/versionmanager.cpp
+++ libs/versionmanager/versionmanager.cpp
@@ -30,8 +30,11 @@
 
 // KDE includes
 
+#include <KConfigGroup>
 #include <KDiskFreeSpaceInfo>
 #include <KDebug>
+#include <KGlobal>
+#include <KUrl>
 
 // Local includes
 
@@ -40,79 +43,386 @@
 namespace Digikam
 {
 
-QString VersionManager::getVersionedFilename(const QString& originalPath, const QString& originalName, 
-                                             qint64 fileSize, const QString& formatForRAW,
-                                             QString& formatForSubversions,
-                                             bool editingOriginal, bool fork, bool editingRAW)
+VersionManagerSettings::VersionManagerSettings()
 {
-    kDebug() << "Original path: " << originalPath << " | Original name: " << originalName
-             << " | Editing original: " << editingOriginal << " | Subversion: " << fork;
+    showAllVersions             = true;
+    saveIntermediateVersions    = NoIntermediates;
+    formatForStoringRAW         = "JPG";
+    formatForStoringSubversions = "JPG";
+}
 
-    //check if we have enough free space for another file
-    KDiskFreeSpaceInfo diskInfo = KDiskFreeSpaceInfo::freeSpaceInfo(originalPath);
+void VersionManagerSettings::readFromConfig(KConfigGroup& group)
+{
+    showAllVersions             = group.readEntry("Show All Available Versions", true);
+    int save                    = group.readEntry("Save Intermediate Versions", (int)NoIntermediates);
+    saveIntermediateVersions    = IntermediateBehavior(save);
+    formatForStoringRAW         = group.readEntry("Format For Storing Versions Of RAW Images", QString("JPG"));
+    formatForStoringSubversions = group.readEntry("Format For Storing Other Versions Of Files", QString("JPG"));
+}
 
-    if(diskInfo.isValid() && diskInfo.available() > (uint)fileSize * 2)
+void VersionManagerSettings::writeToConfig(KConfigGroup& group) const
+{
+    group.writeEntry("Show All Available Versions", showAllVersions);
+    group.writeEntry("Save Intermediate Versions", (int)saveIntermediateVersions);
+    group.writeEntry("Format For Storing Versions Of RAW Images", formatForStoringRAW);
+    group.writeEntry("Format For Storing Other Versions Of Files", formatForStoringSubversions);
+}
+
+// --------------
+
+class DefaultVersionNamingScheme : public VersionNamingScheme
+{
+public:
+
+    virtual QString baseName(const QString& currentPath, const QString& filename);
+    virtual QString versionFileName(const QString& currentPath, const QString& filename,
+                                    const QString& format, const QVariant& counter);
+    virtual QString intermediateFileName(const QString& currentPath, const QString& filename,
+                                         const QString& format, const QVariant& version, const QVariant& counter);
+    virtual QString directory(const QString& currentPath, const QString& filename);
+    virtual QVariant initialCounter();
+    virtual QVariant incrementedCounter(const QVariant& counter);
+};
+
+QVariant DefaultVersionNamingScheme::initialCounter()
+{
+    // start with _v1
+    return 1;
+}
+
+QVariant DefaultVersionNamingScheme::incrementedCounter(const QVariant& counter)
+{
+    return counter.toInt() + 1;
+}
+
+QString DefaultVersionNamingScheme::baseName(const QString& currentPath, const QString& fileName)
+{
+    Q_UNUSED(currentPath);
+    // Perl: /^(.+?)(_v\d+?)?(-\d+?)?\.([^\.]+?)$/
+    // But setMinimal() cannot replace Perl's non-greedy quantifiers, so we need three regexps
+
+    // DSC000636_v5-3.JPG
+    QRegExp versionIntermediate("(.+)(_v\\d+)(-\\d+)\\.([^\\.]+)");
+    if (versionIntermediate.exactMatch(fileName))
+        return versionIntermediate.cap(1);
+
+    // DSC000636_v5.JPG
+    QRegExp version("(.+)(_v\\d+)\\.([^\\.]+)");
+    if (version.exactMatch(fileName))
+        return version.cap(1);
+
+    // DSC000636.JPG
+    QRegExp basename("(.+)\\.([^\\.]+)");
+    if (basename.exactMatch(fileName))
+        return basename.cap(1);
+
+    // failure: no file suffix?
+    // DSC000636
+    return fileName;
+}
+
+QString DefaultVersionNamingScheme::versionFileName(const QString& currentPath, const QString& fileName,
+                                                    const QString& format, const QVariant& counter)
+{
+    Q_UNUSED(currentPath);
+    return QString("%1_v%2.%3").arg(fileName).arg(counter.toInt()).arg(format.toLower());
+}
+
+QString DefaultVersionNamingScheme::intermediateFileName(const QString& currentPath, const QString& fileName,
+                                                         const QString& format, const QVariant& version,
+                                                         const QVariant& counter)
+{
+    Q_UNUSED(currentPath);
+    return QString("%1_v%2-%3.%4").arg(fileName).arg(version.toInt()).arg(counter.toInt()).arg(format.toLower());
+}
+
+QString DefaultVersionNamingScheme::directory(const QString& currentPath, const QString& fileName)
+{
+    Q_UNUSED(fileName);
+    return currentPath;
+}
+
+// --------------
+
+class VersionNameCreator
+{
+public:
+
+    VersionNameCreator(const VersionFileInfo& loadedFile,
+                       const DImageHistory& m_resolvedInitialHistory, const DImageHistory& m_currentHistory,
+                       VersionManager *q);
+
+    void checkNeedNewVersion();
+    void fork();
+    void setSaveDirectory();
+    void setSaveFormat();
+    void setSaveFileName();
+    void nextIntermediateName();
+
+public:
+
+    VersionFileInfo m_result;
+    VersionFileInfo m_loadedFile;
+    VersionFileInfo m_intermediate;
+
+    const DImageHistory m_resolvedInitialHistory;
+    const DImageHistory m_currentHistory;
+
+    bool m_fromRaw;
+    bool m_newVersion;
+
+    QVariant        m_version;
+    QVariant        m_intermediateCounter;
+
+    VersionManager* const q;
+};
+
+VersionNameCreator::VersionNameCreator(const VersionFileInfo& loadedFile,
+                                       const DImageHistory& m_resolvedInitialHistory, const DImageHistory& m_currentHistory,
+                                       VersionManager *q)
+        : m_loadedFile(loadedFile),
+          m_resolvedInitialHistory(m_resolvedInitialHistory), m_currentHistory(m_currentHistory),
+          m_fromRaw(false), m_newVersion(false), q(q)
+{
+    m_loadedFile.format = m_loadedFile.format.toUpper();
+
+    m_fromRaw = (m_loadedFile.format == "RAW");
+
+    m_version = q->namingScheme()->initialCounter();
+    m_intermediateCounter = q->namingScheme()->initialCounter();
+}
+
+void VersionNameCreator::checkNeedNewVersion()
+{
+    // First we check if we have any other files available.
+    // The resolved initial history contains only referred files found in the collection
+    // Note: The loaded file will have type Current
+    kDebug() << m_resolvedInitialHistory.hasReferredImageOfType(HistoryImageId::Original)
+    << m_resolvedInitialHistory.hasReferredImageOfType(HistoryImageId::Intermediate)
+    << m_fromRaw << q->workspaceFileFormats().contains(m_loadedFile.format);
+    if (!m_resolvedInitialHistory.hasReferredImageOfType(HistoryImageId::Original)
+        && !m_resolvedInitialHistory.hasReferredImageOfType(HistoryImageId::Intermediate))
     {
-        //DMetadata metadata(originalPath + "/" + originalName);
-        QString newFileName;
+        m_newVersion = true;
+    }
+    // We check the loaded format: If it is not one of the workspace formats, or even raw, we need a new version
+    else if (m_fromRaw || !q->workspaceFileFormats().contains(m_loadedFile.format))
+    {
+        m_newVersion = true;
+    }
+    else
+    {
+        m_newVersion = false;
+    }
+}
 
-        if(!editingOriginal && !fork)
+void VersionNameCreator::fork()
+{
+    m_newVersion = true;
+}
+
+void VersionNameCreator::setSaveDirectory()
+{
+    m_result.path = q->namingScheme()->directory(m_loadedFile.path, m_loadedFile.fileName);
+}
+
+void VersionNameCreator::setSaveFormat()
+{
+    if (m_fromRaw)
+        m_result.format = q->settings().formatForStoringRAW;
+    else
+        m_result.format = q->settings().formatForStoringSubversions;
+}
+
+void VersionNameCreator::setSaveFileName()
+{
+    kDebug() << "need new version" << m_newVersion;
+    if (!m_newVersion)
+    {
+        m_result.fileName = m_loadedFile.fileName;
+    }
+    else
+    {
+        VersionNamingScheme *scheme = q->namingScheme();
+
+        QString baseName = scheme->baseName(m_loadedFile.path, m_loadedFile.fileName);
+        QDir dirInfo(m_loadedFile.path);
+
+        // To find the right number for the new version, go through all the items in the given dir,
+        // the version number won't be bigger than count()
+        QVariant counter = scheme->initialCounter();
+        for (uint i = 0; i <= dirInfo.count(); i++)
         {
-            kDebug() << "Subversion image, will use this version";
-            kDebug() << originalName;
-            return originalName;
-        }
-        else
-        {
-            //the user is editing the original image itself, determine last saved version number and create a new one
-            kDebug() << "Original image, will create a new version";
-            QFileInfo fileInfo(originalPath + QString("/") + originalName);
-            QDir dirInfo(originalPath);
+            QString suggestedName = scheme->versionFileName(m_loadedFile.path, baseName, m_result.format, counter);
 
-            formatForSubversions = formatForSubversions.toLower();
-
-            // To find the right number for the new version, go through all the items in the given dir, 
-            // the version number won't be bigger than count()
-            for(uint i = 1; i < dirInfo.count(); i++)
+            if (!dirInfo.exists(suggestedName))
             {
-                newFileName.clear();
-                newFileName.append(fileInfo.completeBaseName());
-                if(fork && !editingOriginal)
-                    newFileName.append("v");
-                else newFileName.append("_v");
-                newFileName.append(QString::number(i));
-                newFileName.append(".");
-                if(editingRAW)
-                {
-                    newFileName.append(formatForRAW);
-                }
-                else
-                {
-                    newFileName.append(formatForSubversions);
-                }
-                kDebug() << newFileName;
-                QFile newFile(originalPath + QString("/") + newFileName);
-                if(!newFile.exists()) 
-                {
-                    kDebug() << newFileName;
-                    return newFileName;
-                }
+                m_result.fileName = suggestedName;
+                m_version = counter;
+                break;
             }
+
+            counter = scheme->incrementedCounter(counter);
+        }
+    }
+}
+
+void VersionNameCreator::nextIntermediateName()
+{
+    VersionNamingScheme *scheme = q->namingScheme();
+
+    m_intermediate.path   = m_result.path;
+    m_intermediate.format = m_result.format;
+
+    QString baseName = scheme->baseName(m_loadedFile.path, m_loadedFile.fileName);
+    QDir dirInfo(m_loadedFile.path);
+
+    for (uint i = 0; i <= dirInfo.count(); i++)
+    {
+        QString suggestedName = scheme->intermediateFileName(m_loadedFile.path, baseName,
+                                                             m_result.format, m_version, m_intermediateCounter);
+
+        if (!dirInfo.exists(suggestedName))
+        {
+            m_intermediate.fileName = suggestedName;
+            break;
+        }
+
+        m_intermediateCounter = scheme->incrementedCounter(m_intermediateCounter);
+    }
+}
+
+// --------------
+
+QString VersionFileInfo::filePath() const
+{
+    return path + "/" + fileName;
+}
+
+KUrl VersionFileInfo::fileUrl() const
+{
+    KUrl url = KUrl::fromPath(path);
+    url.addPath(fileName);
+    return url;
+}
+
+// --------------
+
+class VersionManager::VersionManagerPriv
+{
+public:
+
+    VersionManagerPriv()
+    {
+        scheme = 0;
+    }
+
+    VersionManagerSettings  settings;
+    VersionNamingScheme    *scheme;
+
+    DefaultVersionNamingScheme defaultScheme;
+};
+
+/*
+    bool hasFreeSpace(const QString& path, int assumedFileSize) const;
+bool VersionManager::VersionManagerPriv::hasFreeSpace(const QString& path, int assumedFileSize) const
+{
+    KDiskFreeSpaceInfo diskInfo = KDiskFreeSpaceInfo::freeSpaceInfo(originalPath);
+    return diskInfo.isValid() && diskInfo.available() > (uint)assumedFileSize * 2;
+}
+*/
+
+// --------------
+
+VersionManager::VersionManager()
+              : d(new VersionManagerPriv)
+{
+}
+
+VersionManager::~VersionManager()
+{
+    delete d->scheme;
+    delete d;
+}
+
+void VersionManager::setSettings(const VersionManagerSettings& settings)
+{
+    d->settings = settings;
+}
+
+VersionManagerSettings VersionManager::settings() const
+{
+    return d->settings;
+}
+
+void VersionManager::setNamingScheme(VersionNamingScheme *scheme)
+{
+    d->scheme = scheme;
+}
+
+VersionNamingScheme *VersionManager::namingScheme() const
+{
+    if (d->scheme)
+        return d->scheme;
+    else
+        return &d->defaultScheme;
+}
+
+VersionFileOperation VersionManager::operation(FileNameType request, const VersionFileInfo& loadedFile,
+                                               const DImageHistory& initialResolvedHistory,
+                                               const DImageHistory& currentHistory)
+{
+    VersionNameCreator name(loadedFile, initialResolvedHistory, currentHistory, this);
+    if (request == CurrentVersionName)
+        name.checkNeedNewVersion();
+    else if (request == NewVersionName)
+        name.fork();
+    name.setSaveDirectory();
+    name.setSaveFormat();
+    name.setSaveFileName();
+
+    VersionFileOperation operation;
+    operation.loadedFile = loadedFile;
+    operation.currentHistory = currentHistory;
+    operation.saveFile = name.m_result;
+
+    if (name.m_newVersion)
+    {
+        operation.tasks |= VersionFileOperation::NewFile;
+    }
+    else
+    {
+        operation.tasks |= VersionFileOperation::Replace;
+        if (d->settings.saveIntermediateVersions & VersionManagerSettings::AfterEachSession)
+        {
+            operation.tasks |= VersionFileOperation::MoveToIntermediate;
+            name.nextIntermediateName();
+            operation.intermediateForLoadedFile = name.m_intermediate;
         }
     }
 
-    // FIXME: if space is not enough, do not return a null string there.
-    return QString();
+    /* TODO: Implement WhenNecessary / StoreIntermediates */
+
+    return operation;
 }
 
-VersionManager* VersionManager::m_instance = 0;
-
-VersionManager* VersionManager::instance()
+QString VersionManager::toplevelDirectory(const QString& path)
 {
-    if (!m_instance)
-        m_instance = new VersionManager;
+    Q_UNUSED(path);
+    return "/";
+}
 
-    return m_instance;
+QStringList VersionManager::workspaceFileFormats() const
+{
+    QStringList formats;
+    formats << "JPEG" << "PNG" << "TIFF" << "PGF";
+    QString f = d->settings.formatForStoringRAW.toUpper();
+    if (!formats.contains(f))
+        formats << f;
+    f = d->settings.formatForStoringSubversions.toUpper();
+    if (!formats.contains(f))
+        formats << f;
+    return formats;
 }
 
 } // namespace Digikam
--- libs/versionmanager/versionmanager.h
+++ libs/versionmanager/versionmanager.h
@@ -27,32 +27,153 @@
 
 // Qt includes
 
+#include <QFlags>
+#include <QMap>
 #include <QString>
 
 // Local includes
 
 #include "digikam_export.h"
+#include "dimagehistory.h"
+
+class KConfigGroup;
+class KUrl;
 
 namespace Digikam
 {
+
+class DIGIKAM_EXPORT VersionManagerSettings
+{
+public:
+
+    VersionManagerSettings();
+
+    void readFromConfig(KConfigGroup& group);
+    void writeToConfig(KConfigGroup& group) const;
+
+    enum IntermediateSavepoints
+    {
+        NoIntermediates  = 0,
+        AfterEachSession = 1 << 0,
+        WhenNecessary    = 1 << 1
+    };
+    Q_DECLARE_FLAGS(IntermediateBehavior, IntermediateSavepoints)
+
+    bool                 showAllVersions;
+    IntermediateBehavior saveIntermediateVersions;
+    QString              formatForStoringRAW;
+    QString              formatForStoringSubversions;
+};
+
+class DIGIKAM_EXPORT VersionNamingScheme
+{
+public:
+
+    virtual ~VersionNamingScheme() {}
+    virtual QString baseName(const QString& path, const QString& filename) = 0;
+    virtual QString versionFileName(const QString& path, const QString& filename,
+                                    const QString& format, const QVariant& counter) = 0;
+    virtual QString intermediateFileName(const QString& path, const QString& filename,
+                                         const QString& format, const QVariant& version, const QVariant& counter) = 0;
+    virtual QString directory(const QString& path, const QString& filename) = 0;
+    virtual QVariant initialCounter() = 0;
+    virtual QVariant incrementedCounter(const QVariant& counter) = 0;
+};
+
+class DIGIKAM_EXPORT VersionFileInfo
+{
+public:
+
+    VersionFileInfo() {}
+    VersionFileInfo(const QString& path, const QString& fileName, const QString& format)
+        : path(path), fileName(fileName), format(format) {}
+
+    QString filePath() const;
+    KUrl fileUrl() const;
+
+    QString path;
+    QString fileName;
+    QString format;
+};
+
+class DIGIKAM_EXPORT VersionFileOperation
+{
+public:
+
+    /** This class describes an operation necessary for storing an
+     *  image under version control.
+     *  The loadedFile and current history is given to the VersionManager.
+     *  The saveFile is the destination of the save operation.
+     *  If the loadedFile shall be moved to an intermediate,
+     *  the name is given in intermediateForLoadedFile.
+     *  The intermediates map may contain name of intermediates
+     *  to save the state after action i of the history
+     *  (initialResolvedHistory.actionCount() <= i < currentHistory.actionCount() - 1).
+     */
+
+    VersionFileOperation() {}
+
+    enum Task
+    {
+        /// saveFile is a new file. Excludes Replace.
+        NewFile = 1 << 0,
+        /// loadedFile and saveFile are the same - replace. Excludes NewFile.
+        Replace = 1 << 1,
+        /// Move loadedFile to loadedFileToIntermediate
+        MoveToIntermediate = 1 << 2,
+        /// Store additional snapshots from within history
+        StoreIntermediates = 1 << 3
+    };
+    Q_DECLARE_FLAGS(Tasks, Task)
+
+    Tasks           tasks;
+
+    VersionFileInfo loadedFile;
+    DImageHistory   currentHistory;
+
+    VersionFileInfo saveFile;
+
+    VersionFileInfo intermediateForLoadedFile;
+
+    QMap<int,VersionFileInfo> intermediates;
+};
 
 class DIGIKAM_EXPORT VersionManager
 {
 public:
 
-    VersionManager() {};
-    ~VersionManager() {};
+    VersionManager();
+    virtual ~VersionManager();
 
-    static VersionManager* instance();
-    QString getVersionedFilename(const QString& originalPath, const QString& originalName, 
-                                 qint64 fileSize, const QString& formatForRAW, QString& formatForSubversions,
-                                 bool editingOriginal = true, bool fork = false, bool editingRAW = false);
+    void setSettings(const VersionManagerSettings& settings);
+    VersionManagerSettings settings() const;
+
+    void setNamingScheme(VersionNamingScheme *scheme);
+    VersionNamingScheme *namingScheme() const;
+
+    enum FileNameType
+    {
+        CurrentVersionName,
+        NewVersionName
+    };
+
+    VersionFileOperation operation(FileNameType request, const VersionFileInfo& loadedFile,
+                                   const DImageHistory& initialResolvedHistory,
+                                   const DImageHistory& currentHistory);
+
+    virtual QString toplevelDirectory(const QString& path);
+
+    virtual QStringList workspaceFileFormats() const;
 
 private:
 
-    static VersionManager* m_instance;
+    class VersionManagerPriv;
+    VersionManagerPriv* const d;
 };
 
 } // namespace Digikam
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(Digikam::VersionManagerSettings::IntermediateBehavior)
+Q_DECLARE_OPERATORS_FOR_FLAGS(Digikam::VersionFileOperation::Tasks)
 
 #endif // VERSIONMANAGER_H
