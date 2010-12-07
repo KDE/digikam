85eac2dcfea82fbe2b0e468ffba2ce7d82ab2264
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-12-07 09:00:52 +0000

    Clean album root id -> album pointer hash in removePAlbum, to ensure it does not contain an invalid pointer. CCBUG:  256897
    svn path=/trunk/extragear/graphics/digikam/; revision=1204416

--- digikam/albummanager.cpp
+++ digikam/albummanager.cpp
@@ -1305,9 +1305,7 @@ void AlbumManager::scanPAlbums()
 
     foreach (PAlbum* album, topMostOldAlbums)
     {
-        // this might look like there is memory leak here, since removePAlbum
-        // doesn't delete albums and looks like child Albums don't get deleted.
-        // But when the parent album gets deleted, the children are also deleted.
+        // recursively removes all children and the album
         removePAlbum(album);
     }
 
@@ -2770,6 +2768,11 @@ void AlbumManager::removePAlbum(PAlbum* album)
     {
         d->currentAlbum = 0;
         emit signalAlbumCurrentChanged(0);
+    }
+
+    if (album->isAlbumRoot())
+    {
+        d->albumRootAlbumHash.remove(album->albumRootId());
     }
 
     emit signalAlbumDeleted(album);
