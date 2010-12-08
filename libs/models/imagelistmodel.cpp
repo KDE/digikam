9ee9a1c1edd8cffa179ae1905b999847208e6b6c
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-12-08 12:00:28 +0000

    Add a simple image model just based on a static list
    svn path=/branches/extragear/graphics/digikam/; revision=1204607

---
+++ libs/models/imagelistmodel.cpp
@@ -0,0 +1,75 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-12-06
+ * Description : An image model based on a static list
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "imagelistmodel.moc"
+
+// Qt includes
+
+// KDE includes
+
+#include <kdebug.h>
+
+// Local includes
+
+#include "databaseaccess.h"
+#include "databasechangesets.h"
+#include "databasewatch.h"
+#include "imageinfo.h"
+#include "imageinfolist.h"
+
+namespace Digikam
+{
+
+ImageListModel::ImageListModel(QObject* parent)
+    : ImageThumbnailModel(parent)
+{
+    connect(DatabaseAccess::databaseWatch(), SIGNAL(collectionImageChange(const CollectionImageChangeset&)),
+            this, SLOT(slotCollectionImageChange(const CollectionImageChangeset&)));
+}
+
+ImageListModel::~ImageListModel()
+{
+}
+
+void ImageListModel::slotCollectionImageChange(const CollectionImageChangeset& changeset)
+{
+    if (isEmpty())
+    {
+        return;
+    }
+
+    switch (changeset.operation())
+    {
+        case CollectionImageChangeset::Added:
+            break;
+        case CollectionImageChangeset::Removed:
+        case CollectionImageChangeset::RemovedAll:
+            removeImageInfos(ImageInfoList(changeset.ids()));
+            break;
+
+        default:
+            break;
+    }
+}
+
+} // namespace Digikam
---
+++ libs/models/imagelistmodel.h
@@ -0,0 +1,63 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-12-06
+ * Description : An image model based on a static list
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef IMAGELISTMODEL_H
+#define IMAGELISTMODEL_H
+
+// Local includes
+
+#include "imagethumbnailmodel.h"
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class ImageChangeset;
+class CollectionImageChangeset;
+
+class DIGIKAM_DATABASE_EXPORT ImageListModel : public ImageThumbnailModel
+{
+    Q_OBJECT
+
+public:
+
+    ImageListModel(QObject* parent = 0);
+    ~ImageListModel();
+
+    // necessary methods to add and remove ImageInfos to the model are inherited from ImageModel
+
+Q_SIGNALS:
+
+    /**
+     * Emitted when images are removed from the model because they are removed in the database
+     */
+    void imageInfosRemoved(const QList<ImageInfo>& infos);
+
+protected Q_SLOTS:
+
+    void slotCollectionImageChange(const CollectionImageChangeset& changeset);
+};
+
+} // namespace Digikam
+
+#endif // IMAGELISTMODEL_H
