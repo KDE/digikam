6a3a844b8b9bc7f3c5ffc8c5bc13c4e0310a945c
Andi Clemens <andi.clemens@gmx.net>
2010-12-12 13:21:47 +0000

    get rid of warnings: this slot isn't used anywhere in the digiKam code base, I'll comment it for now, maybe we can also remove it later.
    svn path=/trunk/extragear/graphics/digikam/; revision=1205766

--- digikam/contextmenuhelper.cpp
+++ digikam/contextmenuhelper.cpp
@@ -495,39 +495,39 @@ void ContextMenuHelper::addCreateTagFromAddressbookMenu()
 #endif // HAVE_KDEPIMLIBS
 }
 
-void ContextMenuHelper::slotABCContextMenu()
-{
-#ifdef HAVE_KDEPIMLIBS
-    d->ABCmenu->clear();
-
-    KABC::AddressBook* ab = KABC::StdAddressBook::self();
-    QStringList names;
-
-    for ( KABC::AddressBook::Iterator it = ab->begin(); it != ab->end(); ++it )
-    {
-        names.push_back(it->formattedName());
-    }
-
-    qSort(names);
-
-    for ( QStringList::ConstIterator it = names.constBegin(); it != names.constEnd(); ++it )
-    {
-        QString name = *it;
-
-        if (!name.isNull() )
-        {
-            d->ABCmenu->addAction(name);
-        }
-    }
-
-    if (d->ABCmenu->isEmpty())
-    {
-        QAction* nothingFound = d->ABCmenu->addAction(i18n("No address book entries found"));
-        nothingFound->setEnabled(false);
-    }
-
-#endif // HAVE_KDEPIMLIBS
-}
+//void ContextMenuHelper::slotABCContextMenu()
+//{
+//#ifdef HAVE_KDEPIMLIBS
+//    d->ABCmenu->clear();
+//
+//    KABC::AddressBook* ab = KABC::StdAddressBook::self();
+//    QStringList names;
+//
+//    for ( KABC::AddressBook::Iterator it = ab->begin(); it != ab->end(); ++it )
+//    {
+//        names.push_back(it->formattedName());
+//    }
+//
+//    qSort(names);
+//
+//    for ( QStringList::ConstIterator it = names.constBegin(); it != names.constEnd(); ++it )
+//    {
+//        QString name = *it;
+//
+//        if (!name.isNull() )
+//        {
+//            d->ABCmenu->addAction(name);
+//        }
+//    }
+//
+//    if (d->ABCmenu->isEmpty())
+//    {
+//        QAction* nothingFound = d->ABCmenu->addAction(i18n("No address book entries found"));
+//        nothingFound->setEnabled(false);
+//    }
+//
+//#endif // HAVE_KDEPIMLIBS
+//}
 
 void ContextMenuHelper::slotABCMenuTriggered(QAction* action)
 {
