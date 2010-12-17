98a99341709e17b276b1397118927c30d97f8149
Gilles Caulier <caulier.gilles@gmail.com>
2010-12-17 09:42:55 +0000

    more private internal container. constify. fix horizontal gradiant colors. BUGS: 260299
    svn path=/trunk/extragear/graphics/digikam/; revision=1207247

--- libs/dimg/filters/curves/curvesbox.h
+++ libs/dimg/filters/curves/curvesbox.h
@@ -6,7 +6,8 @@
  * Date        : 2009-06-14
  * Description : a curves widget with additional control elements
  *
- * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,7 +41,6 @@ namespace Digikam
 {
 
 class ImageCurves;
-class CurvesBoxPriv;
 class DColor;
 
 class DIGIKAM_EXPORT CurvesBox : public QWidget
@@ -92,8 +92,8 @@ public:
     void resetChannels();
     void reset();
 
-    void readCurveSettings(KConfigGroup& group, QString prefix);
-    void writeCurveSettings(KConfigGroup& group, QString prefix);
+    void readCurveSettings(KConfigGroup& group, const QString& prefix);
+    void writeCurveSettings(KConfigGroup& group, const QString& prefix);
 
     int curvesLeftOffset() const;
 
@@ -123,6 +123,7 @@ private:
 
 private:
 
+    class CurvesBoxPriv;
     CurvesBoxPriv* const d;
 };
 
