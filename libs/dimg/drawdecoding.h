afbd03140fb30a1fb8ef6d22895235d391ea8720
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2010-12-21 18:37:37 +0000

    - create a FilterAction from a DRawDecoding.   With a bit of macro magic, it becomes manageable. - use settings containers for post processing
    svn path=/branches/extragear/graphics/digikam/core/; revision=1208447

--- libs/dimg/drawdecoding.h
+++ libs/dimg/drawdecoding.h
@@ -40,6 +40,9 @@
 // Local includes
 
 #include "digikam_export.h"
+#include "bcgcontainer.h"
+#include "wbcontainer.h"
+#include "curvescontainer.h"
 
 using namespace KDcrawIface;
 
@@ -79,9 +82,8 @@ public:
      */
     bool operator==(const DRawDecoding& other) const;
 
-    /** Equivalent to the copy constructor
-     */
-    DRawDecoding& operator=(const DRawDecoding& prm);
+    void writeToFilterAction(FilterAction& action, const QString& prefix = QString()) const;
+    static DRawDecoding fromFilterAction(const FilterAction& action, const QString& prefix = QString());
 
 public:
 
@@ -91,29 +93,17 @@ public:
 
     /// Post Processing settings ----------------------------------------------------
 
-    /** Lightness correction value.
+    /** BCG correction values.
     */
-    double lightness;
+    BCGContainer bcg;
 
-    /** Contrast correction value.
+    /** White Balance correction values.
     */
-    double contrast;
+    WBContainer wb;
 
-    /** Gamma correction value.
+    /** Curve adjustments.
     */
-    double gamma;
-
-    /** Color saturation correction value.
-    */
-    double saturation;
-
-    /** Exposure compensation value.
-    */
-    double exposureComp;
-
-    /** Luminosity curve adjustments.
-    */
-    QPolygon curveAdjust;
+    CurvesContainer curvesAdjust;
 };
 
 }  // namespace Digikam
