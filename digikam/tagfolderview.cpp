e3f5e336497fd77d50d4988ed459794bfeecbe58
Gilles Caulier <caulier.gilles@gmail.com>
2011-01-19 08:44:01 +0000

    polish
    svn path=/trunk/extragear/graphics/digikam/; revision=1215676

--- digikam/tagfolderview.cpp
+++ digikam/tagfolderview.cpp
@@ -7,9 +7,9 @@
  * Description : tags folder view.
  *
  * Copyright (C) 2005-2006 by Joern Ahrens <joern.ahrens@kdemail.net>
- * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
- * Copyright (C) 2009 by Johannes Wienke <languitar at semipol dot de>
+ * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2011 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2009-2011 by Johannes Wienke <languitar at semipol dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -27,16 +27,19 @@
 #include "tagfolderview.moc"
 
 // Qt includes
-#include <qaction.h>
-#include <qevent.h>
+
+#include <QAction>
+#include <QEvent>
 
 // KDE includes
+
 #include <kdebug.h>
 #include <kmenu.h>
 #include <klocale.h>
 #include <kiconloader.h>
 
 // Local includes
+
 #include "albummanager.h"
 #include "contextmenuhelper.h"
 #include "tagmodificationhelper.h"
@@ -44,9 +47,10 @@
 namespace Digikam
 {
 
-class TagFolderViewPriv
+class TagFolderView::TagFolderViewPriv
 {
 public:
+
     TagFolderViewPriv() :
         showFindDuplicateAction(true),
         model(0),
@@ -55,28 +59,24 @@ public:
     {
     }
 
-    bool showFindDuplicateAction;
+    bool      showFindDuplicateAction;
 
     TagModel* model;
 
-    QAction* resetIconAction;
-    QAction* findDuplAction;
-
+    QAction*  resetIconAction;
+    QAction*  findDuplAction;
 };
 
-TagFolderView::TagFolderView(QWidget* parent, TagModel* model) :
-    TagTreeView(model, parent), d(new TagFolderViewPriv)
+TagFolderView::TagFolderView(QWidget* parent, TagModel* model)
+    : TagTreeView(model, parent), d(new TagFolderViewPriv)
 {
-
-    d->model = model;
-
+    d->model           = model;
     d->resetIconAction = new QAction(SmallIcon("view-refresh"), i18n("Reset Tag Icon"), this);
-    d->findDuplAction = new QAction(SmallIcon("tools-wizard"), i18n("Find Duplicates..."), this);
+    d->findDuplAction  = new QAction(SmallIcon("tools-wizard"), i18n("Find Duplicates..."), this);
 
     setSortingEnabled(true);
     setSelectAlbumOnClick(true);
     setEnableContextMenu(true);
-
 }
 
 TagFolderView::~TagFolderView()
@@ -96,7 +96,6 @@ QString TagFolderView::contextMenuTitle() const
 
 void TagFolderView::addCustomContextMenuActions(ContextMenuHelper& cmh, Album* album)
 {
-
     TAlbum* tag = dynamic_cast<TAlbum*> (album);
 
     if (!tag)
@@ -127,12 +126,10 @@ void TagFolderView::addCustomContextMenuActions(ContextMenuHelper& cmh, Album* a
             this, SLOT(slotTagNewFromABCMenu(const QString&)));
 
     d->resetIconAction->setEnabled(!tag->isRoot());
-
 }
 
 void TagFolderView::slotTagNewFromABCMenu(const QString& personName)
 {
-
     TAlbum* parent = currentAlbum();
 
     if (!parent)
@@ -141,13 +138,11 @@ void TagFolderView::slotTagNewFromABCMenu(const QString& personName)
     }
 
     tagModificationHelper()->slotTagNew(parent, personName, "tag-people");
-
 }
 
 void TagFolderView::handleCustomContextMenuAction(QAction* action, AlbumPointer<Album> album)
 {
-
-    Album* a = album;
+    Album* a    = album;
     TAlbum* tag = dynamic_cast<TAlbum*> (a);
 
     if (!tag)
@@ -169,8 +164,6 @@ void TagFolderView::handleCustomContextMenuAction(QAction* action, AlbumPointer<
     {
         emit signalFindDuplicatesInAlbum(tag);
     }
-
 }
 
-}
-
+} // namespace Digikam
