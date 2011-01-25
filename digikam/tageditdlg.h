772c4905ad169b8704d192618c1f290095071af0
Gilles Caulier <caulier.gilles@gmail.com>
2011-01-25 11:56:28 +0000

    more private internal container polish
    svn path=/trunk/extragear/graphics/digikam/; revision=1217003

--- digikam/tageditdlg.h
+++ digikam/tageditdlg.h
@@ -7,7 +7,7 @@
  * Description : dialog to edit and create digiKam Tags
  *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,7 +40,6 @@
 
 namespace Digikam
 {
-class TagEditDlgPriv;
 
 class TagEditDlg : public KDialog
 {
@@ -77,6 +76,7 @@ private Q_SLOTS:
 
 private:
 
+    class TagEditDlgPriv;
     TagEditDlgPriv* const d;
 };
 
