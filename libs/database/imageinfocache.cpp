6e9ac885b66dc21ded49f4b94b1242a6fc993eaf
Gilles Caulier <caulier.gilles@gmail.com>
2011-02-02 09:58:13 +0000

    missing to drive Color Label in image info cache
    svn path=/branches/extragear/graphics/digikam/core/; revision=1218441

--- libs/database/imageinfocache.cpp
+++ libs/database/imageinfocache.cpp
@@ -6,7 +6,8 @@
  * Date        : 2007-05-01
  * Description : ImageInfo common data
  *
- * Copyright (C) 2007-2008 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2007-2011 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C)      2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -155,6 +156,11 @@ void ImageInfoCache::slotImageChanged(const ImageChangeset& changeset)
                 (*it)->formatCached = false;
             }
 
+            if (changes & DatabaseFields::ColorLabel)
+            {
+                (*it)->colorLabelCached = false;
+            }
+
             if (changes & DatabaseFields::Rating)
             {
                 (*it)->ratingCached = false;
@@ -180,9 +186,9 @@ void ImageInfoCache::slotImageChanged(const ImageChangeset& changeset)
                 (*it)->imageSizeCached = false;
             }
 
-            if (changes & DatabaseFields::LatitudeNumber
-                || changes & DatabaseFields::LongitudeNumber
-                || changes & DatabaseFields::Altitude)
+            if (changes & DatabaseFields::LatitudeNumber     ||
+                changes & DatabaseFields::LongitudeNumber ||
+                changes & DatabaseFields::Altitude)
             {
                 (*it)->positionsCached = false;
             }
