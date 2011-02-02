9d38e1d46d961c837363491dccbb19503010c8d9
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2011-02-02 21:33:02 +0000

    Take care that we only add and remove keywords, not replace all existing keywords. (needs some more testing with complex scenarios)
    CCBUG: 264745
    
    svn path=/branches/extragear/graphics/digikam/core/; revision=1218563

--- digikam/metadatahub.cpp
+++ digikam/metadatahub.cpp
@@ -628,7 +628,8 @@ bool MetadataHub::write(DMetadata& metadata, WriteMode writeMode, const Metadata
         // To fix this constraint (not needed currently), an oldKeywords parameter is needed
 
         // create list of keywords to be added and to be removed
-        QStringList oldTagsPathList, newTagsPathList, oldKeywords, newKeywords;
+        QStringList tagsPathList, oldKeywords, newKeywords;
+        metadata.getImageTagsPath(tagsPathList);
 
         for (QMap<int, TagStatus>::iterator it = d->tags.begin(); it != d->tags.end(); ++it)
         {
@@ -643,15 +644,20 @@ bool MetadataHub::write(DMetadata& metadata, WriteMode writeMode, const Metadata
                 // This works for single and multiple selection.
                 // In both situations, tags which had originally been loaded
                 // have explicitly been removed with setTag.
+                QString tagName = TagsCache::instance()->tagName(it.key());
+                QString tagPath = TagsCache::instance()->tagPath(it.key(), TagsCache::NoLeadingSlash);
                 if (it.value().hasTag)
                 {
-                    newTagsPathList.append(TagsCache::instance()->tagPath(it.key(), TagsCache::NoLeadingSlash));
-                    newKeywords.append(TagsCache::instance()->tagName(it.key()));
+                    if (!tagsPathList.contains(tagPath))
+                    {
+                        tagsPathList << tagPath;
+                    }
+                    newKeywords << tagName;
                 }
                 else
                 {
-                    oldTagsPathList.append(TagsCache::instance()->tagPath(it.key(), TagsCache::NoLeadingSlash));
-                    oldKeywords.append(TagsCache::instance()->tagName(it.key()));
+                    tagsPathList.removeAll(tagPath);
+                    oldKeywords << tagName;
                 }
             }
         }
@@ -660,14 +666,14 @@ bool MetadataHub::write(DMetadata& metadata, WriteMode writeMode, const Metadata
         // synchronize metadata, else contents is not coherent.
 
         // We set Iptc keywords using tags name.
-        dirty |= metadata.setIptcKeywords(metadata.getIptcKeywords(), newKeywords);
+        dirty |= metadata.setIptcKeywords(oldKeywords, newKeywords);
 
         // We add Xmp keywords using tags name.
-        dirty |= metadata.removeXmpKeywords(metadata.getXmpKeywords());
+        dirty |= metadata.removeXmpKeywords(oldKeywords);
         dirty |= metadata.setXmpKeywords(newKeywords);
 
         // We set Tags Path list in digiKam Xmp private namespace using tags path.
-        dirty |= metadata.setImageTagsPath(newTagsPathList);
+        dirty |= metadata.setImageTagsPath(tagsPathList);
     }
 
     return dirty;
