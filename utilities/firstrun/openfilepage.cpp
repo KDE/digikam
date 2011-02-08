1c83ab8339832630f3a1d3a39f9d085ddba2a1f8
Gilles Caulier <caulier.gilles@gmail.com>
2011-02-08 14:28:40 +0100

    In first run dialog, use MetadataSettings to write information to digiKam rc config file Turn on Color Label info to write in metadata if user is agree to update image information to files. Use more private internal containers. CCBUGS: 152424

--- utilities/firstrun/openfilepage.cpp
+++ utilities/firstrun/openfilepage.cpp
@@ -6,7 +6,7 @@
  * Date        : 2009-28-04
  * Description : first run assistant dialog
  *
- * Copyright (C) 2009-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -46,7 +46,7 @@
 namespace Digikam
 {
 
-class OpenFilePagePriv
+class OpenFilePage::OpenFilePagePriv
 {
 public:
 
