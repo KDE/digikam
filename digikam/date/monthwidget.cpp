d596573df7db884881fce6ef0642fb6f6f19b19e
Gilles Caulier <caulier.gilles@gmail.com>
2011-02-24 13:41:38 +0100

    more internal private container

--- digikam/date/monthwidget.cpp
+++ digikam/date/monthwidget.cpp
@@ -7,7 +7,7 @@
  * Description : a widget to perform month selection.
  *
  * Copyright (C) 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -51,7 +51,7 @@
 namespace Digikam
 {
 
-class MonthWidgetPriv
+class MonthWidget::MonthWidgetPriv
 {
 public:
 
--- digikam/date/monthwidget.h
+++ digikam/date/monthwidget.h
@@ -7,7 +7,7 @@
  * Description : a widget to perform month selection.
  *
  * Copyright (C) 2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,7 +40,6 @@ class QMouseEvent;
 namespace Digikam
 {
 class ImageFilterModel;
-class MonthWidgetPriv;
 
 class MonthWidget : public QWidget
 {
@@ -79,6 +78,9 @@ private:
     void resetDayCounts();
     void connectModel();
 
+private:
+
+    class MonthWidgetPriv;
     MonthWidgetPriv* const d;
 };
 
