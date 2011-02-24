6c2b343212349cdf5b62b63d7ddf600689b88143
Gilles Caulier <caulier.gilles@gmail.com>
2011-02-24 16:42:28 +0100

    polish

--- utilities/slideshow/slideshowsettings.h
+++ utilities/slideshow/slideshowsettings.h
@@ -6,7 +6,7 @@
  * Date        : 2007-02-13
  * Description : slide show settings container.
  *
- * Copyright (C) 2007-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -125,7 +125,7 @@ public:
     int                          delay;
 
     /** List of pictures URL to slide */
-    KUrl::List fileList;
+    KUrl::List                   fileList;
 
     QColor                       ratingColor;
 
