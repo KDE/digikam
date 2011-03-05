713bb62bafe7a996e7571ac3aea3bc6932cc0006
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2011-03-05 13:36:17 +0100

    A graphics item which manages mouse events to draw a rectangle. It is positioned on top its parent and is transparent. Both Press-Drag-Release and Click-Move-Click are supported.

---
+++ libs/widgets/graphicsview/clickdragreleaseitem.cpp
@@ -0,0 +1,199 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2011-03-04
+ * Description : A simple item to click, drag and release
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "clickdragreleaseitem.moc"
+
+// Qt includes
+
+#include <QApplication>
+#include <QGraphicsSceneMouseEvent>
+#include <QKeyEvent>
+#include <QObject>
+#include <QPointF>
+
+// KDE includes
+
+#include <KDebug>
+
+// Local includes
+
+#include "itemvisibilitycontroller.h"
+
+namespace Digikam
+{
+
+enum ClickDragState
+{
+    HoverState,
+    PressedState,
+    PressDragState,
+    ClickedMoveState
+};
+
+class ClickDragReleaseItem::ClickDragReleaseItemPriv
+{
+public:
+
+    ClickDragReleaseItemPriv()
+        : state(HoverState)
+    {
+    }
+
+    ClickDragState state;
+    QPointF        pressPos;
+
+    template <class Event> bool isDrag(Event* e)
+    {
+        return (pressPos - e->scenePos()).manhattanLength() > QApplication::startDragDistance();
+    }
+
+    template <class Event> QRectF rect(Event* e)
+    {
+        return QRectF(pressPos, e->scenePos()).normalized();
+    }
+};
+
+ClickDragReleaseItem::ClickDragReleaseItem(QGraphicsItem* parent)
+    : QGraphicsObject(parent),
+      d(new ClickDragReleaseItemPriv)
+{
+    setCursor(Qt::CrossCursor);
+}
+
+ClickDragReleaseItem::~ClickDragReleaseItem()
+{
+    delete d;
+}
+
+QRectF ClickDragReleaseItem::boundingRect() const
+{
+    if (parentItem())
+    {
+        return QRectF(QPointF(0,0), parentItem()->boundingRect().size());
+    }
+    return QRectF();
+}
+
+QGraphicsItem::GraphicsItemFlags ClickDragReleaseItem::flags() const
+{
+    return ItemIsFocusable | ItemHasNoContents;
+}
+
+void ClickDragReleaseItem::paint(QPainter*, const QStyleOptionGraphicsItem*, QWidget*)
+{
+}
+
+/**
+ * 1) Press - Drag - Release:
+ *    mousePress, PressedState -> mouseMoveEvent over threshold, PressDragState -> mouseReleaseEvent, finished
+ * 2) Click - Move - Click:
+ *    mousePressEvent, PressedState -> mouseReleaseEvent, ClickedMoveState ->
+ *    hoverMoveEvent -> mouseReleaseEvent, finished
+ */
+
+void ClickDragReleaseItem::mousePressEvent(QGraphicsSceneMouseEvent* e)
+{
+    if (e->button() != Qt::LeftButton)
+    {
+        emit cancelled();
+        return;
+    }
+
+    if (d->state == HoverState)
+    {
+        d->pressPos = e->scenePos();
+        d->state = PressedState;
+        emit started(e->scenePos());
+    }
+}
+
+void ClickDragReleaseItem::mouseMoveEvent(QGraphicsSceneMouseEvent* e)
+{
+    if (d->state == PressedState && d->isDrag(e))
+    {
+        d->state = PressDragState;
+        setCursor(Qt::SizeFDiagCursor);
+    }
+    if (d->state == PressDragState)
+    {
+        emit moving(d->rect(e));
+    }
+}
+
+void ClickDragReleaseItem::hoverMoveEvent(QGraphicsSceneHoverEvent* e)
+{
+    if (d->state == ClickedMoveState)
+    {
+        emit moving(d->rect(e));
+    }
+}
+
+void ClickDragReleaseItem::mouseReleaseEvent(QGraphicsSceneMouseEvent* e)
+{
+    if (d->state == PressedState)
+    {
+        // click-move-click mode first click.
+        // It cannot be over the drag threshold in this state, would be caught in moveEvent.
+        d->state = ClickedMoveState;
+        setCursor(Qt::SizeFDiagCursor);
+        setAcceptHoverEvents(true);
+    }
+    else if (d->state == ClickedMoveState)
+    {
+        // click-move-click mode second click
+        d->state = HoverState;
+        setCursor(Qt::CrossCursor);
+        setAcceptHoverEvents(false);
+        emit finished(d->rect(e));
+    }
+    else if (d->state == PressDragState)
+    {
+        if (d->isDrag(e))
+        {
+            emit finished(d->rect(e));
+        }
+        else
+        {
+            emit cancelled();
+        }
+        d->state = HoverState;
+        setCursor(Qt::CrossCursor);
+    }
+}
+
+void ClickDragReleaseItem::keyPressEvent(QKeyEvent* e)
+{
+    kDebug() << e;
+    switch (e->key())
+    {
+        case Qt::Key_Backspace:
+        case Qt::Key_Escape:
+            emit cancelled();
+            break;
+        default:
+            e->ignore();
+    }
+}
+
+}
+
---
+++ libs/widgets/graphicsview/clickdragreleaseitem.h
@@ -0,0 +1,83 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2011-03-04
+ * Description : A simple item to click, drag and release
+ *
+ * Copyright (C) 2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef CLICKDRAGRELEASEITEM_H
+#define CLICKDRAGRELEASEITEM_H
+
+// Qt includes
+
+#include <QGraphicsObject>
+
+// KDE includes
+
+// Local includes
+
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+class DIGIKAM_EXPORT ClickDragReleaseItem : public QGraphicsObject
+{
+    Q_OBJECT
+
+public:
+
+    ClickDragReleaseItem(QGraphicsItem* parent);
+    ~ClickDragReleaseItem();
+
+    virtual GraphicsItemFlags flags() const;
+    virtual QRectF boundingRect() const;
+    virtual void paint(QPainter*, const QStyleOptionGraphicsItem*, QWidget*);
+
+Q_SIGNALS:
+
+    /**
+     * Signals are emitted at click, drag and release event.
+     * Reported positions are in scene coordinates.
+     * A drag is reported only if the mouse was moved a certain threshold.
+     * A release is reported after every press.
+     */
+
+    void started(const QPointF& pos);
+    void moving(const QRectF& rect);
+    void finished(const QRectF& rect);
+    void cancelled();
+
+protected:
+
+    virtual void mousePressEvent(QGraphicsSceneMouseEvent*);
+    virtual void mouseMoveEvent(QGraphicsSceneMouseEvent*);
+    virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent*);
+    virtual void hoverMoveEvent(QGraphicsSceneHoverEvent* e);
+    virtual void keyPressEvent(QKeyEvent* e);
+
+private:
+
+    class ClickDragReleaseItemPriv;
+    ClickDragReleaseItemPriv* const d;
+};
+
+}
+
+#endif // CLICKDRAGRELEASEITEM_H
