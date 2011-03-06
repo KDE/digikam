0d39aab7fd9e3c0269d25f618240a3d5205281aa
Script Kiddy <scripty@kde.org>
2011-03-06 08:18:19 +0100

    SVN_SILENT made messages (.desktop file)

--- utilities/hotplug/digikam-opencamera.desktop.cmake
+++ utilities/hotplug/digikam-opencamera.desktop.cmake
@@ -41,7 +41,6 @@ Name[pt]=Obter Fotos com o digiKam
 Name[pt_BR]=Transferir as fotos com o digiKam
 Name[ro]=Descărcați fotografii cu digiKam
 Name[ru]=Скопировать фотографии в альбом digiKam
-Name[sk]=Stiahnuť fotografie pomocou digiKam
 Name[sv]=Ladda ner foton med Digikam
 Name[th]=ดาวน์โหลดภาพถ่ายด้วย digiKam
 Name[tr]=Fotoğrafları digiKam ile İndir
