964c47b324a692845bc913075f6ea49e46f73cea
Andi Clemens <andi.clemens@gmx.net>
2011-03-09 09:27:58 +0100

    Add copyright header

--- project/scripts/fileformatter.py
+++ project/scripts/fileformatter.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
-
-
 # this scripts depends on Python 2.7 or higher
+
+# ============================================================
+# 
+# This file is a part of digiKam project
+# http://www.digikam.org
+# 
+# Date        : 2011-03-09
+# Description : a helper script for formatting the digiKam source code
+# 
+# Copyright (C) 2011 by Andi Clemens <andi dot clemens at gmx dot net>
+# 
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# ============================================================ */
+
 
 import os
 import sys
