5e3d1a6f14a83433997b542a764747b0a8047d9b
Andi Clemens <andi.clemens@gmx.net>
2011-04-10 12:38:07 +0200

    update copyright header

--- utilities/advancedrename/parser/options/dateoption.cpp
+++ utilities/advancedrename/parser/options/dateoption.cpp
@@ -6,7 +6,7 @@
  * Date        : 2009-08-08
  * Description : an option to provide date information to the parser
  *
- * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2009-2011 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
