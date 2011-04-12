2d9daba1177148f9595ebf84ee9d2ce746978726
Pino Toscano <pino@kde.org>
2011-04-12 19:54:52 +0200

    i18n: do not extract messages for sources in 'tests' subdirs

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 (cd data && $PREPARETIPS > ../tips.cpp)
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.h -o -name \*.cpp` `find digikam -name \*.h.cmake` -o $podir/digikam.pot
+$XGETTEXT `find . -name \*.h -o -name \*.cpp | grep -v '/tests/'` `find digikam -name \*.h.cmake` -o $podir/digikam.pot
 rm -f tips.cpp
 rm -f rc.cpp
