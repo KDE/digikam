6af8f3133091c2c4f342b8d4ffe176d2d8afd3fe
Gilles Caulier <caulier.gilles@gmail.com>
2011-04-19 09:50:19 +0200

    more private internal container

--- libs/widgets/common/colorgradientwidget.cpp
+++ libs/widgets/common/colorgradientwidget.cpp
@@ -6,7 +6,7 @@
  * Date        : 2004-07-28
  * Description : a color gradient widget
  *
- * Copyright (C) 2004-2008 by Gilles Caulier<caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2011 by Gilles Caulier<caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -31,7 +31,7 @@
 namespace Digikam
 {
 
-class ColorGradientWidgetPriv
+class ColorGradientWidget::ColorGradientWidgetPriv
 {
 
 public:
--- libs/widgets/common/colorgradientwidget.h
+++ libs/widgets/common/colorgradientwidget.h
@@ -6,7 +6,7 @@
  * Date        : 2004-07-28
  * Description : a color gradient widget
  *
- * Copyright (C) 2004-2008 by Gilles Caulier<caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2011 by Gilles Caulier<caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -36,8 +36,6 @@
 namespace Digikam
 {
 
-class ColorGradientWidgetPriv;
-
 class DIGIKAM_EXPORT ColorGradientWidget : public QWidget
 {
     Q_OBJECT
@@ -55,6 +53,7 @@ protected:
 
 private:
 
+    class ColorGradientWidgetPriv;
     ColorGradientWidgetPriv* const d;
 };
 
