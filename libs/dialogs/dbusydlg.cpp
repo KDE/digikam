eb7e09e41c2a26941bd99cd1eef840d56839b6fa
Gilles Caulier <caulier.gilles@gmail.com>
2011-04-20 09:24:21 +0200

    more private internal container polish

--- libs/dialogs/dbusydlg.cpp
+++ libs/dialogs/dbusydlg.cpp
@@ -6,7 +6,7 @@
  * Date        : 2009-11-03
  * Description : a busy dialog for digiKam
  *
- * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -35,7 +35,19 @@
 namespace Digikam
 {
 
-class DBusyDlgPriv
+DBusyThread::DBusyThread(QObject* parent)
+    : QThread(parent)
+{
+}
+
+DBusyThread::~DBusyThread()
+{
+    wait();
+}
+
+// ----------------------------------------------------------------------------------
+
+class DBusyDlg::DBusyDlgPriv
 {
 public:
 
@@ -84,23 +96,6 @@ void DBusyDlg::slotComplete()
 {
     kDebug() << "Thread is complete";
     accept();
-}
-
-// ----------------------------------------------------------------------------------
-
-DBusyThread::DBusyThread(QObject* parent)
-    : QThread(parent)
-{
-}
-
-DBusyThread::~DBusyThread()
-{
-    wait();
-}
-
-void DBusyThread::run()
-{
-    // NOTE: Reimplement this method with your code to run in a separate thread.
 }
 
 }  // namespace Digikam
--- libs/dialogs/dbusydlg.h
+++ libs/dialogs/dbusydlg.h
@@ -6,7 +6,7 @@
  * Date        : 2009-11-03
  * Description : a busy dialog for digiKam
  *
- * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,8 +40,26 @@
 namespace Digikam
 {
 
-class DBusyThread;
-class DBusyDlgPriv;
+class DIGIKAM_EXPORT DBusyThread : public QThread
+{
+    Q_OBJECT
+
+public:
+
+    DBusyThread(QObject* parent);
+    virtual ~DBusyThread();
+
+Q_SIGNALS:
+
+    void signalComplete();
+
+protected:
+
+    /// Reimplement this method with your code to run in a separate thread.
+    virtual void run() {};
+};
+
+// ----------------------------------------------------------------------------------
 
 class DIGIKAM_EXPORT DBusyDlg : public KProgressDialog
 {
@@ -60,27 +78,8 @@ public Q_SLOTS:
 
 private:
 
+    class DBusyDlgPriv;
     DBusyDlgPriv* const d;
-};
-
-// ----------------------------------------------------------------------------------
-
-class DIGIKAM_EXPORT DBusyThread : public QThread
-{
-    Q_OBJECT
-
-public:
-
-    DBusyThread(QObject* parent);
-    virtual ~DBusyThread();
-
-Q_SIGNALS:
-
-    void signalComplete();
-
-protected:
-
-    virtual void run();
 };
 
 }  // namespace Digikam
