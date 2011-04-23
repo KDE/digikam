7488ce0be6f7d8a72630cde03af3f0f7d7e20170
Gilles Caulier <caulier.gilles@gmail.com>
2011-04-23 13:53:35 +0200

    add comment

--- utilities/lighttable/lighttableview.cpp
+++ utilities/lighttable/lighttableview.cpp
@@ -69,6 +69,7 @@ public:
 
     QGridLayout*       grid;
 
+    // These label are used to draw a frame around preview views to identify easily which item has the focus.
     QLabel*            leftFrame;
     QLabel*            rightFrame;
 
