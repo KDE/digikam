b4a9ff822138af97033b056c9a46820981ca682b
Gilles Caulier <caulier.gilles@gmail.com>
2011-05-05 15:31:13 +0200

    more private internal container

--- libs/widgets/common/splashscreen.cpp
+++ libs/widgets/common/splashscreen.cpp
@@ -7,7 +7,7 @@
  * Description : a widget to display splash with progress bar
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -51,7 +51,7 @@
 namespace Digikam
 {
 
-class SplashScreenPriv
+class SplashScreen::SplashScreenPriv
 {
 
 public:
--- libs/widgets/common/splashscreen.h
+++ libs/widgets/common/splashscreen.h
@@ -7,7 +7,7 @@
  * Description : a widget to display splash with progress bar
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
- * Copyright (C) 2006-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,8 +40,6 @@
 namespace Digikam
 {
 
-class SplashScreenPriv;
-
 class DIGIKAM_EXPORT SplashScreen : public KSplashScreen
 {
 
@@ -66,6 +64,7 @@ public Q_SLOTS:
 
 private:
 
+    class SplashScreenPriv;
     SplashScreenPriv* const d;
 };
 
