36e325f7489c3ec974e28399eefd2e179c7bd3e9
Gilles Caulier <caulier.gilles@gmail.com>
2011-05-06 17:29:07 +0200

    polish

--- libs/widgets/common/dzoombar.cpp
+++ libs/widgets/common/dzoombar.cpp
@@ -6,7 +6,7 @@
  * Date        : 2007-04-15
  * Description : a zoom bar used in status bar.
  *
- * Copyright (C) 2007-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -52,7 +52,7 @@
 namespace Digikam
 {
 
-class DZoomBarPriv
+class DZoomBar::DZoomBarPriv
 {
 
 public:
--- libs/widgets/common/dzoombar.h
+++ libs/widgets/common/dzoombar.h
@@ -6,7 +6,7 @@
  * Date        : 2007-04-15
  * Description : a zoom bar used in status bar.
  *
- * Copyright (C) 2007-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,8 +40,6 @@ class QAction;
 
 namespace Digikam
 {
-
-class DZoomBarPriv;
 
 class DIGIKAM_EXPORT DZoomBar : public KHBox
 {
@@ -96,6 +94,7 @@ private Q_SLOTS:
 
 private:
 
+    class DZoomBarPriv;
     DZoomBarPriv* const d;
 };
 
