5e1ba6fb842b9bc9171fa27b00075896ae146563
Gilles Caulier <caulier.gilles@gmail.com>
2011-07-12 12:29:35 +0200

    more debug info

--- libs/dimg/loaders/tiffloader.cpp
+++ libs/dimg/loaders/tiffloader.cpp
@@ -166,11 +166,12 @@ bool TIFFLoader::load(const QString& filePath, DImgLoaderObserver* observer)
         rows_per_strip == 0 ||
         rows_per_strip > h)
     {
-        kWarning() << "TIFF loader: Encountered invalid value 0 in image."
-                   << " bits_per_sample " << bits_per_sample
-                   << " samples_per_pixel " << samples_per_pixel
-                   << " rows_per_strip " << rows_per_strip
-                   << " Loading file " << filePath;
+        kWarning() << "TIFF loader: Encountered invalid value in image." << endl
+                   << " bits_per_sample   : " << bits_per_sample  << endl
+                   << " samples_per_pixel : " << samples_per_pixel << endl
+                   << " rows_per_strip    : " << rows_per_strip << endl
+                   << " h                 : " << rows_per_strip << endl
+                   << " Loading file      : " << filePath;
         TIFFClose(tif);
         loadingFailed();
         return false;
