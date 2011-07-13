a826157005716a9e25fdd583a821159f55d08eb7
Jaime Torres <jtamate@gmail.com>
2011-07-13 20:51:56 +0200

    Enable the settings of localcontrast in the queue manager
    Reenable the settings of localconstrat disabled at the begining of
    the settings.
    
    BUG: 271622

--- libs/dimg/filters/lc/localcontrastsettings.cpp
+++ libs/dimg/filters/lc/localcontrastsettings.cpp
@@ -521,6 +521,7 @@ void LocalContrastSettings::setSettings(const LocalContrastContainer& settings)
     slotStage4Enabled(d->stageFour->isChecked());
      */
 
+    d->expanderBox->setEnabled(true);
     blockSignals(false);
 }
 
