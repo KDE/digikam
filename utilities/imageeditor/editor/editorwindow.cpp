76e8e3ac11af0052a9003c24403dfb2260c48748
Marcel Wiesweg <marcel.wiesweg@gmx.de>
2011-07-23 16:23:13 +0200

    It seems appropriate to "fork" anything that is saved with saveAs (= Export in non-destructive mode)
    CCBUG: 278040

--- utilities/imageeditor/editor/editorwindow.cpp
+++ utilities/imageeditor/editor/editorwindow.cpp
@@ -2438,6 +2438,9 @@ bool EditorWindow::startingSaveAs(const KUrl& url)
     m_savingContext.executedOperation = SavingContextContainer::SavingStateNone;
     m_savingContext.abortingSaving = false;
 
+    // in any case, destructive (Save as) or non (Export), mark as New Version
+    m_canvas->interface()->setHistoryIsBranch(fork);
+
     m_canvas->saveAs(m_savingContext.saveTempFileName, m_IOFileSettings,
                      m_setExifOrientationTag && m_canvas->exifRotated(),
                      m_savingContext.format.toLower());
