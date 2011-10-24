06f3e0487488297df2b1552fac898b77c74c057e
Andi Clemens <andi.clemens@googlemail.com>
2011-10-24 21:32:14 +0200

    update copyright headers

--- tests/advancedrenametest.cpp
+++ tests/advancedrenametest.cpp
@@ -6,7 +6,7 @@
  * Date        : 2009-06-09
  * Description : a test for the AdvancedRename utility
  *
- * Copyright (C) 2009-2010 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2009-2011 by Andi Clemens <andi dot clemens at gmx dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
