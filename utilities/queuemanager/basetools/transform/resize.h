9e4bab2498b9f009ad675d582fb5bd629e3322d8
Gilles Caulier <caulier.gilles@gmail.com>
2011-11-05 20:37:48 +0100

    fix resize limit for BQM BUGS: 285840

--- utilities/queuemanager/basetools/transform/resize.h
+++ utilities/queuemanager/basetools/transform/resize.h
@@ -6,7 +6,7 @@
  * Date        : 2009-02-17
  * Description : resize image batch tool.
  *
- * Copyright (C) 2009-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
