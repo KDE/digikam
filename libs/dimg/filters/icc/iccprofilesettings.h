b62aa9da836a55dca8addea4b0bb1d0d06a45511
Gilles Caulier <caulier.gilles@gmail.com>
2011-11-18 13:21:00 +0100

    more private internal container

--- libs/dimg/filters/icc/iccprofilesettings.h
+++ libs/dimg/filters/icc/iccprofilesettings.h
@@ -6,7 +6,7 @@
  * Date        : 2010-02-17
  * Description : Icc profile settings view.
  *
- * Copyright (C) 2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -41,8 +41,6 @@
 namespace Digikam
 {
 
-class IccProfilesSettingsPriv;
-
 class DIGIKAM_EXPORT IccProfilesSettings : public KVBox
 {
     Q_OBJECT
@@ -74,6 +72,7 @@ private Q_SLOTS:
 
 private:
 
+    class IccProfilesSettingsPriv;
     IccProfilesSettingsPriv* const d;
 };
 
