a6f9a3e8e866e55483155ae504e076bd68b2f0b9
Andi Clemens <andi.clemens@googlemail.com>
2011-12-25 16:31:55 +0100

    update copyright header

--- utilities/setup/setupplugins.cpp
+++ utilities/setup/setupplugins.cpp
@@ -7,6 +7,7 @@
  * Description : setup Kipi plugins tab.
  *
  * Copyright (C) 2004-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2011 by Andi Clemens <andi dot clemens at googlemail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
