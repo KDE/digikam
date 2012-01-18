4d9a4e667c673f9df749b6b0272f5a3881ce5f30
Gilles Caulier <caulier.gilles@gmail.com>
2012-01-18 09:49:28 +0100

    polish

--- utilities/maintenance/batchalbumssyncmetadata.cpp
+++ utilities/maintenance/batchalbumssyncmetadata.cpp
@@ -7,7 +7,7 @@
  * Description : batch sync pictures metadata from all Albums
  *               with digiKam database
  *
- * Copyright (C) 2007-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -106,7 +106,7 @@ BatchAlbumsSyncMetadata::~BatchAlbumsSyncMetadata()
 
 void BatchAlbumsSyncMetadata::slotStart()
 {
-    d->palbumList   = AlbumManager::instance()->allPAlbums();
+    d->palbumList = AlbumManager::instance()->allPAlbums();
     d->duration.start();
 
     setTitle(i18n("Parsing all albums"));
--- utilities/maintenance/batchalbumssyncmetadata.h
+++ utilities/maintenance/batchalbumssyncmetadata.h
@@ -7,7 +7,7 @@
  * Description : batch sync pictures metadata with
  *               digiKam database
  *
- * Copyright (C) 2007-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
