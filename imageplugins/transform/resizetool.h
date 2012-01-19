92b8ceb11e0a051ee60df8a6e5b087e21bd91c41
Gilles Caulier <caulier.gilles@gmail.com>
2012-01-19 11:12:23 +0100

    simplify layout contruction

--- imageplugins/transform/resizetool.h
+++ imageplugins/transform/resizetool.h
@@ -6,7 +6,7 @@
  * Date        : 2005-04-07
  * Description : a tool to resize an image
  *
- * Copyright (C) 2005-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
