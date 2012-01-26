502836d572a3827b2770e85d738e676ba539c448
Gilles Caulier <caulier.gilles@gmail.com>
2012-01-26 13:03:58 +0100

    polish

--- utilities/kipiiface/kipiinterface.cpp
+++ utilities/kipiiface/kipiinterface.cpp
@@ -8,7 +8,7 @@
  *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
  * Copyright (C) 2004-2005 by Ralf Holzer <ralf at well.com>
- * Copyright (C) 2004-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -360,7 +360,7 @@ QString KipiInterface::progressScheduled(const QString& title, bool canBeCancele
         connect(item, SIGNAL(progressItemCanceled(QString)),
                 this, SIGNAL(progressCanceled(QString)));
     }
-    
+
     return item->id();
 }
 
@@ -401,5 +401,5 @@ void KipiInterface::progressCompleted(const QString& id)
 }
 
 #endif // KIPI_VERSION >= 0x010500
-    
+
 }  // namespace Digikam
