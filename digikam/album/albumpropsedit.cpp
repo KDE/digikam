3a8d9f6faa805b8bb6b8d26c3240c7bb2af75afa
Gilles Caulier <caulier.gilles@gmail.com>
2012-02-01 18:03:11 +0100

    use kseparator

--- digikam/album/albumpropsedit.cpp
+++ digikam/album/albumpropsedit.cpp
@@ -8,7 +8,7 @@
  *
  * Copyright (C) 2003-2004 by Renchi Raju <renchi@pooh.tam.uiuc.edu>
  * Copyright (C) 2005 by Tom Albers <tomalbers@kde.nl>
- * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -56,6 +56,7 @@
 #include <kstandarddirs.h>
 #include <ktextedit.h>
 #include <kurl.h>
+#include <kseparator.h>
 
 // Local includes
 
@@ -143,11 +144,7 @@ AlbumPropsEdit::AlbumPropsEdit(PAlbum* album, bool create)
     topLabel->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
     topLabel->setWordWrap(false);
 
-    // --------------------------------------------------------
-
-    QFrame* topLine = new QFrame(page);
-    topLine->setFrameShape(QFrame::HLine);
-    topLine->setFrameShadow(QFrame::Sunken);
+    KSeparator* topLine = new KSeparator(Qt::Horizontal);
 
     // --------------------------------------------------------
 
