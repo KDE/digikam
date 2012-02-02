7f5d5f34091254295e88efda4b7c15e8e911feec
Gilles Caulier <caulier.gilles@gmail.com>
2012-02-02 11:21:21 +0100

    update welcome page view

--- digikam/views/welcomepageview.h
+++ digikam/views/welcomepageview.h
@@ -7,7 +7,7 @@
  * Description : a widget to display a welcome page
  *               on root album.
  *
- * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009-2011 by Andi Clemens <andi dot clemens at googlemail dot com>
  *
  * This program is free software; you can redistribute it
