4ab43348df70070c79df0bb441eebfb565e82117
Gilles Caulier <caulier.gilles@gmail.com>
2012-02-08 12:14:16 +0100

    polish

--- libs/dmetadata/metadatasettingscontainer.h
+++ libs/dmetadata/metadatasettingscontainer.h
@@ -6,7 +6,7 @@
  * Date        : 2010-08-20
  * Description : Metadata Settings Container.
  *
- * Copyright (C) 2010-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -79,23 +79,23 @@ public:
 
 public:
 
-    bool exifRotate;
-    bool exifSetOrientation;
+    bool                  exifRotate;
+    bool                  exifSetOrientation;
 
-    bool saveComments;
-    bool saveDateTime;
-    bool savePickLabel;
-    bool saveColorLabel;
-    bool saveRating;
+    bool                  saveComments;
+    bool                  saveDateTime;
+    bool                  savePickLabel;
+    bool                  saveColorLabel;
+    bool                  saveRating;
 
-    bool saveTemplate;
-    bool saveTags;
+    bool                  saveTemplate;
+    bool                  saveTags;
 
-    bool writeRawFiles;
-    bool updateFileTimeStamp;
-    bool useXMPSidecar4Reading;
+    bool                  writeRawFiles;
+    bool                  updateFileTimeStamp;
+    bool                  useXMPSidecar4Reading;
 
-    int  metadataWritingMode;
+    int                   metadataWritingMode;
 
     RotationBehaviorFlags rotationBehavior;
 };
