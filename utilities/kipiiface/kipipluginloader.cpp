8514f5b77f1b10988fae642990ee0e46092563ec
Gilles Caulier <caulier.gilles@gmail.com>
2012-03-12 12:37:10 +0100

    polish

--- utilities/kipiiface/kipipluginloader.cpp
+++ utilities/kipiiface/kipipluginloader.cpp
@@ -6,7 +6,8 @@
  * Date        : 2012-10-03
  * Description : kipi Loader Implementation
  *
- * Copyright (C) 2012 by Supreet Pal Singh <supreetpal@gmail.com>
+ * Copyright (C) 2012      by Supreet Pal Singh <supreetpal@gmail.com>
+ * Copyright (C) 2004-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
