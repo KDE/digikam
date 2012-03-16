c91c71553cc98204937eabe82d089052bc2ec5e7
Gilles Caulier <caulier.gilles@gmail.com>
2012-03-16 10:46:31 +0100

    support MSVC 2010

--- project/scripts/shell-msvc.bat
+++ project/scripts/shell-msvc.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set KDE4_INSTALL_DIR=E:\kde4-msvc
+set KDE4_INSTALL_DIR=C:\KDE
 set PATH=%PATH%;%KDE4_INSTALL_DIR%\bin;%KDE4_INSTALL_DIR%\plugins
 set KDE4_INCLUDE_DIR=%KDE4_INSTALL_DIR%\include
 set KDEDIRS=%KDE4_INSTALL_DIR%
@@ -9,8 +9,8 @@ set QTMAKESPEC=%KDE4_INSTALL_DIR%\mkspecs\win32-g++
 set QT_PLUGIN_PATH=%KDE4_INSTALL_DIR%\plugins
 set QTDIR=%KDE4_INSTALL_DIR%
 set QT_INSTALL_DIR=%KDE4_INSTALL_DIR%
-set SVN_EDITOR="E:\mc\mc -e"
+set GIT_EDITOR="C:\KDE\bin\kwrite"
 
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+call "C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
 
-call "E:\mc\mc.exe"
+call "C:\Devel\mc\mc.exe"
