06605380f866c59d339eedb25167fe99b637998b
Gilles Caulier <caulier.gilles@gmail.com>
2012-05-09 14:52:44 +0200

    damned, if QImage is not on the right format, the converted version is never used... Thanks MACOSX to return this important warning

--- libs/threadimageio/pgfutils.cpp
+++ libs/threadimageio/pgfutils.cpp
@@ -6,7 +6,7 @@
  * Date        : 2009-05-29
  * Description : PGF utils.
  *
- * Copyright (C) 2009-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -106,21 +106,27 @@ bool readPGFImageData(const QByteArray& data, QImage& img)
     return true;
 }
 
-bool writePGFImageData(const QImage& img, QByteArray& data, int quality)
+bool writePGFImageData(const QImage& image, QByteArray& data, int quality)
 {
     try
     {
-        if (img.isNull())
+        if (image.isNull())
         {
             kDebug() << "Thumb image is null";
             return false;
         }
 
+        QImage img;
+
         // No need Alpha to optimize space on DB.
-        if (img.format() != QImage::Format_ARGB32)
+        if (image.format() != QImage::Format_ARGB32)
         {
-            img.convertToFormat(QImage::Format_ARGB32);
+            img = image.convertToFormat(QImage::Format_ARGB32);
         }
+        else
+        {
+            img = image;
+        }        
 
         CPGFImage pgfImg;
         PGFHeader header;
