dda5babf34dcfead46b0bdb9ed2f4bca21fcfc22
Islam Wazery <wazery@ubuntu.com>
2012-06-20 23:02:32 +0200

    Added ImportModel the subclass of ImportThumbnailModel

---
+++ utilities/cameragui/models/importmodel.cpp
@@ -0,0 +1,38 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2012-18-06
+ * Description : Camera model for the import interface
+ *
+ * Copyright (C) 2012 by Islam Wazery <wazery at ubuntu dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "importmodel.moc"
+
+namespace Digikam
+{
+
+ImportModel::ImportModel(QObject* parent)
+    : ImportThumbnailModel(parent)
+{
+}
+
+ImportModel::~ImportModel()
+{
+}
+
+}
---
+++ utilities/cameragui/models/importmodel.h
@@ -0,0 +1,46 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2012-18-06
+ * Description : Camera model for the import interface
+ *
+ * Copyright (C) 2012 by Islam Wazery <wazery at ubuntu dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef IMPORTMODEL_H
+#define IMPORTMODEL_H
+
+// Local includes
+
+#include "importthumbnailmodel.h"
+
+namespace Digikam
+{
+
+class ImportModel : public ImportThumbnailModel
+{
+    Q_OBJECT
+
+public:
+
+    ImportModel(QObject* parent = 0);
+    ~ImportModel();
+};
+
+} // namespace Digikam
+
+#endif // IMPORTMODEL_H
