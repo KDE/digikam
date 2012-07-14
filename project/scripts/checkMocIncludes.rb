46e8838d7a55ff9c7c3c60f84299543768a30f2b
Andi Clemens <andi.clemens@googlemail.com>
2012-07-14 19:37:10 +0200

    convert checkMocIncludes script to ruby

---
+++ project/scripts/checkMocIncludes.rb
@@ -0,0 +1,59 @@
+#! /usr/bin/env ruby
+
+# ============================================================
+# 
+# This file is a part of digiKam project
+# http://www.digikam.org
+# 
+# Date        : 2012-07-14
+# Description : a helper script for finding source code with no moc includes
+# 
+# Copyright (C) 2012 by Andi Clemens <andi dot clemens at googlemail dot com>
+# 
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# ============================================================ */
+
+
+# get all header files containing the line "Q_OBJECT"
+pattern = /^\s*Q_OBJECT\s*$/
+candidates = Dir.glob("**/*.h").select do |f|
+  begin
+    File.open(f, "r") do |fp|
+      fp.any? { |line| line =~ pattern }
+    end
+  rescue Exception => e
+    false    
+  end
+end
+
+# get all source files with missing MOC includes
+missingMocIncludes = candidates.select do |f|
+  sourceFile = f.gsub(/\.h$/, ".cpp")
+  mocFile = f.gsub(/\.h$/, ".moc").split("/").last
+  pattern = /#include\s+[<"]#{mocFile}[>"]/ 
+  begin
+    File.open(sourceFile, "r") do |fp|
+      fp.none? { |line| line =~ pattern }
+    end
+  rescue Exception => e
+    false
+  end
+end
+
+# display missing MOC includes
+puts "missing MOC includes:"
+widthStr = "#{missingMocIncludes.length}"
+
+missingMocIncludes.each_with_index do |f, idx|
+  puts "%#{widthStr.length}s: #{f}" % (idx + 1)
+end
