36934efae14fffba221a5757f304861a60c9ef07
Gilles Caulier <caulier.gilles@gmail.com>
2012-08-28 14:22:01 +0200

    backport from master

--- imageplugins/enhance/inpaintingtool.cpp
+++ imageplugins/enhance/inpaintingtool.cpp
@@ -219,7 +219,6 @@ InPaintingTool::InPaintingTool(QObject* parent)
     setToolSettings(d->gboxSettings);
     setToolView(d->previewWidget);
     setPreviewModeMask(PreviewToolBar::UnSplitPreviewModes);
-    init();
 
     // -------------------------------------------------------------
 
