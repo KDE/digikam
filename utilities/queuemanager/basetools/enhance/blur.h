400eb71b0628adbc331c844b89b6ccfe4b64b4a5
Gilles Caulier <caulier.gilles@gmail.com>
2012-09-29 23:17:05 +0200

    polish

--- utilities/queuemanager/basetools/enhance/blur.h
+++ utilities/queuemanager/basetools/enhance/blur.h
@@ -6,7 +6,7 @@
  * Date        : 2010-05-03
  * Description : blur image batch tool.
  *
- * Copyright (C) 2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -41,7 +41,7 @@ class Blur : public BatchTool
 
 public:
 
-    Blur(QObject* parent = 0);
+    Blur(QObject* const parent = 0);
     ~Blur();
 
     BatchToolSettings defaultSettings();
