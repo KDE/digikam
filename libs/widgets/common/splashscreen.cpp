d6d52091849a8b3f7b378956c9caaa0f8f84b33e
Gilles Caulier <caulier.gilles@gmail.com>
2012-10-31 10:10:18 +0100

    polish

--- libs/widgets/common/splashscreen.cpp
+++ libs/widgets/common/splashscreen.cpp
@@ -7,7 +7,7 @@
  * Description : a widget to display splash with progress bar
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi dot raju at gmail dot com>
- * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -36,7 +36,6 @@
 
 // KDE includes
 
-
 #include <klocale.h>
 #include <kstandarddirs.h>
 #include <kglobalsettings.h>
@@ -51,12 +50,12 @@
 namespace Digikam
 {
 
-class SplashScreen::SplashScreenPriv
+class SplashScreen::Private
 {
 
 public:
 
-    SplashScreenPriv()
+    Private()
     {
         state           = 0;
         progressBarSize = 3;
@@ -81,7 +80,7 @@ public:
 };
 
 SplashScreen::SplashScreen()
-    : KSplashScreen(QPixmap()), d(new SplashScreenPriv)
+    : KSplashScreen(QPixmap()), d(new Private)
 {
     if (KGlobal::mainComponent().aboutData()->appName() == QString("digikam"))
     {
--- libs/widgets/common/splashscreen.h
+++ libs/widgets/common/splashscreen.h
@@ -7,7 +7,7 @@
  * Description : a widget to display splash with progress bar
  *
  * Copyright (C) 2003-2005 by Renchi Raju <renchi dot raju at gmail dot com>
- * Copyright (C) 2006-2011 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -42,7 +42,6 @@ namespace Digikam
 
 class DIGIKAM_EXPORT SplashScreen : public KSplashScreen
 {
-
     Q_OBJECT
 
 public:
@@ -64,8 +63,8 @@ public Q_SLOTS:
 
 private:
 
-    class SplashScreenPriv;
-    SplashScreenPriv* const d;
+    class Private;
+    Private* const d;
 };
 
 }   // namespace Digikam
