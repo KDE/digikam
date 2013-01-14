95be91299dde71b74660f33adb6acd6c80ef634f
Gilles Caulier <caulier.gilles@gmail.com>
2013-01-14 12:49:22 +0100

    constify

--- digikam/utils/componentsinfo.h
+++ digikam/utils/componentsinfo.h
@@ -6,7 +6,7 @@
  * Date        : 2008-07-30
  * Description : digiKam components info dialog.
  *
- * Copyright (C) 2008-2010 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -91,20 +91,20 @@ static inline void showDigikamComponentsInfo()
         list.insert(i18n("Database internal server"), internal);
     }
 
-    LibsInfoDlg* dlg = new LibsInfoDlg(kapp->activeWindow());
+    LibsInfoDlg* const dlg = new LibsInfoDlg(kapp->activeWindow());
     dlg->setInfoMap(list);
     dlg->show();
 }
 
 static inline void showDigikamDatabaseStat()
 {
-    DBStatDlg* dlg = new DBStatDlg(kapp->activeWindow());
+    DBStatDlg* const dlg = new DBStatDlg(kapp->activeWindow());
     dlg->show();
 }
 
 static inline void showRawCameraList()
 {
-    RawCameraDlg* dlg = new RawCameraDlg(kapp->activeWindow());
+    RawCameraDlg* const dlg = new RawCameraDlg(kapp->activeWindow());
     dlg->show();
 }
 
