a662c6ee47371da1cb2978731553852ac3545c0e
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-14 12:30:07 +0100

    CR : a cast need to be done here to prevent future warning

--- libs/dimg/loaders/ppmloader.h
+++ libs/dimg/loaders/ppmloader.h
@@ -8,7 +8,7 @@
  *               DImg framework
  *
  * Copyright (C) 2005      by Renchi Raju <renchi dot raju at gmail dot com>
- * Copyright (C) 2005-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
