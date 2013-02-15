d9ab5fdbcdba6a909d1fef3410c9b519954d2285
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-15 07:22:35 +0100

    CR : not init member in ctor

--- utilities/queuemanager/manager/batchtool.cpp
+++ utilities/queuemanager/manager/batchtool.cpp
@@ -6,7 +6,7 @@
  * Date        : 2008-11-24
  * Description : Batch Tool Container.
  *
- * Copyright (C) 2008-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -69,6 +69,7 @@ public:
         cancel(false),
         last(false),
         observer(0),
+        toolGroup(BaseTool),
         rawLoadingRule(QueueSettings::DEMOSAICING)
     {
     }
