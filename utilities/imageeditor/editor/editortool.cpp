c34935eb1455fedc9e0c57c14c86e131161eab31
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-15 07:30:58 +0100

    CR : not init member in ctor

--- utilities/imageeditor/editor/editortool.cpp
+++ utilities/imageeditor/editor/editortool.cpp
@@ -6,7 +6,7 @@
  * Date        : 2008-08-20
  * Description : editor tool template class.
  *
- * Copyright (C) 2008-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -57,7 +57,8 @@ public:
         version(0),
         view(0),
         timer(0),
-        settings(0)
+        settings(0),
+        category(FilterAction::ReproducibleFilter)
     {
     }
 
@@ -475,7 +476,7 @@ void EditorToolThreaded::slotAbort()
     {
         analyser()->cancelFilter();
     }
-    
+
     if (filter())
     {
         filter()->cancelFilter();
