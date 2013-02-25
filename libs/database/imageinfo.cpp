c16f6bc3510eb60d7f696edd59da80a613320c2c
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-26 00:01:20 +0100

    Fix CR SCAN #981288

--- libs/database/imageinfo.cpp
+++ libs/database/imageinfo.cpp
@@ -7,8 +7,8 @@
  * Description : Handling accesss to one image and associated data
  *
  * Copyright (C) 2005      by Renchi Raju <renchi dot raju at gmail dot com>
- * Copyright (C) 2007-2012 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
- * Copyright (C) 2009-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2007-2013 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2009-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -84,6 +84,8 @@ ImageInfoCache* ImageInfoStatic::cache()
     return &m_instance->m_cache;
 }
 
+// ---------------------------------------------------------------
+
 ImageInfoData::ImageInfoData()
 {
     id                     = -1;
@@ -105,7 +107,8 @@ ImageInfoData::ImageInfoData()
 
     groupedImages          = 0;
     groupImage             = -1;
-
+    groupingFlags          = 0;
+    
     defaultTitleCached     = false;
     defaultCommentCached   = false;
     pickLabelCached        = false;
@@ -128,6 +131,8 @@ ImageInfoData::ImageInfoData()
 ImageInfoData::~ImageInfoData()
 {
 }
+
+// ---------------------------------------------------------------
 
 ImageInfo::ImageInfo()
     : m_data(0)
