04df0d37c9ec1c953d00983b121ebd5258630def
Gowtham Ashok <gwty93@gmail.com>
2013-06-17 15:05:44 +0530

    Added License information to tests/imgqsort

--- tests/imgqsort/amisharp/amisharp.cpp
+++ tests/imgqsort/amisharp/amisharp.cpp
@@ -1,4 +1,30 @@
-#include "opencv2/imgproc/imgproc.hpp"
+/** ===========================================================
+ *
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ *
+ * @date   2013-06-17
+ * @brief  a command line tool to estimate amount of  blur in an image.
+ *
+ * @author Copyright (C) 2013-2014 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *         Copyright (C) 2013-2014 by Gowtham Ashok
+ *         <a href="mailto:gwty93 at gmail dot com">gwty93 at gmail dot com</a>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+ 
+ #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
 #include <stdio.h>
