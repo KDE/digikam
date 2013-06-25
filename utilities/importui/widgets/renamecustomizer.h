ef4ad262d76e6a3d6b734cdbf7502eb6414eb622
Andi Clemens <andi.clemens@googlemail.com>
2013-06-25 13:16:52 +0200

    this method is not implemented and therefore should not be declared here. If we don't need it afterwards, we should remove it completely from the header file.

--- utilities/importui/widgets/renamecustomizer.h
+++ utilities/importui/widgets/renamecustomizer.h
@@ -58,7 +58,7 @@ public:
     RenameCustomizer(QWidget* const parent, const QString& cameraTitle);
     ~RenameCustomizer();
 
-    void    setUseDefault(bool val);
+    //void    setUseDefault(bool val);
     bool    useDefault() const;
     QString newName(const QString& fileName, const QDateTime& date) const;
     Case    changeCase() const;
