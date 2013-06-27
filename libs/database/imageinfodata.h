44d922c14edc191540810264d7c92d7eaf9881b4
Michael Georg Hansen <mike@mghansen.de>
2013-06-27 22:50:07 +0200

    Start work on more metadata caching.

--- libs/database/imageinfodata.h
+++ libs/database/imageinfodata.h
@@ -8,6 +8,7 @@
  *
  * Copyright (C) 2007-2013 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  * Copyright (C)      2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2013 by Michael G. Hansen <mike at mghansen dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -143,6 +144,14 @@ public:
     bool                   groupImageCached       : 1;
 
     bool                   invalid                : 1;
+
+    /// @todo Currently only 7 bits of VideoMetadataField are used,
+    ///       we could store only a byte instead.
+    DatabaseFields::VideoMetadata videoMetadataCached;
+    bool                   hasVideoMetadata       : 1;
+
+    typedef DatabaseFields::Hash<QVariant> DatabaseFieldsHashRaw;
+    DatabaseFieldsHashRaw databaseFieldsHashRaw;
 };
 
 }  // namespace Digikam
