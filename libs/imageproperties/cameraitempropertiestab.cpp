786d0993376bb0e610ffef78811995acef18b33d
Gilles Caulier <caulier.gilles@gmail.com>
2013-07-02 11:28:44 +0200

    compile

--- libs/imageproperties/cameraitempropertiestab.cpp
+++ libs/imageproperties/cameraitempropertiestab.cpp
@@ -6,7 +6,7 @@
  * Date        : 2006-02-08
  * Description : A tab to display camera item information
  *
- * Copyright (C) 2006-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -422,9 +422,9 @@ void CameraItemPropertiesTab::setCurrentItem(const CamItemInfo& itemInfo, const 
 
     d->labelFileIsWritable->setText(str);
 
-    if (itemInfo.mtime.isValid())
+    if (itemInfo.ctime.isValid())
     {
-        d->labelFileDate->setText(KGlobal::locale()->formatDateTime(itemInfo.mtime, KLocale::ShortDate, true));
+        d->labelFileDate->setText(KGlobal::locale()->formatDateTime(itemInfo.ctime, KLocale::ShortDate, true));
     }
     else
     {
--- libs/imageproperties/imagepropertiessidebarcamgui.h
+++ libs/imageproperties/imagepropertiessidebarcamgui.h
@@ -7,7 +7,7 @@
  * Description : simple image properties side bar used by
  *               camera GUI.
  *
- * Copyright (C) 2006-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2013      by Michael G. Hansen <mike at mghansen dot de>
  *
  * This program is free software; you can redistribute it
