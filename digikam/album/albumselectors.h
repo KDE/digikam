64f92ce6d60ce4c754f020847bf600d543994a66
Gilles Caulier <caulier.gilles@gmail.com>
2013-08-02 18:48:56 +0200

    extract Album Selector widget from FaceScan dialog for future factoring in FuzzySearch view, and a future implementation in Maintenance dialog.

--- digikam/album/albumselectors.h
+++ digikam/album/albumselectors.h
@@ -4,7 +4,7 @@
  * http://www.digikam.org
  *
  * Date        : 2010-10-09
- * Description : Dialog to choose options for face scanning
+ * Description : A widget to select Physical or virtual albums with combo-box
  *
  * Copyright (C) 2010-2012 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  * Copyright (C) 2012-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
@@ -22,52 +22,35 @@
  *
  * ============================================================ */
 
-#ifndef FACESCANDIALOG_H
-#define FACESCANDIALOG_H
+#ifndef ALBUMSELECTORS_H
+#define ALBUMSELECTORS_H
 
 // Qt includes
 
-#include <QList>
-
-// KDE includes
-
-#include <kdialog.h>
-
-// Local includes
-
-#include "statesavingobject.h"
-#include "facescansettings.h"
+#include <QWidget>
 
 namespace Digikam
 {
 
-class FaceScanDialog : public KDialog, public StateSavingObject
+class Album;
+
+class AlbumSelectors : public QWidget
 {
     Q_OBJECT
 
 public:
 
-    explicit FaceScanDialog(QWidget* const parent = 0);
-    ~FaceScanDialog();
+    explicit AlbumSelectors(const QString& label, const QString& configName, QWidget* const parent = 0);
+    ~AlbumSelectors();
 
-    FaceScanSettings settings() const;
+    QList<Album*> selectedAlbums() const;
 
-protected:
+    void loadState();
+    void saveState();
 
-    void doLoadState();
-    void doSaveState();
-    void accept();
+private Q_SLOTS:
 
-protected Q_SLOTS:
-
-    void setDetectionDefaultParameters();
-    void retrainAllButtonToggled(bool on);
-    void benchmarkButtonToggled(bool on);
-
-private:
-
-    void setupUi();
-    void setupConnections();
+    void slotUpdateClearButtons();
 
 private:
 
@@ -77,4 +60,4 @@ private:
 
 } // namespace Digikam
 
-#endif // FACESCANDIALOG_H
+#endif // ALBUMSELECTORS_H
