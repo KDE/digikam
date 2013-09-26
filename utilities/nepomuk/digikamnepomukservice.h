bde72861e6b67ef71dedc63ded7fc82f12ab7e21
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2013-09-26 21:19:57 +0000

    Add Doxygen comments to digikamnepomukservice.h

--- utilities/nepomuk/digikamnepomukservice.h
+++ utilities/nepomuk/digikamnepomukservice.h
@@ -7,6 +7,7 @@
  * Description : Service to sync digikam and nepomuk storages
  *
  * Copyright (C) 2009-2010 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2013 by Veaceslav Munteanu <veaceslav dot munteanu90 at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -69,26 +70,103 @@ public:
 
 public Q_SLOTS:
 
-    /** Sets the digikam database to watch.
-     *  The parameter is the url() of a KUrl which contains the database parameters
-     *  serialized by DatabaseParameters.
+    /**
+     *  @brief setDatabase        -  Sets the digikam database to watch.
+     *                               The parameter is the url() of a KUrl which
+     *                               contains the database parameters serialized
+     *                               by DatabaseParameters.
      */
     Q_SCRIPTABLE void setDatabase(const QString& parameters);
+
+    /**
+     * @brief enableSyncToDigikam - enables Nepomuk to push image properties
+     *                              and tags to digiKam when it's resource
+     *                              watcher reports changes
+     *                              Q_SCRIPTABLE enables call from D-Bus
+     */
     Q_SCRIPTABLE void enableSyncToDigikam(bool syncToDigikam);
+
+    /**
+     * @brief enableSyncToDigikam - enables digiKam to push image properties
+     *                              and tags to Nepomuk when database emits
+     *                              corresponding signals.
+     *                              Q_SCRIPTABLE enables call from D-Bus
+     */
     Q_SCRIPTABLE void enableSyncToNepomuk(bool syncToNepomuk);
+
+    /**
+     * @brief triggerResync       - will trigger both digiKam -> Nepomuk
+     *                              and Nepomuk -> digiKam full sync
+     *                              Usually called by digiKam via D-bus
+     *                              call
+     */
     Q_SCRIPTABLE void triggerResync();
 
+    /**
+     * @brief syncTagsToDigikam   - sync image tags to digiKam database.
+     *                              Called by NepomukWatcher or NepomukQuery
+     *                              class.
+     *
+     * @param filePath            - path to image to which tags should be added
+     *
+     * @param tags                - list of tags that should be added
+     *
+     */
     void syncTagsToDigikam(const KUrl& filePath, const QList<QUrl>& tags);
+
+    /**
+     * @brief syncRatingToDigikam  - sync image rating to digiKam database.
+     *                               Called by NepomukWatcher of NepomukQuery
+     *                               class.
+     * @param filePath             - path to image to which rating should be added
+     *
+     * @param rating               - current rating value
+     */
     void syncRatingToDigikam(const KUrl& filePath, const int ratings);
+
+    /**
+     * @brief syncCommentToDigikam - sync image comment to digiKam database.
+     *                               Called by NepomukWatcher of NepomukQuery
+     *                               class.
+     * @param filePath             - path to image to which comment should be added
+     *
+     * @param comment              - comment to be set
+     */
     void syncCommentToDigikam(const KUrl& filePath, const QString& comment);
+
+    /**
+     * @brief syncRatingToDigikam  - remove tag from image properties in digiKam
+     *                               database.
+     * @param filePath             - path to image to which tag should be removed
+     *
+     * @param tag                  - tag to be removed
+     */
     void removeTagInDigikam(const KUrl& fileUrl, const QUrl& tag);
 
 protected Q_SLOTS:
 
+    /**
+     * @brief slotImageChange - sync image comment & rating when images comment
+     *                          or rating is changed in digikam
+     * @param changeset       - image changeset from digikam database
+     */
     void slotImageChange(const ImageChangeset& changeset);
+
+    /**
+     * @brief slotImageTagChange  - sync image tags when tags are added or
+     *                              removed
+     * @param changeset           - Database class that hold info about image
+     *                              and tag operations
+     */
     void slotImageTagChange(const ImageTagChangeset& changeset);
+
+    /**
+     * @brief slotTagChange   - sync tag to Nepomuk when a digikam tag
+     *                          is changed: add, rename, delete
+     * @param changeset       - Database structure that holds information about
+     *                          tag and change operation
+     */
     void slotTagChange(const TagChangeset& changeset);
-    //void slotCollectionImageChange(const CollectionImageChangeset& changeset);
 
     void syncNepomukToDigikam();
     void fullSyncDigikamToNepomuk();
@@ -112,11 +190,21 @@ protected:
 
 
     void readConfig();
-    void syncToNepomuk(const QList<qlonglong>& imageid, SyncToNepomukSettings syncSettings);
-    void syncToNepomuk(const QList<ImageInfo>& infos, SyncToNepomukSettings syncSettings);
-    void syncTagsToNepomuk(const QList<qlonglong>& imageIds, const QList<int>& tagIds, bool addOrRemove);
+
+    void syncToNepomuk(const QList<qlonglong>& imageid,
+                       SyncToNepomukSettings syncSettings);
+    void syncToNepomuk(const QList<ImageInfo>& infos,
+                       SyncToNepomukSettings syncSettings);
+    void syncTagsToNepomuk(const QList<qlonglong>& imageIds,
+                           const QList<int>& tagIds, bool addOrRemove);
 
     void syncAddedImagesToDigikam(const QList<qlonglong>& ids);
+
+    /**
+     * @brief pushTagsToNepomuk   - add images tags from digiKam
+     *                              to Nepomuk's resources
+     * @param imageInfos          - images that contain tags
+     */
     void pushTagsToNepomuk(const QList<ImageInfo>& imageInfos);
 
     int bestDigikamTagForTagName(const ImageInfo& info, const QString& tagName);
@@ -143,100 +231,3 @@ private:
 }
 #endif
 
-#if 0
-namespace Digikam
-{
-
-class CollectionImageChangeset;
-class DatabaseParameters;
-class ImageInfo;
-class ImageChangeset;
-class ImageTagChangeset;
-class TagChangeset;
-
-class NepomukService : public Nepomuk::Service
-{
-    Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", "org.kde.digikam.DigikamNepomukService")
-
-public:
-
-    NepomukService(QObject* parent, const QVariantList&);
-    ~NepomukService();
-
-public Q_SLOTS:
-
-    /** Sets the digikam database to watch.
-     *  The parameter is the url() of a KUrl which contains the database parameters
-     *  serialized by DatabaseParameters.
-     */
-    Q_SCRIPTABLE void setDatabase(const QString& parameters);
-    Q_SCRIPTABLE void enableSyncToDigikam(bool syncToDigikam);
-    Q_SCRIPTABLE void enableSyncToNepomuk(bool syncToNepomuk);
-    Q_SCRIPTABLE void triggerResync();
-
-protected Q_SLOTS:
-
-    void slotImageChange(const ImageChangeset& changeset);
-    void slotImageTagChange(const ImageTagChangeset& changeset);
-    void slotTagChange(const TagChangeset& changeset);
-    //void slotCollectionImageChange(const CollectionImageChangeset& changeset);
-
-    void slotStatementAdded(const Soprano::Statement& statement);
-    void slotStatementRemoved(const Soprano::Statement& statement);
-
-    void syncNepomukToDigikam();
-    void fullSyncDigikamToNepomuk();
-
-    void cleanIgnoreList();
-
-    void slotFullSyncJobResult(KJob* job);
-    void slotFullSyncJobData(KIO::Job*, const QByteArray& data);
-
-protected:
-
-    void connectToDatabase(const DatabaseParameters& params);
-
-    enum SyncToNepomukSettings
-    {
-        SyncNothing     = 0x00,
-        SyncRating      = 0x01,
-        SyncHasNoRating = 0x02,
-        SyncComment     = 0x04
-    };
-
-    void readConfig();
-    void syncToNepomuk(const QList<qlonglong>& imageid, SyncToNepomukSettings syncSettings);
-    void syncToNepomuk(const QList<ImageInfo>& infos, SyncToNepomukSettings syncSettings);
-    void syncTagsToNepomuk(const QList<qlonglong>& imageIds, const QList<int>& tagIds, bool addOrRemove);
-    void syncRatingToDigikam(const KUrl::List& filePaths, const QList<int>& ratings);
-    void syncCommentToDigikam(const KUrl::List& filePaths, const QStringList& ratings);
-    void syncTagsToDigikam(const KUrl::List& filePaths, const QList<QUrl>& tags);
-    void syncAddedImagesToDigikam(const QList<qlonglong>& ids);
-    void removeTagInDigikam(const KUrl& fileUrl, const QUrl& tag);
-    void pushTagsToNepomuk(const QList<ImageInfo>& imageInfos);
-
-    int bestDigikamTagForTagName(const ImageInfo& info, const QString& tagName);
-    void markAsSyncedToDigikam();
-    void clearSyncedToDigikam();
-    bool hasSyncToNepomuk();
-    void markAsSyncedToNepomuk();
-    void clearSyncedToNepomuk();
-    QString tagnameForNepomukTag(const QUrl& tagUri) const;
-    QDateTime lastSyncToDigikam() const;
-    DatabaseParameters databaseParameters() const;
-    KSharedConfig::Ptr digikamConfig() const;
-
-public:
-
-    // Declared as public due to use in ChangingNepomuk and ChangingDB classes.
-    class NepomukServicePriv;
-
-private:
-
-    NepomukServicePriv* const d;
-};
-
-} // namespace Digikam
-
-#endif // DIGIKAMNEPOMUKSERVICE_H
