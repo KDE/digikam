1b01770fa63910d4e8fd03163c9ba35904d1bfe3
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2014-01-06 20:24:46 +0200

    License Editing

--- showfoto/main/showfotosettings.cpp
+++ showfoto/main/showfotosettings.cpp
@@ -6,7 +6,7 @@
  * Date        : 2013-12-20
  * Description : Settings for Showfoto
  *
- * Copyright (C) 2013 by Mohamed Anwer <mohammed dot ahmed dot anwer at gmail dot com>
+ * Copyright (C) 2013-2014 by Mohamed Anwer <mohammed dot ahmed dot anwer at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
--- showfoto/setup/setuptooltip.cpp
+++ showfoto/setup/setuptooltip.cpp
@@ -7,6 +7,7 @@
  * Description : item tool tip configuration setup tab
  *
  * Copyright (C) 2006-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2013-2014 by Mohamed Anwer <mohammed dot ahmed dot anwer at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
