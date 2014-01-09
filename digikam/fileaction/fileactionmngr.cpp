4542521dd0ee7a4933a0797af0fe7e5823128134
Gilles Caulier <caulier.gilles@gmail.com>
2014-01-09 09:07:13 +0100

    polish

--- digikam/fileaction/fileactionmngr.cpp
+++ digikam/fileaction/fileactionmngr.cpp
@@ -7,7 +7,7 @@
  * Description : file action manager
  *
  * Copyright (C) 2009-2011 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
- * Copyright (C) 2011-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2011-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -263,6 +263,7 @@ void FileActionMngr::applyMetadata(const QList<ImageInfo>& infos, MetadataHubOnT
         delete hub;
         return;
     }
+
     FileActionImageInfoList taskList = FileActionImageInfoList::create(infos);
     taskList.schedulingForDB(i18n("Applying metadata"), d->dbProgressCreator());
     d->applyMetadata(taskList, hub);
@@ -272,8 +273,11 @@ void FileActionMngr::transform(const QList<ImageInfo>& infos, KExiv2Iface::Rotat
 {
     FileActionImageInfoList taskList = FileActionImageInfoList::create(infos);
     taskList.schedulingForWrite(i18n("Rotating images"), d->fileProgressCreator());
+
     for (ImageInfoTaskSplitter splitter(taskList); splitter.hasNext();)
+    {
         d->transform(splitter.next(), action);
+    }
 }
 
 void FileActionMngr::copyAttributes(const ImageInfo& source, const QString& derivedPath)
@@ -287,6 +291,5 @@ void FileActionMngr::copyAttributes(const ImageInfo& source, const QStringList& 
     taskList.schedulingForDB(i18n("Copying attributes"), d->dbProgressCreator());
     d->copyAttributes(taskList, derivedPaths);
 }
-
 
 } // namespace Digikam
--- digikam/fileaction/fileactionmngr.h
+++ digikam/fileaction/fileactionmngr.h
@@ -7,7 +7,7 @@
  * Description : file action manager
  *
  * Copyright (C) 2009-2011 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
- * Copyright (C) 2011-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2011-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -104,7 +104,6 @@ public Q_SLOTS:
     void copyAttributes(const ImageInfo& source, const QStringList& derivedPaths);
     void copyAttributes(const ImageInfo& source, const QString& derivedPath);
 
-
 public:
 
     // Declared public due to use by FileActionMngrWorker, FileActionMngrDatabaseWorker, and FileActionMngrFileWorker
@@ -112,11 +111,12 @@ public:
 
 private:
 
-    friend class FileActionMngrCreator;
     FileActionMngr();
     ~FileActionMngr();
 
     FileActionMngrPriv* const d;
+
+    friend class FileActionMngrCreator;
 };
 
 } // namespace Digikam
