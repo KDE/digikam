61ab3fadf53793a4fd7ad030000b951449eb17df
Gilles Caulier <caulier.gilles@gmail.com>
2014-01-22 14:28:41 +0100

    set image quality sorter settings as Experimental

--- utilities/setup/setupimagequalitysorter.cpp
+++ utilities/setup/setupimagequalitysorter.cpp
@@ -6,8 +6,8 @@
  * Date        : 2013-08-19
  * Description : Image Quality setup page
  *
- * Copyright (C) 2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2013 by Gowtham Ashok <gwty93 at gmail dot com>
+ * Copyright (C) 2013-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2013-2014 by Gowtham Ashok <gwty93 at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -105,7 +105,7 @@ SetupImageQualitySorter::SetupImageQualitySorter(QWidget* const parent)
 
     QVBoxLayout* const layout = new QVBoxLayout(panel);
 
-    d->enableSorter = new QCheckBox(i18n("Enable Image Quality Sorting"), panel);
+    d->enableSorter = new QCheckBox(i18n("Enable Image Quality Sorting (Experimental)"), panel);
     d->enableSorter->setWhatsThis(i18n("Enable this option to assign automatically Pick Labels based on image quality."));
 
     d->optionsView  = new KVBox(panel);
@@ -115,29 +115,29 @@ SetupImageQualitySorter::SetupImageQualitySorter(QWidget* const parent)
 
     // ------------------------------------------------------------------------------
 
-    d->setSpeed = new KIntNumInput(5, d->optionsView);
+    d->setSpeed    = new KIntNumInput(5, d->optionsView);
     d->setSpeed->setRange(1, 3, 1);
     d->setSpeed->setSliderEnabled(true);
     d->setSpeed->setLabel(i18n("Speed:"), Qt::AlignLeft | Qt::AlignTop);
     d->setSpeed->setWhatsThis(i18n("Tradeoff between speed and accuracy of sorting algorithm"));
 
-    d->detectBlur = new QCheckBox(i18n("Detect Blur"), d->optionsView);
+    d->detectBlur  = new QCheckBox(i18n("Detect Blur"), d->optionsView);
     d->detectBlur->setWhatsThis(i18n("Detect the amount of blur in the images passed to it"));
 
     d->detectNoise = new QCheckBox(i18n("Detect Noise"), d->optionsView);
     d->detectNoise->setWhatsThis(i18n("Detect the amount of noise in the images passed to it"));
 
-    d->detectCompression = new QCheckBox(i18n("Detect Compression"), d->optionsView);
+    d->detectCompression  = new QCheckBox(i18n("Detect Compression"), d->optionsView);
     d->detectCompression->setWhatsThis(i18n("Detect the amount of compression in the images passed to it"));
-    
+
     d->detectOverexposure = new QCheckBox(i18n("Detect Overexposure"), d->optionsView);
     d->detectOverexposure->setWhatsThis(i18n("Detect if the images are overexposed"));
 
     // ------------------------------------------------------------------------------
 
-    KHBox* const hlay1 = new KHBox(d->optionsView);
+    KHBox* const hlay1      = new KHBox(d->optionsView);
 
-    d->setRejected = new QCheckBox(i18n("Assign 'Rejected' Label to Low Quality Pictures"), hlay1);
+    d->setRejected          = new QCheckBox(i18n("Assign 'Rejected' Label to Low Quality Pictures"), hlay1);
     d->setRejected->setWhatsThis(i18n("Low quality images detected by blur, noise, and compression analysis will be assigned to Rejected label."));
 
     QWidget* const hspace1  = new QWidget(hlay1);
@@ -148,9 +148,9 @@ SetupImageQualitySorter::SetupImageQualitySorter(QWidget* const parent)
 
     // ------------------------------------------------------------------------------
 
-    KHBox* const hlay2 = new KHBox(d->optionsView);
+    KHBox* const hlay2      = new KHBox(d->optionsView);
 
-    d->setPending = new QCheckBox(i18n("Assign 'Pending' Label to Medium Quality Pictures"), hlay2);
+    d->setPending           = new QCheckBox(i18n("Assign 'Pending' Label to Medium Quality Pictures"), hlay2);
     d->setPending->setWhatsThis(i18n("Medium quality images detected by blur, noise, and compression analysis will be assigned to Pending label."));
 
     QWidget* const hspace2  = new QWidget(hlay2);
@@ -161,9 +161,9 @@ SetupImageQualitySorter::SetupImageQualitySorter(QWidget* const parent)
 
     // ------------------------------------------------------------------------------
 
-    KHBox* const hlay3 = new KHBox(d->optionsView);
+    KHBox* const hlay3      = new KHBox(d->optionsView);
 
-    d->setAccepted = new QCheckBox(i18n("Assign 'Accepted' Label to High Quality Pictures"), hlay3);
+    d->setAccepted          = new QCheckBox(i18n("Assign 'Accepted' Label to High Quality Pictures"), hlay3);
     d->setAccepted->setWhatsThis(i18n("High quality images detected by blur, noise, and compression analysis will be assigned to Accepted label."));
 
     QWidget* const hspace3  = new QWidget(hlay3);
@@ -180,7 +180,7 @@ SetupImageQualitySorter::SetupImageQualitySorter(QWidget* const parent)
     d->setRejectedThreshold->setLabel(i18n("Rejected threshold:"), Qt::AlignLeft | Qt::AlignTop);
     d->setRejectedThreshold->setWhatsThis(i18n("Threshold below which all pictures are assigned Rejected Label"));
 
-    d->setPendingThreshold = new KIntNumInput(5, d->optionsView);
+    d->setPendingThreshold  = new KIntNumInput(5, d->optionsView);
     d->setPendingThreshold->setRange(1, 100, 1);
     d->setPendingThreshold->setSliderEnabled(true);
     d->setPendingThreshold->setLabel(i18n("Pending threshold:"), Qt::AlignLeft | Qt::AlignTop);
@@ -192,13 +192,13 @@ SetupImageQualitySorter::SetupImageQualitySorter(QWidget* const parent)
     d->setAcceptedThreshold->setLabel(i18n("Accepted threshold:"), Qt::AlignLeft | Qt::AlignTop);
     d->setAcceptedThreshold->setWhatsThis(i18n("Threshold above which all pictures are assigned Accepted Label"));
 
-    d->setBlurWeight = new KIntNumInput(5, d->optionsView);
+    d->setBlurWeight        = new KIntNumInput(5, d->optionsView);
     d->setBlurWeight->setRange(1, 100, 1);
     d->setBlurWeight->setSliderEnabled(true);
     d->setBlurWeight->setLabel(i18n("Blur Weight:"), Qt::AlignLeft | Qt::AlignTop);
     d->setBlurWeight->setWhatsThis(i18n("Weight to assign to Blur Algorithm"));
 
-    d->setNoiseWeight = new KIntNumInput(5, d->optionsView);
+    d->setNoiseWeight       = new KIntNumInput(5, d->optionsView);
     d->setNoiseWeight->setRange(1, 100, 1);
     d->setNoiseWeight->setSliderEnabled(true);
     d->setNoiseWeight->setLabel(i18n("Noise Weight"), Qt::AlignLeft | Qt::AlignTop);
