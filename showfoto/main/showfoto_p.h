3567cce2bc6f352007134f5fff9ec0fb2d828d8b
Gilles Caulier <caulier.gilles@gmail.com>
2014-03-09 23:27:24 +0100

    uninitialized member in private class. Fix includes order Add missing licence header

--- showfoto/main/showfoto_p.h
+++ showfoto/main/showfoto_p.h
@@ -1,8 +1,38 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2004-11-22
+ * Description : stand alone digiKam image editor GUI
+ *
+ * Copyright (C) 2004-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2012 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2013      by Mohamed Anwer <mohammed dot ahmed dot anwer at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef SHOWFOTO_P_H
 #define SHOWFOTO_P_H
 
-#include "showfoto.h"
+// Qt includes
+
 #include "QDir"
+
+// Local includes
+
+#include "showfoto.h"
 #include "showfotoiteminfo.h"
 #include "showfotothumbnailbar.h"
 #include "splashscreen.h"
@@ -11,7 +41,8 @@
 #include "showfotosettings.h"
 #include "showfotodragdrophandler.h"
 
-namespace ShowFoto {
+namespace ShowFoto
+{
 
 class ShowFoto::Private
 {
@@ -27,6 +58,7 @@ public:
         openFilesInFolderAction(0),
         first(0),
         model(0),
+        dDHandler(0),
         filterModel(0),
         thumbLoadThread(0),
         thumbBar(0),
@@ -65,5 +97,7 @@ public:
     Digikam::SplashScreen*           splash;
     ShowfotoSettings*                settings;
 };
+
 } // namespace Showfoto
+
 #endif // SHOWFOTO_P_H
