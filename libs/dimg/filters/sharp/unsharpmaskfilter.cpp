3effb5a2be18ffd0c4052287a1377ddc00211be3
Gilles Caulier <caulier.gilles@gmail.com>
2014-04-29 19:49:11 +0200

    UnsharpedMask tool : add Multicore support through QtConcurrent API CCBUGS : 289204

--- libs/dimg/filters/sharp/unsharpmaskfilter.cpp
+++ libs/dimg/filters/sharp/unsharpmaskfilter.cpp
@@ -6,7 +6,7 @@
  * Date        : 2005-17-07
  * Description : A Sharpen threaded image filter.
  *
- * Copyright (C) 2005-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009      by Matthias Welwarsky <matze at welwarsky dot de>
  * Copyright (C) 2010      by Martin Klapetek <martin dot klapetek at gmail dot com>
  *
@@ -32,6 +32,10 @@
 
 #include <cmath>
 #include <cstdlib>
+
+// Qt includes
+
+#include <QtConcurrentRun>
 
 // KDE includes
 
@@ -71,15 +75,84 @@ UnsharpMaskFilter::~UnsharpMaskFilter()
     cancelFilter();
 }
 
-void UnsharpMaskFilter::filterImage()
+void UnsharpMaskFilter::unsharpMaskMultithreaded(int start, int stop, uint y)
 {
-    int      progress;
-    long int quantum;
     long int zero = 0;
-    double   quantumThreshold;
     double   value;
     DColor   p;
     DColor   q;
+
+    long int quantum        = m_destImage.sixteenBit() ? 65535 : 255;
+    double quantumThreshold = quantum * m_threshold;
+
+    for (uint x = start ; runningFlag() && (x < (uint)stop) ; ++x)
+    {
+        p = m_orgImage.getPixelColor(x, y);
+        q = m_destImage.getPixelColor(x, y);
+
+        // Red channel.
+        value = (double)(p.red()) - (double)(q.red());
+
+        if (fabs(2.0 * value) < quantumThreshold)
+        {
+            value = (double)(p.red());
+        }
+        else
+        {
+            value = (double)(p.red()) + value * m_amount;
+        }
+
+        q.setRed(CLAMP(lround(value), zero, quantum));
+
+        // Green Channel.
+        value = (double)(p.green()) - (double)(q.green());
+
+        if (fabs(2.0 * value) < quantumThreshold)
+        {
+            value = (double)(p.green());
+        }
+        else
+        {
+            value = (double)(p.green()) + value * m_amount;
+        }
+
+        q.setGreen(CLAMP(lround(value), zero, quantum));
+
+        // Blue Channel.
+        value = (double)(p.blue()) - (double)(q.blue());
+
+        if (fabs(2.0 * value) < quantumThreshold)
+        {
+            value = (double)(p.blue());
+        }
+        else
+        {
+            value = (double)(p.blue()) + value * m_amount;
+        }
+
+        q.setBlue(CLAMP(lround(value), zero, quantum));
+
+        // Alpha Channel.
+        value = (double)(p.alpha()) - (double)(q.alpha());
+
+        if (fabs(2.0 * value) < quantumThreshold)
+        {
+            value = (double)(p.alpha());
+        }
+        else
+        {
+            value = (double)(p.alpha()) + value * m_amount;
+        }
+
+        q.setAlpha(CLAMP(lround(value), zero, quantum));
+
+        m_destImage.setPixelColor(x, y, q);
+    }
+}
+
+void UnsharpMaskFilter::filterImage()
+{
+    int progress;
 
     if (m_orgImage.isNull())
     {
@@ -89,74 +162,22 @@ void UnsharpMaskFilter::filterImage()
 
     BlurFilter(this, m_orgImage, m_destImage, 0, 10, (int)(m_radius));
 
-    quantum          = m_destImage.sixteenBit() ? 65535 : 255;
-    quantumThreshold = quantum * m_threshold;
+    int nbCore = QThreadPool::globalInstance()->maxThreadCount();
+    int step   = m_destImage.width() / nbCore;
 
     for (uint y = 0 ; runningFlag() && (y < m_destImage.height()) ; ++y)
     {
-        for (uint x = 0 ; runningFlag() && (x < m_destImage.width()) ; ++x)
+        QList <QFuture<void> > tasks;
+
+        for (int j = 0 ; runningFlag() && (j < nbCore) ; ++j)
         {
-            p = m_orgImage.getPixelColor(x, y);
-            q = m_destImage.getPixelColor(x, y);
-
-            // Red channel.
-            value = (double)(p.red()) - (double)(q.red());
-
-            if (fabs(2.0 * value) < quantumThreshold)
-            {
-                value = (double)(p.red());
-            }
-            else
-            {
-                value = (double)(p.red()) + value * m_amount;
-            }
-
-            q.setRed(CLAMP(lround(value), zero, quantum));
-
-            // Green Channel.
-            value = (double)(p.green()) - (double)(q.green());
-
-            if (fabs(2.0 * value) < quantumThreshold)
-            {
-                value = (double)(p.green());
-            }
-            else
-            {
-                value = (double)(p.green()) + value * m_amount;
-            }
-
-            q.setGreen(CLAMP(lround(value), zero, quantum));
-
-            // Blue Channel.
-            value = (double)(p.blue()) - (double)(q.blue());
-
-            if (fabs(2.0 * value) < quantumThreshold)
-            {
-                value = (double)(p.blue());
-            }
-            else
-            {
-                value = (double)(p.blue()) + value * m_amount;
-            }
-
-            q.setBlue(CLAMP(lround(value), zero, quantum));
-
-            // Alpha Channel.
-            value = (double)(p.alpha()) - (double)(q.alpha());
-
-            if (fabs(2.0 * value) < quantumThreshold)
-            {
-                value = (double)(p.alpha());
-            }
-            else
-            {
-                value = (double)(p.alpha()) + value * m_amount;
-            }
-
-            q.setAlpha(CLAMP(lround(value), zero, quantum));
-
-            m_destImage.setPixelColor(x, y, q);
+            tasks.append(QtConcurrent::run(this,
+                                           &UnsharpMaskFilter::unsharpMaskMultithreaded,
+                                           j*step, (j+1)*step, y));
         }
+
+        foreach(QFuture<void> t, tasks)
+            t.waitForFinished();
 
         progress = (int)(10.0 + ((double)y * 90.0) / m_destImage.height());
 
--- libs/dimg/filters/sharp/unsharpmaskfilter.h
+++ libs/dimg/filters/sharp/unsharpmaskfilter.h
@@ -6,7 +6,7 @@
  * Date        : 2005-17-07
  * Description : A Unsharp Mask threaded image filter.
  *
- * Copyright (C) 2005-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009      by Matthias Welwarsky <matze at welwarsky dot de>
  * Copyright (C) 2010      by Martin Klapetek <martin dot klapetek at gmail dot com>
  *
@@ -80,7 +80,8 @@ public:
 
 private:
 
-    virtual void filterImage();
+    void filterImage();
+    void unsharpMaskMultithreaded(int start, int stop, uint y);
 
 private:
 
