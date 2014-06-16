ead12109595652b05bc15552ebc2380df4637b2e
Gilles Caulier <caulier.gilles@gmail.com>
2014-06-16 22:43:37 +0200

    fix CR #1223080

--- libs/database/imageinfo.h
+++ libs/database/imageinfo.h
@@ -8,7 +8,7 @@
  *
  * Copyright (C) 2005      by Renchi Raju <renchi dot raju at gmail dot com>
  * Copyright (C) 2007-2013 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
- * Copyright (C) 2009-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2013 by Michael G. Hansen <mike at mghansen dot de>
  *
  * This program is free software; you can redistribute it
@@ -481,7 +481,8 @@ inline uint qHash(const ImageInfo& info)
     return info.hash();
 }
 
-DIGIKAM_DATABASE_EXPORT QDebug& operator<<(QDebug& stream, const ImageInfo& info);
+//! kDebug() stream operator. Writes property @a info to the debug output in a nicely formatted way.
+DIGIKAM_DATABASE_EXPORT QDebug operator<<(QDebug stream, const ImageInfo& info);
 
 }  // namespace Digikam
 
