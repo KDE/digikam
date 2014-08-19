01f4823dbab748fb2ee193f052e6af6ba099faf0
Gilles Caulier <caulier.gilles@gmail.com>
2014-08-19 14:14:02 +0200

    new rule about Canon G1X which is not recognized properly by LensFun

--- libs/dimg/filters/lens/lensfuniface.h
+++ libs/dimg/filters/lens/lensfuniface.h
@@ -4,7 +4,7 @@
  * Description : a tool to fix automatically camera lens aberrations
  *
  * Copyright (C) 2008      by Adrian Schroeter <adrian at suse dot de>
- * Copyright (C) 2008-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -86,7 +86,7 @@ public:
 
     /** Return Camera maker string description found in metadata
     */
-    QString makeDescription() const;
+    QString makeDescription()  const;
 
     /** Return Camera model string description found in metadata
     */
@@ -94,7 +94,7 @@ public:
 
     /** Return Lens string description found in metadata
      */
-    QString lensDescription() const;
+    QString lensDescription()  const;
 
     static QString lensFunVersion();
 
