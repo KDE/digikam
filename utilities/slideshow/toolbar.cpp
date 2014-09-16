1e206e4bb2533279766b6bad17bc8616382c6705
Gilles Caulier <caulier.gilles@gmail.com>
2014-09-16 16:50:35 +0200

    Apply patch #88695 to support arrow key to navigate in slideshow BUGS: 339051 FIXED-IN: 4.4.0

--- utilities/slideshow/toolbar.cpp
+++ utilities/slideshow/toolbar.cpp
@@ -7,7 +7,7 @@
  * Description : a tool bar for slideshow
  *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi dot raju at gmail dot com>
- * Copyright (C) 2006-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -189,6 +189,8 @@ void ToolBar::keyPressEvent(QKeyEvent* e)
             break;
         }
 
+        case (Qt::Key_Left):
+        case (Qt::Key_Up):
         case (Qt::Key_PageUp):
         {
             if (d->prevBtn->isEnabled())
@@ -199,6 +201,8 @@ void ToolBar::keyPressEvent(QKeyEvent* e)
             break;
         }
 
+        case (Qt::Key_Right):
+        case (Qt::Key_Down):
         case (Qt::Key_PageDown):
         {
             if (d->nextBtn->isEnabled())
