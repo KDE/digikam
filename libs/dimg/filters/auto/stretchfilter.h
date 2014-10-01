dcc20905a9aa8504e6b73e5fe0785245c060d4d2
Gilles Caulier <caulier.gilles@gmail.com>
2014-10-01 11:51:03 +0200

    polish

--- libs/dimg/filters/auto/stretchfilter.h
+++ libs/dimg/filters/auto/stretchfilter.h
@@ -6,7 +6,7 @@
  * Date        : 2005-24-01
  * Description : stretch contrast image filter.
  *
- * Copyright (C) 2005-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -30,8 +30,6 @@
 #include "dimgthreadedfilter.h"
 #include "globals.h"
 
-using namespace Digikam;
-
 namespace Digikam
 {
 
@@ -41,6 +39,7 @@ class DIGIKAM_EXPORT StretchFilter : public DImgThreadedFilter
 {
 
 public:
+
     explicit StretchFilter(QObject* const parent = 0);
     StretchFilter(DImg* const orgImage, const DImg* const refImage, QObject* const parent=0);
     virtual ~StretchFilter();
