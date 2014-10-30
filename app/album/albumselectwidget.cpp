a9a7b63862ab351bbc1d51df57bf9412c7736c27
Gilles Caulier <caulier.gilles@gmail.com>
2014-10-30 08:04:55 +0100

    polish

--- app/album/albumselectwidget.cpp
+++ app/album/albumselectwidget.cpp
@@ -6,7 +6,7 @@
  * Date        : 2009-26-02
  * Description : a widget to select a physical album
  *
- * Copyright (C) 2009-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009-2010 by Johannes Wienke <languitar at semipol dot de>
  *
  * This program is free software; you can redistribute it
@@ -49,8 +49,6 @@
 #include "albumthumbnailloader.h"
 #include "collectionmanager.h"
 #include "contextmenuhelper.h"
-#include "kipiinterface.h"
-#include "kipiimagecollection.h"
 
 namespace Digikam
 {
