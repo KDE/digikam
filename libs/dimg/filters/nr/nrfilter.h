291d5c507322cd66482316980a4a5ebcdbeb57dc
Gilles Caulier <caulier.gilles@gmail.com>
2014-12-18 08:09:53 -0500

    polish

--- libs/dimg/filters/nr/nrfilter.h
+++ libs/dimg/filters/nr/nrfilter.h
@@ -29,7 +29,7 @@
 // Qt includes
 
 #include <QMetaType>
-
+#include <QDebug>
 
 // Local includes
 
--- utilities/importui/backend/camiteminfo.h
+++ utilities/importui/backend/camiteminfo.h
@@ -7,7 +7,7 @@
  * Description : camera item info container
  *
  * Copyright (C) 2004-2005 by Renchi Raju <renchi dot raju at gmail dot com>
- * Copyright (C) 2006-2013 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2006-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -30,7 +30,7 @@
 #include <QList>
 #include <QByteArray>
 #include <QDateTime>
-
+#include <QDebug>
 
 // KDE includes
 
