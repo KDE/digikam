cbb3ef0291dd9bb1fb05d7456c090151eac6fff2
Gilles Caulier <caulier.gilles@gmail.com>
2014-12-19 21:30:31 +0100

    fix (c)

--- utilities/kipiiface/kipiuploadwidget.cpp
+++ utilities/kipiiface/kipiuploadwidget.cpp
@@ -7,7 +7,7 @@
  * Description : a widget to select an image collection
  *               to upload new items using digiKam album folder views
  *
- * Copyright (C) 2008-2012 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009      by Luka Renko <lure at kubuntu dot org>
  *
  * This program is free software; you can redistribute it
