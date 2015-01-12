ad7260ec2e4c1393e6ca21f27dee7b152c239ccf
Teemu Rytilahti <tpr@iki.fi>
2015-01-12 03:59:49 +0100

    fix compilation

--- libs/dialogs/dprogressdlg.h
+++ libs/dialogs/dprogressdlg.h
@@ -70,7 +70,7 @@ public Q_SLOTS:
 
     void setButtonText(const QString& text);
     void setButtonGuiItem(const KGuiItem& item);
-    void addedAction(const QIcon &icon, const QString& text);
+    void addedAction(const QPixmap &icon, const QString& text);
     void reset();
 
 protected Q_SLOTS:
