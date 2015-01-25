9c7255e948ec0642eeb8f7066e4e9a4004b21c88
Teemu Rytilahti <tpr@iki.fi>
2015-01-25 03:52:18 +0100

    move cimg to dimg/filters/greycstoration as it is the only user

--- libs/dimg/filters/greycstoration/greycstorationfilter.cpp
+++ libs/dimg/filters/greycstoration/greycstorationfilter.cpp
@@ -48,7 +48,7 @@
 
 #include "dynamicthread.h"
 
-#define cimg_plugin "greycstoration.h"
+#define cimg_plugin "cimg/greycstoration.h"
 
 /** Uncomment this line if you use future GreycStoration implementation with GFact parameter
  */
@@ -69,7 +69,7 @@
 
 // CImg includes
 
-#include "CImg.h"
+#include "cimg/CImg.h"
 
 // Restore warnings
 #if not defined(__APPLE__) && defined(__GNUC__)
