ba33194323212466cd3e2fa0833d81f772e38b2c
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2015-02-07 20:11:01 +0100

    Add license details

--- utilities/qtwrappers/qstandardpathwrap.h
+++ utilities/qtwrappers/qstandardpathwrap.h
@@ -1,3 +1,26 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2015-07-02
+ * Description : QStandardPaths wrapper, to check for /usr/share/apps
+ *
+ *
+ * Copyright (C) 2015 by Veaceslav Munteanu <veaceslav dot munteanu90 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 #ifndef QSTANDARTPATHSWRAP_H_
 #define QSTANDARTPATHSWRAP_H_
 
