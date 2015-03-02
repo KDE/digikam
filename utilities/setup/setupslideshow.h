d70704fed9b4f7eb43b0ab85537bd13147ecaafe
Gilles Caulier <caulier.gilles@gmail.com>
2015-03-02 23:23:27 +0100

    use Qt macro

--- utilities/setup/setupslideshow.h
+++ utilities/setup/setupslideshow.h
@@ -6,7 +6,7 @@
  * Date        : 2005-05-21
  * Description : setup tab for slideshow options.
  *
- * Copyright (C) 2005-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -46,7 +46,7 @@ public:
 
     void applySettings();
 
-public slots:
+public Q_SLOTS:
 
     void slotSetUnchecked(int);
 
