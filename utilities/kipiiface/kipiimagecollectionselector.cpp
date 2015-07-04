4681741484a7f4a5cd39c5e969e50fa914eed789
Gilles Caulier <caulier.gilles@gmail.com>
2015-07-04 16:10:13 +0200

    Missing to manage labels selector tab code simplification

--- utilities/kipiiface/kipiimagecollectionselector.cpp
+++ utilities/kipiiface/kipiimagecollectionselector.cpp
@@ -7,7 +7,7 @@
  * Description : a widget to select image collections using
  *               digiKam album folder views
  *
- * Copyright (C) 2008-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2014      by Mohamed Anwer <mohammed dot ahmed dot anwer at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -291,16 +291,9 @@ QList<KIPI::ImageCollection> KipiImageCollectionSelector::selectedImageCollectio
 
 void KipiImageCollectionSelector::enableVirtualCollections(bool flag)
 {
-    if (flag == false)
-    {
-        d->tab->setTabEnabled(1, false);
-        d->tab->setTabEnabled(2, false);
-    }
-    else
-    {
-        d->tab->setTabEnabled(1, true);
-        d->tab->setTabEnabled(2, true);
-    }
+    d->tab->setTabEnabled(1, flag);
+    d->tab->setTabEnabled(2, flag);
+    d->tab->setTabEnabled(3, flag);
 }
 
 }  // namespace Digikam
