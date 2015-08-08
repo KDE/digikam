4ee11fd83b9fe116c5ca285c8c44c58a4fc10652
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2015-08-08 18:15:28 +0200

    Adding Missing License

--- app/views/trashview.cpp
+++ app/views/trashview.cpp
@@ -1,9 +1,35 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-08-07
+ * Description : Trash view
+ *
+ * Copyright (C) 2015 by Mohamed Anwer <m dot anwer at gmx dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include "trashview.h"
+
+// Qt includes
 
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QTableView>
 #include <QPushButton>
+
+// KDE includes
 
 #include "klocalizedstring.h"
 
--- app/views/trashview.h
+++ app/views/trashview.h
@@ -1,3 +1,25 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2010-08-07
+ * Description : central Map view
+ *
+ * Copyright (C) 2015 by Mohamed Anwer <m dot anwer at gmx dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef TRASHVIEW_H
 #define TRASHVIEW_H
 
