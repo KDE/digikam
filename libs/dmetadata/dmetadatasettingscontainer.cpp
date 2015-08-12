392d268af8f878d24c34cbf24d0028f74f995710
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2015-08-12 17:05:12 +0200

    Add copyright

--- libs/dmetadata/dmetadatasettingscontainer.cpp
+++ libs/dmetadata/dmetadatasettingscontainer.cpp
@@ -3,10 +3,10 @@
  * This file is a part of digiKam project
  * http://www.digikam.org
  *
- * Date        : 2015-06-22
- * Description : Metadata Settings Container.
+ * Date        : 2015-08-12
+ * Description : DMetadata Settings Container.
  *
- * Copyright (C) 2010-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2015 by Veaceslav Munteanu <veaceslav dot munteanu90 at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
