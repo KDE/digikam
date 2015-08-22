1033742123fa823705a6f92fe7803b5530f70e91
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2015-08-22 12:57:40 +0200

    Add metadata status bar class

---
+++ libs/fileactionmanager/metadatastatusbar.h
@@ -0,0 +1,64 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2015-08-21
+ * Description : a bar to indicate pending metadata
+ *
+ * Copyright (C) 2015 by Veaceslav Munteanu <veaceslav dot munteanu90 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef METADATASTATUSBAR_H
+#define METADATASTATUSBAR_H
+
+// Qt includes
+
+#include <QWidget>
+
+// Local includes
+#include "metadatahubmngr.h"
+
+namespace Digikam
+{
+
+class MetadataStatusBar : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit MetadataStatusBar(QWidget* const parent);
+    ~MetadataStatusBar();
+
+public Q_SLOTS:
+
+//    void slotFilterMatches(bool);
+    void slotSettingsChanged();
+
+//Q_SIGNALS:
+
+//    void signalResetFilters();
+//    void signalPopupFiltersView();
+
+private:
+
+    class Private;
+    Private* const d;
+};
+
+}  // namespace Digikam
+
+#endif // METADATASTATUSBAR_H
