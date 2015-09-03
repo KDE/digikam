9f55ddddd74214711d6f8ca9fa8b6e69311216df
Gilles Caulier <caulier.gilles@gmail.com>
2015-09-03 14:32:35 +0200

    polish

--- libs/dimg/filters/icc/iccsettings.cpp
+++ libs/dimg/filters/icc/iccsettings.cpp
@@ -7,7 +7,7 @@
  * Description : central place for ICC settings
  *
  * Copyright (C) 2005-2006 by F.J. Cruz <fj dot cruz at supercable dot es>
- * Copyright (C) 2005-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2005-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2009-2011 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  *
  * This program is free software; you can redistribute it
@@ -186,7 +186,7 @@ bool IccSettings::monitorProfileFromSystem() const
  *                2001 John Califf
  *                2004 Boudewijn Rempt <boud@valdyas.org>
  *  Copyright (C) 2007 Thomas Zander <zander@kde.org>
- *  Copyright (C) 2007 Adrian Page <adrian@pagenet.plus.com>IccProfile Private::profileForScreen(QWidget *widget)
+ *  Copyright (C) 2007 Adrian Page <adrian@pagenet.plus.com>
 */
 IccProfile IccSettings::Private::profileFromWindowSystem(QWidget* const widget)
 {
@@ -196,6 +196,7 @@ IccProfile IccSettings::Private::profileFromWindowSystem(QWidget* const widget)
     QString    atomName;
 
     QDesktopWidget* const desktop = QApplication::desktop();
+
     if (!desktop)
     {
         kError() << "No desktop widget available for application";
