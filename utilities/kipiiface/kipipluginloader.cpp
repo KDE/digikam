2d0fe141a64ebfaa31109e04f9e5921ba7addce1
Gilles Caulier <caulier.gilles@gmail.com>
2015-09-12 22:16:44 +0200

    disable MetadataEdit plugin to be loaded with Kipi

--- utilities/kipiiface/kipipluginloader.cpp
+++ utilities/kipiiface/kipipluginloader.cpp
@@ -130,6 +130,7 @@ void KipiPluginLoader::Private::loadPlugins()
     ignores.append(QLatin1String("GPSSync"));        // Renamed as GeoLocator
     ignores.append(QLatin1String("DNGConverter"));   // Moved to digiKam core
     ignores.append(QLatin1String("AcquireImages"));  // Moved to digiKam core
+    ignores.append(QLatin1String("MetadataEdit"));   // Moved to digiKam core
 
     // List of obsolete tool actions to not load
 
