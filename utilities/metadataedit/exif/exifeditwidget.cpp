942cf8b39a23a0fcbd7a607fff4c7b1660a1e693
Gilles Caulier <caulier.gilles@gmail.com>
2015-09-13 20:37:03 +0200

    polish

--- utilities/metadataedit/exif/exifeditwidget.cpp
+++ utilities/metadataedit/exif/exifeditwidget.cpp
@@ -8,7 +8,6 @@
  *
  * Copyright (C) 2006-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2011      by Victor Dodon <dodon dot victor at gmail dot com>
-
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -102,7 +101,8 @@ public:
 };
 
 EXIFEditWidget::EXIFEditWidget(MetadataEditDialog* const parent)
-    : KPageWidget(parent), d(new Private)
+    : KPageWidget(parent),
+      d(new Private)
 {
     d->dlg           = parent;
 
@@ -197,12 +197,7 @@ void EXIFEditWidget::slotItemChanged()
     DMetadata meta;
     meta.load((*d->dlg->currentItem()).path());
 
-#if KEXIV2_VERSION >= 0x010000
     d->exifData = meta.getExifEncoded();
-#else
-    d->exifData = meta.getExif();
-#endif
-
     d->iptcData = meta.getIptc();
     d->xmpData  = meta.getXmp();
     d->captionPage->readMetadata(d->exifData);
