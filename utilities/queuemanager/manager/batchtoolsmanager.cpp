a92c3b6a8e16382de651a45301d85046f5e7854b
Gilles Caulier <caulier.gilles@gmail.com>
2015-10-03 23:03:25 +0200

    Remove cmake option to disable DNG converter. The crash from Exiv2 at digikKam startup is due to poor cmake support in Exiv2 project which lack compilation visibility option from compiler. In other words, Exiv2 must be compiler with automake/autoconf instead cmake. This last one is considerated as incomplete/experimental/buggous by Exiv2 team.

--- utilities/queuemanager/manager/batchtoolsmanager.cpp
+++ utilities/queuemanager/manager/batchtoolsmanager.cpp
@@ -32,7 +32,6 @@
 #include "convert2pgf.h"
 #include "convert2png.h"
 #include "convert2tiff.h"
-
 #include "crop.h"
 #include "flip.h"
 #include "resize.h"
@@ -59,6 +58,7 @@
 #include "convert16to8.h"
 #include "border.h"
 #include "removemetadata.h"
+#include "convert2dng.h"
 
 #ifdef HAVE_JASPER
 #include "convert2jp2.h"
@@ -67,10 +67,6 @@
 #ifdef HAVE_LENSFUN
 #include "lensautofix.h"
 #endif // HAVE_LENSFUN
-
-#ifdef HAVE_DNGCONVERTER
-#include "convert2dng.h"
-#endif // HAVE_DNGCONVERTER
 
 namespace Digikam
 {
@@ -116,10 +112,7 @@ BatchToolsManager::BatchToolsManager()
     registerTool(new Convert2JP2(this));
 #endif // HAVE_JASPER
     registerTool(new Convert2PGF(this));
-
-#ifdef HAVE_DNGCONVERTER
     registerTool(new Convert2DNG(this));
-#endif // HAVE_DNGCOVERTER
 
     // Transform
     registerTool(new Rotate(this));
