80dfce159dd19dd7675620569b0466a628d3539a
Gilles Caulier <caulier.gilles@gmail.com>
2015-10-07 20:45:22 +0200

    polish

--- utilities/firstrun/collectionpage.cpp
+++ utilities/firstrun/collectionpage.cpp
@@ -6,7 +6,7 @@
  * Date        : 2009-28-04
  * Description : first run assistant dialog
  *
- * Copyright (C) 2009-2014 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -46,6 +46,7 @@
 #include <kdebug.h>
 
 // Local includes
+
 #include <databaseparameters.h>
 #include "version.h"
 
