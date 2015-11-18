73a028b3801f88a158dbd8779a1927c13e2ba49c
Gilles Caulier <caulier.gilles@gmail.com>
2015-11-18 15:45:14 +0059

    this class is not used

--- libs/database/engine/dbenginebackend.cpp
+++ libs/database/engine/dbenginebackend.cpp
@@ -56,29 +56,6 @@ DbEngineLocking::DbEngineLocking()
 
 // -----------------------------------------------------------------------------------------
 
-// For whatever reason, these methods are "static protected"
-class sotoSleep : public QThread
-{
-public:
-
-    static void sleep(unsigned long secs)
-    {
-        QThread::sleep(secs);
-    }
-
-    static void msleep(unsigned long msecs)
-    {
-        QThread::msleep(msecs);
-    }
-
-    static void usleep(unsigned long usecs)
-    {
-        QThread::usleep(usecs);
-    }
-};
-
-// -----------------------------------------------------------------------------------------
-
 BdEngineBackendPrivate::BusyWaiter::BusyWaiter(BdEngineBackendPrivate* const d)
     : AbstractWaitingUnlocker(d, &d->busyWaitMutex, &d->busyWaitCondVar)
 {
