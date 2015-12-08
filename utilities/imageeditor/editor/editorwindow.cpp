3134c9b68dd982f09f9a4f976bbab271a55b12e8
Maik Qualmann <metzpinguin@gmail.com>
2015-12-08 22:14:40 +0100

    polish new line in string

--- utilities/imageeditor/editor/editorwindow.cpp
+++ utilities/imageeditor/editor/editorwindow.cpp
@@ -1479,7 +1479,7 @@ bool EditorWindow::promptUserDelete(const QUrl& url)
         }
 
         QString boxMessage = i18nc("@info",
-                                   "The image <b>%1</b> has been modified.<nl/>"
+                                   "The image <b>%1</b> has been modified.<br>"
                                    "All changes will be lost.", url.fileName());
 
         int result = DMessageBox::showContinueCancel(QMessageBox::Warning,
