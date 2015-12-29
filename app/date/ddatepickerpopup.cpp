64f827c25c7fe8a85271ea980e0bd7c33a92f9b8
Gilles Caulier <caulier.gilles@gmail.com>
2015-12-29 23:24:49 +0059

    fix (c)

--- app/date/ddatepickerpopup.cpp
+++ app/date/ddatepickerpopup.cpp
@@ -9,7 +9,7 @@
  *
  * Copyright (C) 2004 Bram Schoenmakers <bramschoenmakers@kde.nl>
  * Copyright (C) 2006 Mikolaj Machowski <mikmach@wp.pl>
- * Copyright (C) 2011-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2011-2016 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
