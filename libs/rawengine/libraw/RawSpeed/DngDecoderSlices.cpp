24c1b81a17ddc2bf8a898cc8823234e1affb57db
Gilles Caulier <caulier.gilles@gmail.com>
2016-01-14 07:43:23 +0059

    apply patch #96627 to fix bool cast with libjpeg 9 and RawSpeed BUGS: 357952 FIXED-IN: 5.0.0

--- libs/rawengine/libraw/RawSpeed/DngDecoderSlices.cpp
+++ libs/rawengine/libraw/RawSpeed/DngDecoderSlices.cpp
@@ -177,7 +177,7 @@ void DngDecoderSlices::decodeSlice(DngDecoderThread* t) {
         CHECKSIZE(e.byteOffset+e.byteCount);
         JPEG_MEMSRC(&dinfo, (unsigned char*)mFile->getData(e.byteOffset), e.byteCount);
 
-        if (JPEG_HEADER_OK != jpeg_read_header(&dinfo, TRUE))
+        if (JPEG_HEADER_OK != jpeg_read_header(&dinfo, (boolean)TRUE))
           ThrowRDE("DngDecoderSlices: Unable to read JPEG header");
 
         jpeg_start_decompress(&dinfo);
