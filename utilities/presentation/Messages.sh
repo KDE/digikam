6fce560b3d65b680262b90cd08d64c630b89d47a
Gilles Caulier <caulier.gilles@gmail.com>
2016-03-12 09:40:46 +0059

    rm

--- utilities/presentation/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-$EXTRACTRC `find . -name "*.ui" -o -name "*.rc" -o -name "*.kcfg" ` >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/kipiplugin_advancedslideshow.pot
-rm -f rc.cpp
