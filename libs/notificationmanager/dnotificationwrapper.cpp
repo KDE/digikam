1c16f7e37dc829810cd6ac2cd99b14bf66d39add
Gilles Caulier <caulier.gilles@gmail.com>
2016-04-03 08:04:32 +0200

    fix clang warnings

--- libs/notificationmanager/dnotificationwrapper.cpp
+++ libs/notificationmanager/dnotificationwrapper.cpp
@@ -81,20 +81,28 @@ private:
 
 // ----------------------------------------------------------------------------------------------
 
+#ifdef HAVE_KNOTIFICATIONS
+
 static inline bool detectKDEDesktopIsRunning()
 {
     const QByteArray xdgCurrentDesktop = qgetenv("XDG_CURRENT_DESKTOP");
 
     if (!xdgCurrentDesktop.isEmpty())
+    {
         return (xdgCurrentDesktop.toUpper() == "KDE");
+	}
 
     // Classic fallbacks
 
     if (!qgetenv("KDE_FULL_SESSION").isEmpty())
+    {
         return true;
+	}
 
     return false;
 }
+
+#endif
 
 // ----------------------------------------------------------------------------------------------
 
