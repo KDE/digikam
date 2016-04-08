5e69d41550457014463c178b5878c4fdda1ce6a7
Pino Toscano <pino@kde.org>
2016-04-08 22:29:27 +0200

    i18n: do not extract ui messages in 'tests' subdirs

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui | grep -v '/tests/'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.h -o -name \*.cpp | grep -v '/tests/'` `find app -name \*.h.cmake.in` -o $podir/digikam.pot
 rm -f rc.cpp
