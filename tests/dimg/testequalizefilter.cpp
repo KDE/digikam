1326c209677436c48af421a8e8b6248f5464f82f
Gilles Caulier <caulier.gilles@gmail.com>
2016-04-08 11:01:40 +0159

    add new test code for dimg equalize filter.

---
+++ tests/dimg/testequalizefilter.cpp
@@ -0,0 +1,66 @@
+/** ===========================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * @date   2016-04-08
+ * @brief  a command line tool to test DImg equalize filter
+ *
+ * @author Copyright (C) 2016 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include <QFileInfo>
+#include <QDebug>
+
+// Local includes
+
+#include "metaengine.h"
+#include "equalizefilter.h"
+#include "dimg.h"
+#include "dimgthreadedfilter.h"
+
+using namespace Digikam;
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        qDebug() << "testdimgloader - test DImg image loader";
+        qDebug() << "Usage: <image>";
+        return -1;
+    }
+
+    MetaEngine::initializeExiv2();
+
+    QFileInfo input(QString::fromUtf8(argv[1]));
+    QString   outFilePath(input.baseName() + QLatin1String(".out.png"));
+
+    DRawDecoderSettings settings;
+    settings.halfSizeColorImage    = false;
+    settings.sixteenBitsImage      = false;
+    settings.RGBInterpolate4Colors = false;
+    settings.RAWQuality            = DRawDecoderSettings::BILINEAR;
+
+    DImg img(input.filePath(), 0, DRawDecoding(settings));
+    EqualizeFilter filter(&img, &img);
+    filter.startFilterDirectly();
+    img.putImageData(filter.getTargetImage().bits());
+    img.save(outFilePath, QLatin1String("PNG"));
+
+    MetaEngine::cleanupExiv2();
+
+    return 0;
+}
