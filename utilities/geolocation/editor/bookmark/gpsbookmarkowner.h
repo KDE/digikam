aa9591a2c66e3fee052810db0f39d5ce1e57f81d
Gilles Caulier <caulier.gilles@gmail.com>
2016-05-14 16:32:10 +0200

    no need this properties here

--- utilities/geolocation/editor/bookmark/gpsbookmarkowner.h
+++ utilities/geolocation/editor/bookmark/gpsbookmarkowner.h
@@ -66,7 +66,7 @@ public:
 
     virtual bool    supportsTabs() const;
     virtual QString currentTitle() const;
-    virtual QUrl    currentUrl()   const Q_DECL_OVERRIDE;
+    virtual QUrl    currentUrl()   const;
     virtual bool enableOption(BookmarkOption option) const;
     virtual void openBookmark(const KBookmark&, Qt::MouseButtons, Qt::KeyboardModifiers);
 
