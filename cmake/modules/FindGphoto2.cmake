401f077455ff1b4537262880b63458cac200c9c9
Gilles Caulier <caulier.gilles@gmail.com>
2016-06-03 06:30:43 +0200

    apply patch #99332 to polish gphoto2 detection CCBUGS: 363638

--- cmake/modules/FindGphoto2.cmake
+++ cmake/modules/FindGphoto2.cmake
@@ -1,8 +1,9 @@
-# cmake macro to detect gphoto2 libraries
+# Cmake macro to detect gphoto2 libraries
+#
 #  GPHOTO2_FOUND - system has the GPHOTO2 library
 #  GPHOTO2_INCLUDE_DIR - the GPHOTO2 include directory
 #  GPHOTO2_LIBRARIES - The libraries needed to use GPHOTO2
-
+#
 # Copyright (c) 2006, 2007 Laurent Montel, <montel@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
@@ -15,19 +16,21 @@ if(GPHOTO2_LIBRARIES AND GPHOTO2_INCLUDE_DIR)
 
 else()
 
-    find_program(GHOTO2CONFIG_EXECUTABLE     NAMES gphoto2-config)
-    find_program(GHOTO2PORTCONFIG_EXECUTABLE NAMES gphoto2-port-config)
+    find_program(GPHOTO2CONFIG_EXECUTABLE     NAMES gphoto2-config)
+    find_program(GPHOTO2PORTCONFIG_EXECUTABLE NAMES gphoto2-port-config)
 
     set(GPHOTO2_LIBRARIES)
     set(GPHOTO2_INCLUDE_DIRS)
 
     # if gphoto2-port-config and gphoto2-config have been found
-    if(GHOTO2PORTCONFIG_EXECUTABLE AND GHOTO2CONFIG_EXECUTABLE)
 
-        exec_program(${GHOTO2PORTCONFIG_EXECUTABLE} ARGS --libs   RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPHOTO2PORT_LIBRARY)
-        exec_program(${GHOTO2CONFIG_EXECUTABLE}     ARGS --libs   RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPHOTO2_LIBRARY)
-        exec_program(${GHOTO2PORTCONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _GPHOTO2PORT_RESULT_INCLUDE_DIR)
-        exec_program(${GHOTO2CONFIG_EXECUTABLE}     ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _GPHOTO2_RESULT_INCLUDE_DIR)
+    if(GPHOTO2PORTCONFIG_EXECUTABLE AND GPHOTO2CONFIG_EXECUTABLE)
+
+        message(STATUS "libgphoto2 execurables check...")
+        exec_program(${GPHOTO2PORTCONFIG_EXECUTABLE} ARGS --libs   RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPHOTO2PORT_LIBRARY)
+        exec_program(${GPHOTO2CONFIG_EXECUTABLE}     ARGS --libs   RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPHOTO2_LIBRARY)
+        exec_program(${GPHOTO2PORTCONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _GPHOTO2PORT_RESULT_INCLUDE_DIR)
+        exec_program(${GPHOTO2CONFIG_EXECUTABLE}     ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _GPHOTO2_RESULT_INCLUDE_DIR)
 
         set(GPHOTO2_LIBRARIES ${GPHOTO2PORT_LIBRARY} ${GPHOTO2_LIBRARY})
 
@@ -35,24 +38,42 @@ else()
         separate_arguments(_GPHOTO2_RESULT_INCLUDE_DIR)
 
         foreach(_includedir ${_GPHOTO2_RESULT_INCLUDE_DIR})
+
             string(REGEX REPLACE "-I(.+)" "\\1" _includedir "${_includedir}")
             set(GPHOTO2_INCLUDE_DIR ${GPHOTO2_INCLUDE_DIR} ${_includedir})
+
         endforeach()
 
         separate_arguments(_GPHOTO2PORT_RESULT_INCLUDE_DIR)
 
         foreach(_includedir ${_GPHOTO2PORT_RESULT_INCLUDE_DIR})
+
             string(REGEX REPLACE "-I(.+)" "\\1" _includedir "${_includedir}")
             set(GPHOTO2PORT_INCLUDE_DIR ${GPHOTO2PORT_INCLUDE_DIR} ${_includedir})
+
         endforeach()
 
         set(GPHOTO2_INCLUDE_DIRS ${GPHOTO2PORT_INCLUDE_DIR} ${GPHOTO2_INCLUDE_DIR} )
 
+    else()
+
+        message(WARNING "Could not find gphoto2-config and/or \
+                         gphoto2-port-config executables.")
+
+        set(GPHOTO2_FOUND FALSE)
+
     endif()
 
     if(GPHOTO2_LIBRARIES AND GPHOTO2_INCLUDE_DIRS)
+
         set(GPHOTO2_FOUND TRUE)
         message(STATUS "Found gphoto2: ${GPHOTO2_LIBRARIES}")
+
+    else()
+
+        message(WARNING "Could not find gphoto2 libraries.")
+        set(GPHOTO2_FOUND FALSE)
+
     endif()
 
 endif()
