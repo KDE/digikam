957b8fe53fd55576ca0ad085fe321bb3e3d91cb8
Yuri Chornoivan <yurchor@ukr.net>
2016-06-22 20:41:28 +0300

    Fix typo in tag

--- libs/database/utils/dbsettingswidget.cpp
+++ libs/database/utils/dbsettingswidget.cpp
@@ -341,7 +341,7 @@ void DatabaseSettingsWidget::setupMainArea()
     d->dbNoticeBox           = new QGroupBox(i18n("Database Server Instructions"), this);
     QVBoxLayout* const vlay2 = new QVBoxLayout(d->dbNoticeBox);
     QLabel* const notice     = new QLabel(i18n("<p>digiKam expects that database is already created with a dedicated user account. "
-                                               "This user name <i>digikam<i> will require full access to the database.<br>"
+                                               "This user name <i>digikam</i> will require full access to the database.<br>"
                                                "If your database is not already set up, you can use the following SQL commands "
                                                "(after replacing the <b><i>password</i></b> with the correct one).<br>"),
                                           d->dbNoticeBox);
