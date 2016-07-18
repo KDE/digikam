a69a18a272346630d4abb63e899e8859f35dbe0f
Gilles Caulier <caulier.gilles@gmail.com>
2016-07-18 10:34:46 +0200

    fix OpenCV warnings with clang

--- libs/facesengine/opencv3-face/facerec.cpp
+++ libs/facesengine/opencv3-face/facerec.cpp
@@ -15,8 +15,20 @@
  *
  *   See <http://www.opensource.org/licenses/bsd-license>
  */
+
+// Pragma directives to reduce warnings from OpenCv header files.
+#if defined(__APPLE__) && defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-align"
+#endif
+
 #include "precomp.hpp"
 #include "face.hpp"
+
+// Restore warnings
+#if defined(__APPLE__) && defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
 namespace cv
 {
