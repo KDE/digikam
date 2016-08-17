5628f3fdd2db49c4554856de2060ee2a8b9cca32
Gilles Caulier <caulier.gilles@gmail.com>
2016-08-17 10:34:40 -0400

    code review and polish

--- libs/facesengine/shape-predictor/vectoroperations.h
+++ libs/facesengine/shape-predictor/vectoroperations.h
@@ -33,7 +33,7 @@ inline T length_squared(const std::vector<T>& diff)
 {
     T sum = 0;
 
-    for(unsigned int i = 0 ; i < diff.size() ; i++)
+    for (unsigned int i = 0 ; i < diff.size() ; i++)
     {
         sum += diff[i] * diff[i];
     }
@@ -47,10 +47,12 @@ std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2)
     assert(v1.size() == v2.size());
 
     std::vector<T> result(v1.size());
-    for(unsigned int i = 0;i<v1.size();i++)
+
+    for (unsigned int i = 0 ; i < v1.size() ; i++)
     {
         result[i] = v1[i] - v2[i];
     }
+
     return result;
 }
 
@@ -58,10 +60,12 @@ template<class T>
 std::vector<T> operator-(const std::vector<T>& v1)
 {
     std::vector<T> result(v1.size());
-    for(unsigned int i = 0;i<v1.size();i++)
+
+    for (unsigned int i = 0 ; i < v1.size() ; i++)
     {
         result[i] = -v1[i];
     }
+
     return result;
 }
 
@@ -69,10 +73,12 @@ template<class T>
 std::vector<T> operator/(const std::vector<T>& v1, int divisor)
 {
     std::vector<T> result(v1.size());
-    for(unsigned int i = 0;i<v1.size();i++)
+
+    for (unsigned int i = 0 ; i < v1.size() ; i++)
     {
-        result[i] = v1[i]/divisor;
+        result[i] = v1[i] / divisor;
     }
+
     return result;
 }
 
@@ -82,14 +88,17 @@ std::vector<std::vector<T> > operator/(const std::vector<std::vector<T> >& v1, i
     //assert(v1[0].size() != v2.size());
 
     std::vector<std::vector<T> > result(v1.size(),std::vector<T>(v1[0].size(),0));
-    for(unsigned int i = 0;i<v1.size();i++)
+
+    for (unsigned int i = 0 ; i < v1.size() ; i++)
     {
-        for(unsigned int j = 0;j<v1[0].size();j++)
+        for(unsigned int j = 0 ; j < v1[0].size() ; j++)
+        {
             result[i][j] = v1[i][j] / divisor;
+        }
     }
+
     return result;
 }
-
 
 template<class T>
 std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2)
@@ -97,105 +106,111 @@ std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2)
     assert(v1.size() == v2.size());
 
     std::vector<T> result(v1.size());
-    for(unsigned int i = 0;i<v1.size();i++)
+
+    for(unsigned int i = 0 ; i < v1.size() ; i++)
     {
         result[i] = v1[i] + v2[i];
     }
+
     return result;
 }
 
 template<class T>
-std::vector<std::vector<T> > operator+(const std::vector<std::vector<T> >& v1,
-                                       const std::vector<std::vector<T> >& v2)
+std::vector<std::vector<T> > operator+(const std::vector<std::vector<T> >& v1, const std::vector<std::vector<T> >& v2)
 {
-    assert(v1.size()    == v2.size() &&
+    assert(v1.size()    == v2.size()     &&
            v1[0].size() == v2[0].size());
 
-    std::vector<std::vector<T> > result(v1.size(),
-                                             std::vector<T>(v1[0].size(),0));
-    for(unsigned int i = 0;i<v1.size();i++)
+    std::vector<std::vector<T> > result(v1.size(), std::vector<T>(v1[0].size(),0));
+
+    for(unsigned int i = 0 ; i < v1.size() ; i++)
     {
-        for(unsigned int j = 0;j<v2[0].size();j++)
+        for(unsigned int j = 0 ; j < v2[0].size() ; j++)
         {
             result[i][j] += v1[i][j] + v2[i][j];
         }
 
     }
+
     return result;
 }
 
 template<class T>
-std::vector<T> operator*(const std::vector<std::vector<T> >& v1,
-                         const std::vector<T>& v2)
+std::vector<T> operator*(const std::vector<std::vector<T> >& v1, const std::vector<T>& v2)
 {
     assert(v1[0].size() == v2.size());
 
     std::vector<T> result(v1.size());
-    for(unsigned int i = 0;i<v1.size();i++)
+
+    for(unsigned int i = 0 ; i < v1.size() ; i++)
     {
         result[i] = 0;
-        for(unsigned int j = 0;j<v1[0].size();j++)
+
+        for(unsigned int j = 0 ; j < v1[0].size() ; j++)
+        {
             result[i] += v1[i][j] * v2[j];
+        }
     }
+
     return result;
 }
 
 template<class T>
-std::vector<std::vector<T> > operator*(const std::vector<std::vector<T> >& v1,
-                                       const std::vector<std::vector<T> >& v2)
+std::vector<std::vector<T> > operator*(const std::vector<std::vector<T> >& v1, const std::vector<std::vector<T> >& v2)
 {
     assert(v1[0].size() == v2.size());
 
-    std::vector<std::vector<T> > result(v1.size(),
-                                             std::vector<T>(v2[0].size(),0));
-    for(unsigned int i = 0;i<v1.size();i++)
+    std::vector<std::vector<T> > result(v1.size(), std::vector<T>(v2[0].size(),0));
+
+    for(unsigned int i = 0 ; i < v1.size() ; i++)
     {
-        for(unsigned int k = 0;k<v1[0].size();k++)
+        for(unsigned int k = 0 ; k < v1[0].size() ; k++)
         {
             // swapping j and k loops for cache optimization
-            for(unsigned int j = 0;j<v2[0].size();j++)
+
+            for(unsigned int j = 0 ; j < v2[0].size() ; j++)
             {
 
                 result[i][j] += v1[i][k] * v2[k][j];
             }
         }
     }
+
     return result;
 }
 
 
 template<class T>
-std::vector<std::vector<T> > operator*(const std::vector<T>& v1,
-                                       const std::vector<T>& v2)
+std::vector<std::vector<T> > operator*(const std::vector<T>& v1, const std::vector<T>& v2)
 {
     assert(v1.size() == v2.size());
 
-    std::vector<std::vector<T> > result(v1.size(),
-                                             std::vector<T>(v2.size(),0));
-    for(unsigned int i = 0;i<v1.size();i++)
+    std::vector<std::vector<T> > result(v1.size(), std::vector<T>(v2.size(), 0));
+
+    for(unsigned int i = 0 ; i < v1.size() ; i++)
     {
-        for(unsigned int j = 0;j<v1.size();j++)
+        for(unsigned int j = 0 ; j < v1.size() ; j++)
         {
-            result[i][j] = v1[i]*v2[j];
+            result[i][j] = v1[i] * v2[j];
         }
     }
+
     return result;
 }
 
 template<class T>
-std::vector<std::vector<T> > operator+(const std::vector<std::vector<T> >& v1,
-                                       float d)
+std::vector<std::vector<T> > operator+(const std::vector<std::vector<T> >& v1, float d)
 {
 //    assert(v1.size()    == v2.size() &&
 //           v1[0].size() == v2[0].size());
 
-    std::vector<std::vector<T> > result(v1.size(),
-                                             std::vector<T>(v1[0].size(),0));
-    for(unsigned int i = 0;i<v1.size();i++)
+    std::vector<std::vector<T> > result(v1.size(), std::vector<T>(v1[0].size(), 0));
+
+    for(unsigned int i = 0 ; i < v1.size() ; i++)
     {
-        for(unsigned int j = 0;j<v1[0].size();j++)
+        for(unsigned int j = 0 ; j < v1[0].size() ; j++)
         {
-            result[i][j] = v1[i][j]*d;
+            result[i][j] = v1[i][j] * d;
         }
 
     }
@@ -204,14 +219,14 @@ std::vector<std::vector<T> > operator+(const std::vector<std::vector<T> >& v1,
 }
 
 template<class T>
-std::vector<T> operator*(const std::vector<T>& v1,
-                                       float d)
+std::vector<T> operator*(const std::vector<T>& v1, float d)
 {
 //    assert(v1.size()    == v2.size() &&
 //           v1[0].size() == v2[0].size());
 
     std::vector<T> result(v1.size());
-    for(unsigned int i = 0;i<v1.size();i++)
+
+    for(unsigned int i = 0 ; i < v1.size() ; i++)
     {
         result[i] = v1[i] * d;
 
