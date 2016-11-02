4785839763500edd3508a74e855940700859adba
Gilles Caulier <caulier.gilles@gmail.com>
2016-11-02 15:28:19 +0100

    more universal solution to load Lens database XML files

--- libs/dimg/filters/lens/lensfuniface.cpp
+++ libs/dimg/filters/lens/lensfuniface.cpp
@@ -4,7 +4,7 @@
  * Description : a tool to fix automatically camera lens aberrations
  *
  * Copyright (C) 2008      by Adrian Schroeter <adrian at suse dot de>
- * Copyright (C) 2008-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2008-2016 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -63,9 +63,7 @@ public:
 LensFunIface::LensFunIface()
     : d(new Private)
 {
-    d->lfDb      = lf_db_new();
-
-#ifdef Q_OS_WIN
+    d->lfDb          = lf_db_new();
     QString lensPath = QStandardPaths::locate(QStandardPaths::GenericDataLocation,
                                               QLatin1String("lensfun"),
                                               QStandardPaths::LocateDirectory);
@@ -74,11 +72,11 @@ LensFunIface::LensFunIface()
 
     foreach(const QString& lens, lensDir.entryList())
     {
+        qCDebug(DIGIKAM_DIMG_LOG) << "Load Lens Database file: " << lens;
         d->lfDb->Load(QFile::encodeName(lensDir.absoluteFilePath(lens)).constData());
     }
-#else
+
     d->lfDb->Load();
-#endif
 
     d->lfCameras = d->lfDb->GetCameras();
 }
