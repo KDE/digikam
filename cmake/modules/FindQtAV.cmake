81c258a9fc94d65447126ed2a68cfb7316dc4cac
Gilles Caulier <caulier.gilles@gmail.com>
2016-12-10 19:10:16 +0100

    add new cmake find script for QtAV libraries

---
+++ cmake/modules/FindQtAV.cmake
@@ -0,0 +1,43 @@
+# - Try to find the QtAV library
+#
+# Once done this will define
+#
+#  QTAV_FOUND        - system has libqtav
+#  QTAV_INCLUDE_DIRS - the libqtav include directory
+#  QTAV_LIBRARIES    - Link these to use libqtav
+#
+# Copyright (c) 2016-2017, Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+find_package(Qt5
+             QUIET REQUIRED NO_MODULE COMPONENTS
+             Core
+)
+
+find_path(QTAV_CORE_INCLUDE_DIR NAMES QtAV/QtAV.h
+          HINTS
+          ${_qt5_install_prefix}
+)
+
+find_path(QTAV_WIDGETS_INCLUDE_DIR NAMES QtAVWidgets/QtAVWidgets.h
+          HINTS
+          ${_qt5_install_prefix}
+)
+
+find_library(QTAV_CORE_LIBRARY NAMES QtAV libQtAV)
+
+find_library(QTAV_WIDGETS_LIBRARY NAMES QtAVWidgets libQtAVWidgets)
+
+set(QTAV_INCLUDE_DIRS "${QTAV_CORE_INCLUDE_DIR} ${QTAV_WIDGETS_INCLUDE_DIR}")
+set(QTAV_LIBRARIES    "${QTAV_CORE_LIBRARY} ${QTAV_WIDGETS_LIBRARY}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(QtAV REQUIRED_VARS QTAV_LIBRARIES QTAV_INCLUDE_DIRS)
+
+mark_as_advanced(QTAV_INCLUDE_DIRS QTAV_LIBRARIES)
+
+message(STATUS "QtAV_FOUND       = ${QTAV_FOUND}")
+message(STATUS "QtAV_INCLUDE_DIR = ${QTAV_INCLUDE_DIRS}")
+message(STATUS "QtAV_LIBRARIES   = ${QTAV_LIBRARIES}")
