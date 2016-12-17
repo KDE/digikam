98c5ad3bf819e4bfc428837f4bbd7f5b3a140dc3
Gilles Caulier <caulier.gilles@gmail.com>
2016-12-17 17:21:52 +0100

    now it compile and link under MacOS

--- cmake/modules/FindQtAV.cmake
+++ cmake/modules/FindQtAV.cmake
@@ -18,42 +18,48 @@ find_package(Qt5
 
 Message(STATUS "QtAV search path: ${_qt5_install_prefix}")
 
-find_path(QTAV_CORE_INCLUDE_DIR
-          NAMES QtAV.h
-          HINTS ${_qt5_install_prefix}                                           # For MXE
-                ${_qt5_install_prefix}/../../include/qt5                         # For Suse
-                ${_qt5_install_prefix}/../../../include/x86_64-linux-gnu/qt5     # For Debian
-                ${_qt5_install_prefix}/../QtAV.framework/Headers                 # For Macports
-          PATH_SUFFIXES QtAV
-)
+if (NOT APPLE)
+    find_path(QTAV_CORE_INCLUDE_DIR
+              NAMES QtAV.h
+              HINTS ${_qt5_install_prefix}                                           # For MXE
+                    ${_qt5_install_prefix}/../../include/qt5                         # For Suse
+                    ${_qt5_install_prefix}/../../../include/x86_64-linux-gnu/qt5     # For Debian
+              PATH_SUFFIXES QtAV
+    )
 
-find_path(QTAV_WIDGETS_INCLUDE_DIR
-          NAMES QtAVWidgets.h
-          HINTS ${_qt5_install_prefix}                                           # For MXE
-                ${_qt5_install_prefix}/../../include/qt5                         # For Suse
-                ${_qt5_install_prefix}/../../../include/x86_64-linux-gnu/qt5     # For Debian
-                ${_qt5_install_prefix}/../QtAVWidgets.framework/Headers          # For Macports
-          PATH_SUFFIXES QtAVWidgets
-)
+    find_path(QTAV_WIDGETS_INCLUDE_DIR
+              NAMES QtAVWidgets.h
+              HINTS ${_qt5_install_prefix}                                           # For MXE
+                    ${_qt5_install_prefix}/../../include/qt5                         # For Suse
+                    ${_qt5_install_prefix}/../../../include/x86_64-linux-gnu/qt5     # For Debian
+              PATH_SUFFIXES QtAVWidgets
+    )
 
-find_library(QTAV_CORE_LIBRARY
-             NAMES QtAV
-                   QtAV1
-                   libQtAV
-                   libQtAV1
-             HINTS ${_qt5_install_prefix}/../
-)
+    find_library(QTAV_CORE_LIBRARY
+                 NAMES QtAV
+                       QtAV1
+                       libQtAV
+                       libQtAV1
+                 HINTS ${_qt5_install_prefix}/../
+    )
 
-find_library(QTAV_WIDGETS_LIBRARY
-             NAMES QtAVWidgets
-                   QtAVWidgets1
-                   libQtAVWidgets
-                   libQtAVWidgets1
-             HINTS ${_qt5_install_prefix}/../
-)
+    find_library(QTAV_WIDGETS_LIBRARY
+                 NAMES QtAVWidgets
+                       QtAVWidgets1
+                       libQtAVWidgets
+                       libQtAVWidgets1
+                 HINTS ${_qt5_install_prefix}/../
+    )
 
-set(QTAV_INCLUDE_DIRS "${QTAV_CORE_INCLUDE_DIR} ${QTAV_WIDGETS_INCLUDE_DIR}")
-set(QTAV_LIBRARIES     ${QTAV_CORE_LIBRARY} ${QTAV_WIDGETS_LIBRARY})
+    set(QTAV_INCLUDE_DIRS "${QTAV_CORE_INCLUDE_DIR} ${QTAV_WIDGETS_INCLUDE_DIR}")
+    set(QTAV_LIBRARIES     ${QTAV_CORE_LIBRARY} ${QTAV_WIDGETS_LIBRARY})
+
+else()
+
+    set(QTAV_INCLUDE_DIRS ${_qt5_install_prefix}/../../include/QtAV ${_qt5_install_prefix}/../../include/QtAVWidgets)
+    set(QTAV_LIBRARIES    "${_qt5_install_prefix}/../lib/QtAV.framework/QtAV;${_qt5_install_prefix}/../lib/QtAVWidgets.framework/QtAVWidgets")
+
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(QtAV REQUIRED_VARS QTAV_LIBRARIES QTAV_INCLUDE_DIRS)
