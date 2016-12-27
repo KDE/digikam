46bd190216c3ef5e4d1c36392ba0f77eb45eb0b0
Gilles Caulier <caulier.gilles@gmail.com>
2016-12-27 17:17:22 +0100

    permit to show video on fullscreen through icon-view item overlay.

--- app/items/overlays/imagefsoverlay.cpp
+++ app/items/overlays/imagefsoverlay.cpp
@@ -6,7 +6,8 @@
  * Date        : 2015-08-30
  * Description : fullscreen overlay
  *
- * Copyright (C) 2015 by Luca Carlon <carlon dot luca at gmail dot com>
+ * Copyright (C)      2015 by Luca Carlon <carlon dot luca at gmail dot com>
+ * Copyright (C) 2015-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -109,7 +110,9 @@ void ImageFsOverlay::slotClicked()
 bool ImageFsOverlay::checkIndex(const QModelIndex& index) const
 {
     ImageInfo info = ImageModel::retrieveImageInfo(index);
-    return (info.category() == DatabaseItem::Image);
+
+    return (info.category() == DatabaseItem::Image ||
+            info.category() == DatabaseItem::Video);
 }
 
 void ImageFsOverlay::widgetEnterEvent()
--- app/items/overlays/imagefsoverlay.h
+++ app/items/overlays/imagefsoverlay.h
@@ -6,7 +6,8 @@
  * Date        : 2015-08-30
  * Description : fullscreen overlay
  *
- * Copyright (C) 2015 by Luca Carlon <carlon dot luca at gmail dot com>
+ * Copyright (C)      2015 by Luca Carlon <carlon dot luca at gmail dot com>
+ * Copyright (C) 2015-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -82,4 +83,4 @@ private Q_SLOTS:
 
 } // namespace Digikam
 
-#endif /* IMAGEFSOVERLAY_H */
+#endif // IMAGEFSOVERLAY_H
