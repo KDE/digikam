f6df1e7ffa7befa3f5bb2ef44fef542bcde02c05
Markus Leuthold <kusi@forum.titlis.org>
2017-01-03 01:53:40 +0100

    allow D&D for clock photo batch queue manager
    Allow to drag'n drop from either BM, digikam main window or from outside digikam to choose a
    clock photo in the time adjust tool
    
    BUGS: 296580
    FIXED-IN: 5.4.0

---
+++ utilities/queuemanager/tools/metadata/detbyclockphotobutton.h
@@ -0,0 +1,53 @@
+/*
+ *  ============================================================
+ *  
+ *   This file is a part of the digikam project
+ *   http://www.digikam.org
+ *  
+ *   Date             : 2017-01-01
+ *   Description : button for choosing time difference photo which accepts drag & drop
+ *  
+ *   Copyright (C) 2017      by Markus Leuthold <kusi at forum dot titlis dot org>
+ *  
+ *   This program is free software; you can redistribute it
+ *   and/or modify it under the terms of the GNU General
+ *   Public License as published by the Free Software Foundation;
+ *   either version 2, or (at your option) any later version.
+ *  
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  
+ * ============================================================ */ 
+
+
+#ifndef DETBYCLOCKPHOTOBUTTON_H
+#define DETBYCLOCKPHOTOBUTTON_H
+
+// Qt includes
+
+#include <QWidget>
+#include <QDragEnterEvent>
+#include <QPushButton>
+
+
+class DetByClockPhotoButton :  public QPushButton
+{
+    Q_OBJECT
+    
+public:
+    DetByClockPhotoButton(const QString &text)  : QPushButton(text)
+    {
+        setAcceptDrops(true);
+    };
+    void dragEnterEvent(QDragEnterEvent* event);
+    void dropEvent(QDropEvent* event);
+
+Q_SIGNALS:
+
+    void signalClockPhotoDropped(QUrl);    
+    
+};
+
+#endif // DETBYCLOCKPHOTOBUTTON_H
