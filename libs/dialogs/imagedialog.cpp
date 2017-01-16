cc1501f8cfaf54fb62b8a1ceafecb28c7a36773e
Maik Qualmann <metzpinguin@gmail.com>
2017-01-16 19:27:06 +0100

    try to fix the freeze in the import upload CCBUGS: 373946

--- libs/dialogs/imagedialog.cpp
+++ libs/dialogs/imagedialog.cpp
@@ -334,11 +334,17 @@ void ImageDialogPreview::slotClearPreview()
 DFileIconProvider::DFileIconProvider()
     : QFileIconProvider()
 {
-    m_catcher = new ThumbnailImageCatcher(ThumbnailLoadThread::defaultThread());
+    ThumbnailLoadThread* const thread = new ThumbnailLoadThread;
+    m_catcher                         = new ThumbnailImageCatcher(thread);
 }
 
 DFileIconProvider::~DFileIconProvider()
 {
+    m_catcher->thread()->stopAllTasks();
+    m_catcher->cancel();
+
+    delete m_catcher->thread();
+    delete m_catcher;
 }
 
 QIcon DFileIconProvider::icon(IconType type) const
