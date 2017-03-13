e2977dd77ec3539d8338e6b016c74ebbb902b954
Gilles Caulier <caulier.gilles@gmail.com>
2017-03-13 16:04:59 +0100

    new tool to read metadat from a file and print a resume of value on the console

---
+++ tests/dmetadata/readfromimage.cpp
@@ -0,0 +1,67 @@
+/** ===========================================================
+ *
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ *
+ * @date   2009-06-11
+ * @brief  a command line tool to read metadata from image
+ *
+ * @author Copyright (C) 2009-2017 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt includes
+
+#include <QString>
+#include <QDebug>
+
+// Local includes
+
+#include "dmetadata.h"
+
+using namespace Digikam;
+
+int main (int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        qDebug() << "readfromimage - test to load metadata from image and print main tags";
+        qDebug() << "Usage: <image>";
+        return -1;
+    }
+
+    QString filePath = QString::fromLocal8Bit(argv[1]);
+
+    DMetadata meta;
+    meta.load(filePath);
+
+    qDebug() << "--- Comments   -------------------------------";
+    qDebug() << meta.getImageComments();
+    qDebug() << "--- Titles     -------------------------------";
+    qDebug() << meta.getImageTitles();
+    qDebug() << "--- IPTC info  -------------------------------";
+    qDebug() << meta.getCreatorContactInfo();
+    qDebug() << meta.getIptcCoreLocation();
+    qDebug() << meta.getIptcCoreSubjects();
+    qDebug() << "--- Media info -------------------------------";
+    qDebug() << meta.getPhotographInformation();
+    qDebug() << meta.getVideoInformation();
+    qDebug() << "--- XMP info   -------------------------------";
+    qDebug() << meta.getXmpKeywords();
+    qDebug() << meta.getXmpSubjects();
+    qDebug() << meta.getXmpSubCategories();
+
+    return 0;
+}
