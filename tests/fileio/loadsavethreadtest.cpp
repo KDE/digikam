0c6089c4ae6d55497d88bd4220b0435fa71b8d83
Gilles Caulier <caulier.gilles@gmail.com>
2017-05-11 22:18:52 +0200

    fix header

--- tests/fileio/loadsavethreadtest.cpp
+++ tests/fileio/loadsavethreadtest.cpp
@@ -1,23 +1,21 @@
-/** ===========================================================
+/* ============================================================
  *
  * This file is a part of digiKam project
  * http://www.digikam.org
  *
- * @date   2012-10-23
- * @brief  a command line tool to test DImg image loader
+ * Date        : 2012-10-23
+ * Description : a command line tool to test DImg image loader
  *
- * @author Copyright (C) 2012-2017 by Gilles Caulier
- *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ * Copyright (C) 2012-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
+ * either version 2, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * ============================================================ */
