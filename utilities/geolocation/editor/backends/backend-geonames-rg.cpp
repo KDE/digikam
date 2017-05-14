121b4cb9dac7d9a6181e3db8366ae0f355fc8ef3
Gilles Caulier <caulier.gilles@gmail.com>
2017-05-14 17:40:05 +0200

    fix hearders

--- utilities/geolocation/editor/backends/backend-geonames-rg.cpp
+++ utilities/geolocation/editor/backends/backend-geonames-rg.cpp
@@ -1,25 +1,23 @@
-/** ===========================================================
- * @file
+/* ============================================================
  *
  * This file is a part of digiKam project
- * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ * http://www.digikam.org
  *
- * @date   2010-05-12
- * @brief  Backend for reverse geocoding using geonames.org (non-US)
+ * Date        : 2010-05-12
+ * Description : Backend for reverse geocoding using geonames.org (non-US)
  *
- * @author Copyright (C) 2010 by Michael G. Hansen
- *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
- * @author Copyright (C) 2010 by Gabriel Voicu
- *         <a href="mailto:ping dot gabi at gmail dot com">ping dot gabi at gmail dot com</a>
+ * Copyright (C) 2010 by Michael G. Hansen <mike at mghansen dot de>
+ * Copyright (C) 2010 by Gabriel Voicu <ping dot gabi at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
+ * either version 2, or (at your option)
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * ============================================================ */
