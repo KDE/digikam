040de2dfc1dfb0db88d1bdc178867f50a22a4789
Gilles Caulier <caulier.gilles@gmail.com>
2017-05-14 23:33:05 +0200

    fix header

--- utilities/geolocation/editor/items/gpsimageitemdelegate.h
+++ utilities/geolocation/editor/items/gpsimageitemdelegate.h
@@ -1,23 +1,23 @@
-/** ===========================================================
- * @file
+/* ============================================================
  *
  * This file is a part of digiKam project
- * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ * http://www.digikam.org
  *
- * @date   2010-03-22
- * @brief  A model for the view to display a list of images.
+ * Date        : 2010-03-22
+ * Description : A model for the view to display a list of images.
  *
- * @author Copyright (C) 2010 by Michael G. Hansen
- *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
+ * Copyright (C) 2010-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010      by Michael G. Hansen <mike at mghansen dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
+ * either version 2, or (at your option)
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * ============================================================ */
@@ -57,6 +57,6 @@ private:
     Private* const d;
 };
 
-} /* namespace Digikam */
+} // namespace Digikam
 
-#endif /* GPSIMAGEITEMDELEGATE_H */
+#endif // GPSIMAGEITEMDELEGATE_H
