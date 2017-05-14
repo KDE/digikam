10850de5fd957c072de2a7d5ff8d98b0e547d625
Gilles Caulier <caulier.gilles@gmail.com>
2017-05-14 23:48:40 +0200

    fix header

--- utilities/geolocation/editor/searches/searchresultmodelhelper.h
+++ utilities/geolocation/editor/searches/searchresultmodelhelper.h
@@ -1,23 +1,23 @@
-/** ===========================================================
- * @file
+/* ============================================================
  *
  * This file is a part of digiKam project
- * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ * http://www.digikam.org
  *
- * @date   2010-06-01
- * @brief  A widget to search for places.
+ * Date        : 2010-06-01
+ * Description : A widget to search for places.
  *
- * @author Copyright (C) 2010 by Michael G. Hansen
- *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
+ * Copyright (C) 2010-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010-2011 by Michael G. Hansen <mike at mghansen dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
+ * either version 2, or (at your option)
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * ============================================================ */
@@ -75,6 +75,6 @@ private:
     Private* const d;
 };
 
-} /* namespace Digikam */
+} // namespace Digikam
 
-#endif /* SEARCHRESULTMODELHELPER_H */
+#endif // SEARCHRESULTMODELHELPER_H
