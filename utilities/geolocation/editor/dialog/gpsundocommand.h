87dd4e9c266ef972bbe956ca97df180f31b0b9b0
Gilles Caulier <caulier.gilles@gmail.com>
2017-05-14 23:22:32 +0200

    fix header

--- utilities/geolocation/editor/dialog/gpsundocommand.h
+++ utilities/geolocation/editor/dialog/gpsundocommand.h
@@ -1,26 +1,27 @@
-/** ===========================================================
- * @file
+/* ============================================================
  *
  * This file is a part of digiKam project
- * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ * http://www.digikam.org
  *
- * @date   2010-04-25
- * @brief  A class to hold undo/redo commands.
+ * Date        : 2010-04-25
+ * Description : A class to hold undo/redo commands.
  *
- * @author Copyright (C) 2010 by Michael G. Hansen
- *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
+ * Copyright (C) 2010-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010      by Michael G. Hansen <mike at mghansen dot de>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
+ * either version 2, or (at your option)
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * ============================================================ */
+
 
 #ifndef GPSUNDOCOMMAND_H
 #define GPSUNDOCOMMAND_H
