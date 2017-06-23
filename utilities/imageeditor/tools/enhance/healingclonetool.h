6928540701b4c7b9982b75a24180894bc1216a39
Shaza Ismail Kaoud <shaza.ismail.k@gmail.com>
2017-06-23 18:50:46 +0200

    prepare healing clone tool interface as an Editor threaded tool

--- utilities/imageeditor/tools/enhance/healingclonetool.h
+++ utilities/imageeditor/tools/enhance/healingclonetool.h
@@ -1,11 +1,62 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2017-06-15
+ * Description : a tool to replace part of the image using another
+ *
+ * Copyright (C) 2004-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2017      by Shaza Ismail Kaoud <shaza dot ismail dot k at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef HEALINGCLONETOOL_H
 #define HEALINGCLONETOOL_H
 
+// Local includes
 
-class HealingCloneTool
+#include "editortool.h"
+
+namespace Digikam
 {
+
+class HealingCloneTool : public EditorToolThreaded
+{
+    Q_OBJECT
+
 public:
-    HealingCloneTool();
+    explicit HealingCloneTool(QObject* const parent);
+
+private Q_SLOTS:
+
+    void slotResetSettings();
+
+private:
+
+    void readSettings();
+    void writeSettings();
+    void preparePreview();
+    void prepareFinal();
+    void setPreviewImage();
+    void setFinalImage();
+
+private:
+
+    class Private;
+    Private* const d;
 };
 
-#endif // HEALINGCLONETOOL_H
+#endif /* HEALINGCLONETOOL_H */
+} // namespace Digikam
