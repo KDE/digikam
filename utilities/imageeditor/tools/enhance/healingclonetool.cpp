5b3b15e5bdc45a69797c4d4e90d968601775169b
Shaza Ismail Kaoud <shaza.ismail.k@gmail.com>
2017-06-25 22:54:55 +0200

    add healing clone tool UI-initial

--- utilities/imageeditor/tools/enhance/healingclonetool.cpp
+++ utilities/imageeditor/tools/enhance/healingclonetool.cpp
@@ -1,6 +1,144 @@
 #include "healingclonetool.h"
 
-HealingCloneTool::HealingCloneTool()
+// Qt includes
+
+#include <QGridLayout>
+#include <QLabel>
+#include <QIcon>
+
+// KDE includes
+
+#include <ksharedconfig.h>
+#include <klocalizedstring.h>
+
+// Local includes
+
+#include "dnuminput.h"
+#include "editortoolsettings.h"
+#include "imageiface.h"
+#include "imageregionwidget.h"
+
+namespace Digikam
 {
+class HealingCloneTool::Private
+{
+public:
+
+    Private() :
+        radiusInput(0),
+        previewWidget(0),
+        gboxSettings(0)
+    {
+    }
+
+    static const QString configGroupName;
+    static const QString configRadiusAdjustmentEntry;
+
+    DIntNumInput*        radiusInput;
+    ImageRegionWidget*   previewWidget;
+    EditorToolSettings*  gboxSettings;
+};
+
+const QString HealingCloneTool::Private::configGroupName(QLatin1String("Healing Clone Tool"));
+const QString HealingCloneTool::Private::configRadiusAdjustmentEntry(QLatin1String("RadiusAdjustment"));
+
+// --------------------------------------------------------
+
+HealingCloneTool::HealingCloneTool(QObject * const parent)
+    : EditorToolThreaded(parent),
+      d(new Private)
+{
+    setObjectName(QLatin1String("healing clone"));
+    setToolName(i18n("Healing Clone Tool"));
+    setToolIcon(QIcon::fromTheme(QLatin1String("healimage")));
+    setToolHelp(QLatin1String("healingclonetool.anchor"));
+
+    d->gboxSettings  = new EditorToolSettings;
+    d->previewWidget = new ImageRegionWidget;
+
+    // --------------------------------------------------------
+
+    QLabel* const label  = new QLabel(i18n("Brush Radius:"));
+    d->radiusInput = new DIntNumInput();
+    d->radiusInput->setRange(0, 20, 1);
+    d->radiusInput->setDefaultValue(0);
+    d->radiusInput->setWhatsThis(i18n("A radius of 0 has no effect, "
+                                      "1 and above determine the brush radius "
+                                      "that determines the size of parts copied in the image."));
+
+    // --------------------------------------------------------
+
+    const int spacing = d->gboxSettings->spacingHint();
+
+    QGridLayout* const grid = new QGridLayout( );
+    grid->addWidget(label,          0, 0, 1, 2);
+    grid->addWidget(d->radiusInput, 1, 0, 1, 2);
+    grid->setRowStretch(2, 10);
+    grid->setContentsMargins(spacing, spacing, spacing, spacing);
+    grid->setSpacing(spacing);
+    d->gboxSettings->plainPage()->setLayout(grid);
+
+    // --------------------------------------------------------
+
+    setPreviewModeMask(PreviewToolBar::AllPreviewModes);
+    setToolSettings(d->gboxSettings);
+    setToolView(d->previewWidget);
+
+    // --------------------------------------------------------
+
+    connect(d->radiusInput, SIGNAL(valueChanged(int)),
+            this, SLOT(slotTimer()));
+}
+
+HealingCloneTool::~HealingCloneTool()
+{
+    delete d;
+}
+
+void HealingCloneTool::readSettings()
+{
+    KSharedConfig::Ptr config = KSharedConfig::openConfig();
+    KConfigGroup group        = config->group(d->configGroupName);
+    d->radiusInput->setValue(group.readEntry(d->configRadiusAdjustmentEntry, d->radiusInput->defaultValue()));
+}
+
+void HealingCloneTool::writeSettings()
+{
+    KSharedConfig::Ptr config = KSharedConfig::openConfig();
+    KConfigGroup group        = config->group(d->configGroupName);
+    group.writeEntry(d->configRadiusAdjustmentEntry, d->radiusInput->value());
+    config->sync();
+}
+
+void HealingCloneTool::slotResetSettings()
+{
+    d->radiusInput->blockSignals(true);
+    d->radiusInput->slotReset();
+    d->radiusInput->blockSignals(false);
+}
+
+void HealingCloneTool::preparePreview()
+{
+    DImg img = d->previewWidget->getOriginalRegionImage();
+    //setFilter(new BlurFilter(&img, this, d->radiusInput->value()));
+}
+
+void HealingCloneTool::setPreviewImage()
+{
+    //DImg preview = filter()->getTargetImage();
+    //d->previewWidget->setPreviewImage(preview);
+}
+
+void HealingCloneTool::prepareFinal()
+{
+    ImageIface iface;
 
 }
+
+void HealingCloneTool::setFinalImage()
+{
+    ImageIface iface;
+
+}
+
+} // namespace Digikam
