2ee7bc4ff5b86dd2c648925b87aa7d062b99fe43
Gilles Caulier <caulier.gilles@gmail.com>
2017-07-17 23:26:59 +0200

    typo

--- utilities/kdesupport/kipi/kipipluginloader.cpp
+++ utilities/kdesupport/kipi/kipipluginloader.cpp
@@ -140,7 +140,7 @@ void KipiPluginLoader::Private::loadPlugins()
 
     // These plugins are replaced by tools from core.
     ignores.append(QLatin1String("SendImages"));
-    ignores.append(QLatin1String("PrintImages"));
+    ignores.append(QLatin1String("Print images"));
 
     // List of obsolete tool actions to not load
 
