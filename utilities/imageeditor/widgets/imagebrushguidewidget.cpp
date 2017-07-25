0b8a460df0f2ef90359116729d23e722068330ea
Shaza Ismail Kaoud <shaza.ismail.k@gmail.com>
2017-07-25 14:00:10 +0200

    add a new widget for the brush

---
+++ utilities/imageeditor/widgets/imagebrushguidewidget.cpp
@@ -0,0 +1,6 @@
+
+#include "imagebrushguidewidget.h"
+namespace Digikam
+{
+
+}
---
+++ utilities/imageeditor/widgets/imagebrushguidewidget.h
@@ -0,0 +1,17 @@
+#ifndef IMAGEBRUSHGUIDEWIDGET_H
+#define IMAGEBRUSHGUIDEWIDGET_H
+
+#include "imageguidewidget.h"
+
+namespace Digikam
+{
+
+class ImageBrushGuideWidget : public ImageGuideWidget
+{
+    Q_OBJECT
+public:
+    using ImageGuideWidget::ImageGuideWidget;
+};
+
+}
+#endif // IMAGEBRUSHGUIDEWIDGET_H
