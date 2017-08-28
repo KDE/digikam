0f5959a16215f981d304b9e0bd68d1e7efb84caa
Shaza Ismail Kaoud <shaza.ismail.k@gmail.com>
2017-08-28 03:04:15 +0200

    add license part to brush widget

--- utilities/imageeditor/widgets/imagebrushguidewidget.cpp
+++ utilities/imageeditor/widgets/imagebrushguidewidget.cpp
@@ -1,3 +1,27 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2017-06-15
+ * Description : a brush for use with tool to replace part of the image using another
+ *
+ * Copyright (C) 2004-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2017      by Shaza Ismail Kaoud <shaza dot ismail dot k at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 // Local includes
 
 #include "imagebrushguidewidget.h"
