4df2ea6a2c873f570be88b46e8ebd9682877765c
Maik Qualmann <metzpinguin@gmail.com>
2017-09-01 21:53:29 +0200

    fix compile warnings for macros

--- utilities/mediaserver/hupnp/src/socket/hmulticast_socket.cpp
+++ utilities/mediaserver/hupnp/src/socket/hmulticast_socket.cpp
@@ -25,7 +25,7 @@
 #ifdef Q_OS_WIN
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#elif Q_OS_SYMBIAN || Q_OS_FREEBSD
+#elif defined Q_OS_SYMBIAN || defined Q_OS_FREEBSD
 #include <netinet/in.h>
 #include <sys/socket.h>
 #else
