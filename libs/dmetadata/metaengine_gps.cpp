bac54a7834064573946a3c380be77b0e3f076e53
Maik Qualmann <metzpinguin@gmail.com>
2017-11-17 18:49:36 +0100

    fix possible wrong return value from Exiv2 toRational() function BUGS: 387000 FIXED-IN: 5.8.0

--- libs/dmetadata/metaengine_gps.cpp
+++ libs/dmetadata/metaengine_gps.cpp
@@ -62,10 +62,10 @@ bool MetaEngine::getGPSLatitudeNumber(double* const latitude) const
 {
     try
     {
-        *latitude=0.0;
+        *latitude = 0.0;
 
         // Try XMP first. Reason: XMP in sidecar may be more up-to-date than EXIF in original image.
-        if ( convertFromGPSCoordinateString(getXmpTagString("Xmp.exif.GPSLatitude"), latitude) )
+        if (convertFromGPSCoordinateString(getXmpTagString("Xmp.exif.GPSLatitude"), latitude))
             return true;
 
         // Now try to get the reference from Exif.
@@ -79,44 +79,34 @@ bool MetaEngine::getGPSLatitudeNumber(double* const latitude) const
 
             if (it != exifData.end() && (*it).count() == 3)
             {
-                // Latitude decoding from Exif.
-                double num, den, min, sec;
+                double deg;
+                double min;
+                double sec;
 
-                num = (double)((*it).toRational(0).first);
-                den = (double)((*it).toRational(0).second);
+                deg = (double)((*it).toFloat(0));
 
-                if (den == 0)
-                    return false;
-
-                *latitude = num/den;
-
-                num = (double)((*it).toRational(1).first);
-                den = (double)((*it).toRational(1).second);
-
-                if (den == 0)
-                    return false;
-
-                min = num/den;
-
-                if (min != -1.0)
-                    *latitude = *latitude + min/60.0;
-
-                num = (double)((*it).toRational(2).first);
-                den = (double)((*it).toRational(2).second);
-
-                if (den == 0)
+                if (deg == -1.0)
                 {
-                    // be relaxed and accept 0/0 seconds. See #246077.
-                    if (num == 0)
-                        den = 1;
-                    else
-                        return false;
+                    return false;
                 }
 
-                sec = num/den;
+                *latitude = deg;
+
+                min = (double)((*it).toFloat(1));
+
+                if (min == -1.0)
+                {
+                    return false;
+                }
+
+                *latitude = *latitude + min/60.0;
+
+                sec = (double)((*it).toFloat(2));
 
                 if (sec != -1.0)
+                {
                     *latitude = *latitude + sec/3600.0;
+                }
             }
             else
             {
@@ -124,7 +114,9 @@ bool MetaEngine::getGPSLatitudeNumber(double* const latitude) const
             }
 
             if (latRef[0] == 'S')
+            {
                 *latitude *= -1.0;
+            }
 
             return true;
         }
@@ -145,10 +137,10 @@ bool MetaEngine::getGPSLongitudeNumber(double* const longitude) const
 {
     try
     {
-        *longitude=0.0;
+        *longitude = 0.0;
 
         // Try XMP first. Reason: XMP in sidecar may be more up-to-date than EXIF in original image.
-        if ( convertFromGPSCoordinateString(getXmpTagString("Xmp.exif.GPSLongitude"), longitude) )
+        if (convertFromGPSCoordinateString(getXmpTagString("Xmp.exif.GPSLongitude"), longitude))
             return true;
 
         // Now try to get the reference from Exif.
@@ -166,46 +158,29 @@ bool MetaEngine::getGPSLongitudeNumber(double* const longitude) const
             {
                 /// @todo Decoding of latitude and longitude works in the same way,
                 ///       code here can be put in a separate function
-                double num, den;
+                double deg;
+                double min;
+                double sec;
 
-                num = (double)((*it).toRational(0).first);
-                den = (double)((*it).toRational(0).second);
+                deg = (double)((*it).toFloat(0));
 
-                if (den == 0)
+                if (deg == -1.0)
                 {
                     return false;
                 }
 
-                *longitude = num/den;
+                *longitude = deg;
 
-                num = (double)((*it).toRational(1).first);
-                den = (double)((*it).toRational(1).second);
+                min = (double)((*it).toFloat(1));
 
-                if (den == 0)
+                if (min == -1.0)
                 {
                     return false;
                 }
 
-                const double min = num/den;
+                *longitude = *longitude + min/60.0;
 
-                if (min != -1.0)
-                {
-                    *longitude = *longitude + min/60.0;
-                }
-
-                num = (double)((*it).toRational(2).first);
-                den = (double)((*it).toRational(2).second);
-
-                if (den == 0)
-                {
-                    // be relaxed and accept 0/0 seconds. See #246077.
-                    if (num == 0)
-                        den = 1;
-                    else
-                        return false;
-                }
-
-                const double sec = num/den;
+                sec = (double)((*it).toFloat(2));
 
                 if (sec != -1.0)
                 {
