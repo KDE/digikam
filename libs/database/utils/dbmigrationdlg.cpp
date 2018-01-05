7d187eb83fd92ec4ac44961724877b8af641dc5e
Maik Qualmann <metzpinguin@gmail.com>
2018-01-05 12:28:14 +0100

    polish and polish i18n

--- libs/database/utils/dbmigrationdlg.cpp
+++ libs/database/utils/dbmigrationdlg.cpp
@@ -213,13 +213,15 @@ void DatabaseMigrationDialog::slotPerformCopy()
 
     if (fromDBParameters == toDBParameters)
     {
-        QMessageBox::critical(this, qApp->applicationName(), i18n("Database type or location must be different!"));
+        QMessageBox::critical(this, qApp->applicationName(),
+                              i18n("Database type or location must be different!"));
         return;
     }
 
     if (fromDBParameters.internalServer && toDBParameters.internalServer)
     {
-        QMessageBox::critical(this, qApp->applicationName(), i18n("Internal server can only be used once!"));
+        QMessageBox::critical(this, qApp->applicationName(),
+                              i18n("Internal server can only be used once!"));
         return;
     }
 
@@ -227,7 +229,8 @@ void DatabaseMigrationDialog::slotPerformCopy()
 
     if ((fromDBParameters.internalServer || toDBParameters.internalServer) && !orgPrms.internalServer)
     {
-        QMessageBox::critical(this, qApp->applicationName(), i18n("Internal server has not been used and started!"));
+        QMessageBox::critical(this, qApp->applicationName(),
+                              i18n("Internal server is not used and is not active!"));
         return;
     }
 
