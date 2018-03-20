7ca446339589d7438a6d64ce444375827cd20363
Gilles Caulier <caulier.gilles@gmail.com>
2018-03-20 12:30:26 +0100

    fix path

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find ./core -name \*.rc -o -name \*.ui | grep -v '/tests/'` >> rc.cpp || exit 11
-$XGETTEXT `find ./core -name \*.h -o -name \*.cpp | grep -v '/tests/'` `find app -name \*.h.cmake.in` -o $podir/digikam.pot
+$XGETTEXT `find ./core -name \*.h -o -name \*.cpp | grep -v '/tests/'` `find ./core/app -name \*.h.cmake.in` -o $podir/digikam.pot
 rm -f rc.cpp
