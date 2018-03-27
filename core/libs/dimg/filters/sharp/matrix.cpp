d55e6acd771153f79a498256f0b2e8fc6b42a193
Human Dynamo <caulier.gilles@gmail.com>
2018-03-27 23:13:46 +0200

    krazy ++

--- core/libs/dimg/filters/sharp/matrix.cpp
+++ core/libs/dimg/filters/sharp/matrix.cpp
@@ -9,7 +9,7 @@
  * Copyright (C) 2005-2018 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * Original implementation from Refocus Gimp plug-in
- * Copyright (C) 1999-2003 Ernst Lippe
+ * Copyright (C) 1999-2003 Ernst Lippe <ernstl at users dot sourceforge dot net>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
