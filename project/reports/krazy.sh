9667ea2f6659f60da3c5dd376f1f49c0b1f61bbf
Gilles Caulier <caulier.gilles@gmail.com>
2018-04-23 10:52:41 +0200

    add krazy local script

---
+++ project/reports/krazy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright (c) 2013-2018, Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+krazy2all --export textedit \
+          --title digiKam \
+          --no-brief \
+          --strict all \
+          --priority all \
+          --verbose \
+          --topdir ../../core \
+          --exclude-types perl,python \
+          --outfile report.krazy
