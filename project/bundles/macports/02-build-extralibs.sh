434de28927fa7285d288f93aabb30bcf1250b6a0
Gilles Caulier <caulier.gilles@gmail.com>
2018-04-26 15:05:10 +0200

    update KF5 application to last 5.45 remove definitively Kwallet dependency (one more -- in the bundles...)

--- project/bundles/macports/02-build-extralibs.sh
+++ project/bundles/macports/02-build-extralibs.sh
@@ -96,7 +96,6 @@ cmake --build . --config RelWithDebInfo --target ext_kbookmarks          -- -j$C
 cmake --build . --config RelWithDebInfo --target ext_kimageformats       -- -j$CPU_CORES
 
 # libksane support
-cmake --build . --config RelWithDebInfo --target ext_kwallet             -- -j$CPU_CORES
 cmake --build . --config RelWithDebInfo --target ext_libksane            -- -j$CPU_CORES
 
 # Geolocation support
