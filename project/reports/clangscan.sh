35c755aa0b231c76b9c2d9968940cfbabd8826a9
Gilles Caulier <caulier.gilles@gmail.com>
2018-05-05 18:25:20 +0200

    update

--- project/reports/clangscan.sh
+++ project/reports/clangscan.sh
@@ -14,19 +14,19 @@
 # checks if branch has something pending
 function parseGitDirty()
 {
-    git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "-M"
+    git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "M"
 }
 
 # gets the current git branch
 function parseGitBranch()
 {
-    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parseGitDirty)/"
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"
 }
 
 # get last commit hash prepended with @ (i.e. @8a323d0)
 function parseGitHash()
 {
-    git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/rev-\1/"
+    git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/-rev-\1/"
 }
 
 # ----------------------------------------------------------
