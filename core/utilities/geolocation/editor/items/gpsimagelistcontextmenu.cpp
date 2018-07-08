bc930755a81ba8e23d0356cef2ff8341bd6f192f
Maik Qualmann <metzpinguin@gmail.com>
2018-07-08 09:35:22 +0200

    polish

--- core/utilities/geolocation/editor/items/gpsimagelistcontextmenu.cpp
+++ core/utilities/geolocation/editor/items/gpsimagelistcontextmenu.cpp
@@ -77,26 +77,26 @@ public:
     {
     }
 
-    bool                               enabled;
+    bool                     enabled;
 
-    QAction*                           actionBookmark;
-    GPSBookmarkOwner*                  bookmarkOwner;
+    QAction*                 actionBookmark;
+    GPSBookmarkOwner*        bookmarkOwner;
 
-    QAction*                           actionCopy;
-    QAction*                           actionPaste;
-    QAction*                           actionRemoveCoordinates;
-    QAction*                           actionRemoveAltitude;
-    QAction*                           actionRemoveUncertainty;
-    QAction*                           actionRemoveSpeed;
-    QAction*                           actionLookupMissingAltitudes;
+    QAction*                 actionCopy;
+    QAction*                 actionPaste;
+    QAction*                 actionRemoveCoordinates;
+    QAction*                 actionRemoveAltitude;
+    QAction*                 actionRemoveUncertainty;
+    QAction*                 actionRemoveSpeed;
+    QAction*                 actionLookupMissingAltitudes;
 
-    GPSImageList*                      imagesList;
+    GPSImageList*            imagesList;
 
     // Altitude lookup
     QPointer<LookupAltitude> altitudeLookup;
-    GPSUndoCommand*                    altitudeUndoCommand;
-    int                                altitudeRequestedCount;
-    int                                altitudeReceivedCount;
+    GPSUndoCommand*          altitudeUndoCommand;
+    int                      altitudeRequestedCount;
+    int                      altitudeReceivedCount;
 };
 
 GPSImageListContextMenu::GPSImageListContextMenu(GPSImageList* const imagesList,
@@ -355,10 +355,10 @@ void GPSImageListContextMenu::pasteActionTriggered()
                         continue;
                     }
 
-                    double    ptAltitude  = 0.0;
-                    double    ptLatitude  = 0.0;
-                    double    ptLongitude = 0.0;
-                    bool haveAltitude     = false;
+                    double ptAltitude  = 0.0;
+                    double ptLatitude  = 0.0;
+                    double ptLongitude = 0.0;
+                    bool haveAltitude  = false;
 
                     // Get GPS position. If not available continue to next point.
                     const QString lat     = wptElem.attribute(QString::fromLatin1("lat"));
@@ -440,11 +440,11 @@ void GPSImageListContextMenu::pasteActionTriggered()
 
             if ((parts.size() == 3) || (parts.size() == 2))
             {
-                bool   okay         = true;
-                double ptLongitude  = 0.0;
-                double ptLatitude   = 0.0;
-                double ptAltitude   = 0.0;
-                bool   haveAltitude = false;
+                double ptLongitude = 0.0;
+                double ptLatitude  = 0.0;
+                double ptAltitude  = 0.0;
+                bool haveAltitude  = false;
+                bool okay          = true;
 
                 ptLongitude = parts[0].toDouble(&okay);
 
