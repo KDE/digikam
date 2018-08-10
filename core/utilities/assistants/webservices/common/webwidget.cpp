b2647c5446d82a20753f13c80219bd80b0e53a66
Tarek Talaat <tarektalaat93@gmail.com>
2018-08-10 23:29:13 +0200

    Adding support for QWebView

---
+++ core/utilities/assistants/webservices/common/webwidget.cpp
@@ -0,0 +1,53 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2009-12-01
+ * Description : Widget for displaying HTML in the backends
+ *
+ * Copyright (C) 2010-2018 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2011 by Michael G. Hansen <mike at mghansen dot de>
+ * Copyright (C) 2015      by Mohamed_Anwer <m_dot_anwer at gmx dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+ #include "webwidget.h"
+
+namespace Digikam
+{
+class WebWidget::Private
+{
+public:
+
+    explicit Private()
+      : parent(0)
+    {
+    }
+
+    QWidget*        parent;
+};
+WebWidget::WebWidget(QWidget* const parent)
+    : QWebView(parent),
+      d(new Private())
+{
+    d->parent = parent;
+    connect(this, SIGNAL(loadProgress(int)),
+            this, SLOT(progress(int)));
+}
+WebWidget::~WebWidget()
+{
+    delete d;
+}
+} // namespace Digikam
