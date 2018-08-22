b53b51621cd6be755a18ecbbb496366fe1002229
Gilles Caulier <caulier.gilles@gmail.com>
2018-08-22 14:19:55 +0200

    install drmingw under MXE

--- project/bundles/mxe/01-build-mxe.sh
+++ project/bundles/mxe/01-build-mxe.sh
@@ -175,6 +175,7 @@ ${MXE_BUILD_TARGETS}-cmake $ORIG_WD/../3rdparty \
 ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_exiv2      -- -j$CPU_CORES
 ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_qtav       -- -j$CPU_CORES
 ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_lensfun    -- -j$CPU_CORES
+${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_drmingw    -- -j$CPU_CORES
 
 #################################################################################################
 
