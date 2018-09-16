f575be4024b26bc647dafc721f4a2cc4ac5b7d03
Human Dynamo <caulier.gilles@gmail.com>
2018-09-16 23:41:24 +0200

    compile

--- core/libs/database/coredb/coredbfields.h
+++ core/libs/database/coredb/coredbfields.h
@@ -368,10 +368,9 @@ enum CustomEnumFlags
 Q_DECLARE_FLAGS(CustomEnum, CustomEnumFlags)
 
 // krazy:exclude=explicit
-#define DATABASEFIELDS_SET_DECLARE_METHODS(Flag, variable)                     \
-    // cppcheck-suppress noExplicitConstructor                                 \
+// cppcheck-suppress noExplicitConstructor
+#define DATABASEFIELDS_SET_DECLARE_METHODS(Flag, variable)                    \
     Set(const Flag& f)                      { initialize(); variable = f;    } \
-    // cppcheck-suppress noExplicitConstructor                                 \
     Set(const Flag##Field& f)               { initialize(); variable = f;    } \
     inline Flag& operator=(const Flag& f)   { return variable.operator=(f);  } \
     inline Flag& operator|=(Flag f)         { return variable.operator|=(f); } \
