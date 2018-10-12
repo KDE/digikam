495d2aa4c00aa05f75de47235c5546ceb9a90082
Gilles Caulier <caulier.gilles@gmail.com>
2018-10-12 22:07:56 +0200

    install all

--- project/bundles/mxe/01-build-mxe.sh
+++ project/bundles/mxe/01-build-mxe.sh
@@ -173,7 +173,6 @@ ${MXE_BUILD_TARGETS}-cmake $ORIG_WD/../3rdparty \
 # NOTE: The order to compile each component here is very important.
 
 ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_exiv2      -- -j$CPU_CORES
-exit
 ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_qtav       -- -j$CPU_CORES
 ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_lensfun    -- -j$CPU_CORES
 #${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_libgphoto2 -- -j$CPU_CORES
