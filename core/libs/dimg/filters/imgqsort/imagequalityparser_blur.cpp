56c92c3c05cfff7b186043134e5f9792bc48009a
Human Dynamo <caulier.gilles@gmail.com>
2018-10-13 07:03:42 +0200

    add url as reference

--- core/libs/dimg/filters/imgqsort/imagequalityparser_blur.cpp
+++ core/libs/dimg/filters/imgqsort/imagequalityparser_blur.cpp
@@ -9,6 +9,8 @@
  * Copyright (C) 2013-2018 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2013-2014 by Gowtham Ashok <gwty93 at gmail dot com>
  *
+ * References  : https://stackoverflow.com/questions/7765810/is-there-a-way-to-detect-if-an-image-is-blurry
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
