c53c2ca745ed98ef36bfdfbb1ea0f127aa5ab0ad
Maik Qualmann <metzpinguin@gmail.com>
2018-10-21 13:25:50 +0200

    Revert "try with QMutexLocker in DImg"

--- core/libs/dimg/dimg_p.h
+++ core/libs/dimg/dimg_p.h
@@ -30,7 +30,6 @@
 #include <QString>
 #include <QByteArray>
 #include <QVariant>
-#include <QMutex>
 #include <QMap>
 
 // Local includes
@@ -81,6 +80,9 @@ public:
         lanczos_func = 0;
         alpha        = false;
         sixteenBit   = false;
+
+        attributes.setSharable(false);
+        embeddedText.setSharable(false);
     }
 
     ~Private()
@@ -145,8 +147,6 @@ public:
     QMap<QString, QString>  embeddedText;
     IccProfile              iccProfile;
     DImageHistory           imageHistory;
-
-    QMutex                  mutex;
 };
 
 } // namespace Digikam
