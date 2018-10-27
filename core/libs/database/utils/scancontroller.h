b54d0fb1a8612c05342ae3f8689acb4342750000
Gilles Caulier <caulier.gilles@gmail.com>
2018-10-27 19:39:27 +0200

    ScanControler: prepare to separate start/progress/stop methods

--- core/libs/database/utils/scancontroller.h
+++ core/libs/database/utils/scancontroller.h
@@ -45,7 +45,8 @@ namespace Digikam
 class CollectionScanner;
 class PAlbum;
 
-class DIGIKAM_EXPORT ScanController : public QThread, public InitializationObserver
+class DIGIKAM_EXPORT ScanController : public QThread,
+                                      public InitializationObserver
 {
     Q_OBJECT
 
@@ -60,11 +61,71 @@ public:
 
 public:
 
+    /**
+     * When writing metadata to the file, the file content on disk changes,
+     * but the information is taken from the database; therefore,
+     * the resulting scanning process can be optimized.
+     *
+     * Thus, if you write metadata of an ItemInfo from the database to disk,
+     * do this in the scope of a FileMetadataWrite object.
+     */
+    class FileMetadataWrite
+    {
+    public:
+
+        explicit FileMetadataWrite(const ItemInfo& info);
+        ~FileMetadataWrite();
+
+        void changed(bool wasChanged);
+
+    protected:
+
+        ItemInfo m_info;
+        bool     m_changed;
+    };
+
+public:
+
     static ScanController* instance();
 
-    /** Wait for the thread to finish. Returns after all tasks are done. */
-    void shutDown();
+    /**
+     * Carries out a complete collection scan, at the same time updating
+     * the unique hash in the database and thumbnail database.
+     * Synchronous, returns when ready.
+     * The database will be locked while the scan is running.
+     */
+    void updateUniqueHash();
 
+    void allowToScanDeferredFiles();
+
+    /**
+     * If necessary (modified or newly created, scans the file directly
+     * Returns the up-to-date ItemInfo.
+     */
+    ItemInfo scannedInfo(const QString& filePath);
+
+protected:
+
+    virtual void run();
+
+private:
+
+    explicit ScanController();
+    ~ScanController();
+
+    void setInitializationMessage();
+    void createProgressDialog();
+    virtual bool continueQuery();
+    virtual void connectCollectionScanner(CollectionScanner* const scanner);
+
+    // -----------------------------------------------------------------------------
+
+    /** @name Start Operations
+     */
+
+    //@{
+
+public:
 
     /**
      * Calls CoreDbAccess::checkReadyForUse(), providing progress
@@ -84,20 +145,11 @@ public:
      */
     void completeCollectionScan(bool defer = false);
     void completeCollectionScanDeferFiles();
-    void allowToScanDeferredFiles();
 
     /**
      * Scan Whole collection without to display a progress dialog or to manage splashscreen, as for NewItemsFinder tool.
      */
     void completeCollectionScanInBackground(bool defer);
-
-    /**
-     * Carries out a complete collection scan, at the same time updating
-     * the unique hash in the database and thumbnail database.
-     * Synchronous, returns when ready.
-     * The database will be locked while the scan is running.
-     */
-    void updateUniqueHash();
 
     /**
      * Schedules a scan of the specified part of the collection.
@@ -125,130 +177,22 @@ public:
     void scheduleCollectionScanExternal(const QString& path);
 
     /**
-     * If necessary (modified or newly created, scans the file directly
-     * Returns the up-to-date ItemInfo.
-     */
-    ItemInfo scannedInfo(const QString& filePath);
-
-    /**
-     * When writing metadata to the file, the file content on disk changes,
-     * but the information is taken from the database; therefore,
-     * the resulting scanning process can be optimized.
-     *
-     * Thus, if you write metadata of an ItemInfo from the database to disk,
-     * do this in the scope of a FileMetadataWrite object.
-     */
-    class FileMetadataWrite
-    {
-    public:
-
-        explicit FileMetadataWrite(const ItemInfo& info);
-        ~FileMetadataWrite();
-
-        void changed(bool wasChanged);
-
-    protected:
-
-        ItemInfo m_info;
-        bool      m_changed;
-    };
-
-    /** If the controller is currently processing a database update
-     *  (typically after first run),
-     *  cancel this hard and as soon as possible. Any progress may be lost. */
-    void abortInitialization();
-
-    /** If the controller is currently doing a complete scan
-     *  (typically at startup), stop this operation.
-     *  It can be resumed later. */
-    void cancelCompleteScan();
-
-    /** Cancels all running or scheduled operations and suspends scanning.
-     *  This method returns when all scanning has stopped.
-     *  This includes a call to suspendCollectionScan().
-     *  Restart with resumeCollectionScan. */
-    void cancelAllAndSuspendCollectionScan();
-
-    /** Temporarily suspend collection scanning.
-     *  All scheduled scanning tasks are queued
-     *  and will be done when resumeCollectionScan()
-     *  has been called.
-     *  Calling these methods is recursive, you must resume
-     *  as often as you called suspend.
-     */
-    void suspendCollectionScan();
-    void resumeCollectionScan();
-
-    /** Hint at the imminent copy, move or rename of an album, so that the
-     *  collection scanner is informed about this.
-     *  If the album is renamed, give the new name in newAlbumName.
-     *  DstAlbum is the new parent album /
-     *  dstPath is the new parent directory of the album, so
-     *  do not include the album name to dstPath.
-     */
-    void hintAtMoveOrCopyOfAlbum(const PAlbum* const album, const PAlbum* const dstAlbum, const QString& newAlbumName = QString());
-    void hintAtMoveOrCopyOfAlbum(const PAlbum* const album, const QString& dstPath, const QString& newAlbumName = QString());
-
-    /** Hint at the imminent copy, move or rename of items, so that the
-     *  collection scanner is informed about this.
-     *  Give the list of existing items, specify the destination with dstAlbum,
-     *  and give the names at destination in itemNames (Unless for rename, names wont usually change.
-     *  Give them nevertheless.)
-     */
-    void hintAtMoveOrCopyOfItems(const QList<qlonglong> ids, const PAlbum* const dstAlbum, const QStringList& itemNames);
-    void hintAtMoveOrCopyOfItem(qlonglong id, const PAlbum* const dstAlbum, const QString& itemName);
-
-    /** Hint at the fact that an item may have changed, although its modification date may not have changed.
-     *  Note that a scan of the containing directory will need to be triggered nonetheless for the hints to take effect. */
-    void hintAtModificationOfItems(const QList<qlonglong> ids);
-    void hintAtModificationOfItem(qlonglong id);
-
-    /**
      * Implementation of FileMetadataWrite, see there. Calling these methods is equivalent.
      */
     void beginFileMetadataWrite(const ItemInfo& info);
-    void finishFileMetadataWrite(const ItemInfo& info, bool changed);
 
 Q_SIGNALS:
 
     void databaseInitialized(bool success);
-    void completeScanDone();
-    void completeScanCanceled();
-    void triggerShowProgressDialog();
-    void incrementProgressDialog(int);
-    void errorFromInitialization(const QString&);
-    void progressFromInitialization(const QString&, int);
-
-    void totalFilesToScan(int);
-    void filesScanned(int);
-
     void collectionScanStarted(const QString& message);
-    void scanningProgress(float progress);
-    void collectionScanFinished();
-    void partialScanDone(const QString& path);
 
 private Q_SLOTS:
 
     void slotStartCompleteScan();
-    void slotTotalFilesToScan(int count);
     void slotStartScanningAlbum(const QString& albumRoot, const QString& album);
-    void slotScannedFiles(int scanned);
     void slotStartScanningAlbumRoot(const QString& albumRoot);
     void slotStartScanningForStaleAlbums();
     void slotStartScanningAlbumRoots();
-
-    void slotShowProgressDialog();
-    void slotTriggerShowProgressDialog();
-    void slotCancelPressed();
-
-    void slotProgressFromInitialization(const QString& message, int numberOfSteps);
-    void slotErrorFromInitialization(const QString& errorMessage);
-
-    void slotRelaxedScanning();
-
-protected:
-
-    virtual void run();
 
 private:
 
@@ -258,27 +202,147 @@ private:
      */
     void scanFileDirectly(const QString& filePath);
     void scanFileDirectlyNormal(const ItemInfo& info);
-
-    void createProgressDialog();
-    void setInitializationMessage();
-
     void completeCollectionScanCore(bool needTotalFiles, bool defer);
+
+    //@}
+
+    // -----------------------------------------------------------------------------
+
+    /** @name Progress Operations
+     */
+
+    //@{
+
+public:
+
+    /**
+     * Hint at the imminent copy, move or rename of an album, so that the
+     * collection scanner is informed about this.
+     * If the album is renamed, give the new name in newAlbumName.
+     * DstAlbum is the new parent album /
+     * dstPath is the new parent directory of the album, so
+     * do not include the album name to dstPath.
+     */
+    void hintAtMoveOrCopyOfAlbum(const PAlbum* const album, const PAlbum* const dstAlbum, const QString& newAlbumName = QString());
+    void hintAtMoveOrCopyOfAlbum(const PAlbum* const album, const QString& dstPath, const QString& newAlbumName = QString());
+
+    /**
+     * Hint at the imminent copy, move or rename of items, so that the
+     * collection scanner is informed about this.
+     * Give the list of existing items, specify the destination with dstAlbum,
+     * and give the names at destination in itemNames (Unless for rename, names wont usually change.
+     * Give them nevertheless.)
+     */
+    void hintAtMoveOrCopyOfItems(const QList<qlonglong> ids, const PAlbum* const dstAlbum, const QStringList& itemNames);
+    void hintAtMoveOrCopyOfItem(qlonglong id, const PAlbum* const dstAlbum, const QString& itemName);
+
+    /**
+     * Hint at the fact that an item may have changed, although its modification date may not have changed.
+     * Note that a scan of the containing directory will need to be triggered nonetheless for the hints to take effect.
+     */
+    void hintAtModificationOfItems(const QList<qlonglong> ids);
+    void hintAtModificationOfItem(qlonglong id);
+
+Q_SIGNALS:
+
+    void totalFilesToScan(int);
+    void filesScanned(int);
+    void scanningProgress(float progress);
+    void triggerShowProgressDialog();
+    void incrementProgressDialog(int);
+    void progressFromInitialization(const QString&, int);
+
+private Q_SLOTS:
+
+    void slotTotalFilesToScan(int count);
+    void slotScannedFiles(int scanned);
+    void slotShowProgressDialog();
+    void slotTriggerShowProgressDialog();
+    void slotProgressFromInitialization(const QString& message, int numberOfSteps);
+    void slotErrorFromInitialization(const QString& errorMessage);
+
+private:
 
     virtual void moreSchemaUpdateSteps(int numberOfSteps);
     virtual void schemaUpdateProgress(const QString& message, int numberOfSteps);
-    virtual void finishedSchemaUpdate(UpdateResult result);
-    virtual void connectCollectionScanner(CollectionScanner* const scanner);
     virtual void error(const QString& errorMessage);
-    virtual bool continueQuery();
+
+    //@}
+
+    // -----------------------------------------------------------------------------
+
+    /** @name Stop Operations
+     */
+
+    //@{
+
+public:
+
+    /**
+     * Wait for the thread to finish. Returns after all tasks are done.
+     */
+    void shutDown();
+
+    /**
+     * If the controller is currently processing a database update
+     * (typically after first run),
+     * cancel this hard and as soon as possible. Any progress may be lost.
+     */
+    void abortInitialization();
+
+    /**
+     * If the controller is currently doing a complete scan
+     * (typically at startup), stop this operation.
+     * It can be resumed later.
+     */
+    void cancelCompleteScan();
+
+    /**
+     * Cancels all running or scheduled operations and suspends scanning.
+     * This method returns when all scanning has stopped.
+     * This includes a call to suspendCollectionScan().
+     * Restart with resumeCollectionScan.
+     */
+    void cancelAllAndSuspendCollectionScan();
+
+    /**
+     * Temporarily suspend collection scanning.
+     * All scheduled scanning tasks are queued
+     * and will be done when resumeCollectionScan()
+     * has been called.
+     * Calling these methods is recursive, you must resume
+     * as often as you called suspend.
+     */
+    void suspendCollectionScan();
+    void resumeCollectionScan();
+
+    /**
+     * Implementation of FileMetadataWrite, see there. Calling these methods is equivalent.
+     */
+    void finishFileMetadataWrite(const ItemInfo& info, bool changed);
+
+Q_SIGNALS:
+
+    void collectionScanFinished();
+    void partialScanDone(const QString& path);
+    void completeScanDone();
+    void completeScanCanceled();
+    void errorFromInitialization(const QString&);
+
+private Q_SLOTS:
+
+    void slotCancelPressed();
+    void slotRelaxedScanning();
 
 private:
 
-    explicit ScanController();
-    ~ScanController();
+    virtual void finishedSchemaUpdate(UpdateResult result);
+
+    //@}
+
+private:
 
     friend class ScanControllerCreator;
-
-private:
 
     class Private;
     Private* const d;
