51be418c9ca515df69baf35fcccfb45869621d3b
Gilles Caulier <caulier.gilles@gmail.com>
2018-10-29 15:56:51 +0100

    re-organize properties classes by sub-dir categories

--- core/libs/properties/import/imagepropertiessidebarcamgui.cpp
+++ core/libs/properties/import/imagepropertiessidebarcamgui.cpp
@@ -4,8 +4,7 @@
  * http://www.digikam.org
  *
  * Date        : 2006-02-08
- * Description : simple image properties side bar used by
- *               camera GUI.
+ * Description : simple image properties side bar used by import tool.
  *
  * Copyright (C) 2006-2018 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2013      by Michael G. Hansen <mike at mghansen dot de>
