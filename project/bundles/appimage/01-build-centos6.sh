b46c9494e1590663151f17f45c6b61b786f47f0f
Gilles Caulier <caulier.gilles@gmail.com>
2018-10-30 10:07:40 +0100

    polish

--- project/bundles/appimage/01-build-centos6.sh
+++ project/bundles/appimage/01-build-centos6.sh
@@ -144,9 +144,9 @@ if [[ ! -f /opt/rh/devtoolset-4/enable ]] ; then
 
         # Newer compiler for CentOS 6 32 bits
         cd /etc/yum.repos.d
-        wget https://copr.fedorainfracloud.org/coprs/mlampe/devtoolset-6.1/repo/epel-6/mlampe-devtoolset-6.1-epel-6.repo
-        yum -y --nogpgcheck install devtoolset-6-gcc devtoolset-6-gcc-c++
-        rm -f /etc/yum.repos.d/mlampe-devtoolset-6.1-epel-6.repo
+        wget https://copr.fedorainfracloud.org/coprs/mlampe/devtoolset-4.1/repo/epel-6/mlampe-devtoolset-4.1-epel-6.repo
+        yum -y --nogpgcheck install devtoolset-4-gcc devtoolset-4-gcc-c++
+        rm -f /etc/yum.repos.d/mlampe-devtoolset-4.1-epel-6.repo
 
     fi
 
