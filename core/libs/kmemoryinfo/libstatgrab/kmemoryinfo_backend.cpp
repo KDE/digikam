40ce07bcee926bc3767f8ee16605e0c11101a97d
Heath Nielson <heathn@gmail.com>
2018-11-02 12:19:15 +0100

    Fix FreeBSD kvm backend.
    Summary: See: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=178063
    
    Reviewers: #automotive, #digikam
    
    Subscribers: cgilles
    
    Differential Revision: https://phabricator.kde.org/D16608

--- core/libs/kmemoryinfo/libstatgrab/kmemoryinfo_backend.cpp
+++ core/libs/kmemoryinfo/libstatgrab/kmemoryinfo_backend.cpp
@@ -120,7 +120,7 @@ kvm_t* sg_get_kvm()
         return kvmd;
     }
 
-    kvmd = kvm_openfiles(NULL, NULL, NULL, O_RDONLY, NULL);
+    kvmd = kvm_openfiles(NULL, "/dev/null", NULL, O_RDONLY, NULL);
 
     if (kvmd == NULL)
     {
@@ -222,9 +222,9 @@ int get_mem_stats(Digikam::KMemoryInfo::KMemoryInfoData* const data)
     int    mib[2];
     u_long physmem;
     size_t size;
-    u_long free_count;
-    u_long cache_count;
-    u_long inactive_count;
+    u_int free_count;
+    u_int cache_count;
+    u_int inactive_count;
     int    pagesize;
 #endif // defined(Q_OS_FREEBSD) || defined(Q_OS_DFBSD)
 
