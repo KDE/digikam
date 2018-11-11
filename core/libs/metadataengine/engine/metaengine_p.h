ce781f5c035e6fa3a056d72de41dc2927a89b931
Human Dynamo <caulier.gilles@gmail.com>
2018-11-11 06:36:16 +0100

    separate merger helper

--- core/libs/metadataengine/engine/metaengine_p.h
+++ core/libs/metadataengine/engine/metaengine_p.h
@@ -95,6 +95,10 @@
 
 // End of Exiv2 headers ------------------------------------------------------
 
+// Local includes
+
+#include "metaengine_mergehelper.h"
+
 namespace Digikam
 {
 
@@ -197,129 +201,6 @@ public:
 
     QSharedDataPointer<MetaEngineData::Private> data;
 };
-
-// --------------------------------------------------------------------------------------------
-
-template <class Data, class Key, class KeyString, class KeyStringList = QList<KeyString> >
-
-class Q_DECL_HIDDEN MetaEngineMergeHelper
-{
-public:
-
-    KeyStringList keys;
-
-public:
-
-    MetaEngineMergeHelper& operator<<(const KeyString& key)
-    {
-        keys << key;
-        return *this;
-    }
-
-    /**
-     * Merge two (Exif,IPTC,Xmp) Data packages, where the result is stored in dest
-     * and fields from src take precedence over existing data from dest.
-     */
-    void mergeAll(const Data& src, Data& dest)
-    {
-        for (typename Data::const_iterator it = src.begin() ; it != src.end() ; ++it)
-        {
-            typename Data::iterator destIt = dest.findKey(Key(it->key()));
-
-            if (destIt == dest.end())
-            {
-                dest.add(*it);
-            }
-            else
-            {
-                *destIt = *it;
-            }
-        }
-    }
-
-    /**
-     * Merge two (Exif,IPTC,Xmp) Data packages, the result is stored in dest.
-     * Only keys in keys are considered for merging.
-     * Fields from src take precedence over existing data from dest.
-     */
-    void mergeFields(const Data& src, Data& dest)
-    {
-        foreach (const KeyString& keyString, keys)
-        {
-            Key key(keyString.latin1());
-            typename Data::const_iterator it = src.findKey(key);
-
-            if (it == src.end())
-            {
-                continue;
-            }
-
-            typename Data::iterator destIt = dest.findKey(key);
-
-            if (destIt == dest.end())
-            {
-                dest.add(*it);
-            }
-            else
-            {
-                *destIt = *it;
-            }
-        }
-    }
-
-    /**
-     * Merge two (Exif,IPTC,Xmp) Data packages, the result is stored in dest.
-     * The following steps apply only to keys in "keys":
-     * The result is determined by src.
-     * Keys must exist in src to kept in dest.
-     * Fields from src take precedence over existing data from dest.
-     */
-    void exclusiveMerge(const Data& src, Data& dest)
-    {
-        foreach (const KeyString& keyString, keys)
-        {
-            Key key(keyString.latin1());
-            typename Data::const_iterator it = src.findKey(key);
-            typename Data::iterator destIt   = dest.findKey(key);
-
-            if (destIt == dest.end())
-            {
-                if (it != src.end())
-                {
-                    dest.add(*it);
-                }
-            }
-            else
-            {
-                if (it == src.end())
-                {
-                    dest.erase(destIt);
-                }
-                else
-                {
-                    *destIt = *it;
-                }
-            }
-        }
-    }
-};
-
-class Q_DECL_HIDDEN ExifMetaEngineMergeHelper
-    : public MetaEngineMergeHelper<Exiv2::ExifData, Exiv2::ExifKey, QLatin1String>
-{
-};
-
-class Q_DECL_HIDDEN IptcMetaEngineMergeHelper
-    : public MetaEngineMergeHelper<Exiv2::IptcData, Exiv2::IptcKey, QLatin1String>
-{
-};
-
-#ifdef _XMP_SUPPORT_
-class Q_DECL_HIDDEN XmpMetaEngineMergeHelper
-    : public MetaEngineMergeHelper<Exiv2::XmpData, Exiv2::XmpKey, QLatin1String>
-{
-};
-#endif
 
 } // namespace Digikam
 
