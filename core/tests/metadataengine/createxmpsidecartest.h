38aca52f1ab8818812bdaf3c74d173787004c892
Gilles Caulier <caulier.gilles@gmail.com>
2018-12-22 16:44:49 +0100

    fix header

--- core/tests/metadataengine/createxmpsidecartest.h
+++ core/tests/metadataengine/createxmpsidecartest.h
@@ -7,7 +7,7 @@
  * Description : an unit-test to test XMP sidecar creation with DMetadata
  *
  * Copyright (C) 2010      by Jakob Malm <jakob dot malm at gmail dot com>
- * Copyright (C) 2010-2018 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
