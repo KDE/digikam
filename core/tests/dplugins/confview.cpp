480e22114d5e9f8f864ba45cf7a693cf81e75d0c
Gilles Caulier <caulier.gilles@gmail.com>
2018-12-31 18:48:36 +0100

    add test tool to check DPlugins configuration widget

---
+++ core/tests/dplugins/confview.cpp
@@ -0,0 +1,49 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * http://www.digikam.org
+ *
+ * Date        : 2018-07-30
+ * Description : stand alone test application for plugin configuration view.
+ *
+ * Copyright (C) 2018 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt Includes
+
+#include <QApplication>
+#include <QDebug>
+
+// Local includes
+
+#include "dpluginloader.h"
+#include "dplugin.h"
+#include "dpluginconfview.h"
+
+using namespace Digikam;
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    DPluginLoader::instance()->init();
+
+    DPluginConfView view;
+    view.show();
+
+    app.exec();
+
+    return 0;
+}
