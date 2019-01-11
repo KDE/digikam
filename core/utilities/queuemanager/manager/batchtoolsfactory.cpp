fbb198b9c9029bf983dd54ff9541b645d8f476ad
Gilles Caulier <caulier.gilles@gmail.com>
2019-01-11 14:01:30 +0100

    prepare BQM tool to be ported to DPlugin API

--- core/utilities/queuemanager/manager/batchtoolsfactory.cpp
+++ core/utilities/queuemanager/manager/batchtoolsfactory.cpp
@@ -30,42 +30,6 @@
 #include "dpluginloader.h"
 #include "dpluginbqm.h"
 
-#include "assigntemplate.h"
-#include "autocorrection.h"
-#include "crop.h"
-#include "flip.h"
-#include "resize.h"
-#include "restoration.h"
-#include "rotate.h"
-#include "sharpen.h"
-#include "blur.h"
-#include "watermark.h"
-#include "noisereduction.h"
-#include "bcgcorrection.h"
-#include "hslcorrection.h"
-#include "colorbalance.h"
-#include "iccconvert.h"
-#include "channelmixer.h"
-#include "bwconvert.h"
-#include "whitebalance.h"
-#include "curvesadjust.h"
-#include "filmgrain.h"
-#include "colorfx.h"
-#include "localcontrast.h"
-#include "antivignetting.h"
-#include "redeyecorrection.h"
-#include "invert.h"
-#include "convert8to16.h"
-#include "convert16to8.h"
-#include "border.h"
-#include "removemetadata.h"
-#include "convert2dng.h"
-#include "timeadjust.h"
-
-#ifdef HAVE_LENSFUN
-#   include "lensautofix.h"
-#endif // HAVE_LENSFUN
-
 namespace Digikam
 {
 
@@ -122,51 +86,6 @@ BatchToolsFactory::BatchToolsFactory()
             }
         }
     }
-
-    // Transform
-    registerTool(new Rotate(this));
-    registerTool(new Flip(this));
-    registerTool(new Resize(this));
-    registerTool(new Crop(this));
-
-    // Decorate
-    registerTool(new WaterMark(this));
-    registerTool(new Border(this));
-
-    // Metadata
-    registerTool(new AssignTemplate(this));
-    registerTool(new RemoveMetadata(this));
-    registerTool(new TimeAdjust(this));
-
-    // Enhance
-    registerTool(new Blur(this));
-    registerTool(new Sharpen(this));
-    registerTool(new NoiseReduction(this));
-    registerTool(new Restoration(this));
-    registerTool(new LocalContrast(this));
-    registerTool(new AntiVignetting(this));
-    registerTool(new RedEyeCorrection(this));
-#ifdef HAVE_LENSFUN
-    registerTool(new LensAutoFix(this));
-#endif // HAVE_LENSFUN
-
-    // Color
-    registerTool(new BCGCorrection(this));
-    registerTool(new HSLCorrection(this));
-    registerTool(new ColorBalance(this));
-    registerTool(new AutoCorrection(this));
-    registerTool(new IccConvert(this));
-    registerTool(new ChannelMixer(this));
-    registerTool(new BWConvert(this));
-    registerTool(new WhiteBalance(this));
-    registerTool(new CurvesAdjust(this));
-    registerTool(new Invert(this));
-    registerTool(new Convert8to16(this));
-    registerTool(new Convert16to8(this));
-
-    // Filters
-    registerTool(new FilmGrain(this));
-    registerTool(new ColorFX(this));
 }
 
 BatchToolsFactory::~BatchToolsFactory()
