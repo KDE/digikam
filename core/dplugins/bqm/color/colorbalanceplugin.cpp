a6f7fccc837f1d52cd681c19c12795e522d4e2b0
Gilles Caulier <caulier.gilles@gmail.com>
2019-01-12 10:25:09 +0100

    port++

--- core/dplugins/bqm/color/colorbalanceplugin.cpp
+++ core/dplugins/bqm/color/colorbalanceplugin.cpp
@@ -4,7 +4,7 @@
  * http://www.digikam.org
  *
  * Date        : 2018-07-30
- * Description : a BQM plugin to apply color effects
+ * Description : a BQM plugin to fix colors balance
  *
  * Copyright (C) 2018-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
@@ -20,7 +20,7 @@
  *
  * ============================================================ */
 
-#include "colorfxplugin.h"
+#include "colorbalanceplugin.h"
 
 // Qt includes
 
@@ -35,57 +35,57 @@
 // Local includes
 
 #include "digikam_debug.h"
-#include "colorfx.h"
+#include "colorbalance.h"
 
 namespace Digikam
 {
 
-ColorFXPlugin::ColorFXPlugin(QObject* const parent)
+ColorBalancePlugin::ColorBalancePlugin(QObject* const parent)
     : DPluginBqm(parent)
 {
 }
 
-ColorFXPlugin::~ColorFXPlugin()
+ColorBalancePlugin::~ColorBalancePlugin()
 {
 }
 
-QString ColorFXPlugin::name() const
+QString ColorBalancePlugin::name() const
 {
-    return i18n("Color Effects");
+    return i18n("Color Balance");
 }
 
-QString ColorFXPlugin::iid() const
+QString ColorBalancePlugin::iid() const
 {
     return QLatin1String(DPLUGIN_IID);
 }
 
-QIcon ColorFXPlugin::icon() const
+QIcon ColorBalancePlugin::icon() const
 {
-    return QIcon::fromTheme(QLatin1String("colorfx"));
+    return QIcon::fromTheme(QLatin1String("adjustrgb"));
 }
 
-QString ColorFXPlugin::description() const
+QString ColorBalancePlugin::description() const
 {
-    return i18n("Apply color effects");
+    return i18n("A tool to adjust color balance");
 }
 
-QString ColorFXPlugin::details() const
+QString ColorBalancePlugin::details() const
 {
-    return i18n("<p>This batch Queue Manager tool can apply color effects over images.</p>");
+    return i18n("<p>This batch Queue Manager tool can adjust color balance from images.</p>");
 }
 
-QList<DPluginAuthor> ColorFXPlugin::authors() const
+QList<DPluginAuthor> ColorBalancePlugin::authors() const
 {
     return QList<DPluginAuthor>()
-            << DPluginAuthor(QLatin1String("Alexander Dymo"),
-                             QLatin1String("adymo at develop dot org"),
-                             QLatin1String("(C) 2012"))
+            << DPluginAuthor(QLatin1String("Gilles Caulier"),
+                             QLatin1String("caulier dot gilles at gmail dot com"),
+                             QLatin1String("(C) 2010-2019"))
             ;
 }
 
-void ColorFXPlugin::setup(QObject* const parent)
+void ColorBalancePlugin::setup(QObject* const parent)
 {
-    ColorFX* const tool = new ColorFX(parent);
+    ColorBalance* const tool = new ColorBalance(parent);
     tool->setPlugin(this);
 
     addTool(tool);
