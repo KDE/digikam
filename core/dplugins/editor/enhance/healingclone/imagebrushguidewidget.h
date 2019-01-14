53cdcbe941bd505b7258d4e7fd0d3b7717375f96
Gilles Caulier <caulier.gilles@gmail.com>
2019-01-14 10:49:54 +0100

    isolate healing tool code for a future review by a new student

--- core/dplugins/editor/enhance/healingclone/imagebrushguidewidget.h
+++ core/dplugins/editor/enhance/healingclone/imagebrushguidewidget.h
@@ -6,7 +6,6 @@
  * Date        : 2017-06-15
  * Description : a brush for use with tool to replace part of the image using another
  *
- * Copyright (C) 2004-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2017      by Shaza Ismail Kaoud <shaza dot ismail dot k at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -28,12 +27,11 @@
 // Local includes
 
 #include "imageguidewidget.h"
-#include "digikam_export.h"
 
 namespace Digikam
 {
 
-class DIGIKAM_EXPORT ImageBrushGuideWidget : public ImageGuideWidget
+class ImageBrushGuideWidget : public ImageGuideWidget
 {
     Q_OBJECT
 
