5d51eb6f0c162a9be4bb40b5a2e5ecc46c1b8809
Gilles Caulier <caulier.gilles@gmail.com>
2019-03-02 16:37:37 +0100

    fix list of openCV modules required for DK

--- project/scripts/bootstrap.opencv-minimal-linux.sh
+++ project/scripts/bootstrap.opencv-minimal-linux.sh
@@ -28,33 +28,36 @@ export OpenCVOptions='-DBUILD_EXAMPLES=OFF \
                       -DBUILD_PERF_TESTS=OFF \
                       -DBUILD_NEW_PYTHON_SUPPORT=OFF \
                       -DBUILD_ZLIB=OFF \
-                      -DBUILD_opencv_videoio=OFF \
-                      -DBUILD_opencv_apps=OFF \
-                      -DBUILD_opencv_calib3d=OFF \
-                      -DBUILD_opencv_dnn=OFF \
-                      -DBUILD_opencv_features2d=OFF \
-                      -DBUILD_opencv_flann=OFF \
-                      -DBUILD_opencv_gapi=OFF \
-                      -DBUILD_opencv_ml=OFF \
-                      -DBUILD_opencv_photo=OFF \
-                      -DBUILD_opencv_imgcodecs=OFF \
-                      -DBUILD_opencv_shape=OFF \
-                      -DBUILD_opencv_videostab=OFF \
-                      -DBUILD_opencv_highgui=OFF \
-                      -DBUILD_opencv_superres=OFF \
-                      -DBUILD_opencv_stitching=OFF \
+                      -DOPENCV_BUILD_3RDPARTY_LIBS=OFF \
+                      -DINSTALL_C_EXAMPLES=OFF \
+                      -DINSTALL_PYTHON_EXAMPLES=OFF \
+                      -DBUILD_opencv_core=ON \
+                      -DBUILD_opencv_imgproc=ON \
+                      -DBUILD_opencv_imgcodecs=ON \
+                      -DBUILD_opencv_objdetect=ON \
+                      -DBUILD_opencv_calib3d=ON \
+                      -DBUILD_opencv_features2d=ON \
                       -DBUILD_opencv_java=OFF \
                       -DBUILD_opencv_java_bindings_generator=OFF \
                       -DBUILD_opencv_js=ON \
                       -DBUILD_opencv_python2=OFF \
                       -DBUILD_opencv_python3=OFF \
                       -DBUILD_opencv_python_bindings_generator=OFF \
-                      -DOPENCV_BUILD_3RDPARTY_LIBS=OFF \
-                      -DINSTALL_C_EXAMPLES=OFF \
-                      -DINSTALL_PYTHON_EXAMPLES=OFF \
+                      -DBUILD_opencv_ml=OFF \
+                      -DBUILD_opencv_photo=OFF \
+                      -DBUILD_opencv_shape=OFF \
+                      -DBUILD_opencv_highgui=OFF \
+                      -DBUILD_opencv_superres=OFF \
+                      -DBUILD_opencv_stitching=OFF \
+                      -DBUILD_opencv_videostab=OFF \
+                      -DBUILD_opencv_videoio=OFF \
+                      -DBUILD_opencv_video=OFF \
+                      -DBUILD_opencv_apps=OFF \
+                      -DBUILD_opencv_dnn=OFF \
+                      -DBUILD_opencv_gapi=OFF \
+                      -DBUILD_opencv_flann=OFF \
                       -DWITH_1394=OFF \
                       -DWITH_VTK=OFF \
-                      -DWITH_CUDA=OFF \
                       -DWITH_DIRECTX=OFF \
                       -DWITH_DSHOW=OFF \
                       -DWITH_EIGEN=OFF \
@@ -80,15 +83,31 @@ export OpenCVOptions='-DBUILD_EXAMPLES=OFF \
                       -DWITH_VFW=OFF \
                       -DWITH_VIDEOINPUT=OFF \
                       -DWITH_XINE=OFF \
+                      -DWITH_VA_INTEL=OFF \
+                      -DWITH_GPHOTO2=OFF \
+                      -DWITH_PROTOBUF=OFF \
+                      -DWITH_WEBP=OFF \
+                      -DWITH_IMGCODEC_HDR=OFF \
+                      -DWITH_IMGCODEC_SUNRASTER=OFF \
+                      -DWITH_IMGCODEC_PXM=OFF \
+                      -DWITH_CUDA=OFF \
+                      -DWITH_CUFFT=OFF \
+                      -DWITH_CUBLAS=OFF \
+                      -DWITH_NVCUVID=OFF \
                       -DWITH_OPENCL=OFF \
+                      -DWITH_OPENCL_SVM=OFF \
                       -DWITH_OPENCLAMDFFT=OFF \
                       -DWITH_OPENCLAMDBLAS=OFF \
-                      -DWITH_VA_INTEL=OFF \
-                      -DWITH_GPHOTO2=OFF'
+                      -DCV_ENABLE_INTRINSICS=OFF \
+                      -DCV_DISABLE_OPTIMIZATION=ON \
+                      -DCV_TRACE=OFF'
 
 cmake -G "$MAKEFILES_TYPE" . \
       -DCMAKE_INSTALL_PREFIX=/usr \
       -Wno-dev \
       $OpenCVOptions \
       ..
+
+exit
+
 
