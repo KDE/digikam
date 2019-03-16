6335b27190cffd6e23ea844843b8cdc050ac1b17
Gilles Caulier <caulier.gilles@gmail.com>
2019-03-16 10:05:19 +0100

    change order again

--- project/bundles/appimage/01-build-centos6.sh
+++ project/bundles/appimage/01-build-centos6.sh
@@ -236,13 +236,13 @@ cmake3 --build . --config RelWithDebInfo --target ext_jpeg          -- -j$CPU_CO
 cmake3 --build . --config RelWithDebInfo --target ext_jasper        -- -j$CPU_CORES
 cmake3 --build . --config RelWithDebInfo --target ext_png           -- -j$CPU_CORES
 cmake3 --build . --config RelWithDebInfo --target ext_tiff          -- -j$CPU_CORES
-cmake3 --build . --config RelWithDebInfo --target ext_boost         -- -j$CPU_CORES
 #cmake3 --build . --config RelWithDebInfo --target ext_openssl       -- -j$CPU_CORES
 cmake3 --build . --config RelWithDebInfo --target ext_freetype      -- -j$CPU_CORES
 cmake3 --build . --config RelWithDebInfo --target ext_fontconfig    -- -j$CPU_CORES    # depend of freetype
 cmake3 --build . --config RelWithDebInfo --target ext_qt            -- -j$CPU_CORES    # depend of fontconfig, freetype, libtiff, libjpeg, libpng
 cmake3 --build . --config RelWithDebInfo --target ext_libicu        -- -j$CPU_CORES
 cmake3 --build . --config RelWithDebInfo --target ext_qtwebkit      -- -j$CPU_CORES    # depend of qt and libicu
+cmake3 --build . --config RelWithDebInfo --target ext_boost         -- -j$CPU_CORES
 cmake3 --build . --config RelWithDebInfo --target ext_libass        -- -j$CPU_CORES    # depend of fontconfig
 cmake3 --build . --config RelWithDebInfo --target ext_ffmpeg        -- -j$CPU_CORES    # depend of libass
 cmake3 --build . --config RelWithDebInfo --target ext_qtav          -- -j$CPU_CORES    # depend of qt and ffmpeg
