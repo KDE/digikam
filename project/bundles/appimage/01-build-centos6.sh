2d90dda2c24b4c574e73e4964171a6eaa1bb4e2c
Gilles Caulier <caulier.gilles@gmail.com>
2019-03-17 23:23:47 +0100

    disable webkit

--- project/bundles/appimage/01-build-centos6.sh
+++ project/bundles/appimage/01-build-centos6.sh
@@ -268,7 +268,6 @@ cmake3 --build . --config RelWithDebInfo --target ext_freetype      -- -j$CPU_CO
 cmake3 --build . --config RelWithDebInfo --target ext_fontconfig    -- -j$CPU_CORES    # depend of freetype
 cmake3 --build . --config RelWithDebInfo --target ext_libicu        -- -j$CPU_CORES
 cmake3 --build . --config RelWithDebInfo --target ext_qt            -- -j$CPU_CORES    # depend of fontconfig, freetype, libtiff, libjpeg, libpng, libicu
-cmake3 --build . --config RelWithDebInfo --target ext_qtwebkit      -- -j$CPU_CORES    # depend of qt and libicu
 
 # --- digiKam dependencies stage2 -------------
 
