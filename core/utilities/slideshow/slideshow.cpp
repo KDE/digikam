187f53b0ef3cee38a240ca01563884e035137e1d
Maik Qualmann <metzpinguin@gmail.com>
2019-03-17 22:34:50 +0100

    do not try the video loader if the image failed to load CCBUGS: 405232

--- core/utilities/slideshow/slideshow.cpp
+++ core/utilities/slideshow/slideshow.cpp
@@ -371,19 +371,10 @@ void SlideShow::slotImageLoaded(bool loaded)
             {
                 d->osd->pause(false);
             }
-
-            preloadNextItem();
         }
     }
-    else
-    {
-#ifdef HAVE_MEDIAPLAYER
-        // Try to load item as video
-        d->videoView->setCurrentUrl(currentItem());
-#else
-        slotVideoLoaded(false);
-#endif
-    }
+
+    preloadNextItem();
 }
 
 void SlideShow::slotVideoLoaded(bool loaded)
