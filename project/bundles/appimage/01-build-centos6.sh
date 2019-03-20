4f88d233440857991030fc2ca1cb606a9f3a1cf6
Gilles Caulier <caulier.gilles@gmail.com>
2019-03-20 15:24:48 +0100

    polish

--- project/bundles/appimage/01-build-centos6.sh
+++ project/bundles/appimage/01-build-centos6.sh
@@ -165,6 +165,7 @@ else
 
         curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
         curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+        curl -L get.rvm.io
         curl -L get.rvm.io | bash -s stable
         source /etc/profile.d/rvm.sh
         rvm reload
