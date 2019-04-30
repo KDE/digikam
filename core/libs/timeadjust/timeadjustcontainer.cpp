8579896fd3df1ddb451431ba2f833408253d0bbc
Maik Qualmann <metzpinguin@gmail.com>
2019-04-30 07:42:06 +0200

    add case with fractional seconds

--- core/libs/timeadjust/timeadjustcontainer.cpp
+++ core/libs/timeadjust/timeadjustcontainer.cpp
@@ -93,6 +93,8 @@ QDateTime TimeAdjustContainer::getDateTimeFromUrl(const QUrl& url) const
     QStringList regExpStrings;
     // Do not change the order of the list.
     regExpStrings << QLatin1String("(.+)?([0-9]{4}[-_:/]?[0-9]{2}[-_:/]?[0-9]{2})"
+                                   "(.+)?([0-9]{2}[-_:/]?[0-9]{2}[-_:/]?[0-9]{5})(.+)?");
+    regExpStrings << QLatin1String("(.+)?([0-9]{4}[-_:/]?[0-9]{2}[-_:/]?[0-9]{2})"
                                    "(.+)?([0-9]{2}[-_:/]?[0-9]{2}[-_:/]?[0-9]{2})(.+)?");
     regExpStrings << QLatin1String("(.+)?([0-9]{2}[-_:/]?[0-9]{2}[-_:/]?[0-9]{4})"
                                    "(.+)?([0-9]{2}[-_:/]?[0-9]{2}[-_:/]?[0-9]{2})(.+)?");
@@ -104,13 +106,14 @@ QDateTime TimeAdjustContainer::getDateTimeFromUrl(const QUrl& url) const
                                    "(.+)?([0-9]{2}[0-9]{2})(.+)?");
 
     QList <QPair<QString, QString> > formatStrings;
-    formatStrings << qMakePair(QLatin1String("yyyyMMddhhmmss"), QString());
-    formatStrings << qMakePair(QLatin1String("ddMMyyyyhhmmss"), QLatin1String("MMddyyyyhhmmss"));
-    formatStrings << qMakePair(QLatin1String("yyyyMMdd"),       QString());
-    formatStrings << qMakePair(QLatin1String("ddMMyyyy"),       QLatin1String("MMddyyyy"));
-    formatStrings << qMakePair(QLatin1String("ddMMyyyy"),       QLatin1String("MMddyyyy"));
-    formatStrings << qMakePair(QLatin1String("ddMM yy"),        QString());
-    formatStrings << qMakePair(QLatin1String("ddMMyyhhmm"),     QString());
+    formatStrings << qMakePair(QLatin1String("yyyyMMddhhmmsszzz"), QString());
+    formatStrings << qMakePair(QLatin1String("yyyyMMddhhmmss"),    QString());
+    formatStrings << qMakePair(QLatin1String("ddMMyyyyhhmmss"),    QLatin1String("MMddyyyyhhmmss"));
+    formatStrings << qMakePair(QLatin1String("yyyyMMdd"),          QString());
+    formatStrings << qMakePair(QLatin1String("ddMMyyyy"),          QLatin1String("MMddyyyy"));
+    formatStrings << qMakePair(QLatin1String("ddMMyyyy"),          QLatin1String("MMddyyyy"));
+    formatStrings << qMakePair(QLatin1String("ddMM yy"),           QString());
+    formatStrings << qMakePair(QLatin1String("ddMMyyhhmm"),        QString());
 
     QDateTime dateTime;
 
