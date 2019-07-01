532faf672a74607b381f2765232bfbf64afa5982
Maik Qualmann <metzpinguin@gmail.com>
2019-07-01 08:04:25 +0200

    change QStandardPaths search location for bookmark file

--- core/utilities/geolocation/geoiface/bookmark/gpsbookmarkowner.cpp
+++ core/utilities/geolocation/geoiface/bookmark/gpsbookmarkowner.cpp
@@ -69,9 +69,8 @@ GPSBookmarkOwner::GPSBookmarkOwner(GPSItemModel* const gpsItemModel, QWidget* co
     d->parent = parent;
 
     const QString bookmarksFileName =
-        QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) +
-                                         QLatin1Char('/') +
-                                         QLatin1String("digikam/geobookmarks.xml");
+        QStandardPaths::writableLocation(QStandardPaths::DataLocation) +
+                                         QLatin1String("/geobookmarks.xml");
     d->bookmarkManager              = new BookmarksManager(bookmarksFileName, this);
     d->bookmarkManager->load();
     d->bookmarkMenu                 = new BookmarksMenu(d->bookmarkManager, d->parent);
